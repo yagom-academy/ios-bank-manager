cf25c97a139ecca71e08e365fc0048e6ecac62a2
yeon <jryoun1@naver.com>
2021-01-04 22:42:30 +0900

    feat: BankManager 타입 생성

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,18 @@
 //
 
 import Foundation
+
+struct BankManager {
+    let tellerNumber: Int
+    init(number: Int) {
+        self.tellerNumber = number
+    }
+    
+    func printStartTask(customerNumber: Int) {
+        print("\(customerNumber)번 고객 업무 시작")
+    }
+    
+    func printFinishTask(customerNumber: Int) {
+        print("\(customerNumber)번 고객 업무 완료")
+    }
+}
