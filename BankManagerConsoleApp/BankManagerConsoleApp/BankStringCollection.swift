b2c4ce4897a908832915107894d82405abef47a7
yeon <jryoun1@naver.com>
2021-01-04 22:50:34 +0900

    Feat: Bank에서 사용되는 String들을 enum으로 모아둠

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankStringCollection.swift
@@ -0,0 +1,14 @@
+//
+//  BankStringCollection.swift
+//  BankManagerConsoleApp
+//
+//  Created by Yeon on 2021/01/04.
+//
+
+import Foundation
+
+enum BankMenu {
+    static let start = "1"
+    static let exit = "2"
+    static let Menu = "1 : 은행 개점 \n2 : 종료"
+}
