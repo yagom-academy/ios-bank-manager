daf405d38ccec66cbc9fc922fa883973ee3569c3
yeon <jryoun1@naver.com>
2021-01-04 22:41:28 +0900

    feat: Customer 타입 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,17 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by Yeon on 2021/01/04.
+//
+
+import Foundation
+
+struct Customer {
+    let waitNumber: Int
+    let timeNeedToFinishTask: Double
+    init(waitNumber: Int, needTime: Double = 0.7) {
+        self.waitNumber = waitNumber
+        self.timeNeedToFinishTask = needTime
+    }
+}
