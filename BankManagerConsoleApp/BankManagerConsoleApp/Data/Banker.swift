2a672f73add954acc7e1b9ac358ce64c7ba73523
wonhee <wonhee010@gmail.com>
2021-01-04 23:07:04 +0900

    feat: add bankder type

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Data/Banker.swift
@@ -0,0 +1,13 @@
+//
+//  Banker.swift
+//  BankManagerConsoleApp
+//
+//  Created by Wonhee on 2021/01/04.
+//
+
+import Foundation
+
+struct Banker {
+    let windowNumber: Int
+    let processingTime: Double
+}
