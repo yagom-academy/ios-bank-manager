e375e84c15246d9358c8d60b3023bfd5a7c86861
HakJu <gkrwnrh@gmail.com>
2021-01-04 21:04:09 +0900

    Feat: inputNumber 함수 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -10,4 +10,10 @@ func printOpenBank() {
     print("1 : 은행 개점\n2 : 종료\n입력 : ", terminator:"")
 }
 
+func inputNumber() -> String {
+    guard let input = readLine() else { print("입력하지 않아서 종료합니다."); return "0" }
+    return input
+}
+
 printOpenBank()
+inputNumber()
