08cd2121beafbcd9ea230adb8cb4e77b9368d0e6
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-04 19:28:58 +0900

    Feat: Queue 타입 추가

---
+++ Queue.swift
@@ -0,0 +1,36 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kyungmin Lee on 2021/01/04.
+//
+
+import Foundation
+
+struct Queue<T> {
+    private var data = [T]()
+    
+    var count: Int {
+        return data.count
+    }
+    
+    var isEmpty: Bool {
+        return data.isEmpty
+    }
+    
+    mutating func enqueue(element: T) {
+        data.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        return data.removeFirst()
+    }
+    
+    public func peek() -> T? {
+        return data.first
+    }
+    
+    mutating func clear() {
+        data.removeAll()
+    }
+}
