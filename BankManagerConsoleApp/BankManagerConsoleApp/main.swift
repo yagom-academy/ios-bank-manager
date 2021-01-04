4c44ae471118e58320dffe6665fb64777aa5a36c
yeon <jryoun1@naver.com>
2021-01-04 22:53:57 +0900

    Feat: getUserInput() -> 사용자로부터 Bank메뉴를 입력받는 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -10,3 +10,10 @@ private func generateRandomNumberOfCustomer() -> Int {
     let randomNumber = Int.random(in: 10...30)
     return randomNumber
 }
+
+private func getUserInput() -> String {
+    guard let input = readLine() else {
+        return BankError.noEnterFromUser.localizedDescription
+    }
+    return input
+}
