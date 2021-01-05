5923019455c0211635e247fdd75bf9e3dfcef60f
odong-Tree <ss12357@naver.com>
2021-01-05 13:17:36 +0900

    feat: BankClerk에 업무상태를 변경하는 switchStatus 메서드 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
@@ -4,7 +4,7 @@ struct BankClerk {
     var isWorking: Bool = false
     
     mutating func handleClientBusiness(client: Client) {
-        isWorking = true
+        switchStatus(waitingNumber: client.waitingNumber)
         
         if client.business == .basic {
             totalWorkingTime += 0.7
@@ -12,6 +12,17 @@ struct BankClerk {
             print("죄송합니다! 제가 처리할 수 없는 업무군요 ㅠ")
         }
         
-        isWorking = false
+        switchStatus(waitingNumber: client.waitingNumber)
+    }
+    
+    private mutating func switchStatus(waitingNumber: Int) {
+        switch isWorking {
+        case true:
+            print("\(waitingNumber)번 고객 업무 완료")
+            isWorking = false
+        case false:
+            print("\(waitingNumber)번 고객 업무 시작")
+            isWorking = true
+        }
     }
 }
