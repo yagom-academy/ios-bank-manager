6d914d2dc82096d74df6e29d4c9375a73d1550d8
HakJu <gkrwnrh@gmail.com>
2021-01-05 21:06:01 +0900

    Refactor: setClientNumber 함수 제거, openBank 함수 수정, bankTask 함수 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -23,17 +23,14 @@ func vertifyInput(input: String) {
         return
     default:
         print("잘못 입력하였습니다. 다시 입력하세요.")
-        printOpenBank()
-        vertifyInput(input: inputNumber())
+        main()
     }
 }
 
-func setClientNumber() {
-    var clients: BankClient? = BankClient(clientNumber: 0, numberOfClient: 0, client: [0])
+func openBank() {
+    var clients: BankClient? = BankClient(clientNumber: 0, numberOfClient: Int.random(in: 10...30), client: [0])
     
-    clients?.numberOfClient = Int.random(in: 10...30)
     guard let maxNumberOfClient: Int = clients?.numberOfClient else { print("maxNumber error"); return }
-    clients?.clientNumber = 0
     guard var clientNumber: Int = clients?.clientNumber else { print("clientNumber error"); return }
     clients?.client = [Int](repeating: clientNumber, count: maxNumberOfClient)
     guard var client: [Int] = clients?.client else { print("clientArray error"); return }
@@ -41,11 +38,17 @@ func setClientNumber() {
     for index in 0...maxNumberOfClient - 1 {
         clientNumber += 1
         client[index] = clientNumber
+        bankTask(clientNumber: clientNumber)
     }
+    let taskTime: String = String(format: "%.2f", round( Double(maxNumberOfClient) * 0.7 * 100) / 100)
+    print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(maxNumberOfClient)명이며, 총 업무시간은 \(taskTime)초입니다.")
+    main()
 }
 
-func openBank() {
-    setClientNumber()
+func bankTask(clientNumber: Int) {
+    print("\(clientNumber)번 고객 업무 시작")
+    
+    print("\(clientNumber)번 고객 업무 완료")
 }
 
 func main() {
