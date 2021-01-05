a66018d02148ae49cf8e73c0e5a5daf16880b31d
wonhee <wonhee010@gmail.com>
2021-01-05 18:23:38 +0900

    feat: add showError func

--- BankManagerConsoleApp/BankManagerConsoleApp/ErrorHandling.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/ErrorHandling.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class ErrorHandling {
+    func showError(_ error: Error) {
+        let errorMessage: String
+        if let bankError = error as? BankError {
+            errorMessage = bankError.localizedDescription
+        } else {
+            errorMessage = BankError.unknow.localizedDescription
+        }
+        print(errorMessage)
+        exitApp()
+    }
+    
+    private func exitApp() {
+        exit(1)
+    }
+}
