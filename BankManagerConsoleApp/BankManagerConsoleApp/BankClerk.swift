37925ba56d421e3bbe70db356225a0466a620b37
odong-Tree <ss12357@naver.com>
2021-01-05 13:00:53 +0900

    feat: BankClerk의 handleClientBusiness 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
@@ -3,6 +3,15 @@ struct BankClerk {
     var totalWorkingTime: Float = 0
     var isWorking: Bool = false
     
-    func handleClientBusiness() {
+    mutating func handleClientBusiness(client: Client) {
+        isWorking = true
+        
+        if client.business == .basic {
+            totalWorkingTime += 0.7
+        } else {
+            print("죄송합니다! 제가 처리할 수 없는 업무군요 ㅠ")
+        }
+        
+        isWorking = false
     }
 }
