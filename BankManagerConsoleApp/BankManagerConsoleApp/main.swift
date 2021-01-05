ee69d97204984e313d1f2d3a723442e561b381d4
yeon <jryoun1@naver.com>
2021-01-05 23:31:01 +0900

    Refactor: openBank()->main() 함수명 변경, 기존의 switch문을 활용한 재귀호출에서 while문을 사용하도록 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -21,3 +21,35 @@ private func getUserInput() -> String {
 var todayCustomerNumber = generateRandomNumberOfCustomer()
 let bankManagerNumber = 1
 var bank = Bank(customerNumber: todayCustomerNumber, bankManagerNumber: bankManagerNumber)
+var firstTimeBankOpen: Bool = true
+
+private func main() {
+    while bank.isBankOpen {
+        print(BankMenu.Menu)
+        
+        if !firstTimeBankOpen {
+            todayCustomerNumber = generateRandomNumberOfCustomer()
+            bank.initializeCustomers(customerNumber: todayCustomerNumber)
+            bank.initializeBankManagers(bankManagerNumber: bankManagerNumber)
+        }
+        
+        firstTimeBankOpen = false
+        
+        switch getUserInput() {
+        case BankMenu.start:
+            bank.performBankTask()
+            bank.printFinishBank()
+            bank.resetBank()
+        case BankMenu.exit:
+            bank.isBankOpen = false
+            break
+        default:
+            guard let message = BankError.wrongUserInput.errorDescription else {
+                return
+            }
+            print(message)
+        }
+    }
+}
+
+main()
