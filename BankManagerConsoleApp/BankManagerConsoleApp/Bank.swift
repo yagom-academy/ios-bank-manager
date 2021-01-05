686e01ce7be11a9b02df01685711102fd299e7ae
innieminnie <rkddlsl1001@gmail.com>
2021-01-05 09:19:03 +0900

    feat: Client 구조체 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -1,1 +1,8 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by 강인희 on 2021/01/05.
+//
 
+import Foundation
--- BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
@@ -1,1 +1,5 @@
-
+struct Client {
+    let waitingNumber: Int
+    let requirement: BusinessType
+    let assignedCounterNumber: Int
+}
