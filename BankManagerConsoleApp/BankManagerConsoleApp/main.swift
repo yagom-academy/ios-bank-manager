8c8670375095686091332c4517e380b4913f5d11
lina0322 <llim@student.42seoul.kr>
2021-01-05 14:00:06 +0900

    Feat: main

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,32 @@
 // 
 
 import Foundation
+
+func main() {
+    let bank = BankManager()
+    let tellerNumber = 1
+    var isStart = true
+    var clientNumber: Int {
+        return Int.random(in: 10...30)
+    }
+    
+    while isStart {
+        bank.printMenu()
+        
+        guard let input = readLine() else {
+            print(BankError.wrongInput.localizedDescription)
+            continue
+        }
+        
+        switch input {
+        case BankMenu.start.rawValue:
+            bank.operateBank(teller: tellerNumber, client: clientNumber)
+        case BankMenu.end.rawValue:
+            isStart = false
+        default:
+            print(BankError.wrongInput.localizedDescription)
+        }
+    }
+}
+
+main()
