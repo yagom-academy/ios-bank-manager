050b0b0e13d0e7f10cdded977f4a23e1dbcba7d8
wonhee <wonhee010@gmail.com>
2021-01-05 13:24:51 +0900

    feat: add Error Type with Error Protocol

--- BankManagerConsoleApp/BankManagerConsoleApp/Error/BankError.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Error/BankError.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+enum BankError: Error {
+    case input
+    case number
+    case unknow
+}
+
+extension BankError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .input:
+            return "입력을 받지 못했습니다.\n다시 시도해 주세요."
+        case .number:
+            return "숫자를 입력해주세요."
+        case .unknow:
+            return "알 수 없는 오류가 발생했습니다.\n다시 시도해 주세요."
+        }
+    }
+}
