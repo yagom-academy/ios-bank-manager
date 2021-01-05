11cb82ed0234a4ce5e6759eb8d04ed6be2871f36
odong-Tree <ss12357@naver.com>
2021-01-05 09:28:12 +0900

    feat: Bank 구조체 타입으로 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -1,1 +1,18 @@
 
+struct Bank {
+    var operatingCounter: [BankClerk]
+    // Dictionary?
+    var waitingList: [Client]
+    var totalClientsProcessedNumber: Int
+    var totalOperatingTime: Float
+    
+    // 마지막 번호표를 기억하는 프로퍼티가 필요하지 않을까?
+    
+    func assignCounter() {
+        
+    }
+    
+    func closeWork() {
+        
+    }
+}
