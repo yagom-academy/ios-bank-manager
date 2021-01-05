158cfe9e263174cc691a7076bbe9ad1e2813cadd
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-01-05 11:47:37 +0900

    feat : configureBankers 메서드 정의

--- BankManager.swift
+++ BankManager.swift
@@ -4,3 +4,21 @@ struct Banker {
     var windowNumber: UInt
     var isWorking: Bool = false
 }
+
+struct Customer {
+    var customerNumber: UInt
+    var taskTime: Double
+}
+
+struct BankManager {
+    private var bankers = [Banker]()
+    private var customers = [Customer]()
+    private var businessTimes: Double = 0.0
+    private var visitedCustomers: UInt = 0
+    
+    mutating func configureBankers(numberOfBankers: UInt) {
+        for window in 0..<numberOfBankers {
+            bankers.append(Banker(windowNumber: window, isWorking: false))
+        }
+    }
+}
