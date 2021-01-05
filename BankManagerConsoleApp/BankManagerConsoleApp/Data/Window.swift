a7fa278b4c39a4577c32173fef46911b52cfba0d
wonhee <wonhee010@gmail.com>
2021-01-05 15:34:15 +0900

    feat: add check func in Window

--- BankManagerConsoleApp/BankManagerConsoleApp/Data/Window.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Data/Window.swift
@@ -16,11 +16,49 @@ class Window {
         self.number = windowNumber
     }
     
+    // MARK: - set property
     func setBanker(_ banker: Banker) {
         self.banker = banker
     }
     
     func setCustomer(_ customer: Customer) {
         self.customer = customer
+    }
+    
+    // MARK: - check func
+    func checkEndCustomerTask(currentTime: Double) throws {
+        guard let assignedBanker = banker,
+              let taskStartTime = assignedBanker.taskStartTime else {
+            throw BankError.invalidateBanker
+        }
+        let progressTime = currentTime - taskStartTime
+        
+        if progressTime >= assignedBanker.processingTime {
+            try endCustomerTask()
+        }
+    }
+    
+    private func endCustomerTask() throws {
+        guard let currentCustomer = customer else {
+            throw BankError.invalidateCustomer
+        }
+        print(currentCustomer.endMessage)
+        customer = nil
+    }
+    
+    func canStartTask() -> Bool {
+        return customer == nil
+    }
+    
+    func startCustomerTask(currentTime: Double, customer: Customer) throws {
+        print("time: \(currentTime), customer: \(customer.waitingNumber)")
+        guard let assignedBanker = banker else {
+            throw BankError.invalidateBanker
+        }
+        var nextBanker = assignedBanker
+        nextBanker.setTaskStartTime(currentTime)
+        banker = nextBanker
+        self.customer = customer
+        print(customer.startMessage)
     }
 }
