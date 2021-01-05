253413a16eb5c87a726f387e5aa857abcc4107bd
wonhee <wonhee010@gmail.com>
2021-01-05 15:34:27 +0900

    feat: add BankError case

--- BankManagerConsoleApp/BankManagerConsoleApp/Error/BankError.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Error/BankError.swift
@@ -11,6 +11,10 @@ enum BankError: Error {
     case input
     case number
     case mismatchNumber
+    case emptyBank
+    case invalidateTime
+    case invalidateBanker
+    case invalidateCustomer
     case unknow
 }
 
@@ -23,6 +27,14 @@ extension BankError: LocalizedError {
             return "숫자를 입력해주세요."
         case .mismatchNumber:
             return "알맞은 숫자를 입력해주세요."
+        case .emptyBank:
+            return "은행이 없습니다.\n다시 시도해 주세요."
+        case .invalidateTime:
+            return "시간이 없습니다.\n다시 시도해 주세요."
+        case .invalidateBanker:
+            return "은행원이 없는 창구입니다.\n다시 시도해 주세요."
+        case .invalidateCustomer:
+            return "배정된 고객이 없는 창구입니다.\n다시 시도해 주세요."
         case .unknow:
             return "알 수 없는 오류가 발생했습니다.\n다시 시도해 주세요."
         }
