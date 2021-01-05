6a4e74e9d89b6f7cac252a4535bd3ab2c7721912
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-01-05 11:49:16 +0900

    feat : configureCustomers 메서드 구현

--- BankManager.swift
+++ BankManager.swift
@@ -15,10 +15,17 @@ struct BankManager {
     private var customers = [Customer]()
     private var businessTimes: Double = 0.0
     private var visitedCustomers: UInt = 0
+    private let taskTime: Double = 0.7
     
     mutating func configureBankers(numberOfBankers: UInt) {
         for window in 0..<numberOfBankers {
             bankers.append(Banker(windowNumber: window, isWorking: false))
+        }
+    }
+    
+    mutating func configureCustomers(numberOfCustomers: UInt) {
+        for customer in 0..<numberOfCustomers {
+            customers.append(Customer(customerNumber: customer, taskTime: taskTime))
         }
     }
 }
