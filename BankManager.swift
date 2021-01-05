b387a5842bb1ba7a7d6baf870ad98db14c1affa6
iluxsm <be9.smile@gmail.com>
2021-01-06 02:05:17 +0900

    feat: BankManager 타입 구현
    - 은행 매니저로써 하는 역할인, 은행 업무의 시작 또는 종료, 고객 대기열 생성 등을 구현했습니다.

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,33 @@
 //
 
 import Foundation
+
+struct BankManager {
+    private var bankState: Int = 0
+    
+    mutating func startTask() {
+        repeat {
+            let initializationText: String = """
+            1 : 은행개점
+            2 : 종료
+            입력 : 
+            """
+            print(initializationText, terminator: "")
+            
+            guard let userInput: String = readLine(), let userInputNumber: Int = Int(userInput), (userInputNumber == 1 || userInputNumber == 2) else {
+                print("올바른 입력을 부탁드립니다. 개점은 숫자 1을, 종료는 숫자 2를 입력해주세요!")
+                return
+            }
+            self.bankState = userInputNumber
+            let customers = Array(1...Int.random(in: 10...30))
+            
+            if self.bankState == 1 {
+                let taskedTime = BankClerk().serveCustomers(customers: customers)
+                print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customers.count)명이며, 총 업무시간은 \(Double(taskedTime) / 1000000)초 입니다.")
+            }
+        } while self.bankState == 1
+    }
+}
+
+
+    
