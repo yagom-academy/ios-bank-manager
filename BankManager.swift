7f96ad043ec619060284444de8ed886b97fb4f69
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-05 14:43:54 +0900

    Feat: Banker 타입 추가, BankManager init할 때 Banker 인원 설정 기능 추가

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,29 @@
 //
 
 import Foundation
+
+struct BankManager {
+    
+    struct Banker {
+        var counterNumber: Int
+        var isWorking: Bool
+    }
+    
+    private var bankers = [Banker]()
+        
+    mutating func addBanker(of: Int) {
+        guard of > 0 else {
+            return
+        }
+        
+        let lastCounterNumber = bankers.last?.counterNumber ?? 0
+        for i in 1...of {
+            let banker = Banker(counterNumber: i + lastCounterNumber, isWorking: false)
+            bankers.append(banker)
+        }
+    }
+        
+    init(numberOfBanker: Int) {
+        addBanker(of: numberOfBanker)
+    }
+}
