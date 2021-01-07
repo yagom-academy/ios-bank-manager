599fbb39bd963848284fab757e915f8f8cca81f8
HakJu <gkrwnrh@gmail.com>
2021-01-08 00:46:07 +0900

    Refactor: 타입 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,8 +6,8 @@
 
 import Foundation
 
-var customer: BankManager.BankClient = BankManager.BankClient(number: 0)
-var banker: BankManager.BankClerk = BankManager.BankClerk(bankWindowNumber: 1, taskState: true, client: customer)
+var customer: BankClient = BankClient(number: 0)
+var banker: BankClerk = BankClerk(bankWindowNumber: 1, taskState: true, client: customer)
 
 func printMenu() {
     print("1 : 은행 개점\n2 : 종료\n입력 : ", terminator:"")
