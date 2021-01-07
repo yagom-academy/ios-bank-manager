1acaa75b1dac436ce4714933018f271f705876a4
yeon <jryoun0404@gmail.com>
2021-01-08 00:19:17 +0900

    Refactor: customers 프로퍼티 접근제어자 변경 및 Bank 내부에서 initialize 하지않도록 제거

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,52 +8,20 @@
 import Foundation
 
 struct Bank {
-    private var customers: [Customer] = []
+    var customers: [Customer] = []
     private var bankManagers: [BankManager] = []
     var numberOfCustomer: Int {
         return customers.count
     }
-    private var totalTime: Double {
-        var sum: Double = 0
-        customers.forEach { customer in
-            sum += customer.timeNeedToFinishTask
-        }
-        return sum
-    }
-    var isBankOpen: Bool = true
     
-    init(customerNumber: Int, bankManagerNumber: Int) {
-        self.initializeCustomers(customerNumber: customerNumber)
+    init(bankManagerNumber: Int) {
         self.initializeBankManagers(bankManagerNumber: bankManagerNumber)
     }
     
-    mutating func initializeCustomers(customerNumber: Int) {
-        for number in 1...customerNumber {
-            customers.append(Customer(waitNumber: number))
-        }
-    }
-    
-    mutating func initializeBankManagers(bankManagerNumber: Int) {
+    mutating private func initializeBankManagers(bankManagerNumber: Int) {
         for number in 1...bankManagerNumber {
             bankManagers.append(BankManager(number: number))
         }
-    }
-    
-    func performBankTask() {
-        let bankTeller = bankManagers[0]
-        for index in 1...numberOfCustomer {
-            bankTeller.printStartTask(customerNumber: index)
-            bankTeller.printFinishTask(customerNumber: index)
-        }
-    }
-    
-    func printFinishBank() {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomer)명이며, 총 업무시간은 \(totalTime)초입니다.")
-    }
-    
-    mutating func resetBank() {
-        customers.removeAll()
-        bankManagers.removeAll()
     }
 }
 
