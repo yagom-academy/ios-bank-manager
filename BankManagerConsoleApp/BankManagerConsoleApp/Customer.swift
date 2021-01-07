0bd99a0b919bd40129fa553ed8ded25197a5bf90
sole <subuchappyv2@gmail.com>
2021-01-08 00:44:15 +0900

    feat: costmer 타입구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,18 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by sole on 2021/01/08.
+//
+
+import Foundation
+
+struct Custmer {
+    let index: Int
+    let taskTime: Double
+    
+    init(index: Int, taskTime: Double) {
+        self.index = index
+        self.taskTime = taskTime
+    }
+}
