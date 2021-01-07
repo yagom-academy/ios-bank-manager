bcbe66b6122add8101e5125fb0294bc1b1a8e0e8
taetae <taetae.dev@gmail.com>
2021-01-07 18:38:14 +0900

    refactor: 소수섬 2자리 까지 출력하는 format 설정

--- BankManagerConsoleApp/BankManagerConsoleApp/MessageCollection.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/MessageCollection.swift
@@ -13,7 +13,7 @@ struct MessageCollection {
     """
     static let inputErrorText = "올바른 입력을 부탁드립니다. 개점은 숫자 1을, 종료는 숫자 2를 입력해주세요!"
     static func printCloseBankText(customers: Int, taskedTime: Double) {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customers)명이며, 총 업무시간은 \(Double(taskedTime) / 1000000)초 입니다.")
+        print(("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customers)명이며, 총 업무시간은 \(String.init(format: "%.2f", Double(taskedTime) / 1000000))초 입니다."))
     }
     static func printTaskCompletionText(customer: Int) {
         print("\(customer)번 고객 업무 완료")
