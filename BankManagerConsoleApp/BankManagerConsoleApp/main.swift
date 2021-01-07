7846c0a7790a285f0f0383641e5002dd842bd5ad
lina0322 <llim@student.42seoul.kr>
2021-01-07 09:46:50 +0900

    Refactor: isStart -> continue

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -8,12 +8,12 @@ import Foundation
 
 func main() {
     let tellerNumber = 1
-    var isStart = true
+    var `continue` = true
     var clientNumber: Int {
         return Int.random(in: 10...30)
     }
     
-    while isStart {
+    while `continue` {
         BankManager.shared.printMenu()
         
         guard let input = readLine() else {
@@ -25,7 +25,7 @@ func main() {
         case BankMenu.start.rawValue:
             BankManager.shared.operateBank(teller: tellerNumber, client: clientNumber)
         case BankMenu.end.rawValue:
-            isStart = false
+            `continue` = false
         default:
             print(BankError.wrongInput.description)
         }
