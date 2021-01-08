03d8356fb56840264a6f72a3407af178997f1d92
HakJu <gkrwnrh@gmail.com>
2021-01-08 18:07:27 +0900

    Refactor: input값 자료형 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -13,20 +13,19 @@ func printMenu() {
     print("1 : 은행 개점\n2 : 종료\n입력 : ", terminator:"")
 }
 
-func inputText() -> String{
-    guard let input: String = readLine() else { return "입력하지 않았습니다" }
+func inputText() -> String {
+    guard let input: String = readLine() else { return "입력되지 않았습니다." }
     return input
 }
 
 func main() {
     printMenu()
-    let menu: String = inputText()
-    
+    let menu: Int? = Int(inputText())
     switch menu {
-    case "1":
+    case 1:
         banker.task()
         main()
-    case "2":
+    case 2:
         return
     default:
         print("잘못 입력하였습니다. 다시 입력해주세요.")
