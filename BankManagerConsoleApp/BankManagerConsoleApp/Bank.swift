60836faccafb17a5871f85819b6d8a5cac4fa5a1
odong-Tree <ss12357@naver.com>
2021-01-09 19:43:46 +0900

    refactor: BankCounter 딕셔너리를 Int타입 프로퍼티 bankClerkNumber로 변경에 따른 코드 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -2,30 +2,18 @@
 import Foundation
 
 class Bank {
-    private var serviceCounter: [Int : BankClerk] = [ : ]
+    private var bankClerkNumber: Int
     private var waitingList: [Client] = []
     private var totalVistedClientsNumber: Int = 0
     
     var endingMent: String {
-        return "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(calculateTotalProcessedClientsNumber())명이며, 총 업무시간은 \(calculateTotalOperatingTime())초입니다."
+        return "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 명이며, 총 업무시간은 초입니다."
     }
     
-    init(employeeNumber: Int) throws {
-        self.serviceCounter = try loadBankClerks(of: employeeNumber)
+    init(employeeNumber: Int) {
+        self.bankClerkNumber = employeeNumber
     }
-    
-    private func loadBankClerks(of number: Int) throws -> [Int : BankClerk] {
-        guard number > 0 else {
-            throw BankOperationError.invalidValue
-        }
-        
-        for counterNumber in 1 ... number {
-            let newBankClerk = BankClerk(counterNumber: counterNumber)
-            self.serviceCounter[counterNumber] = newBankClerk
-        }
-        return self.serviceCounter
-    }
-    
+
     func updateWaitingList(of size: Int) throws {
         guard size > 0 else {
             throw BankOperationError.invalidValue
@@ -53,24 +41,3 @@ class Bank {
 //    }
 }
 
-// MARK : bank endingment
-extension Bank {
-    private func calculateTotalOperatingTime() -> Float {
-        let longestWorkingTime = serviceCounter.map { (key: Int, value: BankClerk) -> Float in
-            return value.totalWorkingTime
-        }.max() ?? 0
-        
-        let roundedNumber = round(longestWorkingTime * 100) / 100
-        return roundedNumber
-    }
-    
-    private func calculateTotalProcessedClientsNumber() -> Int {
-        let totalProcessedClientsNumber = serviceCounter.map { (key: Int, value: BankClerk) -> Int  in
-            return value.totalProcessedClients
-        }.reduce(0) { (result, currentNumber) -> Int in
-            return result + currentNumber
-        }
-        
-        return totalProcessedClientsNumber
-    }
-}
