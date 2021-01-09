b00b490861b64de4d9f45a701bc9a2d40905e323
taetae <taetae.dev@gmail.com>
2021-01-09 18:06:01 +0900

    add: 업무종류에 따른 정보를 담고있는 TaskType 구조체 타입 정의

--- BankManagerConsoleApp/BankManagerConsoleApp/TaskTypes.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/TaskTypes.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct TaskType {
+    enum RequiredTime: useconds_t {
+        case deposit = 70000
+        case loan = 110000
+    }
+    
+    enum TaskMessage: String {
+        case deposit = "예금"
+        case loan = "대출"
+    }
+}
