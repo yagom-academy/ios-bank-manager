ee6146c018ad7bb6f6a2744301e60b9a32d302a9
innieminnie <rkddlsl1001@gmail.com>
2021-01-10 14:50:00 +0900

    feat: generateRandomClients() 를 통해 고객의 우선순위/업무종류 랜덤 생성 및 우선순위에 따른 queue 정렬

--- BankManager.swift
+++ BankManager.swift
@@ -8,16 +8,35 @@ struct BankManager {
     private var bank: Bank
     private let clerkNumber: Int = 3
     private lazy var randomClientNumber = 0
+    private var queue = [Client]()
     
     init() {
         self.bank = Bank(employeeNumber: clerkNumber)
     }
     
     mutating func openBank() throws {
-        randomClientNumber = Int.random(in: 10...30)
-        try bank.updateWaitingList(of: randomClientNumber)
+        try generateRandomClients()
         print("은행개점")
         bank.makeAllClerksWork()
+    }
+    
+    private mutating func generateRandomClients() throws {
+        randomClientNumber = Int.random(in: 10...30)
+        for i in 1...randomClientNumber {
+            guard let businessType = BusinessType.allCases.randomElement(),
+                  let clientGrade = ClientGrade.allCases.randomElement() else {
+                return
+            }
+            
+            let newClient = Client(waitingNumber: i, business: businessType, grade: clientGrade)
+            queue.append(newClient)
+        }
+        
+        queue.sort { (client1, client2) -> Bool in
+            client1.grade.rawValue < client2.grade.rawValue
+        }
+
+        try bank.updateWaitingList(to: queue)
     }
     
     func closeBank() {
