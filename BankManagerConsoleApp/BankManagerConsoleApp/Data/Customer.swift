906c5d8461a2e27c2e4df52322a39b84b740f454
wonhee <wonhee010@gmail.com>
2021-01-11 23:13:21 +0900

    feat: edit customer property

--- BankManagerConsoleApp/BankManagerConsoleApp/Data/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Data/Customer.swift
@@ -7,19 +7,8 @@
 
 import Foundation
 
-private enum CustomerMessage {
-    static let start = "번 고객 업무 시작"
-    static let end = "번 고객 업무 완료"
-}
-
 struct Customer {
     let waitingNumber: Int
-    let taskAmount: Int
-    
-    var startMessage: String {
-        return "\(waitingNumber)\(CustomerMessage.start)"
-    }
-    var endMessage: String {
-        return "\(waitingNumber)\(CustomerMessage.end)"
-    }
+    let customerGrame: Grade
+    let taskType: TaskType
 }
