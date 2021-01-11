312d201b860e4c1d739e529947772fa7759c5bc3
jae57 <ggapsong@naver.com>
2021-01-11 18:32:53 +0900

    chore: add string formatter for decimal point

--- BankManager.swift
+++ BankManager.swift
@@ -47,7 +47,7 @@ enum Task: CaseIterable {
     var processTime: Double {
         switch self {
         case .loan:
-            return 1.1
+            return 0.7
         case .deposit:
             return 0.7
         }
@@ -148,6 +148,6 @@ final class BankManager {
     }
     
     private func printCloseMessage() {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(totalTime)초입니다.")
+        print(String(format: "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 %d명이며, 총 업무시간은 %.2f초입니다.", customerCount, totalTime))
     }
 }
