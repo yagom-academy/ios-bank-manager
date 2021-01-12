37bce730e5d620bb85d5992f24ad87c2111411aa
lina0322 <llim@student.42seoul.kr>
2021-01-12 23:19:10 +0900

    Add: Clients 파일

--- BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-struct Clients {
-    var list: [Client] = []
-    
-    init(count: Int) {
-        for waitingNumber in 1...count {
-            guard let businessType = BusinessType.allCases.randomElement(), let priority = Client.Priority.allCases.randomElement() else {
-                return
-            }
-            let clinet = Client(waitingNumber: waitingNumber, businessType: businessType, priority: priority)
-            list.append(clinet)
-        }
-    }
-}
-
 struct Client {
     enum Priority: Comparable, CaseIterable, CustomStringConvertible {
         case VVIP
