e7694effcd23dff5301073a2915c6872a108d23a
yeon <jryoun0404@gmail.com>
2021-01-12 23:26:55 +0900

    Feat: BankHeadquarter에 Task라는 enum 타입 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/BankHeadquarter.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankHeadquarter.swift
@@ -18,9 +18,20 @@ class BankHeadquarter {
     
     func approveLoanTask(customer: Customer) {
         queue.sync { 
-            print("\(BankManangerMessage.start)".format(customer.waitNumber, customer.priority.description, BankTask.loanJudgement.rawValue))
-            usleep(useconds_t(BankTask.loanJudgement.timeForTask * second))
-            print("\(BankManangerMessage.end)".format(customer.waitNumber, customer.priority.description, BankTask.loanJudgement.rawValue))
+            print("\(BankManagerMessage.start)".format(customer.waitNumber, customer.priority.description, BankHeadquarter.Task.loanJudgement.rawValue))
+            usleep(useconds_t(BankHeadquarter.Task.loanJudgement.timeForTask * second))
+            print("\(BankManagerMessage.end)".format(customer.waitNumber, customer.priority.description, BankHeadquarter.Task.loanJudgement.rawValue))
+        }
+    }
+    
+    enum Task: String {
+        case loanJudgement = "대출심사"
+        
+        var timeForTask: TimeInterval {
+            switch self {
+            case .loanJudgement:
+                return 0.5
+            }
         }
     }
 }
