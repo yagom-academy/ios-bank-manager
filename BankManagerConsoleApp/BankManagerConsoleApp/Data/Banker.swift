f1436db24b6c611e6b47830b7d70391372fcc2ff
wonhee <wonhee010@gmail.com>
2021-01-14 00:16:36 +0900

    feat: edit property naming

--- BankManagerConsoleApp/BankManagerConsoleApp/Data/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Data/Banker.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Banker {
-    private let bankerNumber: Int
+    private let number: Int
     private let workingQueue: DispatchQueue
     private let startTaskMessgae = "%d번 %@고객 %@업무 시작"
     private let endTaskMessgae = "%d번 %@고객 %@업무 완료"
@@ -16,12 +16,29 @@ class Banker {
     // MARK: - init func
     init(_ number: Int) {
         self.workingQueue = DispatchQueue(label: "\(number)")
-        self.bankerNumber = number
+        self.number = number
+    }
+    
+    private func setNotification() {
+        
     }
     
     func startWork(customer: Customer, group: DispatchGroup) {
         group.enter()
         print(String(format: startTaskMessgae, customer.waitingNumber, customer.grade.description, customer.taskType.description))
+        switch customer.taskType {
+        case .loan:
+            processLoan(customer: customer, group: group)
+        case .deposit:
+            processDeposit(customer: customer, group: group)
+        }
+    }
+    
+    private func processLoan(customer: Customer, group: DispatchGroup) {
+        BankHeadOffice.shared.addJudge(bankerNumber: self.number, customer: customer, group: group)
+    }
+    
+    private func processDeposit(customer: Customer, group: DispatchGroup) {
         workingQueue.asyncAfter(deadline: .now() + customer.taskType.time) {
             self.finishWork(customer: customer, group: group)
         }
@@ -29,7 +46,7 @@ class Banker {
     
     private func finishWork(customer: Customer, group: DispatchGroup) {
         print(String(format: endTaskMessgae, customer.waitingNumber, customer.grade.description, customer.taskType.description))
-        NotificationCenter.default.post(name: .finishBankerTask, object: self.bankerNumber)
+        NotificationCenter.default.post(name: .finishBankerTask, object: self.number)
         group.leave()
     }
 }
