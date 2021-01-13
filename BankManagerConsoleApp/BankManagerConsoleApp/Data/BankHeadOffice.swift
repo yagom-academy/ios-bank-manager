7741a6835f3c9a71ee904dfc9f4fc372e35ee45c
wonhee <wonhee010@gmail.com>
2021-01-14 00:17:02 +0900

    feat: bankHeadOffice singleton

--- BankManagerConsoleApp/BankManagerConsoleApp/Data/BankHeadOffice.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Data/BankHeadOffice.swift
@@ -8,9 +8,28 @@
 import Foundation
 
 class BankHeadOffice {
-    private let workingQueue: DispatchQueue
+    static let shared = BankHeadOffice()
     
-    init() {
+    private let workingQueue: DispatchQueue
+    private let startJudgeMessage = "%d번 %@고객 대출심사 시작"
+    private let endJudgeMessage = "%d번 %@고객 대출심사 완료"
+    private let judgeTime = 0.5
+    
+    private init() {
         self.workingQueue = DispatchQueue(label: "headOffice")
+    }
+    
+    func addJudge(bankerNumber: Int, customer: Customer, group: DispatchGroup) {
+        group.enter()
+        print(String(format: startJudgeMessage, customer.waitingNumber, customer.grade.description))
+        workingQueue.asyncAfter(deadline: .now() + judgeTime) {
+            self.finishJudge(bankerNumber: bankerNumber, customer: customer, group: group)
+        }
+    }
+    
+    private func finishJudge(bankerNumber: Int, customer: Customer, group: DispatchGroup) {
+        print(String(format: endJudgeMessage, customer.waitingNumber, customer.grade.description))
+        NotificationCenter.default.post(name: .finishLoanJudge, object: bankerNumber)
+        group.leave()
     }
 }
