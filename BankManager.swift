5aac7232e684be122ff570d1ca53ed2dad5a1dc2
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-15 01:47:39 +0900

    Refactor: 은행원이 각 창구번호를 label로 하는 DispatchQueue가지고 있고 이것으로 task수행
    - 작업시간 처리를 async(deadline:)이 아닌 Thread.sleep(forTimeInterval:)로 변경
    - workTime() 메서드명, 반환타입 변경

--- BankManager.swift
+++ BankManager.swift
@@ -28,13 +28,14 @@ private class BankClerk {
     }
     var currentClient: Client?
     var finishedClients: Int = 0
+    let taskQueue: DispatchQueue
     
     // MARK: - Methods
     func startWork(for client: Client) {
         currentClient = client
         print("\(client.tag)번 \(client.priority.string)고객 \(client.bankBusiness.string)업무 시작")
-        
-        DispatchQueue.global().asyncAfter(deadline: workTime(bankBusiness: client.bankBusiness)) {
+        taskQueue.async {
+            Thread.sleep(forTimeInterval: self.taskTime(bankBusiness: client.bankBusiness))
             self.finishWork()
         }
     }
@@ -47,17 +48,18 @@ private class BankClerk {
         currentClient = nil
     }
     
-    func workTime(bankBusiness: BankBusiness) -> DispatchTime {
+    func taskTime(bankBusiness: BankBusiness) -> TimeInterval {
         switch bankBusiness {
         case .loan:
-            return .now() + .milliseconds(1100)
+            return 1.1
         case .deposit:
-            return .now() + .milliseconds(700)
+            return 0.7
         }
     }
     
-    init(bankWindowNumber: Int) {
-        self.bankWindowNumber = bankWindowNumber
+    init(bankWindow number: Int) {
+        bankWindowNumber = number
+        taskQueue = DispatchQueue(label: "\(number)번 창구")
     }
 }
 
@@ -89,7 +91,7 @@ struct BankManager {
         
         let lastCounterNumber = bankClerks.last?.bankWindowNumber ?? 0
         for i in 1...count {
-            let bankClerk = BankClerk(bankWindowNumber: i + lastCounterNumber)
+            let bankClerk = BankClerk(bankWindow: i + lastCounterNumber)
             bankClerks.append(bankClerk)
         }
     }
