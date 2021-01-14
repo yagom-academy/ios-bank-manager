7b05af7b6cae2904ff9a2cb4f3b9155993f52052
taetae <taetae.dev@gmail.com>
2021-01-14 21:59:42 +0900

    Add: 심사 업무를 위한 본사 타입 구조체 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Headquarter.swift
@@ -0,0 +1,40 @@
+//
+//  Headquarter.swift
+//  BankManagerConsoleApp
+//
+//  Created by 김태형 on 2021/01/14.
+//
+
+import Foundation
+enum JudgementState {
+    case start
+    case end
+    
+    var description: String {
+        switch self {
+        case .start:
+            return "대출심사 시작"
+        case .end:
+            return "대출심사 완료"
+        }
+    }
+}
+
+struct Headquarter {
+    let judgementTime : useconds_t = 300000
+    
+    func judgeLoan(customer: Customer) {
+        printJudgementState(state: .start, customer: customer.waitingNumber, customerClass: customer.grade.description)
+        usleep(judgementTime)
+        printJudgementState(state: .end, customer: customer.waitingNumber, customerClass: customer.grade.description)
+    }
+    
+    func printJudgementState(state: JudgementState, customer: Int, customerClass: String) {
+        switch state {
+        case .start:
+            print("\(customer)번" + customerClass + state.description)
+        case .end:
+            print("\(customer)번" + customerClass + state.description)
+        }
+    }
+}
