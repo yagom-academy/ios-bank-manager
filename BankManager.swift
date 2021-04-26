2a695c9658fd86184e004c91eb60724eadce8d51
Neph <tony3322@naver.com>
2021-04-26 22:00:30 +0900

    [#1] Feat : Implement Customer protocol

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,26 @@
 //
 
 import Foundation
+
+protocol Bank: class {
+    var numberTicket: Int { get set }
+    
+    func openAlert()
+    func closeAlert()
+    func giveNumberTicket() -> Int
+}
+
+extension Bank {
+    var numerTicket: Int {
+        numberTicket += 1
+        return numberTicket
+    }
+    
+    func giveNumberTicket() -> Int {
+        return 1
+    }
+}
+
+protocol Customer {
+    var ticketNumber: Int { get }
+}
