e4d9c11e64a175fc17cfd44fb4beda251cb20dd8
kangkyung <rudgns0508@naver.com>
2021-04-27 00:00:44 +0900

    Feat: Bank class 함수 기능 추가
    - open(): loop를 이용한 처리로직 구현
    - work(): 해당 Customer에 대한 처리로직 구현
    - close(): 마무리 print()구현
    
    - Issue
      1. 처리가 끝난 고객은 배열에서 제거해야 하는데, 배열의 인덱스를 알 수 없음.

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -10,11 +10,14 @@ import Foundation
 class Bank {
   var customers = [Customer]()
   var bankManagers = [BankManager]()
+  var totalCompletedCustomer = 0
+  var totalWorkedTime = 0.0
+  var currentOrderNumber = 1
   
   init(numOfManagers: Int) {
     let randomNumber = Int.random(in: 10...30)
     for orderNumber in 1...randomNumber {
-      customers.append(Customer(orderNumber))
+      customers.append(Customer(order: orderNumber))
     }
     
     for counterNumber in 1...numOfManagers {
@@ -23,10 +26,42 @@ class Bank {
   }
   
   func open() {
+    var isRepeat = true
     
+    repeat {
+      if customers.isEmpty {
+        isRepeat = false
+      } else if customers.contains(Customer(order: currentOrderNumber)) {
+        continue
+      } else {
+        work(order: currentOrderNumber)
+        // FIXME: - 처리가 끝난 고객은 배열에서 제거해야 함
+      }
+    } while isRepeat
+    
+    close()
+  }
+  
+  func work(order: Int) {
+    let workingTime = 0.7
+
+    print("\(order)번 고객 업무 시작")
+    OperationQueue().addOperation {
+      let unit = 1000000.0
+      usleep(UInt32(workingTime * unit))
+    }
+    // TODO: - 비동기실험해야함
+    
+    print("\(order)번 고객 업무 완료")
+    totalWorkedTime += workingTime
   }
   
   func close() {
-    
+    let complateString = """
+    업무가 마감되었습니다.
+    오늘 업무를 처리한 고객은 총 \(totalCompletedCustomer)명이며,
+    총 업무 시간은 \(totalWorkedTime)초입니다.
+    """
+    print(complateString)
   }
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -7,10 +7,18 @@
 
 import Foundation
 
-class Customer {
-  let orderNumber: Int
+class Customer: Equatable {
+  static func == (lhs: Customer, rhs: Customer) -> Bool {
+    if lhs.orderNumber == rhs.orderNumber {
+      return true
+    } else {
+      return false
+    }
+  }
   
-  init(_ orderNumber: Int) {
+  private let orderNumber: Int
+  
+  init(order orderNumber: Int) {
     self.orderNumber = orderNumber
   }
 }
