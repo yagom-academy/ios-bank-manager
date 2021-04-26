49d9663407a99f3c6b05a9c45b8c1b54ca0d209d
kangkyung <rudgns0508@naver.com>
2021-04-26 20:35:47 +0900

    Feat: Customer class 구현
    - let orderNumber: Int
    - init()

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,15 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by 강경 on 2021/04/26.
+//
+
+import Foundation
+class Customer {
+  let orderNumber: Int
+  
+  init(orderNumber: Int) {
+    self.orderNumber = orderNumber
+  }
+}
