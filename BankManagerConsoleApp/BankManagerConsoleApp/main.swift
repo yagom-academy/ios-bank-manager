de69882cf3a730dd8c20a79a2c01a7faf4a5fe0b
kangkyung <rudgns0508@naver.com>
2021-04-26 21:59:36 +0900

    Feat: main.swift 시작부분 기능 추가
    - 개점/종료 loop 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -7,4 +7,23 @@
 import Foundation
 
 let testBank = Bank(numOfManagers: 1)
-testBank.open()
+var isRepeat = true
+
+repeat {
+  print("1: 은행 개점")
+  print("2: 종료")
+  guard let userChoice = Int(readLine() ?? "0") else {
+    print("올바른 값을 입력해주세요.")
+    continue
+  }
+
+  switch userChoice {
+  case 1:
+    testBank.open()
+  case 2:
+    isRepeat = false
+    break
+  default:
+    break
+  }
+} while isRepeat
