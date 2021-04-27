2da1349b3ecab34c1b792e13aae06beb1f45f2f8
inwoodev <dlsdn1207@gmail.com>
2021-04-27 18:53:55 +0900

    Refactor : Bank 클래스 수정
    bankManager를 외부에서 주입받는 구체로 구현 및 초기화

--- BankManagerUIApp/BankManagerUIApp/Bank.swift
+++ BankManagerUIApp/BankManagerUIApp/Bank.swift
@@ -2,26 +2,16 @@
 //  Bank.swift
 //  BankManagerUIApp
 //
-//  Created by 황인우 on 2021/04/27.
+//  Created by James on 2021/04/27.
 //
 
 import Foundation
 
-final class BankTeller: FrontServiceable {
-    var windowsNumber: Int
-    
-    init(number: Int) {
-        self.windowsNumber = number
-    }
-    
-}
-
 final class Bank {
-    var bankTeller = BankTeller(number: 3)
-    let bankManager = OperationQueue()
-    var numbersOfBankTeller: Int = 0
+    var bankManager: BankManageable
+    private var teller = OperationQueue()
     
-    func serveClient() {
-        bankManager.maxConcurrentOperationCount = bankTeller.windowsNumber
+    init(bankManager: BankManageable) {
+        self.bankManager = bankManager
     }
 }
