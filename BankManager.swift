2024ac29152fa8570b4c06f171226e5df87f1590
ryan-son <ryan.son1002@gmail.com>
2021-04-27 15:47:34 +0900

    Feat: BankManager 타입 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,22 @@
 //
 
 import Foundation
+
+struct BankManager {
+    func start() {
+        print("시작: 1, 종료: 2")
+        guard let userInput: String = readLine(),
+              let userInputNumber: Int = Int(userInput) else {
+            return
+        }
+        switch userInputNumber {
+        case 1:
+            print("은행개점")
+        case 2:
+            print("종료")
+            return
+        default:
+            print("유효하지 않은 입력입니다.")
+        }
+    }
+}
