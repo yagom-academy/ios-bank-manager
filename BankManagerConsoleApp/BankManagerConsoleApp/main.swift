a11aa9104aab291c86346ac5a915fa4d038a9f6e
cjm <stepsw112@gmail.com>
2021-04-27 15:41:48 +0900

    Refactor: 함수 나누기 - 페어프로그래밍 JM

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,51 +6,57 @@
 
 import Foundation
 
-func startBankMenu() throws {
-    /// 입력 기능
+func startBankMenu() -> Bool{
     print("1. 은행 개점 \n2. 종료")
     print("입력 :", terminator: " " )
-    guard let userInput = readLine() else { throw BankError.userInput }
-    guard let userInputNumber = Int(userInput) else { return }
-    if userInputNumber == 1 {
-        startBank() 
-    }
+    guard let userInput = readLine() else { return false }
+    guard let userInputNumber = Int(userInput) else { return false }
+    return true
+}
+
+func checkInputValidation(userInputNumber: Int) {
     /// 입력 validation 체크
-    else if userInputNumber == 2 { // userInputNumber 사용됨
-        return
-    } else if userInputNumber != 1 { // userInputNumber 사용됨
-        throw BankError.userInput
+//    if userInputNumber == 2 { // userInputNumber 사용됨
+//        return
+//    } else if userInputNumber != 1 { // userInputNumber 사용됨
+//        throw BankError.userInput
+//    }
+}
+
+func createBanker(numberOfBanker: Int) -> [Banker] {
+    var bankerArray : [Banker] = []
+    for identityNumber in 1...numberOfBanker {
+        bankerArray.append(Banker(identityNumber: identityNumber, counterNumber: identityNumber))
+    }
+    return bankerArray
+}
+
+func createClient(numberOfClinet: Int , bankerArray: [Banker]) -> [Client] {
+    var clientArray : [Client] = []
+    for waitingNumber in 1...numberOfClinet {
+        let assignedBanker = bankerArray[ waitingNumber % bankerArray.count ] // numberOfBanker
+        clientArray.append(Client(BussinessProcessState: false, waitingNumber: waitingNumber, assignedBankerNumber: assignedBanker.identityNumber))
+    }
+    return clientArray
+}
+
+func handleClient(clientArray: [Client]) {
+    for client in clientArray { // clientArray
+        print("\(client.waitingNumber)번 고객 업무 시작")
+        Banker.handleClientBussiness()
+        print("\(client.waitingNumber)번 고객 업무 종료")
     }
 }
 
-func startBank() {
+func startBank() throws {
     while true {
+        var isValid = startBankMenu()
         
-        /// Banker 생성
-        let numberOfBanker = 1
-        var bankerArray : [Banker] = []
-        for identityNumber in 1...numberOfBanker {
-            bankerArray.append(Banker(identityNumber: identityNumber, counterNumber: identityNumber))
-        }
-    
-        /// Client 생성
-        let numberOfClinet = Int.random(in: 10...30)
-        var clientArray : [Client] = []
-        for waitingNumber in 1...numberOfClinet {
-            let assignedBanker = bankerArray[ waitingNumber % numberOfBanker ] // numberOfBanker
-            clientArray.append(Client(BussinessProcessState: false, waitingNumber: waitingNumber, assignedBankerNumber: assignedBanker.identityNumber))
-        }
+        let bankerArray = createBanker(numberOfBanker: 1)
+        let clientArray = createClient(numberOfClinet: Int.random(in: 10...30), bankerArray: bankerArray)
+        handleClient(clientArray: clientArray)
         
-        /// 고객 업무 처리 기능
-        for client in clientArray { // clientArray
-            print("\(client.waitingNumber)번 고객 업무 시작")
-            Banker.handleClientBussiness()
-            print("\(client.waitingNumber)번 고객 업무 종료")
-        }
-        
-        
-        
-        let bank = Bank(numberOfBanker: numberOfBanker, numberOfWaitingClient: numberOfClinet, totalNumberOfClinet: numberOfClinet)
+        let bank = Bank(numberOfBanker: bankerArray.count, numberOfWaitingClient: clientArray.count, totalNumberOfClinet: clientArray.count)
         bank.closeBusiness()
     }
 }
@@ -62,7 +68,7 @@ func startBank() {
 
 
 do {
-    try startBankMenu()
+    try startBank()
 } catch {
     print(error.localizedDescription)
 }
