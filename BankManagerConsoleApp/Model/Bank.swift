56d40407247ccaf8afaed8a54f5efc920293afa2
summer0817 <step33@naver.com>
2021-04-27 10:53:42 +0900

    feat: 변수에 랜덤생성추가, closeBusiness함수내용추가 - 페어프로그래밍 summer

--- BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/Model/Bank.swift
@@ -9,12 +9,17 @@ import Foundation
 
 struct Bank {
     private var numberOfBanker: Int
-    private var numberOfWaitingClient: Int
+    private var numberOfWaitingClient = Int.random(in: 10...30)
     var totalNumberOfClinet: Int
     static var totalBusinessTime: Float = 0 // 소수점 두번째 자리가 출력하도록 구현이 안되어 있음
 
-    func closeBusiness() {
+    
+    mutating func closeBusiness() {
+        if numberOfWaitingClient == totalNumberOfClinet {
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalNumberOfClinet)명이며, 총 업무시간은 초 \(Bank.totalBusinessTime)입니다.")
+        } else {
+            numberOfWaitingClient += 1
+        }
     }
 }
 
