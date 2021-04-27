6795d90b2a07c0e2813d27ad1752786bc02ec801
summer0817 <step33@naver.com>
2021-04-27 11:19:37 +0900

    chore: startBank함수 작성내용 추가 - 페어프로그래밍 summer

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -8,8 +8,19 @@ import Foundation
 
 func startBank() {
     print("1. 은행 개점 \n2. 종료")
-    let userInput = readLine()
-//    var bank = Bank(numberOfBanker: <#Int#>, totalNumberOfClinet: <#Int#>)
+    guard let userInput = readLine() else { return print("nil값입니다. 숫자를 입력해주세요")}
+    let userInputNumber = Int(userInput)
+    if userInputNumber == 1 {
+        print("입력 : 1")
+       
+    } else if userInputNumber == 2 {
+        print("입력 : 2")
+        return
+    } else {
+        print("메뉴 숫자 1과 2중에 입력해주세요")
+      startBank()
+    }
+    //    var bank = Bank(numberOfBanker: <#Int#>, totalNumberOfClinet: <#Int#>)
 }
 
 
