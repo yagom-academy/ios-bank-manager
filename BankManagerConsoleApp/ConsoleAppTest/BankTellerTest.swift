336d36491508d64d21710b74a20ec9d648f31193
Neph <tony3322@naver.com>
2021-04-28 00:19:34 +0900

    [#5] BankTeller, ConsoleViewer, Looper unit test 진행

--- BankManagerConsoleApp/ConsoleAppTest/BankTellerTest.swift
+++ BankManagerConsoleApp/ConsoleAppTest/BankTellerTest.swift
@@ -7,20 +7,12 @@
 
 import XCTest
 @testable import BankManagerConsoleApp
+
 class BankTellerTest: XCTestCase {
     struct DummyBankTeller: BankTeller {}
     var dummyBankTeller = DummyBankTeller()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_BankTeller_handleBanking() {
+        dummyBankTeller.handleBanking(ofCustomerNumber: 8)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
