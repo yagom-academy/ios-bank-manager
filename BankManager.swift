c0d9f658a645f1029380b26cfe6f6e08e51bb9c1
steven <steven@stevenui-Macmini.local>
2021-04-28 17:13:59 +0900

    feat: step1의 요구사항처럼 타입 구현 및 기능 작동

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,123 @@
 //
 
 import Foundation
+
+struct MenuPrinter {
+    enum MenuPhrase {
+        static let bankOpen = "1 : 은행개점"
+        static let exit = "2 : 종료"
+        static let input = "입력 : "
+    }
+    
+    static func printMenu() {
+        print(MenuPhrase.bankOpen)
+        print(MenuPhrase.exit)
+        print(MenuPhrase.input, terminator:"")
+    }
+}
+
+struct Inputer {
+    enum StringConstant {
+        static let blank = ""
+    }
+    
+    static func receive() -> String {
+        guard let input = readLine() else {
+            return StringConstant.blank
+        }
+        return input
+    }
+}
+
+class Bank {
+    private var bankManager: BankManager
+    
+    init() {
+        bankManager = BankManager(numberOfBanker: 1)
+    }
+    
+    private func startWork() {
+        let startTime = CFAbsoluteTimeGetCurrent()
+        bankManager.inputCustomersToWaitingLine()
+        let endTime = CFAbsoluteTimeGetCurrent()
+        print(endTime - startTime)
+    }
+    
+    func open() {
+        while (true) {
+            MenuPrinter.printMenu()
+            let input = Inputer.receive()
+            switch input {
+            case "1":
+                startWork()
+            case "2":
+                return
+            default:
+                print("잘못된 입력입니다. 다시 입력해주세요.")
+            }
+        }
+    }
+}
+
+class BankManager {
+    private var numberOfBanker: Int
+    private var waitingLine: OperationQueue
+    private var customerMaker: CustomerMaker
+    
+    init(numberOfBanker: Int) {
+        self.numberOfBanker = numberOfBanker
+        waitingLine = OperationQueue()
+        customerMaker = CustomerMaker()
+        waitingLine.maxConcurrentOperationCount = self.numberOfBanker
+    }
+    
+    func inputCustomersToWaitingLine() {
+        customerMaker.makeCustomers().forEach{ waitingLine.addOperation($0.bankTask) }
+        waitingLine.waitUntilAllOperationsAreFinished()
+    }
+}
+
+class Customer {
+    private var _bankTask: BankTask
+    private var waitingNumber: Int
+    var bankTask: BankTask {
+        return _bankTask
+    }
+    
+    init(waitingNumber: Int) {
+        self._bankTask = BankTask(taskTime: 0.7, waitingNumber)
+        self.waitingNumber = waitingNumber
+    }
+}
+
+class CustomerMaker {
+    private var numberOfCustomer: Int
+    
+    init() {
+        self.numberOfCustomer = Int.random(in: 10...30)
+    }
+    
+    func makeCustomers() -> [Customer] {
+        var customers: [Customer] = []
+        for i in 1...numberOfCustomer {
+            customers.append(Customer(waitingNumber: i))
+        }
+        return customers
+    }
+}
+
+class BankTask: Operation {
+    private var taskTime: Double
+    private var waitingNumber: Int
+    
+    init(taskTime: Double, _ waitingNumber: Int) {
+        self.taskTime = taskTime
+        self.waitingNumber = waitingNumber
+    }
+    
+    override func main() {
+        print(waitingNumber, "번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: taskTime)
+        print(waitingNumber, "번 고객 업무 완료")
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -1,7 +1,108 @@
+////
+////  BankManagerConsoleApp - main.swift
+////  Created by yagom. 
+////  Copyright © yagom academy. All rights reserved.
+//// 
 //
-//  BankManagerConsoleApp - main.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
+//import Foundation
+//
+////let queue = OperationQueue()
+////queue.name = "custom Queue"
+////queue.qualityOfService = .default
+////
+////let bankOperation = BlockOperation {
+////    print("업무 시작")
+////    Thread.sleep(forTimeInterval: 0.7)
+////    print("업무 종료")
+////}
+////
+////let bankOperation2 = BlockOperation {
+////    print("업무 시작")
+////    Thread.sleep(forTimeInterval: 0.7)
+////    print("업무 종료")
+////}
+////
+////queue.maxConcurrentOperationCount = 2
+////
+////queue.addOperation(bankOperation)
+////queue.addOperation(bankOperation2)
+//
+//
+//class Task: Operation {
+//    var number: Int
+//    
+//    init (number: Int) {
+//        self.number = number
+//    }
+//    
+//    override func main() {
+//        print(number, "번 고객 업무 시작")
+//        Thread.sleep(forTimeInterval: 0.7)
+//        print(number, "번 고객 업무 종료")
+//    }
+//}
+//
+//class Bank {
+//    let queue = OperationQueue()
+//    
+//    func open() {
+//        
+//        queue.maxConcurrentOperationCount = 3
+//        
+//        while (true) {
+//            print("1. 은행 개점")
+//            print("2. 은행 종료")
+//            print("입력: ", terminator: "")
+//            let input = readLine()!
+//            
+//            switch input {
+//            case "1":
+//                handleCustomerTask()
+//            case "2":
+//                exit(1)
+//            default:
+//                print("다시 입력")
+//            }
+//        }
+//    }
+//    
+//    func handleCustomerTask() {
+//        // 랜덤으로 고객 수 만들기
+//        let numberOfCustomer = 10
+//        
+//        // 고객 수 만큼 일을 만들기
+//        let start = CFAbsoluteTimeGetCurrent()
+//        print(start)
+//        for i in 1...numberOfCustomer {
+//            let task = Task(number: i)
+//            queue.addOperation(task)
+//            task.completionBlock = {
+//                print(task.number, "done")
+//            }
+//        }
+//        queue.waitUntilAllOperationsAreFinished()
+//        let end = CFAbsoluteTimeGetCurrent()
+//        print(end)
+//        print("업무가 마갑되었습니다. 오늘 업무를 처리한 고객은 총", numberOfCustomer, "명이며 총 업무 시간은", (end-start).showUptoTwoDecimalPoint(), "초입니다")
+//    }
+//    
+//    func close() {
+//        
+//    }
+//    
+//}
+//
+//extension Double {
+//    func showUptoTwoDecimalPoint() -> String {
+//        let numbarFormatter = NumberFormatter()
+//        numbarFormatter.roundingMode = .floor
+//        numbarFormatter.minimumSignificantDigits = 2
+//        numbarFormatter.maximumSignificantDigits = 3
+//        let result = numbarFormatter.string(for: self)
+//        return result!
+//    }
+//}
+//
+//Bank().open()
 
-import Foundation
+Bank().open()
