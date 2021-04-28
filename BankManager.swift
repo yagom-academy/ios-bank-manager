2b8b761cca4cd80ad3778daf9210e3fdd861804c
Wody95 <kwu0626@gmail.com>
2021-04-28 10:51:28 +0900

    chore : 메서드 접근제한자 설정, 코드 스타일 수정

--- BankManager.swift
+++ BankManager.swift
@@ -11,32 +11,23 @@ class BankManager {
     func openBank() {
         var bankState: Bool = choiceBankState()
         
-        if bankState {
+        while bankState {
             let customerManager: CustomerManager = CustomerManager()
             let todayTotalVisitCustomers: Int = customerManager.countCustomers()
             
             while bankState {
                 let customer: Int = customerManager.matchBankerAndCustomer()
+                bankState = checkRemainingCustomer(countRemainingCustomer: customerManager.countCustomers(),
+                                                   todayTotalVisitCustomers: todayTotalVisitCustomers)
                 
-                workProgress(customerNumber: customer)
-                
-                if customerManager.countCustomers() == 0 {
-                    print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(todayTotalVisitCustomers)명이며, 총 업무 시간은 \(Double(todayTotalVisitCustomers) * 0.7)초 입니다.")
-                    
-                    bankState = false
-                }
-                
+                bankerWorkProgress(customerNumber: customer)
             }
             
             bankState = choiceBankState()
-            
-            if bankState {
-                openBank()
-            }
         }
     }
     
-    func choiceBankState() -> Bool {
+    private func choiceBankState() -> Bool {
         while true {
             print("1 : 은행개점")
             print("2 : 종료")
@@ -56,10 +47,20 @@ class BankManager {
         }
     }
     
-    func workProgress(customerNumber: Int) {
+    private func bankerWorkProgress(customerNumber: Int) {
         print("\(customerNumber)번 고객 업무 시작")
         usleep(700000)
         print("\(customerNumber)번 고객 업무 완료")
     }
 
+    private func checkRemainingCustomer(countRemainingCustomer: Int, todayTotalVisitCustomers: Int) -> Bool {
+        if countRemainingCustomer == 0 {
+            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(todayTotalVisitCustomers)명이며, 총 업무 시간은 \(Double(todayTotalVisitCustomers) * 0.7)초 입니다.")
+            
+            return false
+        }
+        
+        return true
+    }
+    
 }
