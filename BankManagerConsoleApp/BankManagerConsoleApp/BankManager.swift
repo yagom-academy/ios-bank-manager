aba7b84e342a8cc8dccf5692f6b055bc127ba2a0
stevenkim18 <11882@naver.com>
2021-04-28 17:31:02 +0900

    style: BankManager 관련 클래스들을 BankManager.swift로 이동

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -0,0 +1,43 @@
+//
+//  BankManager.swift
+//  BankManagerConsoleApp
+//
+//  Created by steven on 2021/04/28.
+//
+
+import Foundation
+
+class CustomerMaker {
+    private var numberOfCustomer: Int
+    
+    init() {
+        self.numberOfCustomer = Int.random(in: 10...30)
+    }
+    
+    func makeCustomers() -> [Customer] {
+        var customers: [Customer] = []
+        for i in 1...numberOfCustomer {
+            customers.append(Customer(waitingNumber: i))
+        }
+        return customers
+    }
+}
+
+class BankManager {
+    private var numberOfBanker: Int
+    private var waitingLine: OperationQueue
+    private var customerMaker: CustomerMaker
+    
+    init(numberOfBanker: Int) {
+        self.numberOfBanker = numberOfBanker
+        waitingLine = OperationQueue()
+        customerMaker = CustomerMaker()
+        waitingLine.maxConcurrentOperationCount = self.numberOfBanker
+    }
+    
+    func inputCustomersToWaitingLine() {
+        customerMaker.makeCustomers().forEach{ waitingLine.addOperation($0.bankTask) }
+        waitingLine.waitUntilAllOperationsAreFinished()
+    }
+}
+
