ae756240913e7f6f7fa87b840a528c3b7b8568f7
eunseo5355 <eunseo5355@naver.com>
2021-04-29 23:35:05 +0900

    [Feat] Bank 클래스 생성
    - 고객을 생성해주는 createClient() 메소드 구현
    - 은행원을 생성해주는 createTeller() 메소드 구현
    - 메뉴 선택해서 메뉴 번호 반환해주는 selectMenu() 메소드 구현
    - 은행 개점하는 open() 메소드 구현
    - 고객의 일을 처리해주는 handleTask() 메소드 구현
    - 은행 마감하는 close() 메소드 구현
    - 은행 운영을 결정하는 operate() 메소드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -6,4 +6,80 @@
 //
 
 import Foundation
-
+final class Bank {
+    private var clients: [Client] = []
+    private var tellers: [Teller] = []
+    private var tasks: [Operation] = []
+    private var totalTaskTime: Double = 0
+    
+    private func createClient() {
+        for number in 1...Int.random(in: 10...30) {
+            clients.append(Client(waitingNumber: number))
+        }
+    }
+    
+    private func createTeller(number: Int) {
+        for number in 1...number {
+            tellers.append(Teller(counterNumber: number, identityNumber: number))
+        }
+    }
+    
+    private func selectMenu() -> String? {
+        print("""
+        1 : 은행 개점
+        2 : 종료
+        입력 :
+        """, terminator: " ")
+        
+        guard let menuNumber = readLine() else { return nil }
+        
+        return menuNumber
+    }
+    
+    private func open() {
+        let clientWaitingLineQueue = OperationQueue()
+        
+        createTeller(number: 1)
+        createClient()
+        
+        clientWaitingLineQueue.maxConcurrentOperationCount = tellers.count
+        
+        handleTask(clientWaitingLineQueue)
+    }
+    
+    private func handleTask(_ clientWaitingLine: OperationQueue) {
+        for client in clients {
+            tasks.append(client.task)
+            totalTaskTime += client.task.taskTime
+        }
+        
+        clientWaitingLine.addOperations(tasks, waitUntilFinished: true)
+    }
+    
+    private func close() {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(clients.count)명이며, 총 업무시간은 \(String(format: "%.2f", totalTaskTime))초입니다.")
+        
+        clients.removeAll()
+        tellers.removeAll()
+        tasks.removeAll()
+    }
+    
+    func operate() {
+        var menuNumber: String?
+        
+        while true {
+            menuNumber = selectMenu()
+            
+            switch menuNumber {
+            case "1":
+                open()
+                close()
+            case "2":
+                return
+            default:
+                print("잘못된 입력입니다.")
+                continue
+            }
+        }
+    }
+}
