fe7203aea8d5b1babf003f92fb5b6a140df13c6b
kio <un19451024@gmail.com>
2021-04-29 22:06:08 +0900

    [Feat] 은행창구 업무처리 메서드 분리 1. sendToCounter() - n번 고객업무시작-업무완료 분리 -> workTask() 2. workdTask() 생성

--- BankManager.swift
+++ BankManager.swift
@@ -19,13 +19,17 @@ class BankManager {
         numberOfClient = Int.random(in: 10...30)
     }
     
+    func workTask(order: Int) {
+        print("\(order)번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: 0.7)
+        print("\(order)n번 고객 업무 완료★")
+    }
+    
     func sendToCounter() {
         counter.maxConcurrentOperationCount = numberOfTeller
         for index in 1...numberOfClient {
             counter.addOperation {
-                print("\(index)번 고객 업무 시작")
-                Thread.sleep(forTimeInterval: 0.7)
-                print("\(index)n번 고객 업무 완료★")
+                self.workTask(order: index)
             }
         }
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClient)명이며, 총 업무시간은 \(Double(numberOfClient) * 0.7)초입니다.")
