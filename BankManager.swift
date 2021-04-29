5ed22a588a968f41163080721e9d13f0ce0db934
kio <un19451024@gmail.com>
2021-04-29 21:49:34 +0900

    [Feat] 은행창구 업무 메서드 추가 - sendToCounter() 생성

--- BankManager.swift
+++ BankManager.swift
@@ -14,8 +14,20 @@ class BankManager {
     init(numberOfTeller: Int) {
         self.numberOfTeller = numberOfTeller
     }
-
+    
     func generateNumberOfClient() {
         numberOfClient = Int.random(in: 10...30)
+    }
+    
+    func sendToCounter() {
+        counter.maxConcurrentOperationCount = numberOfTeller
+        for index in 1...numberOfClient {
+            counter.addOperation {
+                print("\(index)번 고객 업무 시작")
+                Thread.sleep(forTimeInterval: 0.7)
+                print("\(index)n번 고객 업무 완료★")
+            }
+        }
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClient)명이며, 총 업무시간은 \(Double(numberOfClient) * 0.7)초입니다.")
     }
 }
