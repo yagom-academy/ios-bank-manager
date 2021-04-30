db985c576a81923d785916102be7cee8462fec90
fezravien <yjaewoong@naver.com>
2021-04-30 21:57:17 +0900

    feat : BankService 구현
    고객등급 구현
    - 등급 문자열 반환
    업무종료
    - 소요시간 반환

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankService.swift
@@ -0,0 +1,39 @@
+//
+//  BankService.swift
+//  BankManagerConsoleApp
+//
+//  Created by 윤재웅 on 2021/04/30.
+//
+
+import Foundation
+
+enum creditRating: CustomStringConvertible {
+    case vvip
+    case vip
+    case normal
+    
+    var description: String {
+        switch self {
+        case .vvip:
+            return "VVIP"
+        case .vip:
+            return "VIP"
+        case .normal:
+            return "일반"
+        }
+    }
+}
+
+enum workType {
+    case deposit
+    case loan
+    
+    var duration: Double {
+        switch self {
+        case .deposit:
+            return 0.7
+        case .loan:
+            return 1.1
+        }
+    }
+}
