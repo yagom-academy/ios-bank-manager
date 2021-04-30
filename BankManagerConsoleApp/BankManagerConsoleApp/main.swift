3db0781bc63e3512ee1e71f24f920e27931b2c68
Ian <isnn0103@gmail.com>
2021-04-30 12:33:54 +0900

    - 이 저장소를 자신의 저장소로 fork하여 프로젝트를 진행합니다
    feat: Operation Queue 미적용 코드 구현
    
    Operation Queue를 적용하지 않은 Step1 코드를 구현함.

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,42 @@
 // 
 
 import Foundation
+
+struct Bank {
+    func displayConsoleMenu() {
+        provideOption()
+        let userNumberInput = receiveUserNumberInput()
+        switch userNumberInput {
+        case 1:
+            let numberOfCustomers = makeRandomNumberOfCustomers()
+            for i in 1...numberOfCustomers {
+                print("\(i)번 고객 업무 시작")
+                usleep(700000)
+                print("\(i)번 고객 업무 완료")
+            }
+            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomers)명이며, 총 업무시간은 \(Double(numberOfCustomers) * 0.7)초 입니다.")
+            displayConsoleMenu()
+        case 2:
+            exit(0)
+        default:
+            print("잘못된 입력입니다.")
+            displayConsoleMenu()
+        }
+    }
+
+    func provideOption() {
+        print("1 : 은행개점")
+        print("2 : 종료")
+    }
+    
+    func receiveUserNumberInput() -> Int {
+        guard let optionalInput = readLine(), let inputNumber = Int(optionalInput) else { return 0 }
+        return inputNumber
+    }
+    
+    func makeRandomNumberOfCustomers() -> Int {
+        return Int.random(in: 10...30)
+    }
+}
+
+Bank().displayConsoleMenu()
