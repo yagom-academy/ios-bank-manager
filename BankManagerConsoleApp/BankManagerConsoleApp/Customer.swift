d3aee55ad20b3c50b6e6877610677454904ad298
Sookim18 <scstnghks@naver.com>
2021-04-30 13:28:53 +0900

    refactor : BankTask, CustomerMaker 파일 분리, BankManager 파일 중복 제거

--- BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -7,24 +7,6 @@
 
 import Foundation
 
-class BankTask: Operation {
-    
-    private var taskTime: Double
-    private var waitingNumber: Int
-    
-    init(taskTime: Double, _ waitingNumber: Int) {
-        self.taskTime = taskTime
-        self.waitingNumber = waitingNumber
-    }
-    
-    override func main() {
-        print(waitingNumber, "번 고객 업무 시작")
-        Thread.sleep(forTimeInterval: taskTime)
-        print(waitingNumber, "번 고객 업무 완료")
-    }
-    
-}
-
 struct Customer {
     
     private var _bankTask: BankTask
@@ -32,7 +14,7 @@ struct Customer {
     var bankTask: BankTask {
         return _bankTask
     }
-    
+
     init(waitingNumber: Int) {
         self._bankTask = BankTask(taskTime: 0.7, waitingNumber)
         self.waitingNumber = waitingNumber
