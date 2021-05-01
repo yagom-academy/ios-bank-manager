d52880be0867f7879245ce74a917eb1b7ef44e12
dacodaco <imochi@naver.com>
2021-05-01 16:00:44 +0900

    [Feat] #5 NotificationBoard 타입 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -12,8 +12,29 @@ class Bank {
     let customerQueue: CustomerQueue = CustomerQueue()
     let notificationBoard: NotificationBoard = NotificationBoard()
 
-    class NotificationBoard {}
-
+    class NotificationBoard {
+        var observers: [CustomerQueue] = []
+        
+        func addObserver(_ observer: CustomerQueue) {
+            observers.append(observer)
+        }
+        
+        func removeObserver(_ observer: CustomerQueue) {
+            observers.removeAll(where: $0.name == observer.name)
+        }
+        
+        func call(by teller: Teller) {
+            floatOnBoard(by: teller)
+        }
+        
+        func floatOnBoard(by teller: Teller) {
+            for customerQueue in observers {
+                guard let customer = customerQueue.dequeue() else { continue }
+                customer.go(to: teller)
+            }
+        }
+    }
+    
     class Teller {}
 
     struct CustomerQueue {}
