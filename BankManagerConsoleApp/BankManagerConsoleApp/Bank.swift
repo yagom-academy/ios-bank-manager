3413142a7842030879ff8bfbc92c5993a13bc8f8
duckbok <duckbok22@gmail.com>
2021-05-01 15:56:30 +0900

    [Feat] #5 CustomerQueue 타입 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -9,14 +9,29 @@ import Foundation
 
 class Bank {
     var counters: [Teller] = []
-    let customerQueue: CustomerQueue = CustomerQueue()
+    let customerQueue: CustomerQueue = CustomerQueue(name: "고객 대기열")
     let notificationBoard: NotificationBoard = NotificationBoard()
 
     class NotificationBoard {}
 
     class Teller {}
 
-    struct CustomerQueue {}
+    struct CustomerQueue {
+        private var queue: [Customer] = []
+        let name: String
+
+        init(name: String) {
+            self.name = name
+        }
+
+        mutating func enqueue(customer: Customer) {
+            queue.append(customer)
+        }
+
+        mutating func dequeue() -> Customer? {
+            return queue.removeFirst()
+        }
+    }
 
     class Customer {}
 }
