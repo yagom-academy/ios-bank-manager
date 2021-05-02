24b03887a7b7aceacfef5e79e1a63d4cf733964a
cjm <stepsw112@gmail.com>
2021-05-02 21:30:23 +0900

    Feat: ClientTask 타입과 ClientGrade 타입 추가

---
+++ BankManagerConsoleApp/Constatnt/ClientGrade.swift
@@ -0,0 +1,14 @@
+//
+//  ClientGrade.swift
+//  BankManagerConsoleApp
+//
+//  Created by 최정민 on 2021/05/02.
+//
+
+import Foundation
+
+enum ClientGrade {
+    static let vvip = "VVIP고객"
+    static let vip = "VIP고객"
+    static let general = "일반고객"
+}
---
+++ BankManagerConsoleApp/Constatnt/ClientTask.swift
@@ -0,0 +1,13 @@
+//
+//  ClientTask.swift
+//  BankManagerConsoleApp
+//
+//  Created by 최정민 on 2021/05/02.
+//
+
+import Foundation
+
+enum ClientTask {
+    static let loan = "대출"
+    static let deposit = "예금"
+}
