01f4ace0d132d393899366615494501f64bc2775
kio <un19451024@gmail.com>
2021-05-03 19:34:48 +0900

    [Refactor] 메서드 접근제한 설정 및 문자열의 지역 변수 할당

--- BankManager.swift
+++ BankManager.swift
@@ -15,14 +15,17 @@ class BankManager {
         self.numberOfTeller = numberOfTeller
     }
     
-    func generateNumberOfClient() {
+    private func generateNumberOfClient() {
         numberOfClient = UInt.random(in: 10...30)
     }
     
-    func workTask(order: UInt) {
-        print("\(order)번 고객 업무 시작")
+    private func workTask(order: UInt) {
+        let tellerStartWorkMessage = "\(order)번 고객 업무 시작"
+        let tellerFinishWorkMessage = "\(order)번 고객 업무 완료★"
+        
+        print(tellerStartWorkMessage)
         Thread.sleep(forTimeInterval: 0.7)
-        print("\(order)번 고객 업무 완료★")
+        print(tellerFinishWorkMessage)
     }
     
     func sendToCounter() {
@@ -36,7 +39,9 @@ class BankManager {
         closeBank()
     }
     
-    func closeBank() {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClient)명이며, 총 업무시간은 \(Double(numberOfClient) * 0.7)초입니다.")
+    private func closeBank() {
+        let closeBankMessage = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClient)명이며, 총 업무시간은 \(Double(numberOfClient) * 0.7)초입니다."
+        
+        print(closeBankMessage)
     }
 }
