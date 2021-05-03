c2bf04834ca0a469d80644f38a81655b0554d3d8
Sookim18 <scstnghks@naver.com>
2021-05-03 16:37:44 +0900

    style : BankPrinter, InputHelper 파일 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -7,25 +7,6 @@
 
 import Foundation
 
-struct BankPrinter {
-
-    static func printMenu() {
-        print("1 : 은행개점")
-        print("2 : 종료")
-        print("입력 : ", terminator:"")
-    }
-    
-    static func printFinishPharse(_ totalCustomerNumber: Int, _ totalSecond: Double) {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerNumber)명이며, 총 업무시간은 \(totalSecond.cutNumberToTwoDecimalPoint())초입니다.")
-
-    }
-    
-    static func printWrongInputPharse() {
-        print("잘못된 입력입니다. 다시 입력해주세요.")
-    }
-
-}
-
 extension Double {
 
     func cutNumberToTwoDecimalPoint() -> String {
@@ -38,15 +19,6 @@ extension Double {
         return result
     }
 
-}
-
-struct InputHelper {
-
-    static func receive() -> String? {
-        guard let input = readLine() else { return nil }
-        return input
-    }
-    
 }
 
 struct Bank {
