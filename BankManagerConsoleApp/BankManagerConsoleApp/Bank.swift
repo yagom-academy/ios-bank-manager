4b4a1e46fd1b695a42f0573768858914bbd98961
Sookim18 <scstnghks@naver.com>
2021-05-03 16:31:22 +0900

    style : printToTwoDecimalPoint -> cutNumberToTwoDecimalPoint 로 네이밍변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -16,7 +16,7 @@ struct BankPrinter {
     }
     
     static func printFinishPharse(_ totalCustomerNumber: Int, _ totalSecond: Double) {
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerNumber)명이며, 총 업무시간은 \(totalSecond.printToTwoDecimalPoint())초입니다.")
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerNumber)명이며, 총 업무시간은 \(totalSecond.cutNumberToTwoDecimalPoint())초입니다.")
 
     }
     
@@ -28,7 +28,7 @@ struct BankPrinter {
 
 extension Double {
 
-    func printToTwoDecimalPoint() -> String {
+    func cutNumberToTwoDecimalPoint() -> String {
         let numberFormatter = NumberFormatter()
         numberFormatter.roundingMode = .floor
         numberFormatter.maximumFractionDigits = 2
