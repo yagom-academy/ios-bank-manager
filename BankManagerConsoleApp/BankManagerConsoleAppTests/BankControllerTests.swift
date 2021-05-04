4aac7ade725b219cb45121c9920f06ae80e73845
dacodaco <imochi@naver.com>
2021-05-04 11:25:16 +0900

    [Test] #2 receiveCustomer 메서드 테스트 진행

--- BankManagerConsoleApp/BankManagerConsoleAppTests/BankControllerTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/BankControllerTests.swift
@@ -8,4 +8,30 @@
 import XCTest
 
 class BankControllerTests: XCTestCase {
+    var bank: Bank!
+    var sut: BankController!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        bank = Bank()
+        sut = BankController(of: bank)
+    }
+
+    override func tearDownWithError() throws {
+        try super.setUpWithError()
+        sut = nil
+        bank = nil
+    }
+
+    func test_customer2명이찾아왔을때_customerQueue의_count가_2가되는가() {
+        sut.receiveCustomer(number: 2)
+
+        XCTAssertEqual(bank.customerQueue.queue.count, 2)
+    }
+
+    func test_customer2명이찾아왔을떄_두번째cutomer의_waitingNumber가_1이되는가() {
+        sut.receiveCustomer(number: 2)
+
+        XCTAssertEqual(bank.customerQueue.queue[1].waitingNumber, 1)
+    }
 }
