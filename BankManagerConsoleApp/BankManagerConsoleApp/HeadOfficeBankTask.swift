47afe149e0db15ce1eccdaae8b4795fb635690dd
Sookim18 <scstnghks@naver.com>
2021-05-04 20:14:05 +0900

    refactor : HeadOfficeBankTask클래스 내 customerGrade프로퍼티 추가, print문 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/HeadOfficeBankTask.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/HeadOfficeBankTask.swift
@@ -8,15 +8,19 @@
 import Foundation
 
 class HeadOfficeBankTask: Operation {
-    private var waitingNumber: Int
-   
-    init(waitingNumber: Int) {
-        self.waitingNumber = waitingNumber
-    }
     
-    override func main() {
-        print(waitingNumber, "대출 심사 시작")
-        Thread.sleep(forTimeInterval: 0.5)
-        print(waitingNumber, "대출 심사 종료")
+    private var waitingNumber: Int
+    private var customerGrade: String
+
+    init(waitingNumber: Int, customerGrade: String) {
+        self.waitingNumber = waitingNumber
+        self.customerGrade = customerGrade
     }
+
+    override func main() {
+        print("🟡\(waitingNumber)번 \(customerGrade)고객 대출심사 시작")
+        Thread.sleep(forTimeInterval: 0.5)
+        print("🟢\(waitingNumber)번 \(customerGrade)고객 대출심사 완료")
+    }
+
 }
