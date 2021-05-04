5aa4b5324bc65bb92060a7d145a8597e65d36841
fezravien <yjaewoong@naver.com>
2021-05-05 00:50:28 +0900

    refactor : LocalBank 수정
    기존 LocalBank에서 고객의 정보를 랜덤으로 할당하던 작업을
    고객 자체가 행할 수 있도록 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/LocalBank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LocalBank.swift
@@ -7,50 +7,35 @@
 
 import Foundation
 
-final class LocalBank: Bankable {
+final class LocalBank {
     var bankWindow = OperationQueue()
     var workTime = Double.zero
     var taskList: [LocalBankTask] = []
     var headBank: HeadBank?
     
-    func serveClient() {
-        bankWindow.maxConcurrentOperationCount = 3
-        let totalCustomer = customerNumber()
+    func serveClient(_ bankTeller: Int) {
+        bankWindow.maxConcurrentOperationCount = bankTeller
+        let totalCustomer = acceptClient()
         for waitNumber in 1...totalCustomer {
-            let randomClient = acceeptRandomClient()
-            let randomCredit = randomClient.0
-            let randomWorkType = randomClient.1
-            let localBankTask = LocalBankTask()
-            localBankTask.waitingNumber = UInt(waitNumber)
-            localBankTask.creditRate = randomCredit
-            localBankTask.workType = randomWorkType
+            guard let localBankTask = LocalBankTask(UInt(waitNumber)) else {
+                return
+            }
             assignPriority(localBankTask)
             taskList.append(localBankTask)
             localBankTask.completionBlock = {
-                guard let typeOfTask = localBankTask.workType else { return }
-                self.workTime += typeOfTask.duration
+                self.workTime += localBankTask.workType.duration
             }
         }
         bankWindow.addOperations(taskList, waitUntilFinished: true)
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은\(String(format: "%.2f", workTime))초 입니다.")
     }
     
-    private func customerNumber() -> Int {
+    private func acceptClient() -> Int {
         return Int.random(in: 10...30)
     }
     
-    private func acceeptRandomClient() -> (CreditRating, WorkType) {
-        guard let creditRate = CreditRating.allCases.shuffled().first,
-              let typeOfWork = WorkType.allCases.shuffled().first else {
-            fatalError()
-        }
-        
-        return (creditRate, typeOfWork)
-    }
-    
     private func assignPriority(_ task: LocalBankTask) {
-        guard let rate = task.creditRate else { return }
-        switch rate {
+        switch task.creditRate {
         case .vvip:
             return task.queuePriority = .high
         case .vip:
