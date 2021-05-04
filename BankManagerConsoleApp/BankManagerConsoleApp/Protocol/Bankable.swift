3d5364232e0bbfda00589ecda6afb8ad4a0ede9d
fezravien <yjaewoong@naver.com>
2021-05-04 17:51:37 +0900

    feat : Bankable 프로토콜 생성
    HeadBank, LocalBank 클래스에서 채택하여 추상화

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Protocol/Bankable.swift
@@ -0,0 +1,12 @@
+//
+//  Bankable.swift
+//  BankManagerConsoleApp
+//
+//  Created by 윤재웅 on 2021/05/04.
+//
+
+import Foundation
+
+protocol Bankable {
+    var bankWindow: OperationQueue { get set }
+}
