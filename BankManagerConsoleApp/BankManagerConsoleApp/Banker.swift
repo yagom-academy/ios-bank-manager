5abbf2101568f66e6c98bbc5d535f622cdaa6662
Wody95 <kwu0626@gmail.com>
2021-05-04 20:04:46 +0900

    feat : Banker타입 workTime 프로퍼티 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 struct Banker {
+    var workTime: Double = 0
     
-    func bankerWorkProgress(customer: Customer) {
+    mutating func bankerWorkProgress(customer: Customer) {
         print("\(customer.waitNumber)번 \(customer.tier.rawValue)고객 \(customer.business.rawValue)업무 시작")
         switch customer.business {
         case .deposit:
             usleep(700000)
+            self.workTime += 0.7
         default:
             usleep(1100000)
+            self.workTime += 1.1
         }
         
         print("\(customer.waitNumber)번 \(customer.tier.rawValue)고객 \(customer.business.rawValue)업무 완료")
