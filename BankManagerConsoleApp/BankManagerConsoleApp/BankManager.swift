a9b224886ecff7c7be156d16f8181b92922b1f14
eunseo5355 <eunseo5355@naver.com>
2021-05-06 02:13:09 +0900

    [Feat] 고객 등급에 따라 업무 처리할 수 있도록 구현
    - 고객 등급에 따라 업무 우선순위를 할당해주는 assignTaskPriority() 메소드 구현
    - 업무 시간을 계산해주는 calculateTaskTime() 메소드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -11,20 +11,41 @@ struct BankManager {
     let numberOfManager: Int
     
     func handleTask(_ clients: [Client]) -> Double {
-        var totalTaskTime: Double = 0
-        var tasks: [Operation] = []
+        var tasks: [BankTaskOperation] = []
         let clientWaitingLineQueue = OperationQueue()
         
         clientWaitingLineQueue.maxConcurrentOperationCount = numberOfManager
         
         for client in clients {
-            let task = client.getTask()
+            var task = client.getTask()
+            
+            assignTaskPriority(&task)
             
             tasks.append(task)
-            totalTaskTime += task.getTaskTime()
         }
         
         clientWaitingLineQueue.addOperations(tasks, waitUntilFinished: true)
+        
+        return calculateTaskTime(tasks)
+    }
+    
+    private func assignTaskPriority(_ task: inout BankTaskOperation) {
+        switch task.getClientRating() {
+        case .vvip:
+            task.queuePriority = .veryHigh
+        case .vip:
+            task.queuePriority = .high
+        case .normal:
+            task.queuePriority = .normal
+        }
+    }
+    
+    private func calculateTaskTime(_ tasks: [BankTaskOperation]) -> Double {
+        var totalTaskTime: Double = 0
+        
+        for task in tasks {
+            totalTaskTime += task.getTaskTime()
+        }
         
         return totalTaskTime
     }
