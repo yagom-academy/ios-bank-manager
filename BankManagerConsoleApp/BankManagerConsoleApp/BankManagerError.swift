b3d5311055583e9d65b95529d562cbf240b47294
blanche37 <wkddbsghk16@gmail.com>
2021-05-05 14:51:51 +0900

    Style: BankManagerError타입 프로토콜별 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManagerError.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManagerError.swift
@@ -5,9 +5,11 @@
 //  Created by Yun, Ryan on 2021/04/29.
 //
 
-enum BankManagerError: Error, CustomStringConvertible {
+enum BankManagerError: Error {
     case invalidMenu
-    
+}
+
+extension BankManagerError: CustomStringConvertible {
     var description: String {
         switch self {
         case .invalidMenu:
