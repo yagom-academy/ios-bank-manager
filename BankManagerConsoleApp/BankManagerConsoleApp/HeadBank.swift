f1f03c694e30fdd93148af6ca49545e2cdb4570d
inwoodev <dlsdn1207@gmail.com>
2021-05-05 16:13:18 +0900

    Feat : Semaphore 구현 추후에 추가 Operation이 HeadBank로 보내질 때 경쟁상황에 대해서 상호 배제적으로 작동하게 하기 위해서 semaphore 설정(value: 1)

--- BankManagerConsoleApp/BankManagerConsoleApp/HeadBank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/HeadBank.swift
@@ -14,13 +14,20 @@ final class HeadBank {
     private init() { }
     
     func serveClient(_ data: HeadBankTask) {
+        let semaphore = DispatchSemaphore(value: 1)
         bankWindow.maxConcurrentOperationCount = 1
+        let headBankTask = acceptClientDocument(data)
+        semaphore.wait()
+        bankWindow.addOperation(headBankTask)
+        semaphore.signal()
+        headBankTask.waitUntilFinished()
+    }
+    
+    private func acceptClientDocument(_ data: HeadBankTask) -> HeadBankTask {
         let headBankTask = HeadBankTask()
         headBankTask.waitingNumber = data.waitingNumber
         headBankTask.creditRate = data.creditRate
         headBankTask.workType = data.workType
-        bankWindow.addOperation(headBankTask)
-        headBankTask.waitUntilFinished()
+        return headBankTask
     }
 }
-
