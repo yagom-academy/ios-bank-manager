5c6f5eb4d3f525cbfca47a758a2919b22d7bc2f2
eunseo5355 <eunseo5355@naver.com>
2021-05-06 02:17:53 +0900

    [Feat] 오류 처리 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -9,9 +9,13 @@ import Foundation
 while true {
     switch Bank.selectMenu() {
     case .start:
-        let bankManger = BankManager(numberOfManager: 3)
-        let clients: [Client] = Client.create()
-        Bank(bankManger, clients).operate()
+        do {
+            let bankManger = BankManager(numberOfManager: 3)
+            let clients: [Client] =  try Client.create()
+            Bank(bankManger, clients).operate()
+        } catch let error {
+            print("\(error.localizedDescription)")
+        }
     case .end:
         exit(0)
     default:
