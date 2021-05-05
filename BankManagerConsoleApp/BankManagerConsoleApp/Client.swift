9d1cdb016fa1a535704cc133f34ba3e573f7b087
blanche37 <wkddbsghk16@gmail.com>
2021-05-05 15:08:14 +0900

    Feat: 고객등급 생성 및 랜덤 적용

--- BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
@@ -10,10 +10,11 @@ import Foundation
 final class Client: Operation {
     // MARK: - Properties
     let waitingNumber: Int
+    let grade: Grade
     
     init(_ waitingNumber: Int) {
         self.waitingNumber = waitingNumber
-        
+        self.grade = Grade.random
     }
     
     // MARK: - NameSpaces
@@ -21,13 +22,37 @@ final class Client: Operation {
         static let deposit: Double = 0.7
     }
     
+    enum Grade: CaseIterable {
+        case vvip
+        case vip
+        case normal
+        
+        var name: String {
+            switch self {
+            case .vvip:
+                return "VVIP"
+            case .vip:
+                return "VIP"
+            case .normal:
+                return "일반"
+            }
+        }
+        
+        static var random: Grade {
+            guard let randomGrade: Grade = Grade.allCases.randomElement() else {
+                return .normal
+            }
+            return randomGrade
+        }
+    }
+    
     // MARK: - Private Method
     func startTask() -> String {
-        return "\(waitingNumber) 번 고객 업무 시작."
+        return "\(waitingNumber)번 \(grade.name) 고객 업무 시작."
     }
     
     func endTask() -> String {
-        return "\(waitingNumber) 번 고객 업무 종료!"
+        return "\(waitingNumber)번 \(grade.name) 고객 업무 종료!"
     }
     
     // MARK: - Override Method from the Operation Class
