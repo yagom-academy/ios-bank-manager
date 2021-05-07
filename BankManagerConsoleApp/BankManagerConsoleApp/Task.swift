dcfa617789f2f64251f518bc688bd98974ad7127
Ian <isnn0103@gmail.com>
2021-05-07 10:47:13 +0900

    refactor: struct Task 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Task.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Task.swift
@@ -11,4 +11,18 @@ class Task: Operation {
     var waitingNumber: Int
     var grade: CustomerPriority
     var visitPurpose: TaskType
+    
+    init(waitingNumber: Int, grade: CustomerPriority) {
+        self.waitingNumber = waitingNumber
+        self.grade = grade
+        self.visitPurpose = TaskType.random
+        super.init()
+        super.queuePriority = grade.queuePriority
+    }
+    
+    override func main() {
+        print("\(waitingNumber)번 \(grade.tier)고객 \(visitPurpose.purpose)업무 시작")
+        Thread.sleep(forTimeInterval: visitPurpose.taskTime)
+        print("\(waitingNumber)번 \(grade.tier)고객 \(visitPurpose.purpose)업무 완료")
+    }
 }
