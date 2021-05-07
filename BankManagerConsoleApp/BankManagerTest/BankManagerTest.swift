69e9b67b0711dd4340deae3071dd37d6de08b399
kane-young <lyw2100@naver.com>
2021-05-07 12:26:14 +0900

    Feat : BankManager를 위한 Stub 생성 및 테스트 작성

--- BankManagerConsoleApp/BankManagerTest/BankManagerTest.swift
+++ BankManagerConsoleApp/BankManagerTest/BankManagerTest.swift
@@ -8,18 +8,45 @@
 import XCTest
 
 class BankManagerTest: XCTestCase {
-  var bankManager: BankManager!
-  
-    override func setUpWithError() throws {
+  class bankManagerStub {
+    let operationQueue: OperationQueue = OperationQueue()
+    var customers: [Customer] = []
+    
+    init() {
       do {
-        try bankManager = BankManager(numberOfBankers: 3)
+        customers = try CustomerMaker().makeCustomer(count: 10)
       } catch {
-        XCTFail("bankManager 생성 실패")
+        XCTFail("customers 생성 실패")
       }
+      operationQueue.maxConcurrentOperationCount = 3
     }
-
-    override func tearDownWithError() throws {
-      bankManager = nil
+    
+    func inputCustomersIntoOperationQueue(completion: @escaping ()->Void) {
+      operationQueue.addOperations(customers.map { $0.task }, waitUntilFinished: true)
+      operationQueue.waitUntilAllOperationsAreFinished()
+      completion()
     }
-
+  }
+  
+  var bankManager: bankManagerStub!
+  
+  override func setUpWithError() throws {
+    bankManager = bankManagerStub()
+  }
+  
+  override func tearDownWithError() throws {
+    bankManager = nil
+  }
+  
+  ///10명 고객 최대 업무 걸리는 시간 11초
+  ///대출 - 총 1.1초
+  ///예금 - 총 0.7초
+  func test_bankManager_Stub을_통한_inputCustomersIntoOperationQueue_메서드_테스트() {
+    let expectation = XCTestExpectation(description: "Customers 업무 완료")
+    bankManager.inputCustomersIntoOperationQueue(completion: {
+      expectation.fulfill()
+    })
+    
+    wait(for: [expectation], timeout: 12)
+  }
 }
