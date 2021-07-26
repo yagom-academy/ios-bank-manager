4e6a14397aa09cfc851ec43fa28bc2807a8a217b
YesCoach <sbsii1020@gmail.com>
2021-07-26 21:48:58 +0900

    feat: Add Node class

---
+++ BankManagerConsoleApp/DataStructure/Node.swift
@@ -0,0 +1,24 @@
+//
+//  Node.swift
+//  BankManagerConsoleApp
+//
+//  Created by 박태현 on 2021/07/26.
+//
+
+import Foundation
+
+class Node<Type> {
+    let value: Type
+    var next: Node?
+    
+    init(value: Type, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+extension Node: CustomStringConvertible {
+    var description: String {
+        return String(describing: value)
+    }
+}
