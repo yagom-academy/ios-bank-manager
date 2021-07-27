b465c410da4439ff6870e3cf0119a66fc9728d91
Ldoy <leedoy7989@gmail.com>
2021-07-27 17:12:13 +0900

    feat: LinkedList 타입 구현을 위한 Node 타입 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -0,0 +1,17 @@
+//
+//  Node.swift
+//  BankManagerConsoleApp
+//
+//  Created by Do Yi Lee on 2021/07/27.
+//
+
+import Foundation
+
+class Node<Value> {
+    var value: Value
+    var next: Node?
+    
+    init(value: Value, next: Node? = nil) {
+        self.value = value
+    }
+}
