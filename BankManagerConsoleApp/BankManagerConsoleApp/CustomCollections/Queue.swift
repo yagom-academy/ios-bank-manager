582a9b57a90deb2a2fb4587336d174a1cd5136d4
hosinging <pdhou23@gmail.com>
2021-07-27 15:27:51 +0900

    feat: Queue, Node 타입 구현 - isEmpty() : 큐가 비었는지 확인하는 메서드 구현 - enqueue(value:) : 데이터를 삽입하는 메서드 구현 - dequeue() : 큐에서 데이터를 꺼내는 메서드 구현 - peek() : 큐에서 맨위에 있는 데이터를 확인하는 메서드 구현 - clear() : 큐의 데이터를 모두 삭제하는 메서드 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/CustomCollections/Queue.swift
@@ -0,0 +1,43 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jost, Hosinging on 2021/07/27.
+//
+
+import Foundation
+
+class Queue<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    
+    func isEmpty() -> Bool {
+        return head == nil
+    }
+    
+    func enqueue(value: T) {
+        let newNode = Node(value: value)
+        
+        if head == nil {
+            head = newNode
+        } else {
+            tail?.next = newNode
+        }
+        tail = newNode
+    }
+    
+    func dequeue() -> T? {
+        guard let headNode = head else { return nil }
+        head = headNode.next
+        return headNode.value
+    }
+    
+    func peek() -> T? {
+        return head?.value
+    }
+    
+    func clear() {
+        head = nil
+        tail = nil
+    }
+}
