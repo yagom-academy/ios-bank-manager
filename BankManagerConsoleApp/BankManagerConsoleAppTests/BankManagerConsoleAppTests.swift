016576d322a50b3585358e90d0fd89042e2b2093
KimWanki <seoulkwk95@gmail.com>
2021-07-27 22:40:13 +0900

    test: BankManagerConsoleAppTests 테스트 메서드 추가
    LinkedList 타입의 push(), append() 메서드 테스트 추가, peekFirst() 메서드 테스트 코드 삭제
    변수명 변경 : testArr -> dummyNodes, Pointer -> DummyNode
    메서드 검증을 위한 extension peekLastNode() 구현

--- BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
@@ -10,77 +10,101 @@ import XCTest
 
 class BankManagerConsoleAppTests: XCTestCase {
     var sut = LinkedList<Int>()
- 
-    func test_sut에서_peekFirst을_실패한다() {
-        // given
-        
-        // when
-        let result = sut.peekFirst()
-        // then
-        XCTAssertEqual(nil, result)
+
+    func test_실패케이스_링크드리스트의_push메서드를_호출하면_head노드에_새노드가추가된다() {
+        //given
+        let inputNumber = 1
+        //when
+        sut.push(inputNumber)
+        //then
+        XCTAssertNotEqual(sut.first?.value, nil)
     }
     
-    func test_sut에서_peekFirst을_성공한다() {
-        // given
-        let firstNumber = 1
-        sut.append(1)
-        // when
-        let test = sut.peekFirst()
-        // then
-        XCTAssertEqual(firstNumber, test)
+    func test_성공케이스_링크드리스트의_push메서드를_호출하면_head노드에_새노드가추가된다() {
+        //given
+        let inputNumber = 1
+        //when
+        sut.push(inputNumber)
+        //then
+        XCTAssertEqual(sut.first?.value, inputNumber)
     }
     
-    func test_sut에서_clear을_실패한다() {
+    func test_실패케이스_링크드리스트의_append메서드를_호출하면_tail노드에_새노드가추가된다() {
+        //given
+        let inputNumber = 1
+        //when
+        sut.append(inputNumber)
+        //then
+        XCTAssertNotEqual(sut.peekLastNode()?.value, nil)
+    }
+    
+    func test_성공케이스_링크드리스트의_append메서드를_호출하면_taii노드에_새노드가추가된다() {
+        //given
+        let inputNumber = 1
+        //when
+        sut.append(inputNumber)
+        //then
+        XCTAssertEqual(sut.peekLastNode()?.value, inputNumber)
+    }
+    
+    func test_실패케이스_sut에서_clear을하면_노드의개수가_0이_나온다() {
         // given
-        var testArr = [
-            Pointer(value: 1, weakPointer: nil),
-            Pointer(value: 2, weakPointer: nil),
-            Pointer(value: 3, weakPointer: nil),
-            Pointer(value: 4, weakPointer: nil)
+        var dummyNodes = [
+            DummyNode(value: 1, weakPointer: nil),
+            DummyNode(value: 2, weakPointer: nil),
+            DummyNode(value: 3, weakPointer: nil),
+            DummyNode(value: 4, weakPointer: nil)
         ]
-        testArr.enumerated().forEach({ value in
+        dummyNodes.enumerated().forEach({ value in
             sut.append(value.element.value)
-            testArr[value.offset].weakPointer = sut.peekLastNode()
+            dummyNodes[value.offset].weakPointer = sut.peekLastNode()
         })
-        
         // when
         sut.clear()
-        let result = testArr.filter({ $0.weakPointer != nil }).count
-                
+        let result = dummyNodes.filter({ $0.weakPointer != nil }).count
         // then
         XCTAssertNotEqual(result, 4)
     }
-
-    func test_sut에서_clear을_성공한다() {
+    
+    func test_성공케이스_sut에서_clear을하면_노드의개수가_0이_나온다() {
         // given
-        var testArr = [
-            Pointer(value: 1, weakPointer: nil),
-            Pointer(value: 2, weakPointer: nil),
-            Pointer(value: 3, weakPointer: nil),
-            Pointer(value: 4, weakPointer: nil)
+        var dummyNodes = [
+            DummyNode(value: 1, weakPointer: nil),
+            DummyNode(value: 2, weakPointer: nil),
+            DummyNode(value: 3, weakPointer: nil),
+            DummyNode(value: 4, weakPointer: nil)
         ]
-        testArr.enumerated().forEach({ value in
+        dummyNodes.enumerated().forEach({ value in
             sut.append(value.element.value)
-            testArr[value.offset].weakPointer = sut.peekLastNode()
+            dummyNodes[value.offset].weakPointer = sut.peekLastNode()
         })
         
         // when
         sut.clear()
-        let result = testArr.filter({ $0.weakPointer != nil }).count
-                
+        let result = dummyNodes.filter({ $0.weakPointer != nil }).count
+        
         // then
         XCTAssertEqual(result, 0)
     }
 }
 
-struct Pointer {
+struct DummyNode {
     let value: Int
     weak var weakPointer: Node<Int>?
 }
 
-extension Node: Equatable {
+extension Node: Equatable where T: Numeric {
     static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
-        return true
+        return lhs.value == rhs.value
     }
 }
 
+extension LinkedList {
+    func peekLastNode() -> Node<T>? {
+        var current = self.first
+        while current?.next != nil {
+            current = current?.next
+        }
+        return current
+    }
+}
