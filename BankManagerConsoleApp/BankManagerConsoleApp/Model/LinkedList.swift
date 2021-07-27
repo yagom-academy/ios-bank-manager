ebae03c160a866e552609c433315b6059b30abf6
taeyoung-Kim-KR <kty72301@naver.com>
2021-07-27 15:14:16 +0900

    feat: LinkedList 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/LinkedList.swift
@@ -0,0 +1,19 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by 김태영 on 2021/07/27.
+//
+
+import Foundation
+
+class LinkedList {
+    
+    var head: Node?
+    var tail: Node?
+    
+    init(head: Node?, tail: Node?) {
+        self.head = head
+        self.tail = tail
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Queue.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 protocol Queue {
+    
     func enqueue()
     func dequeue()
     func clear()
