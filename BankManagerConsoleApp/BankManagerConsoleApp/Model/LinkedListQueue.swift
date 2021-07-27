c125ff3874426d8c7cf51be4983d5818bd2b4b8d
ellenJ <jcrescent61@gmail.com>
2021-07-27 16:53:48 +0900

    refactor: Queue 파일을 LinkedListQueue로 변경 protocol Queue를 struct LinkedListQueue로 변경

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/LinkedListQueue.swift
@@ -0,0 +1,19 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by 김태영 on 2021/07/27.
+//
+
+import Foundation
+
+struct LinkedListQueue<T> {
+    
+    let list = LinkedList(head: nil, tail: nil)
+    
+    func enqueue() {}
+    func dequeue() {}
+    func clear() {}
+    func peek() {}
+    func isEmpty() {}
+}
