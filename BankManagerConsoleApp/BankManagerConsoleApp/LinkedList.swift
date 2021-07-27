2395b5bdf84b9e4d598b122ad46124b4d0fbef6d
ictechgy <ictechgy@gmail.com>
2021-07-27 14:27:15 +0900

    feat: LinkedList타입의 append(data:)메소드 구현
    * dummy node와 tail을 만들어서 Linked list 구현의 편의성 증대

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -15,3 +15,20 @@ class Node<T> {
         self.data = data
     }
 }
+
+class LinkedList<T> {
+    var head: Node<T>
+    var tail: Node<T>
+    
+    init(data: T) {
+        let dummyNode = Node(data: data)
+        head = dummyNode
+        tail = dummyNode
+    }
+    
+    func append(data: T) {
+        let newNode = Node(data: data)
+        tail.next = newNode
+        tail = newNode
+    }
+}
