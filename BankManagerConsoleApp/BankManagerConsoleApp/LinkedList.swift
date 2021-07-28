2277d4ca50251a20e38cca5dd8d512042c6da853
blanche37 <wkddbsghk16@gmail.com>
2021-07-28 17:48:47 +0900

    feat: Node 타입 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 final class LinkedList<T> {
-    
+    private final class Node<T> {
+        var data: T?
+        var next: Node?
+        
+        init(data: T?, next: Node? = nil) {
+            self.data = data
+            self.next = next
+        }
+    }
 }
