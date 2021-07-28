74aa5e788070caab0a4db0b8741be755a7a96caa
yaewonLee <yw990324@gmail.com>
2021-07-28 14:09:52 +0900

    feat: Queue 구조체 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Queue<T> {
+    private var linkedList = LinkedList<T>()
+    
+    mutating func enqueue(_ value: T) {
+        linkedList.append(value)
+    }
+    
+    mutating func dequeue() -> T? {
+        return linkedList.remove()
+    }
+    
+    mutating func clear() {
+        linkedList.removeAll()
+    }
+    
+    mutating func peek() -> T? {
+        return linkedList.checkFirst()
+    }
+    
+    mutating func isEmpty() -> Bool {
+        return linkedList.isEmpty()
+    }
+}
