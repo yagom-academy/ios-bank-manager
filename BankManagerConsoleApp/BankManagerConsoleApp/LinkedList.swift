f58827bc0a7d7748e07ffdd8c4f559adfcb10c72
soll4u <dasollpark@gmail.com>
2021-07-28 13:32:31 +0900

    refactor: LinkedListQueue타입을 LinkedList타입으로 축소 * LinkedList 타입을 class에서 구조체로 변경 * append, checkFirst, remove, removeAll 로 메서드 이름 변경 * count를 증감시키는 메서드 분리 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,65 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by Charlotte, Soll on 2021/07/28.
+//
+
+import Foundation
+
+struct LinkedList<T> {
+    private var head: Node<T>? = nil
+    private var tail: Node<T>? = nil
+    private var count: Int = 0
+    
+    func isEmpty() -> Bool {
+        return count == 0
+    }
+    
+    mutating func append(_ value: T) {
+        let newNode = Node<T>(value: value)
+        if isEmpty() {
+            head = newNode
+        } else {
+            tail?.next = newNode
+        }
+        tail = newNode
+        increaseCount()
+    }
+    
+    mutating func checkFirst() -> T? {
+        if isEmpty() {
+            return nil
+        }
+        return head?.value
+    }
+    
+    mutating func remove() -> T? {
+        if isEmpty() {
+            return nil
+        }
+        guard let checkedNode = checkFirst(),
+              let nextNode = head?.next
+        else {
+            return nil
+        }
+        head?.value = nil
+        head?.next = nil
+        head = nextNode
+        decreaseCount()
+        return checkedNode
+    }
+    
+    mutating func removeAll() {
+        head = nil
+        tail = nil
+    }
+    
+    mutating func increaseCount() {
+        count += 1
+    }
+    
+    mutating func decreaseCount() {
+        count -= 1
+    }
+}
