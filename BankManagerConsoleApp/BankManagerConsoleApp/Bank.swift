09dafad007b94a705d3ecbd771f2290b6face6c8
shpairo711 <shapiro7@ajou.ac.kr>
2021-07-29 19:59:55 +0900

    feat: Bank 타입 checkInputUserMenuNumber 메서드 구현
    - UserInput enum 타입 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -12,6 +12,30 @@ protocol Workable {
     func businessProcessing(for client: BankClient)
 }
 
+enum UserInput: String {
+    case open = "1"
+    case close = "2"
+}
+
 class Bank {
+    let bankClerk = BankClerk()
+    let bankClients = Queue<BankClient>()
+    private var totalWorkTime: Double = 0
+    private var bankWork: UserInput?
     
+    func checkInputUserMenuNumber() -> UserInput? {
+        print("입력 : ", terminator: "")
+        guard let inputNumber = readLine() else {
+            return nil
+        }
+        switch inputNumber {
+        case UserInput.open.rawValue:
+            return UserInput.open
+        case UserInput.close.rawValue:
+            return UserInput.close
+        default:
+            print("잘못된 입력입니다. 다시 입력해주세요.")
+            return checkInputUserMenuNumber()
+        }
+    }
 }
