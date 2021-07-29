8caee6e3e8abc8c57c43a89dab977870532450d3
joey-ful <isjoeyful@gmail.com>
2021-07-29 21:29:37 +0900

    test: BankManagerConsoleAppTests UnitTesting Bundle 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankManagerConsoleAppTests.swift
+//  BankManagerConsoleAppTests
+//
+//  Created by 홍정아 on 2021/07/29.
+//
+
+@testable import BankManagerConsoleApp
+import XCTest
+
+class BankManagerConsoleAppTests: XCTestCase {
+    let sut: Queue<String>!
+    
+    override func setUp() {
+        sut = Queue()
+    }
+}
