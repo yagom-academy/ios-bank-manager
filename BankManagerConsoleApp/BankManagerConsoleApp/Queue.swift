224811660b0ec940f4aff7ef741028036c78aee9
joey-ful <isjoeyful@gmail.com>
2021-07-29 21:14:35 +0900

    feat: Queue 타입에 dequeue() 메서드 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -8,10 +8,14 @@
 import Foundation
 
 class Queue<Element> {
-    //TODO: enqueue, dequeue, clear, peek, isEmpty
+    //TODO: clear, peek, isEmpty
     private var linkedList = LinkedList<Element>()
     
     func enqueue(_ newElement: Element) {
         linkedList.append(data: newElement)
+    }
+    
+    func dequeue() -> Element? {
+        linkedList.pop()?.data
     }
 }
