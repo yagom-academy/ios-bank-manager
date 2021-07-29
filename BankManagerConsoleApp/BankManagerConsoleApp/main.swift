c0dbe961a951d9a96a58f261e9a1f0decc80755f
YongwooKim <keepingitflow@gmail.com>
2021-07-29 23:05:33 +0900

    feat: decideToStartProgram 매서드 구현
    사용자 입력받고 프로그램 시작을 결정하 매서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,20 +6,29 @@
 
 import Foundation
 
-func inputUserCommand() -> Bool {
+enum Decision: String {
+    case excute = "1"
+    case exit = "2"
+}
+
+func announceMessage() {
+    print("1 : 은행개점\n2 : 종료")
+    print("입력 : ", terminator: "")
+}
+
+func decideToStartProgram() -> Decision {
     while true {
-        print("1 : 은행개점")
-        print("2 : 종료")
-        print("입력 : ", terminator: "")
-        if let str = readLine(),
-           str == "1" || str == "2" {
-            return str == "1"
+        announceMessage()
+        if let unwrappedString = readLine(),
+           let decision = Decision.init(rawValue: unwrappedString) {
+            return decision
         }
     }
 }
 
 // MARK: --- main Program
 while true {
-    guard inputUserCommand() else { break }
-    
+    guard decideToStartProgram() == .excute else { break }
+    print("실행") // test code
 }
+print("종료") // test code
