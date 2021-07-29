ff0c2e8c98e81022595aa1f2f1a1846cee47b1e3
ohsg0226 <ohsg0226@naver.com>
2021-07-29 12:44:02 +0900

    test: enqueue, dequeue 테스트
    * enqueue테스트: 3번 enqueue후 첫번째 값 확인
    * dequeue테스트 : 빈 queue일 때 dequeue했을때 nil반환 확인

--- BankManagerConsoleApp/QueueTests/QueueTests.swift
+++ BankManagerConsoleApp/QueueTests/QueueTests.swift
@@ -31,4 +31,27 @@ class QueueTests: XCTestCase {
         //then
         XCTAssertFalse(expectResult)
     }
+    
+    func test_queue에_1_2_3을_enqueue했을때_첫번째요소는_1이다() {
+        //given
+        let one = 1
+        let two = 2
+        let three = 3
+        //when
+        queueSut.enqueue(data: one)
+        queueSut.enqueue(data: two)
+        queueSut.enqueue(data: three)
+        let expectResult = queueSut.peek
+        //then
+        XCTAssertEqual(expectResult, one)
+        XCTAssertNotEqual(expectResult, two)
+        XCTAssertNotEqual(expectResult, three)
+    }
+    
+    func test_빈queue에_dequeue를했을때_반환값은_nil이다() {
+        //when
+        let expectResult = queueSut.dequeue()
+        //then
+        XCTAssertNil(expectResult)
+    }
 }
