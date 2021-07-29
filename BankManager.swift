5d2611f91a4f03a3f9a0f35399b1df675bc06c7c
YongwooKim <keepingitflow@gmail.com>
2021-07-30 00:05:01 +0900

    feat: 은행개점과 은행원에 업무를 관리할 Bank 타입 구현
    은행원 manager, 대기열 customerQueue, 총 처리고객 customerCount,
    총 업무시간 totalTime  등등 저장 프로퍼티

--- BankManager.swift
+++ BankManager.swift
@@ -13,3 +13,18 @@ struct BankManager {
 struct Customer {
     let waitingNumber: Int
 }
+
+struct Bank {
+    private var customerQueue: Queue<Customer>
+    private var manager: BankManager = BankManager()
+    private var customerCount: Int = 0
+    private var totalTime: Double = 0
+    
+    func open() {
+        
+    }
+    
+    func close() {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(totalTime)초입니다.")
+    }
+}
