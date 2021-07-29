40f7a7de5e4d1e4466029625576adf5d3a4f10e0
ictechgy <ictechgy@gmail.com>
2021-07-29 21:53:49 +0900

    test: Queue타입의 enqueue테스트 추가

--- BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
@@ -9,9 +9,20 @@
 import XCTest
 
 class BankManagerConsoleAppTests: XCTestCase {
-    let sut: Queue<String>!
+    var sut: Queue<String>!
     
     override func setUp() {
         sut = Queue()
+    }
+    
+    func test_혀나블짱짱을_enqueue했을때peek해보면_혀나블짱짱이다() {
+        //given
+        let expectInputValue = "혀나블 짱짱"
+        //when
+        sut.enqueue(expectInputValue)
+        let expectResult = "혀나블 짱짱"
+        let outputValue = sut.peek
+        //then
+        XCTAssertEqual(expectResult, outputValue)
     }
 }
