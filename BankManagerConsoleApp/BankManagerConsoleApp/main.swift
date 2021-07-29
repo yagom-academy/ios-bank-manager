4ceaa10d3444924290ab197eb63219d4e671085a
YongwooKim <keepingitflow@gmail.com>
2021-07-29 21:25:27 +0900

    feat: 프로그램시작

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,21 @@
 // 
 
 import Foundation
+
+func inputUserCommand() -> Bool {
+    while true {
+        print("1 : 은행개점")
+        print("2 : 종료")
+        print("입력 : ", terminator: "")
+        if let str = readLine(),
+           str == "1" || str == "2" {
+            return str == "1"
+        }
+    }
+}
+
+// MARK: --- main Program
+while true {
+    guard inputUserCommand() else { break }
+    
+}
