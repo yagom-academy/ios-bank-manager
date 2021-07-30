28c90316812a011813c7b122f59805cec296da77
joey-ful <isjoeyful@gmail.com>
2021-07-30 23:49:21 +0900

    feat: main.swift 파일에 기본적인 프로그램 실행 코드 작성

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,3 +6,20 @@
 
 import Foundation
 
+var needContinue = true
+let bankManager = BankManager()
+
+while needContinue {
+    print("1 : 은행개졈")
+    print("2 : 종료")
+    print("입력 : ", terminator: "")
+    guard let userInput = readLine(),
+          let inputNumber = Int(userInput),
+          inputNumber == 1 || inputNumber == 2 else {
+        continue
+    }
+    if inputNumber == 2 {
+        break
+    }
+    bankManager.runBank()
+}
