919d758ae5607295ddd34ed60b2e323445589a26
YesCoach <sbsii1020@gmail.com>
2021-07-30 13:49:35 +0900

    refactor: Add access control, fix finishTime form
    * Fix finishTime output form to under 2 digits

--- BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/Model/Bank.swift
@@ -8,28 +8,28 @@
 import Foundation
 
 struct Bank {
-    enum BankStatus: Int {
+    private enum BankStatus: Int {
         case open = 1
         case close = 2
     }
     
-    var queue = Queue<Customer>()
-    var defaultFrom = 1
-    var defaultTo: Int {
+    private var queue = Queue<Customer>()
+    private var defaultFrom = 1
+    private var defaultTo: Int {
         return Int.random(in: 10...30)
     }
     
-    func receiveCustomer(range: ClosedRange<Int>) {
+    private func receiveCustomer(range: ClosedRange<Int>) {
         for order in range {
             queue.enqueue(value: Customer(id: order))
         }
     }
     
-    func receiveCustomer() {
+    private func receiveCustomer() {
         receiveCustomer(range: defaultFrom...defaultTo)
     }
     
-    func doTask() {
+    private func doTask() {
         var customer: Customer?
         while !queue.isEmpty {
             customer = queue.dequeue()
@@ -42,8 +42,6 @@ struct Bank {
         guard let customer = customer else {
             return
         }
-        let startTime = Double(customer.id - 1) * 0.7
-        let finishTime = Double(customer.id) * 0.7
         print("\(customer.id)번 고객 업무 시작")
         print("\(customer.id)번 고객 업무 종료")
     }
@@ -53,13 +51,13 @@ struct Bank {
             return
         }
         let finishTime = Double(customer.id) * 0.7
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customer.id)명이며, 총 업무시간은 \(finishTime) 입니다.")
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customer.id)명이며, 총 업무시간은 \(String(format: "%.2f", finishTime)) 입니다.")
     }
 }
 
 extension Bank: Process {
     
-    func choiceMenuWithGuide() -> BankStatus? {
+    private func choiceMenuWithGuide() -> BankStatus? {
         print("1 : 은행개점")
         print("2 : 종료")
         print("입력 : ", terminator: "")
