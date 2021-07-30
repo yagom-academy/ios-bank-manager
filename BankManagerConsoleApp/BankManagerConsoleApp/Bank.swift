850e79ab581083dfa1122ff84a1ba7877c4cc801
hosinging <pdhou23@gmail.com>
2021-07-30 15:08:48 +0900

    feat: Bank클래스 내부에 프로퍼티, 메서드 프로토타입 선언 - BankTeller 클래스  타입 구현 - TaskMessage 열거형 타입 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,5 +8,45 @@
 import Foundation
 
 class Bank {
+    enum TaskMessage {
+        case beginning
+        case completion
+        
+        func showMessage(number: Int) -> String {
+            switch self {
+            case .beginning:
+                return "\(number)번 고객 업무 시작"
+            case .completion:
+                return "\(number)번 고객 업무 완료"
+            }
+        }
+    }
     
+    var clientQueue: Queue<Client> = Queue<Client>()
+    var bankTellerQueue: Queue<BankTeller> = Queue<BankTeller>()
+    var openingHour: TimeInterval = 0
+    var closingHour: TimeInterval = 0
+    var numberOfClient: UInt = 0
+    
+    init(numberOfBankTeller: UInt = 1) {
+        for _ in 1...numberOfBankTeller {
+            bankTellerQueue.enqueue(value: BankTeller())
+        }
+    }
+    
+    func finishWork() {
+        
+    }
+    
+    func showClosingMessage() {
+        
+    }
+    
+    func receiveClient() {
+        
+    }
+    
+    func doTask() {
+        
+    }
 }
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankTeller.swift
@@ -0,0 +1,12 @@
+//
+//  BankTeller.swift
+//  BankManagerConsoleApp
+//
+//  Created by 편대호 on 2021/07/30.
+//
+
+import Foundation
+
+class BankTeller {
+    
+}
