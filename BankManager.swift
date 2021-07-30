587ef8230ae516f9c22f7935f2b802f340248fa5
ictechgy <ictechgy@gmail.com>
2021-07-30 21:59:57 +0900

    feat: BankManager타입 생성
    * createBankTellers() 메소드 구현
    * random한 고객 수를 생성하는 generateRandomCustomerNumber() 메소드 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,17 @@
 //
 
 import Foundation
+
+class BankManager {
+    let bank = Bank()
+    
+    func createBankTellers() -> [BankTeller] {
+        return [OrdinaryBankTeller()]
+    }
+    
+    func generateRandomCustomerNumber() -> UInt {
+        let minimumCustomerNumber: UInt = 10
+        let maximumCustomerNumber: UInt = 30
+        return UInt.random(in: minimumCustomerNumber...maximumCustomerNumber)
+    }
+}
