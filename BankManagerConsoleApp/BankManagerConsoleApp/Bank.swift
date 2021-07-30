a000e7d9c49e753175773c791a713fbc51710a1e
joey-ful <isjoeyful@gmail.com>
2021-07-30 21:40:23 +0900

    feat: Bank 타입에 serveCustomers() 메서드 구현
    * bankTellers 대신 bankTellersQueue를 만들어 손님과 행원이 1:1 매칭이 되도록
      구현하였다

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 class Bank {
-    var bankTellers = [BankTeller]()
+    var numberOfBankTellers: Int = .zero
+    var bankTellerQueue = Queue<BankTeller>()
     var customerQueue = Queue<Customer>()
     var totalNumberOfVisitors: UInt = .zero
     
     func reset() {
-        bankTellers.removeAll()
+        numberOfBankTellers = .zero
+        bankTellerQueue.clear()
         customerQueue.clear()
         totalNumberOfVisitors = .zero
     }
@@ -24,10 +26,28 @@ class Bank {
     }
     
     func hire(employees: [BankTeller]) {
-        bankTellers = employees
+        numberOfBankTellers = employees.count
+        employees.forEach { bankTellerQueue.enqueue($0) }
     }
     
     func receive(customers: [Customer]) {
         customers.forEach { customerQueue.enqueue($0) }
+    }
+    
+    func serveCustomers() {
+        let semaphore = DispatchSemaphore(value: numberOfBankTellers)
+        while let currentCustomer = customerQueue.dequeue() {
+            semaphore.wait()
+            guard let bankTeller = bankTellerQueue.dequeue() else {
+                customerQueue.enqueue(currentCustomer)
+                semaphore.signal()
+                continue
+            }
+            DispatchQueue.global().async {
+                bankTeller.serve(customer: currentCustomer)
+                self.bankTellerQueue.enqueue(bankTeller)
+                semaphore.signal()
+            }
+        }
     }
 }
