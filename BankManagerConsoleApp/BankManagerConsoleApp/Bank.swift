a61392bff90a7ca2ad14bcfe503b92c983eb08a2
hrjy6278 <hrjy6278@naver.com>
2021-07-30 16:17:51 +0900

    feat: 콘솔에 표시되는 로직 구현
    - 유저에게 입력을 받는 userInput() 메서드 구현
    - 은행개점 및 종료를 출력하는 selectMode() 메서드 구현
    - 은행개점시 동작하는 로직이 담긴 startTask() 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -12,10 +12,33 @@ struct Bank {
     let customerQueue = BankManagerQueue<Int>()
     let banker: Banker
     
+    func startTask() {
+        selectMode()
+        if userInput() {
+            initCustomerQueue()
+            while !customerQueue.isEmpty {
+                banker.doBusiness()
+            }
+            print(BankMessage.bankClosed(customers: customer.numbers))
+        }
+    }
+    
     func initCustomerQueue() {
         for customer in Int.one...customer.numbers {
             customerQueue.enqueue(data: customer)
         }
     }
     
+    func selectMode() {
+        print(BankMessage.openBank)
+        print(BankMessage.closedBank)
+    }
+    
+    func userInput() -> Bool {
+        if let input = readLine(), let intInput = Int(input), intInput == Int.one {
+            return true
+        }
+        print("잘못된 입력입니다.")
+        return false
+    }
 }
