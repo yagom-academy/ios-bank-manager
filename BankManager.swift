56230979347b4379f900283a3d144d04e63e91fd
blanche37 <wkddbsghk16@gmail.com>
2021-08-02 20:17:08 +0900

    feat : processTask() 수정
    * 총 작업시간을 소수점 2번째 자리까지 표기

--- BankManager.swift
+++ BankManager.swift
@@ -31,14 +31,16 @@ struct BankManager {
     private func processTask() {
         receiveCustomers()
         var totalCustomer = 0
+        var totalTaskTime: Double = 0
         while !customerQueue.isEmpty {
             guard let customer = customerQueue.dequeue() else {
                 return
             }
             clerk.doTask(customer: Customer(waitingNumber: customer.waitingNumber))
             totalCustomer += 1
+            totalTaskTime += clerk.businessProcessingTime
         }
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(Double(totalCustomer) * clerk.businessProcessingTime)초입니다.")
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(String(format: "%.2f", totalTaskTime))초입니다.")
     }
     
     private func printMenu() -> Menu? {
@@ -64,3 +66,4 @@ struct BankManager {
         }
     }
 }
+
