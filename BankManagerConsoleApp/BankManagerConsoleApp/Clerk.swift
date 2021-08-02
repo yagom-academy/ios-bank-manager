5f250ca7a5e7488dd1a4f0956b45c31d25b516c3
ohsg0226 <ohsg0226@naver.com>
2021-08-02 18:37:20 +0900

    feat: doTask(customer:) 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Clerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Clerk.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 struct Clerk {
-    
+    private let businessProcessingTime: Double = 0.7
+   
+    func doTask(customer: Customer) {
+        print("\(customer.waitingNumber) 시작")
+        Thread.sleep(forTimeInterval: businessProcessingTime)
+        print("\(customer.waitingNumber) 완료")
+    }
 }
