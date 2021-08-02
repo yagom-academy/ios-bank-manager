f9aba8553385e15de2d695eb164f1b51dd93d631
ellenJ <jcrescent61@gmail.com>
2021-08-02 21:04:18 +0900

    test: LinkedList 실패케이스 테스트코드 구현

--- BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/BankManagerConsoleAppTests.swift
@@ -19,8 +19,8 @@ class BankManagerConsoleAppTests: XCTestCase {
         sutQueue = LinkedListQueue()
         sutList = LinkedList()
     }
-
-    // MARK: LinkedListQueue의 테스트 코드
+    
+    // MARK: LinkedListQueue의 성공케이스 테스트 코드
     func test_enqueue_했을때_새로운_노드가_추가된다() {
         // given
         let value = 1
@@ -61,7 +61,50 @@ class BankManagerConsoleAppTests: XCTestCase {
         XCTAssertTrue(result)
     }
     
-    // MARK: LinkedList의 테스트 코드
+    // MARK: LinkedListQueue의 실패케이스 테스트 코드
+    func test_enqueue_했을때_헤드가_nil이면_실패() {
+        // given
+        let value = 1
+        // when
+        sutQueue.enqueue(value: value)
+        let result = sutQueue.peek
+        // then
+        XCTAssertNotEqual(nil, result)
+    }
+    
+    func test_dequeue_했을때_head가_nil이면실패() {
+        // given
+        sutQueue.enqueue(value: 1)
+        // when
+        let result = sutQueue.dequeue()
+        // then
+        XCTAssertNotEqual(nil, result)
+    }
+    
+    func test_clear_했을때_큐의_head가_nil이_아니면_실패() {
+        // given
+        let wrongResult = 1
+        sutQueue.enqueue(value: 1)
+        sutQueue.enqueue(value: 2)
+        sutQueue.enqueue(value: 3)
+        // when
+        sutQueue.clear()
+        // then
+        let result = sutQueue.peek
+        XCTAssertNotEqual(wrongResult, result)
+    }
+    
+    func test_Queue에_값이있는경우_isEmpty_했을때_nil이면_실패() {
+        // given
+        sutQueue.enqueue(value: 1)
+        // when
+        let result = sutQueue.isEmpty()
+        // then
+        XCTAssertNotEqual(nil, result)
+    }
+    
+    
+    // MARK: LinkedList의 성공케이스 테스트 코드
     func test_headIsEmpty_했을때_head에_값이_없는_경우_true다() {
         // given
         var result: Bool
@@ -135,45 +178,72 @@ class BankManagerConsoleAppTests: XCTestCase {
         
     }
     
-    // MARK: LinkedListQueue의 실패 테스트 코드
-    func test_enqueue_했을때_헤드가_nil이면_실패() {
+    // MARK: LinkedList의 실패케이스 테스트 코드
+    func test_headIsEmpty_했을때_head에_값이_없는_경우_false면_실패() {
         // given
-        let value = 1
+        var result: Bool
         // when
-        sutQueue.enqueue(value: value)
-        let result = sutQueue.peek
+        result = sutList.headIsEmpty
         // then
-        XCTAssertNotEqual(nil, result)
+        XCTAssertNotEqual(false, result)
     }
-
-    func test_dequeue_했을때_head가_nil이면실패() {
+    
+    func test_headValue에_1을_넣었는데_2가_반환되면_실패() {
         // given
-        sutQueue.enqueue(value: 1)
+        let dummyValue = 1
+        sutList.append(value: dummyValue)
         // when
-        let result = sutQueue.dequeue()
+        let result = sutList.headValue
+        // then
+        XCTAssertNotEqual(2, result)
+    }
+    
+    func test_tailValue에_1을_넣었는데_2가_반환되면_실패() {
+        // given
+        let dummyValue = 1
+        sutList.append(value: dummyValue)
+        // when
+        let result = sutList.tailValue
+        // then
+        XCTAssertNotEqual(2, result)
+    }
+    
+    func test_append_했을때_headIsEmpty_가_true면_실패() {
+        // given
+        sutList.append(value: 1)
+        // when
+        let result = sutList.headIsEmpty
+        // then
+        XCTAssertNotEqual(true, result)
+    }
+    
+    func test_값이있는_노드에서_removeFirst_했을때_head의_값이_nil이면_실패() {
+        // given
+        sutList.append(value: 1)
+        // when
+        let result = sutList.removeFirst()
         // then
         XCTAssertNotEqual(nil, result)
     }
     
-    func test_clear_했을때_큐의_head가_nil이_아니면_실패() {
+    func test_removeFirst_했을때_head가_다음_노드가_2가아니면_실패() {
         // given
-        let wrongResult = 1
-        sutQueue.enqueue(value: 1)
-        sutQueue.enqueue(value: 2)
-        sutQueue.enqueue(value: 3)
+        sutList.append(value: 1)
+        sutList.append(value: 2)
         // when
-        sutQueue.clear()
+        let result = sutList.removeFirst()
         // then
-        let result = sutQueue.peek
-        XCTAssertNotEqual(wrongResult, result)
+        XCTAssertNotEqual(2, result)
     }
     
-    func test_Queue에_값이있는경우_isEmpty_했을때_nil이면_실패() {
+    func test_removeAll_했을때_head와_tail이_nil이_아니면_실패() {
         // given
-        sutQueue.enqueue(value: 1)
+        sutList.append(value: 1)
+        sutList.append(value: 2)
         // when
-        let result = sutQueue.isEmpty()
+        sutList.removeAll()
+        let result: Bool = sutList.headValue == nil && sutList.tailValue == nil
         // then
-        XCTAssertNotEqual(nil, result)
+        XCTAssertNotEqual(false, result)
     }
 }
