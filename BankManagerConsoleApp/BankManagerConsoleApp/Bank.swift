ce9fffb37a1bb1d275f3594e9bd4147c28bc427b
yaewonLee <yw990324@gmail.com>
2021-08-04 14:42:48 +0900

    feat: numberFormatter 기능 추가
    * 총 업무시간 소수점 둘째자리까지 나오도록 기능 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -11,8 +11,9 @@ class Bank {
     let bankManager: [BankManager]
     let waitList = Queue<Int>()
     var customerNumber: Int = 0
-    let minCustomerNumber = 1
-    let maxCustomerNumber = 3
+    let minCustomerNumber = 10
+    let maxCustomerNumber = 30
+    let numberFormatter = NumberFormatter()
     
     init(bankManager: [BankManager]) {
         self.bankManager = bankManager
@@ -30,11 +31,16 @@ class Bank {
     }
     
     func openBank() {
+        numberFormatter.roundingMode = .floor
+        numberFormatter.maximumSignificantDigits = 2
+       
         let totalWorkTime = checkTotalTime {
             makeRandomCustomerNumber()
             bankManager[0].startWork(customerNumber)
         }
         
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerNumber)이며, 총 업무시간은 \(totalWorkTime)초입니다.")
+        if let formattedTime = numberFormatter.string(for: totalWorkTime) {
+            print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerNumber)이며, 총 업무시간은 \(formattedTime)초입니다.")
+        }
     }
 }
