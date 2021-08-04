3e9e2ae2e7df13d31c4afb24d7e17713e6929b88
YongwooKim <keepingitflow@gmail.com>
2021-08-04 21:55:30 +0900

    refactor: 각 스레드 시간지연방식 변경 시도
    Thread.sleep으로 구현된 시간지연 방식을 Runloop - Timer를 이용한 방식으로 수정 시도함

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankTeller.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankTeller.swift
@@ -10,9 +10,14 @@ struct BankTeller {
     let bankingTask: BankingTask
     
     func serve(_ client: Client, completion: () -> Void) {
-        print("\(client.waitingNumber)번 고객 업무 시작")
-        Thread.sleep(forTimeInterval: bankingTask.processingTime)
-        print("\(client.waitingNumber)번 고객 업무 완료")
+        print("\(client.waitingNumber)번 고객 \(bankingTask)업무 시작")
+//        Thread.sleep(forTimeInterval: bankingTask.processingTime)
+        let runLoop = RunLoop.current
+        Timer.scheduledTimer(withTimeInterval: bankingTask.processingTime, repeats: false) { _ in
+            print("\(client.waitingNumber)번 고객 \(bankingTask)업무 완료")
+        }
+        runLoop.run()
+        
         completion()
     }
 }
