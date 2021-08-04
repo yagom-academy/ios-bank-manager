d134632aa8136879b70fb5821e0d3f8f9ed55c6b
soll4u <dasollpark@gmail.com>
2021-08-04 18:29:12 +0900

    feat: Customer 타입 구현, Task 내 랜덤한 element를 반환하는 메서드 구현
    * Customer 타입 내의 task 프로퍼티를 Task의 랜덤한 element로 초기화

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,16 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by Charlotte, Soll on 2021/08/04.
+//
+
+import Foundation
+
+class Customer {
+    let task: Task
+    
+    init() {
+        self.task = Task.random()
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/Task.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Task.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
-enum Task: String, CustomStringConvertible {
+enum Task: String, CustomStringConvertible, CaseIterable {
     case deposit = "예금업무"
     case loan = "대출업무"
     
     var description: String {
         return self.rawValue
+    }
+    
+    static func random<G: RandomNumberGenerator>(using generator: inout G) -> Task {
+        return Task.allCases.randomElement(using: &generator)!
+    }
+    
+    static func random() -> Task {
+        var g = SystemRandomNumberGenerator()
+        return Task.random(using: &g)
     }
 }
