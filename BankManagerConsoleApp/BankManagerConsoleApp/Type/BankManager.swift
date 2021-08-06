d18a972bffa8f39145e446a4325374aa3521913c
jaemuyeo <jemking94@gmail.com>
2021-08-06 15:19:20 +0900

    refactor: while 반복문 내 조건문 분리
    * judgeCustomer() 메서드 생성 - 예금고객과 대출 고객을 판단
    * isLoan(of:) 메서드 생성 - 대출 고객이면 true를 반환
    * 흐름에 따른 메서드 위치 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Type/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Type/BankManager.swift
@@ -21,15 +21,7 @@ struct BankManager {
         let depositSemaphore = DispatchSemaphore(value: window.deposit)
 
         while !customerQueue.isEmpty {
-            guard let customer = customerQueue.dequeue() else {
-                return
-            }
-            
-            if customer.businessType == .loan {
-                handleLoan(for: customer, group: group, semaphore: loanSemaphore)
-            } else {
-                handleDeposit(for: customer, group: group, semaphore: depositSemaphore)
-            }
+            judgeCustomer(group, loanSemaphore, depositSemaphore)
             totalCustomer += increaseOne
         }
 
@@ -37,19 +29,32 @@ struct BankManager {
         close(customerCount: totalCustomer, taskTime: CFAbsoluteTimeGetCurrent() - startTime)
     }
     
-    private func close(customerCount: Int, taskTime: Double) {
-        let time = String(format: "%.2f", taskTime)
-            
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(time)초입니다.")
-    }
-    
     private func receiveCustomers(customer: Queue<Customer>) {
-        let firstNumber = 1
-        for number in firstNumber...numberOfCustomer {
+        let firstCustomerNumber = 1
+
+        for number in firstCustomerNumber...numberOfCustomer {
             guard let customerType = BusinessType.allCases.randomElement() else { return }
             let customer = Customer(waitingNumber: number, businessType: customerType)
             customerQueue.enqueue(data: customer)
         }
+    }
+    
+    private func judgeCustomer(_ group: DispatchGroup,
+                               _ loanSemaphore: DispatchSemaphore,
+                               _ depositSemaphore: DispatchSemaphore) {
+        guard let customer = customerQueue.dequeue() else {
+            return
+        }
+        
+        if isLoan(of: customer) {
+            handleLoan(for: customer, group: group, semaphore: loanSemaphore)
+        } else {
+            handleDeposit(for: customer, group: group, semaphore: depositSemaphore)
+        }
+    }
+    
+    private func isLoan(of customer: Customer) -> Bool {
+        return customer.businessType == .loan
     }
     
     private func handleLoan(for customer: Customer,
@@ -72,5 +77,11 @@ struct BankManager {
                                              businessType: customer.businessType))
             semaphore.signal()
         }
+    }
+    
+    private func close(customerCount: Int, taskTime: Double) {
+        let time = String(format: "%.2f", taskTime)
+            
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(time)초입니다.")
     }
 }
