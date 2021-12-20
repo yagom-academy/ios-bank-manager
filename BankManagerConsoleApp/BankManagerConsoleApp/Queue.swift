99f8de62cdfdb4b9dce337bee95326a2002aba55
hoBahk <qudgh849@gmail.com>
2021-12-20 16:06:45 +0900

    feat: 큐의 isEmpty, enqueue 기능 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -1,13 +1,32 @@
-import Foundation
-
 struct Queue<T> {
+    private var head: Node<T>?
+    private var tail: Node<T>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating func enqueue(_ value: T) {
+        if isEmpty {
+            let node = Node(value)
+            head = node
+            tail = node
+        } else {
+            let node = Node(value)
+            tail?.next = node
+            tail = node
+        }
+    }
+}
+
+extension Queue {
     class Node<S> {
-        var data: S
+        var value: S
         var next: Node?
         
-        init(data: S, next: Node? = nil) {
+        init(_ value: S, next: Node? = nil) {
             self.next = next
-            self.data = data
+            self.value = value
         }
     }
 }
