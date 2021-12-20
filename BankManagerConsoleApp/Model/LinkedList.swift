43fd8c35db39c91348d02040f320e4bf9e77c764
July911 <min95911@nate.com>
2021-12-20 17:34:05 +0900

    feat: LinkedList 클래스 구현 #1

--- BankManagerConsoleApp/Model/LinkedList.swift
+++ BankManagerConsoleApp/Model/LinkedList.swift
@@ -1,10 +1,74 @@
 import Foundation
 
-class Node<Element> {
-    var value: Element
-    var pointer: Node?
+class LinkedList<Element> {
+    class ListNode<Element> {
+        var value: Element
+        var pointer: ListNode?
+        
+        init(value: Element) {
+            self.value = value
+        }
+    }
     
-    init(value: Element) {
-        self.value = value
+    typealias Node = ListNode<Element>
+    
+    var head: Node?
+    var tail: Node? {
+        var finderToTail: Node? = head
+        
+        while let nextNode = finderToTail?.pointer {
+            finderToTail = nextNode
+        }
+        
+        return finderToTail
+    }
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    init(value: Element?) {
+        if let value = value {
+            self.head = Node(value: value)
+        }
+    }
+    
+    func append(value: Element) {
+        let newNode = Node(value: value)
+        
+        if isEmpty {
+            head = newNode
+        } else {
+            tail?.pointer = newNode
+        }
+    }
+    
+    func removeFirst() -> Element? {
+        if isEmpty { return nil }
+        
+        let valueToRemove = head?.value
+        head = head?.pointer
+        
+        return valueToRemove
+    }
+    
+    func removeAll() {
+        head = nil
+    }
+    
+    func scanAllValues() -> [Element]? {
+        var scanResult: [Element]?
+        
+        guard var finderToTail = head else { return nil }
+
+        scanResult?.append(finderToTail.value)
+
+        while let nextNode = finderToTail.pointer {
+            scanResult?.append(nextNode.value)
+            finderToTail = nextNode
+        }
+        
+        return scanResult
     }
 }
+
+
