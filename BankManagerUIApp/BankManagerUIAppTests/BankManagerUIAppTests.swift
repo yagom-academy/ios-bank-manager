4c9851c30ab89e6a973f8f0a5164bc43e60066b8
JT <dd3557@naver.com>
2021-12-20 17:56:39 +0900

    feat: Unit Test 구현

--- BankManagerUIApp/BankManagerUIAppTests/BankManagerUIAppTests.swift
+++ BankManagerUIApp/BankManagerUIAppTests/BankManagerUIAppTests.swift
@@ -8,25 +8,18 @@ import XCTest
 @testable import BankManagerUIApp
 
 class BankManagerUIAppTests: XCTestCase {
-
+    var sut: Queue<Int>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = Queue<Int>.init()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_enqueue_메서드() {
+        sut.enqueue(1)
+        XCTAssertFalse(sut.isEmpty)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
