0beb34281020f6be6a53630d6dce3be1f40c33c6
hwangjeha <hyhpwang@gmail.com>
2021-12-20 20:28:37 +0900

    style: 테스트 타겟에 있던 Qeueu 타입 모델로 이동  #2

--- BankManagerConsoleApp/BankManagerConsoleAppTests/QueueTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/QueueTests.swift
@@ -1,34 +1,5 @@
 import XCTest
 
-struct Queue<Element> {
-    let items: LinkedList<Element>
-    
-    init(items: LinkedList<Element> = LinkedList<Element>()) {
-        self.items = items
-    }
-    
-    var isEmpty: Bool {
-        return items.isEmpty
-    }
-    
-    var peek: Element? {
-        return items.first
-    }
-    
-    func enqueue(_ item: Element) {
-        items.append(item)
-    }
-    
-    @discardableResult
-    func dequeue() -> Element? {
-        return items.removeFirst()
-    }
-    
-    func clear() {
-        items.removeAll()
-    }
-}
-
 class QueueTests: XCTestCase {
     var sut: Queue<Int>!
     
