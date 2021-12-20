a307163efd4c294140d1fc2aff382e815caf5691
zoe <kny0651@naver.com>
2021-12-20 17:45:28 +0900

    feat: Node 타입의 convenience initializer 구현 #1
    - 이니셜라이저를 편리하게 사용하기 위해 추가구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue/Node.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue/Node.swift
@@ -11,8 +11,12 @@ class Node<Element> {
     var data: Element
     var next: Node?
     
-    init(data: Element, next: Node? = nil) {
+    init(data: Element, next: Node?) {
         self.data = data
         self.next = next
+    }
+    
+    convenience init(data: Element) {
+        self.init(data: data, next: nil)
     }
 }
