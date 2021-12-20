c08fc47fe8d2f4af740e297ae9b2dbef0e1da686
GOSARI <goeunrhdms@gmail.com>
2021-12-20 17:38:43 +0900

    feat: LinkedList의 enqueue 기능 구현 #1

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue/LinkedList.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class LinkedList<Element> {
+    var head: Node<Element>?
+    
+    func enqueue(data: Element) {
+        if head == nil {
+            head = Node(data: data)
+            return
+        }
+        
+        var node = head
+        while node?.next != nil  {
+            node = node?.next
+        }
+        
+        node?.next = Node(data: data)
+    }
+}
