d81d24c30780a8517109252e57330845aafbd473
JT <dd3557@naver.com>
2021-12-20 17:17:32 +0900

    feat: Queue에 대한 enqueue, dequeue, clear 메소드 구현

--- BankManager.swift
+++ BankManager.swift
@@ -8,4 +8,16 @@ struct Queue<T> {
     init() {
         items = LinkedList<T>.init()
     }
+    
+    mutating func enqueue(_ item: T) {
+        items.append(item)
+    }
+    
+    mutating func dequeue() -> T {
+        items.removeFirst()
+    }
+    
+    mutating func clear() {
+        items = LinkedList<T>.init()
+    }
 }
