b579136c74bbb2035a1f8457f1bd0d939adabcd0
yeahg-dev <yeahg.dev@gmail.com>
2021-12-20 18:51:13 +0900

    feat: isEmpty 프로퍼티 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -9,6 +9,9 @@ import Foundation
 
 struct Queue<Element> {
     var items: LinkedList<Element>
+    var isEmpty: Bool {
+        return items.isEmpty
+    }
     
     init() {
         self.items = LinkedList()
