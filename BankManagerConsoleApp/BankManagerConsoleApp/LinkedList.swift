8c30ca365eb16ee95a956ae6568d0a668d7fc72b
siwonkim0 <siyeon0616@naver.com>
2021-12-20 22:32:29 +0900

    feat: Queue 타입 구현을 위한 LinkedList 기능 구현 #1

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -16,4 +16,47 @@ class LinkedList<Element> {
             self.value = value
         }
     }
+    
+    private var head: Node?
+    private var tail: Node?
+    
+    var isEmpty: Bool {
+        head == nil
+    }
+    
+    func append(value: Element) {
+        let node = Node(value: value)
+        
+        if let tailNode = tail {
+            tailNode.next = node
+        } else {
+            head = node
+        }
+        
+        tail = node
+    }
+    
+    func removeFirst() -> Element? {
+        if isEmpty {
+            return nil
+        } else {
+            let firstNode = head
+            head = head?.next
+            return firstNode?.value
+        }
+    }
+    
+    func retrieveFirst() -> Element? {
+        if isEmpty {
+            return nil
+        } else {
+            return head?.value
+        }
+    }
+    
+    func removeAll() {
+        head = nil
+        tail = nil
+    }
 }
+
