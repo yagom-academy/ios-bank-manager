94961b59a25edf4174db5aca1414ab78679bf3a7
ICS-Asan <carpediem1015@gmail.com>
2021-12-20 16:28:34 +0900

    feat: LinkedList 클래스 구현, head/tail 프로퍼티 생성 #1

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,15 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by Seul Mac on 2021/12/20.
+//
+
+import Foundation
+
+class LinkedList<Element> {
+    
+    var head: Node<Element>?
+    var tail: Node<Element>?
+
+}
