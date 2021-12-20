128db458cdf18c9b458de15cf467103d6bf4434c
forestjae <forestsj30@gmail.com>
2021-12-20 16:20:47 +0900

    feat: LinkedList 구조체 추가 및 append 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -16,3 +16,19 @@ class Node<Element> {
         self.next = next
     }
 }
+
+struct LinkedList<Element> {
+    var head: Node<Element>?
+    var tail: Node<Element>?
+    
+    init(data: Element?){
+        self.head = Node(data: data, next: nil)
+        self.tail = head
+    }
+    
+    mutating func append(data: Element) {
+        let nextNode = Node(data: data, next: nil)
+        tail?.next = nextNode
+        tail = tail?.next
+    }
+}
