dc36c83f52129bfe2e6dee130080296bb53a9ac9
zoe <kny0651@naver.com>
2021-12-20 21:52:42 +0900

    test: LinkedList의 테스트코드 추가 #1

---
+++ BankManagerConsoleApp/BanckManagerConsoleAppTests/BanckManagerConsoleAppTests.swift
@@ -0,0 +1,57 @@
+//
+//  BanckManagerConsoleAppTests.swift
+//  BanckManagerConsoleAppTests
+//
+//  Created by 권나영 on 2021/12/20.
+//
+
+import XCTest
+
+class BanckManagerConsoleAppTests: XCTestCase {
+
+    var sut: LinkedList<Int>!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = LinkedList()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_enqueue메서드로_1_2_3을넣었을때_first가_1인지() {
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        sut.enqueue(data: 3)
+        
+        XCTAssertEqual(sut.first, 1)
+    }
+    
+    func test_head가_비어있을때_dequeue를하면_nil을반환하는지() {
+        let dequeueElement = sut.dequeue()
+        XCTAssertNil(dequeueElement)
+    }
+    
+    func test_1_2_3을넣었을때_dequeue를하면_1을반환하는지() {
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        sut.enqueue(data: 3)
+        
+        let dequeueElement = sut.dequeue()
+        
+        XCTAssertEqual(dequeueElement, 1)
+    }
+    
+    func test_removeAll로_요소를모두지우면_isEmpty가_true인지() {
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        sut.enqueue(data: 3)
+        
+        sut.removeAll()
+        
+        XCTAssertTrue(sut.isEmpty)
+    }
+
+}
