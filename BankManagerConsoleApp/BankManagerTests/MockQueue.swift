7ae5932f44b4686400cad1875e4fc65b8b61f846
namu <simjh27@gmail.com>
2021-12-20 21:29:51 +0900

    test: Queue타입에 대한 Mock 객체 구현

---
+++ BankManagerConsoleApp/BankManagerTests/MockQueue.swift
@@ -0,0 +1,32 @@
+//
+//  MockQueue.swift
+//  BankManagerTests
+//
+//  Created by Jae-hoon Sim on 2021/12/20.
+//
+
+import Foundation
+
+struct Queue<Element> {
+    
+    var list: LinkedList<Element> = LinkedList()
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    mutating func enqueue(element: Element) {
+        list.append(value: element)
+    }
+    
+    mutating func dequeue() -> Element? {
+        return list.removeFirst()
+    }
+    
+    mutating func clear() {
+        list.removeAll()
+    }
+    
+    func peek() -> Element? {
+        return list.first
+    }
+}
