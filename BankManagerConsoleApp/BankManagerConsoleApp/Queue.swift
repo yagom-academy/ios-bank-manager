0e7bb79bb091e45de33f4566c019be73dfcf6796
ChaminLee <trueman9512@naver.com>
2021-12-21 10:29:29 +0900

    feat: Queue 타입 구현 #1

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -0,0 +1,32 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by 이차민 on 2021/12/21.
+//
+
+import Foundation
+
+struct Queue<Element> {
+    private(set) var items = LinkedList<Element>()
+    
+    var isEmpty: Bool {
+        return items.isEmpty
+    }
+    
+    func enqueue(value: Element) {
+        items.append(value: value)
+    }
+    
+    func dequeue() -> Element? {
+        items.removeFirst()
+    }
+    
+    func clear() {
+        items.removeAll()
+    }
+    
+    func peek() -> Element? {
+        items.retrieveFirst()
+    }
+}
