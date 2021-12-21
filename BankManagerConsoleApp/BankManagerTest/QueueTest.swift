15acddc80c0a2a9943b9468e32359513fc2b22c9
siwonkim0 <siyeon0616@naver.com>
2021-12-21 17:39:46 +0900

    test: sut의 타입을 암시적 추출 옵셔널로 변경 #2

--- BankManagerConsoleApp/BankManagerTest/QueueTest.swift
+++ BankManagerConsoleApp/BankManagerTest/QueueTest.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 class QueueTest: XCTestCase {
-    var sut: Queue<Int>?
+    var sut: Queue<Int>!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -21,46 +21,33 @@ class QueueTest: XCTestCase {
     }
     
     func test_큐가_비어있을때_true를_반환하는지() {
-        guard let result = sut?.isEmpty else {
-            return
-        }
-        
-        XCTAssertTrue(result)
+        XCTAssertTrue(sut.isEmpty)
     }
     
     func test_큐에_1을_enqueue했을때_1이_들어있는지() {
-        sut?.enqueue(value: 1)
-        let result = sut?.dequeue()
+        sut.enqueue(value: 1)
+        let result = sut.dequeue()
         
         XCTAssertEqual(result, 1)
     }
     
     func test_빈큐에_dequeue했을때_nil이_나오는지() {
-        XCTAssertNil(sut?.dequeue())
+        XCTAssertNil(sut.dequeue())
     }
     
     func test_1과_2가_들어있는_큐를_clear했을때_비어있는지() {
-        sut?.enqueue(value: 1)
-        sut?.enqueue(value: 2)
-        sut?.clear()
+        sut.enqueue(value: 1)
+        sut.enqueue(value: 2)
+        sut.clear()
         
-        guard let result = sut?.isEmpty else {
-            return
-        }
-        
-        XCTAssertTrue(result)
+        XCTAssertTrue(sut.isEmpty)
     }
     
     func test_1이_들어있는_큐에_peek했을때_값만_반환되고_1이_사라지지않는지() {
-        sut?.enqueue(value: 1)
-        let result = sut?.peek()
+        sut.enqueue(value: 1)
+        let result = sut.peek()
         
         XCTAssertEqual(result, 1)
-        
-        guard let result = sut?.isEmpty else {
-            return
-        }
-        
-        XCTAssertFalse(result)
+        XCTAssertFalse(sut.isEmpty)
     }
 }
