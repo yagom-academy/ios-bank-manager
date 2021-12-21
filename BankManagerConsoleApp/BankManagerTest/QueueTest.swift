c9aed877725dd294dd51e40faf43dfe6aece81ab
ChaminLee <trueman9512@naver.com>
2021-12-21 18:34:59 +0900

    test: 테스트 케이스 추가 #2

--- BankManagerConsoleApp/BankManagerTest/QueueTest.swift
+++ BankManagerConsoleApp/BankManagerTest/QueueTest.swift
@@ -50,4 +50,26 @@ class QueueTest: XCTestCase {
         XCTAssertEqual(result, 1)
         XCTAssertFalse(sut.isEmpty)
     }
+    
+    func test_enqueue와_dequeue를_번갈아_호출했을_때_올바른_값이_나오는지() {
+        sut.enqueue(value: 1)
+        sut.enqueue(value: 2)
+        let firstResult = sut.dequeue()
+        XCTAssertEqual(1, firstResult)
+        
+        sut.enqueue(value: 3)
+        let secondResult = sut.dequeue()
+        XCTAssertEqual(2, secondResult)
+    }
+
+    func test_큐를_clear한_이후_enqueue하고_dequeue_했을_때_올바른_값이_나오는지() {
+        sut.enqueue(value: 1)
+        sut.enqueue(value: 2)
+        sut.clear()
+        
+        sut.enqueue(value: 3)
+        let firstResult = sut.dequeue()
+        XCTAssertEqual(3, firstResult)
+    }
+
 }
