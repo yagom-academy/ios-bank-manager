1b4f27ba0d2c7858a5920c800ba3289f567ca54f
llghdud921 <llghdud921@naver.com>
2021-12-21 10:44:59 +0900

    test: testcode 추가 및 test 메서드 동기화안되는 문제 해결

--- BankManagerUIApp/BankManagerUIAppTests/LinkedListTests.swift
+++ BankManagerUIApp/BankManagerUIAppTests/LinkedListTests.swift
@@ -6,10 +6,12 @@ class LinkedListTests: XCTestCase {
     var sut: LinkedList<Int>!
     
     override func setUpWithError() throws {
+        try super.setUpWithError()
         sut = LinkedList<Int>.init()
     }
 
     override func tearDownWithError() throws {
+        try super.tearDownWithError()
         sut = nil
     }
     
@@ -37,5 +39,22 @@ class LinkedListTests: XCTestCase {
         
         XCTAssertEqual(sut.remove(), 2)
         XCTAssertTrue(sut.isEmpty)
+    }
+    
+    func test_append_후에_peek_메서드() {
+        // given
+        let item = 6
+        
+        // when
+        sut.append(item)
+        let result = sut.peek()
+        
+        // then
+        XCTAssertEqual(result, item)
+    }
+    
+    func test_비어있는_LinkedList에서_peek_메서드() {
+        // then
+        XCTAssertNil(sut.peek())
     }
 }
