ee503f57a2eebf7c8084b47b51df446ef32ae99a
llghdud921 <llghdud921@naver.com>
2021-12-21 11:03:39 +0900

    test: LinkedList내 test 메서드 네이밍 변경 및 주석 추가

--- BankManagerUIApp/BankManagerUIAppTests/LinkedListTests.swift
+++ BankManagerUIApp/BankManagerUIAppTests/LinkedListTests.swift
@@ -15,25 +15,32 @@ class LinkedListTests: XCTestCase {
         sut = nil
     }
     
-    func test_빈값에_remove_메서드() {
+    func test_빈값에_remove_실행() {
+        // when
         let result = sut.remove()
         
+        // then
         XCTAssertNil(result)
     }
     
-    func test_append_와_remove_메서드() {
+    func test_append_와_remove_실행() {
+        // given
         sut.append(1)
         
+        // when
         let result = sut.remove()
 
+        // then
         XCTAssertEqual(result, 1)
         XCTAssertTrue(sut.isEmpty)
     }
     
     func test_append_와_remove_두번씩_실행() {
+        // given
         sut.append(1)
         sut.append(2)
 
+        // then
         XCTAssertEqual(sut.remove(), 1)
         XCTAssertFalse(sut.isEmpty)
         
@@ -41,7 +48,7 @@ class LinkedListTests: XCTestCase {
         XCTAssertTrue(sut.isEmpty)
     }
     
-    func test_append_후에_peek_메서드() {
+    func test_append_후에_peek_실행() {
         // given
         let item = 6
         
@@ -53,17 +60,17 @@ class LinkedListTests: XCTestCase {
         XCTAssertEqual(result, item)
     }
     
-    func test_비어있는_LinkedList에서_peek_메서드() {
+    func test_비어있는_LinkedList에서_peek_실행() {
         // then
         XCTAssertNil(sut.peek())
     }
     
-    func test_비어있는_LinkedList에서_isEmpty_프로퍼티() {
+    func test_비어있는_LinkedList에서_isEmpty_값_확인() {
         // then
         XCTAssertTrue(sut.isEmpty)
     }
     
-    func test_값이_있는_LinkedList에서_isEmpty_프로퍼티() {
+    func test_값이_있는_LinkedList에서_isEmpty_값_확인() {
         // given
         let item = 7
         sut.append(item)
