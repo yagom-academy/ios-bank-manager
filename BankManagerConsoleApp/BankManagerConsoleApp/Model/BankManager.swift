68d29c2d100d5424796f4f2437e73eea06c0343a
yim2627 <yim2627@gmail.com>
2021-12-22 15:06:48 +0900

    feat: Bank 타입 추가 및 메뉴 출력 구현, 파일 분류 #5 #6

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManager.swift
@@ -1,1 +1,15 @@
 import Foundation
+
+struct BankManager {
+    let bank = Bank()
+    
+    func selectMenu() {
+        print("1: 은행개점")
+        print("2: 종료")
+        print("입력: ",terminator: "")
+        
+        guard let selectedMenu = readLine() else {
+            return
+        }
+    }
+}
