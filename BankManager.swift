6b2a5c6713446257960de37c681a993a7447612f
aCafela-coffee <acafelacoffee@gmail.com>
2021-12-22 18:13:10 +0900

    feat: 콘솔 메뉴 구현
    고객수를 난수로 결정

--- BankManager.swift
+++ BankManager.swift
@@ -3,3 +3,25 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
+
+struct BankManager {
+    func runConsoleApp() {
+        let menu = """
+                   1 : 은행 개점
+                   2 : 종료
+                   입력 :
+                   """
+        while true {
+            print(menu, terminator: " ")
+            let randomNumber = Int.random(in: 10...30)
+            switch readLine() {
+            case "1":
+                Bank(numberOfClients: randomNumber, numberOfBankTellers: 1).startBusiness()
+            case "2":
+                return
+            default:
+                continue
+            }
+        }
+    }
+}
