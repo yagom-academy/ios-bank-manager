47d7dbddb3415421e01c09af59de726913a05f33
hwangjeha <hyhpwang@gmail.com>
2021-12-22 21:17:12 +0900

    feat: BankManager 타입 생성 #5
    - 사용자 입력을 받는 receiveUserInput 메소드 구현
    - 메뉴를 출력하는 showMenu 메소드 구현
    - 10~30사이의 고객의 수를 생성하는 getterCustomers 메소드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
@@ -3,6 +3,10 @@ import Foundation
 struct BankClerk {
     private let workSpeed: TimeInterval
     
+    init(workSpeed: TimeInterval) {
+        self.workSpeed = workSpeed
+    }
+    
     func work(for customer: Customer) {
         print("\(customer.waitingNumber)번 고객 업무 시작")
         Thread.sleep(forTimeInterval: workSpeed)
--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManager.swift
@@ -1,1 +1,36 @@
 import Foundation
+
+struct BankManager {
+    private let bank = Bank(bankClerk: BankClerk(workSpeed: 0.7))
+    
+    func receiveUserInput() -> Bool {
+        guard let input = readLine(), input.isEmpty == false else {
+            print("잘못된 입력입니다. 다시 확인해주세요.")
+            return true
+        }
+        
+        switch input {
+        case "1":
+            bank.openBank()
+            return true
+        case "2":
+            return false
+        default:
+            print("잘못된 입력입니다. 다시 확인해주세요.")
+            return true
+        }
+    }
+    
+    func showMenu() {
+        print("""
+        1 : 은행 개점
+        2 : 종료
+        입력 :
+        """, terminator: "")
+    }
+    
+    func getterCustomers(to range: ClosedRange<Int> = 10...30) {
+        let numberOfCustomers = Int.random(in: range)
+        bank.handOutWaitingNumber(from: numberOfCustomers)
+    }
+}
