28e161b6c8d94d73be4ab01616f712e3cffd5ff5
ICS-Asan <carpediem1015@gmail.com>
2021-12-23 16:40:55 +0900

    feat: Bank내부 enqueueCustomer/numberOfCustomerRange구현 #4

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -12,10 +12,20 @@ struct Bank {
     let customerQueue: CustomerQueue<Customer>
     let numberOfCustomer: Int
     let taskTime: Double
+    var numberOfCustomerRange: ClosedRange<Int> {
+        return 1...numberOfCustomer
+    }
     
     init(numberOfCustomerRange: ClosedRange<Int> = 10...30, taskTime: Double = 0.7) {
         self.customerQueue = CustomerQueue()
         self.numberOfCustomer = Int.random(in: numberOfCustomerRange)
         self.taskTime = taskTime
+    }
+    
+    func enqueueCustomer() {
+        for count in numberOfCustomerRange {
+            let customer = Customer(turn: count)
+            customerQueue.enqueue(data: customer)
+        }
     }
 }
