293478ebc9b4b80e92c9af6d4ae5eadcb8b3ed16
smart8612 <smart8612@gmail.com>
2021-12-23 16:53:26 +0900

    feat: task 메서드 구현 #4

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -11,12 +11,12 @@ struct Bank {
     
     let customerQueue: CustomerQueue<Customer>
     let numberOfCustomer: Int
-    let taskTime: Double
+    let taskTime: TimeInterval
     var numberOfCustomerRange: ClosedRange<Int> {
         return 1...numberOfCustomer
     }
     
-    init(numberOfCustomerRange: ClosedRange<Int> = 10...30, taskTime: Double = 0.7) {
+    init(numberOfCustomerRange: ClosedRange<Int> = 10...30, taskTime: TimeInterval = 0.7) {
         self.customerQueue = CustomerQueue()
         self.numberOfCustomer = Int.random(in: numberOfCustomerRange)
         self.taskTime = taskTime
@@ -27,5 +27,12 @@ struct Bank {
             let customer = Customer(turn: count)
             customerQueue.enqueue(data: customer)
         }
+    }
+    
+    func task(of customer: Customer) {
+        let turn = customer.turn
+        print("\(turn)번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: taskTime)
+        print("\(turn)번 고객 업무 완료")
     }
 }
