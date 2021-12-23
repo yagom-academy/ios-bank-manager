fdd2ed985036547606ad4709fd41ee5288a73c95
yeahg-dev <yeahg.dev@gmail.com>
2021-12-23 18:56:10 +0900

    feat: BankManager타입 프로퍼티, 메서드 기본구현

--- BankManager.swift
+++ BankManager.swift
@@ -1,1 +1,24 @@
 import Foundation
+
+struct BankManager {
+    private var bank: Bank
+    
+    init(bank: Bank) {
+        self.bank = bank
+    }
+    
+    func run() {
+        while true {
+            printMenu()
+            receiveInput()
+        }
+    }
+    
+    func printMenu() {
+        
+    }
+    
+    func receiveInput() {
+        
+    }
+}
