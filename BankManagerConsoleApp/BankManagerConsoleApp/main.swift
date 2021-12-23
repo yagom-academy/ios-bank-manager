80364d96b80baabcc885418a9665bcabc0db43d4
hwangjeha <hyhpwang@gmail.com>
2021-12-23 16:30:21 +0900

    feat: main 로직 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -1,1 +1,14 @@
 import Foundation
+
+let bank = BankManager()
+
+func run() {
+    var flag = true
+
+    while flag {
+        bank.showMenu()
+        flag = bank.receiveUserInput()
+    }
+}
+
+run()
