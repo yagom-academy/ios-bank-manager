bfd1634d5931977d710d37e411a001354da5a292
siwonkim0 <siyeon0616@naver.com>
2021-12-23 19:01:46 +0900

    feat: Bank 타입의 close() 메서드 구현 #4
    - BankClerk 타입의 startWork() 메서드 내에 bank의 close() 메서드 호출하여 정보 전달하도록 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -19,4 +19,8 @@ class Bank {
     func open() {
         bankClerk.startWork()
     }
+    
+    func close(totalCustomerCount: Int, totalProcessingTime: Double) {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerCount)명이며, 총 업무시간은 \(totalProcessingTime)초입니다.")
+    }
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
@@ -11,14 +11,21 @@ class BankClerk {
     var bank: Bank?
     
     func startWork() {
+        var customerCount: Int = 0
+        var totalProcessingTime: Double = 0
         while true {
             guard let customer = bank?.customerQueue.dequeue() else {
+                bank?.close(totalCustomerCount: customerCount, totalProcessingTime: totalProcessingTime)
                 break
             }
             
             print("\(customer.turn)번 고객 업무 시작")
             Thread.sleep(forTimeInterval: TimeInterval(customer.processingTime))
             print("\(customer.turn)번 고객 업무 완료")
+            customerCount += 1
+            totalProcessingTime += customer.processingTime
         }
     }
+    
+
 }
