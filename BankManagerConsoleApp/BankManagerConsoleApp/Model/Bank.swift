a200bc605be3a31f13f733fbdd52d2c2d905ff7b
yeahg-dev <yeahg.dev@gmail.com>
2021-12-23 18:07:54 +0900

    feat: Bank타입 프로퍼티, 메서드 기본구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 struct Bank {
+    var clientQueue: Queue<Client>
+    var bankClerks: [BankClerk]
+    var numberOfBanker: Int
     
+    func open() {
+    }
+  
+    func close() {
+        
+    }
+    
+    private func receiveClient() {
+        
+    }
 }
