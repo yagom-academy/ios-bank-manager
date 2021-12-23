6f39330de20d440e7a9a953b751067975407773d
ChaminLee <trueman9512@naver.com>
2021-12-23 21:04:36 +0900

    feat: main에 프로그램을 실행하는 메서드들 생성 #4
    - 메뉴를 출력하는 printMenu 메서드 생성
    - 사용자 입력에 따라 실행/종료가 되도록 하는 runProgram 메서드 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,34 @@
 // 
 
 import Foundation
+
+let bankClerk = BankClerk()
+let bank = Bank(bankClerk: bankClerk)
+let bankManager = BankManager(bank: bank)
+
+func printMenu() {
+    let menu = """
+          1 : 은행개점
+          2 : 종료
+          입력 :
+          """
+    print(menu, terminator: " ")
+}
+
+func runProgram() {
+    while true {
+        printMenu()
+        let userInput = readLine()
+        
+        switch userInput {
+        case "1":
+            bankManager.bank.open()
+        case "2":
+            return
+        default:
+            continue
+        }
+    }
+}
+
+runProgram()
