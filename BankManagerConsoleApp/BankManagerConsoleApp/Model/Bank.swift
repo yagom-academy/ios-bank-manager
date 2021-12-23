7e0848eb1c18dc9afe71be6bf7fac098cbc57b7f
wooyani77 <wooyani77@gmail.com>
2021-12-23 17:22:21 +0900

    feat: 업무마감 출력 메소드 구현 #6

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -18,12 +18,22 @@ struct Bank {
         
         return customer
     }
-
     
     mutating func letClerkWork() {
+        let taskTime = 0.7
+        var numberOfCustomer = 0
+        
         while waitingLine.isEmpty == false {
             let customer = dequeueWaitingLine()
             bankClerk.handleTask(of: customer)
+            numberOfCustomer += 1
         }
+        noticeCloseDown(totalCustomer: numberOfCustomer, taskTime: taskTime)
+    }
+    
+    func noticeCloseDown(totalCustomer: Int, taskTime: Double) {
+        let totalTime = taskTime * Double(totalCustomer)
+        
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(totalTime)초입니다.")
     }
 }
