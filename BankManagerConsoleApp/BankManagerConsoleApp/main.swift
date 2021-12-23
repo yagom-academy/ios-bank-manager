2355370fe15b2fd4b82c690e0237fd2f877e72bc
smart8612 <smart8612@gmail.com>
2021-12-23 20:11:10 +0900

    feat: BankMenu 타입 구현 #4

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,10 +6,21 @@
 
 import Foundation
 
+enum BankMenu: String {
+    case openBank = "1"
+    case exitProgram = "2"
+    
+    var menuNumber: String {
+        return self.rawValue
+    }
+    
+    static let main = """
+                      1 : 은행 개점
+                      2 : 종료
+                      입력 :
+                      """
+}
+
 func printMenu() {
-    print("""
-          1 : 은행 개점
-          2 : 종료
-          입력 :
-          """, terminator: "")
+    print(BankMenu.main, terminator: " ")
 }
