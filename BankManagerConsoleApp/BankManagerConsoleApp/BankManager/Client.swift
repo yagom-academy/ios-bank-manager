5e0a2eda09a9cbc37ceb8b70f7a174ac1916a9d6
namu <simjh27@gmail.com>
2021-12-23 16:17:40 +0900

    feat: 고객을 정의하는 Client 타입 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager/Client.swift
@@ -0,0 +1,13 @@
+//
+//  Client.swift
+//  BankManagerConsoleApp
+//
+//  Created by Jae-hoon Sim on 2021/12/23.
+//
+
+import Foundation
+
+struct Client {
+    let waitingNumber: Int
+    let task: Task
+}
