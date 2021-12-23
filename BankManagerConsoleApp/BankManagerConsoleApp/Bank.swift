6889966211bb4ae308c61f3076d8db837ffe862a
ICS-Asan <carpediem1015@gmail.com>
2021-12-23 16:17:36 +0900

    feat: Bank 타입 구현 #4

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,16 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by Seul Mac on 2021/12/23.
+//
+
+import Foundation
+
+struct Bank {
+    
+    let customerQueue: CustomerQueue<Customer>
+    let numberOfCustomer: Int
+    let taskTime: Double
+    
+}
