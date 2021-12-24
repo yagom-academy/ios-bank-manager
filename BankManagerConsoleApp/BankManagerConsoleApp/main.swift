6b263232b74a02cb3b324ce02e1b9ea14d3cb6c6
smart8612 <smart8612@gmail.com>
2021-12-24 10:59:55 +0900

    feat: BankManagerConsole 타입 구현 #4

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,57 +6,60 @@
 
 import Foundation
 
-enum BankMenu: String {
-    case openBank = "1"
-    case exitProgram = "2"
-    
-    init?(number: String) {
-        self.init(rawValue: number)
-    }
-    
-    var menuNumber: String {
-        return self.rawValue
-    }
-    
-    static let main = """
+class BankManagerConsole {
+    enum BankMenu: String {
+        case openBank = "1"
+        case exitProgram = "2"
+        
+        init?(number: String) {
+            self.init(rawValue: number)
+        }
+        
+        var menuNumber: String {
+            return self.rawValue
+        }
+        
+        static let main = """
                       1 : 은행 개점
                       2 : 종료
                       입력 :
                       """
-}
-
-var bank = Bank()
-
-func printMenu() {
-    print(BankMenu.main, terminator: " ")
-}
-
-func inputMenu() -> BankMenu? {
-    guard let input = readLine(),
-         let selectedMenu = BankMenu(number: input) else {
-             return nil
     }
-    return selectedMenu
-}
-
-func operateMenu(input menu: BankMenu?) {
-    switch menu {
-    case .openBank:
-        bank.open()
-    case .exitProgram:
-        print("종료")
-    default:
-        print("다시 입력")
+    
+    var bank = Bank()
+    
+    func printMenu() {
+        print(BankMenu.main, terminator: " ")
+    }
+    
+    func inputMenu() -> BankMenu? {
+        guard let input = readLine(),
+              let selectedMenu = BankMenu(number: input) else {
+                  return nil
+              }
+        return selectedMenu
+    }
+    
+    func operateMenu(input menu: BankMenu?) {
+        switch menu {
+        case .openBank:
+            bank.open()
+        case .exitProgram:
+            print("종료")
+        default:
+            print("다시 입력")
+        }
+    }
+    
+    func run() {
+        var menu: BankMenu?
+        repeat {
+            printMenu()
+            menu = inputMenu()
+            operateMenu(input: menu)
+        } while menu != .exitProgram
     }
 }
 
-func run() {
-    var menu: BankMenu?
-    repeat {
-        printMenu()
-        menu = inputMenu()
-        operateMenu(input: menu)
-    } while menu != .exitProgram
-}
-
-run()
+let bankManagerConsole = BankManagerConsole()
+bankManagerConsole.run()
