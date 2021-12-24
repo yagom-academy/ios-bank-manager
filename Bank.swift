47807801c8f78cce72070da562755ad53064c39b
llghdud921 <llghdud921@naver.com>
2021-12-24 09:53:53 +0900

    feat: Banker 타입 내부 구현 및 assign 사용 로직 작성

--- Bank.swift
+++ Bank.swift
@@ -2,9 +2,23 @@ import Foundation
 
 struct Bank {
     private struct Banker {
-        var customer: Customer?
+        var customer: Customer? {
+            didSet {
+                task()
+            }
+        }
         
         init() {}
+        
+        private func task() {
+            guard let customer = customer else {
+                return
+            }
+
+            print("\(customer.customerNumber)번 고객 업무 시작")
+            // time 7초
+            print("\(customer.customerNumber)번 고객 업무 완료")
+        }
     }
     
     private var banker: Banker
@@ -15,13 +29,18 @@ struct Bank {
         banker = Banker()
     }
     
-    func open() {
+    mutating func open() {
         let customerCount = Int.random(in: 1...10)
         
         (1...customerCount).forEach {
             let customer = Customer(customerNumber: $0)
             queue.enqueue(customer)
         }
+        
+        while let customer = queue.dequeue() {
+            assign(customer: customer, to: &banker)
+        }
+        
     }
     
     private func assign(customer: Customer, to banker: inout Banker) {
