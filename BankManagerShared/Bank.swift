47bfdc7356437f0ce1e7f76f115f9a2b9be3822a
JT <dd3557@naver.com>
2021-12-24 16:16:36 +0900

    refactor: Bank에 nested된 타입 Banker를 외부로 분리

--- BankManagerShared/Bank.swift
+++ BankManagerShared/Bank.swift
@@ -5,37 +5,7 @@ protocol BankDelegate {
     func checkCustomer()
 }
 
-class Bank {
-    private struct Banker {
-        var delegate: BankDelegate?
-        var customer: Customer? {
-            didSet {
-                task()
-            }
-        }
-        
-        init() {}
-        
-        private func task() {
-            do {
-                try taskWithError()
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
-        
-        private func taskWithError() throws {
-            guard let customer = customer else {
-                throw CustomerError.notExistCustomer
-            }
-
-            print("\(customer.customerNumber)번 고객 업무 시작")
-            delegate?.checkTime()
-            print("\(customer.customerNumber)번 고객 업무 완료")
-            delegate?.checkCustomer()
-        }
-    }
-    
+class Bank {    
     private var banker: Banker
     private var queue: Queue<Customer>
     private var totalTime: TimeInterval
