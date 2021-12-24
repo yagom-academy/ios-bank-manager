e87a49126da48031e473bf3b5fd2735a38e06ead
GOSARI <goeunrhdms@gmail.com>
2021-12-24 10:23:38 +0900

    feat: Bank 타입의 open 메서드, Customer 타입 구현 #2
    - 랜덤한 숫자를 받고, 그 숫자만큼 고객 인스턴스를 생성하여 enqueue 해주는 open 메서드 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,22 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by 고은 on 2021/12/24.
+//
+
+import Foundation
+
+class Bank {
+    var waitingLine = Queue<Customer>()
+    
+    func open() {
+        let randomNumber = Int.random(in: 10...30)
+        
+        for number in 1...randomNumber {
+            let customer = Customer(waitingNumber: number)
+            
+            waitingLine.enqueue(data: customer)
+        }
+    }
+}
