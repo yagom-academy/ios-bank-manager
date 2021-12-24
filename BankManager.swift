3548daf6a161a313831f3de6032e1129d783a97b
horeng2 <huski.a.a@gmail.com>
2021-12-24 11:54:24 +0900

    refactor: 사용자입력 케이스를 enum으로 구현

--- BankManager.swift
+++ BankManager.swift
@@ -7,6 +7,11 @@
 import Foundation
 
 struct BankManager {
+  enum Menu: String {
+    case openBank = "1"
+    case exit = "2"
+  }
+  
   private func chooseMenu() -> String {
     print("""
           1 : 은행 개점
@@ -27,12 +32,12 @@ struct BankManager {
       let choiceMenu = self.chooseMenu()
       
       switch choiceMenu {
-      case "1" :
+      case Menu.openBank.rawValue :
         var bank: Bank = Bank(numberOfBankers: 1)
         bank.doBanking()
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(bank.numberOfClients)명이며,"
               + "총 업무시간은 \(bank.workingTime())초입니다.")
-      case "2" :
+      case Menu.exit.rawValue :
         isExit = true
       default :
         print("잘못된 입력입니다. 확인해주세요.")
