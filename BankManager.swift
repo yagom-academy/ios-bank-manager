70d7ce190fc005aecb20d711946d51177d829ab5
oksunwoo <oksunwo@naver.com>
2021-12-24 16:16:16 +0900

    feat: BankManager 타입 구현 #3
    - 랜덤갯수의 client 들을 생성하는 generateClients 메서드 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,19 @@
 //
 
 import Foundation
+
+struct BankManager {
+    var bank = Bank()
+    
+    private func generateClients() -> [Client] {
+        let clientNumber = Int.random(in: 10...30)
+        var clients: [Client] = []
+        
+        (0..<clientCounts).forEach { _ in
+            let client = Client()
+            clients.append(client)
+        }
+        
+        return clients
+    }
+}
