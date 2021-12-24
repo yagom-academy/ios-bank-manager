79a484ab1c726f6c23b50569a7759dff45946dc9
yanghojoon <yanghojoon8487@gmail.com>
2021-12-24 11:57:31 +0900

    feat: 초기 메뉴 구현 #5
    - 실행 시 메뉴를 출력하는 printMenu 메서드 구현
    - 사용자 입력값을 받는 receiveInput 메서드 구현
    - 입력값에 따라 메뉴를 실행하는 runMenu 메서드 구현
    - Namespace 생성
    - LinkedList, Queue를 DataStructure 파일로 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,31 @@
 // 
 
 import Foundation
+
+fileprivate func printMenu() {
+    print(Menu.initialMessage, terminator: " ")
+}
+
+fileprivate func receiveInput() -> String {
+    guard let input = readLine() else {
+        return Menu.Option.invalidInput
+    }
+    
+    return input
+}
+
+fileprivate func runMenu(with input: String) {
+    switch input {
+    case Menu.Option.open:
+        print("1번")
+    case Menu.Option.close:
+        print("2번")
+    default:
+        print(Menu.Error.wrongInput)
+        printMenu()
+        runMenu(with: receiveInput())
+    }
+}
+
+printMenu()
+runMenu(with: receiveInput())
