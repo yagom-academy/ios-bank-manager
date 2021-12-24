baaa583c197881940ede4b8cd0fcfac308fbb6d2
forestjae <forestsj30@gmail.com>
2021-12-24 10:43:40 +0900

    feat: BankClerk타입의 work메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
@@ -2,6 +2,8 @@ import Foundation
 
 struct BankClerk {
     func work(for client: Client) {
-        
+        print("\(client.number)번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: 0.7)
+        print("\(client.number)번 고객 업무 완료")
     }
 }
