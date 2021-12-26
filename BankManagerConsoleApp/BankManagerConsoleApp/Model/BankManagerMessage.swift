c626140993c66e463efea2c9cd969dcc28e62186
Ari <lee_ari95@icloud.com>
2021-12-26 21:06:31 +0900

    refactor: BankManagerMessage menuList의 하드코딩을 개선 #7
    - 문자열 "1" 과 "2"를 Menu의 프로퍼티로 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManagerMessage.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankManagerMessage.swift
@@ -3,8 +3,8 @@ import Foundation
 enum BankManagerMessage {
     static let wrongInput = "잘못된 입력입니다. 다시 확인해주세요."
     static let menuList = """
-                          1 : 은행 개점
-                          2 : 종료
+                          \(Menu.open) : 은행 개점
+                          \(Menu.close) : 종료
                           입력 : 
                           """
 }
