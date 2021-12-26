82a6b2555a70bd79d7724321c13927a0cd0f7ec7
Ari <lee_ari95@icloud.com>
2021-12-26 21:52:25 +0900

    refactor: CFAbsoluteTime 타입 대신에 Date 타입을 활용하여 연산시간 측정하도록 수정 #7

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -25,7 +25,7 @@ final class Bank {
     
     func openBank() {
         var numberOfCustomers = 0
-        let openTime = CFAbsoluteTimeGetCurrent()
+        let openTime = Date()
         while customerQueue.isEmpty == false {
             respondToCustomer()
             numberOfCustomers += 1
@@ -33,8 +33,8 @@ final class Bank {
         closeBank(numberOfCustomers, from: openTime)
     }
     
-    private func closeBank(_ numberOfCustomers: Int, from openTime: CFAbsoluteTime) {
-        let durationTime = CFAbsoluteTimeGetCurrent() - openTime
+    private func closeBank(_ numberOfCustomers: Int, from openTime: Date) {
+        let durationTime = -openTime.timeIntervalSinceNow
         delegate?.bank(didFinish: numberOfCustomers, durationTime: durationTime.roundedOffDescription(for: 2))
     }
 
