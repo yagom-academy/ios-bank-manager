0d9af6d4b0c3215e2184563548f360241df57807
just1103 <hyojuson1103@gmail.com>
2021-12-27 15:40:13 +0900

    refactor: main파일의 MenuHandler를 BankingProgramHandler으로 변경하고 파일을 분리 #4
    - GuideDescription 열거형 추가
    - Menu 열거형 description 프로퍼티 추가
    - startToReceiveInput 메서드 default 수정

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankingProgramHandler.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+struct BankingProgramHandler {
+    enum Menu: String {
+        case openBank = "1"
+        case exitProgram = "2"
+        
+        var description: String {
+            return self.rawValue
+        }
+    }
+    
+    enum GuideDescription: String, CustomStringConvertible {
+        case menuForInput = """
+        1 : 은행개점
+        2 : 종료
+        입력 :
+        """
+        case invalidInput = "잘못 입력하셨습니다. 메뉴를 확인해주세요."
+        
+        var description: String {
+            return self.rawValue
+        }
+    }
+    
+    func startToReceiveInput() {
+        print(GuideDescription.menuForInput.description, terminator: " ")
+        
+        guard let input = readLine() else {
+            return
+        }
+        
+        switch input {
+        case Menu.openBank.description:
+            setBankWithRandomCustomer()
+            startToReceiveInput()
+            return
+        case Menu.exitProgram.description:
+            return
+        default:
+            print(GuideDescription.invalidInput.description)
+            startToReceiveInput()
+            return
+        }
+    }
+    
+    private func setBankWithRandomCustomer() {
+        let randomNumberOfCustomer = Int.random(in: 10...30)
+        var bank = Bank(numberOfBankClerk: 1, numberOfCustomer: randomNumberOfCustomer)
+        bank.openBank()
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -1,44 +1,8 @@
 import Foundation
 
-struct MenuHandler {
-    enum Menu: String {
-        case one = "1"
-        case two = "2"
-    }
-    
-    func startToReceiveInput() {
-        print("""
-           1: 은행개점
-           2: 종료
-           입력:
-           """, terminator: "")
-        
-        guard let input = readLine(), input.isEmpty == false else {
-            return
-        }
-        
-        switch input {
-        case "1":
-            setBankWithRandomCustomer()
-            startToReceiveInput()
-            return
-        case "2":
-            return
-        default:
-            return
-        }
-    }
-    
-    private func setBankWithRandomCustomer() {
-        let randomNumberOfCustomer = Int.random(in: 10...30)
-        var bank = Bank(numberOfBankClerk: 1, numberOfCustomer: randomNumberOfCustomer)
-        bank.openBank()
-    }
+func startProgram() {
+    let bankingProgram = BankingProgramHandler()
+    bankingProgram.startToReceiveInput()
 }
 
-func startBankManager() {
-    let menuHandler = MenuHandler()
-    menuHandler.startToReceiveInput()
-}
-
-startBankManager()
+startProgram()
