d757ffacac4f4ce411cd76863cddc086bc655cc6
GOSARI <goeunrhdms@gmail.com>
2021-12-27 20:45:58 +0900

    fix: BankManager 타입 추가 push 오류로 인한 재커밋 #8

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,50 @@
 //
 
 import Foundation
+
+class BankManager {
+    var employee: Employee
+    var bank: WaitingLineManageable?
+    let calculator = Calculator()
+    let randomNumber = Int.random(in: 10...30)
+    
+    init(employee: Employee) {
+        self.employee = employee
+    }
+    
+    func open() {
+        let openTime = CFAbsoluteTimeGetCurrent()
+        
+        lineUp()
+        assignWork()
+        
+        let closeTime = CFAbsoluteTimeGetCurrent()
+        
+        guard let calculateResult = calculator.calculate(from: openTime, to: closeTime) else {
+            return
+        }
+        
+        let close = Speaker.Situation.close(time: calculateResult)
+        
+        Speaker.speak(when: close, number: randomNumber)
+    }
+}
+
+//MARK: - Private Methods
+
+extension BankManager {
+    private func lineUp() {
+        for number in 1...randomNumber {
+            let customer = Customer(waitingNumber: number)
+            
+            bank?.waitingLine.enqueue(customer)
+        }
+    }
+    
+    private func assignWork() {
+        while bank?.waitingLine.isEmpty == false {
+            employee.startJob()
+            employee.finishJob()
+        }
+    }
+}
