182e4d1ef7d6a7dec7b16d376183c4d4c1f16151
llghdud921 <llghdud921@naver.com>
2021-12-27 14:43:46 +0900

    refactor: main에서 은행 개점 여부를 판단하는 메소드 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -3,25 +3,11 @@ import Foundation
 func runBankManagerConsole() {
     while true {
         printMenu()
-        do {
-            let inputValue = try input(with: "입력 : ", useTrim: true)
-            
-            switch inputValue {
-            case "1":
-                let bank = Bank()
-                bank.run()
-            case "2":
-                return
-            default:
-                print("입력값이 잘못되었습니다.")
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
+        determineBankOpen()
     }
 }
 
-func printMenu() {
+private func printMenu() {
     let message = """
     1 : 은행 개점
     2 : 종료
@@ -29,7 +15,25 @@ func printMenu() {
     print(message)
 }
 
-func input(with message: String = "", useTrim: Bool = false) throws -> String {
+private func determineBankOpen() {
+    do {
+        let inputValue = try input(with: "입력 : ", useTrim: true)
+        
+        switch inputValue {
+        case "1":
+            let bank = Bank()
+            bank.run()
+        case "2":
+            return
+        default:
+            print("입력값이 잘못되었습니다.")
+        }
+    } catch {
+        print(error.localizedDescription)
+    }
+}
+
+private func input(with message: String = "", useTrim: Bool = false) throws -> String {
     let emptyString = ""
     print(message, separator: emptyString, terminator: emptyString)
     guard var inputValue = readLine() else {
