0c0ff59465ad18c1fc33d95b108958c00aa7747b
wooyani77 <wooyani77@gmail.com>
2021-12-27 23:55:22 +0900

    refactor: 총 업무시간 Formatting 수정 #10
    - NumberFormatter 제거 후 String(format:)으로 대체

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -55,14 +55,8 @@ class Bank {
     }
     
     private func close(totalCustomer: Int, taskTime: Double) {
-        let totalTime = taskTime
-        let numberFormatter = NumberFormatter()
-        numberFormatter.roundingMode = .up
-        numberFormatter.minimumFractionDigits = 2
-        guard let convertedTotalTime = numberFormatter.string(for: totalTime) else {
-            return
-        }
+        let totalTime = String(format: "%.2f", taskTime)
         
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(convertedTotalTime)초입니다.")
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(totalTime)초입니다.")
     }
 }
