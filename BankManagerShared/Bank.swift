f1e3bfbf4bcc8075260e94e13531096b3d8da346
JT <dd3557@naver.com>
2021-12-27 16:45:14 +0900

    refactor: Banker 타입의 task 메서드 관련 코드 수정

--- BankManagerShared/Bank.swift
+++ BankManagerShared/Bank.swift
@@ -39,7 +39,7 @@ extension Bank {
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(totalTimeText)초입니다.")
     }
     
-    private func assign(customer: Customer, to banker: inout Banker) {
-        banker.customer = customer
+    private func assign(customer: Customer, to banker: Banker) {
+        banker.task(of: customer)
     }
 }
--- BankManagerShared/Banker.swift
+++ BankManagerShared/Banker.swift
@@ -1,27 +1,7 @@
 import Foundation
 
 struct Banker {
-    var customer: Customer? {
-        didSet {
-            task()
-        }
-    }
-    
-    init() {}
-    
-    private func task() {
-        do {
-            try taskWithError()
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-    
-    private func taskWithError() throws {
-        guard let customer = customer else {
-            throw CustomerError.notExistCustomer
-        }
-
+    func task(of customer: Customer) {
         print("\(customer.customerNumber)번 고객 업무 시작")
         print("\(customer.customerNumber)번 고객 업무 완료")
     }
