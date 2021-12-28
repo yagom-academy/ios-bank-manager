2186d646ae79a4a190595a31c2add5e0aef7b1cf
oksunwoo <oksunwo@naver.com>
2021-12-28 15:30:41 +0900

    refactor: bankManager타입의 역할 분리
    - MenuOption 타입 main으로 이동
    - startConsoleProgram 메서드 main으로 이동
    - userInput 메서드 main으로 이동
    - showMenu 메서드 main으로 이동

--- BankManager.swift
+++ BankManager.swift
@@ -7,58 +7,14 @@
 import Foundation
 
 struct BankManager {
-    //MARK: - 중첩 타입
-    private enum MenuOption: String {
-        case openBank = "1"
-        case exit = "2"
-    }
-    
     //MARK: - 저장 속성
     private var bank = Bank(numberOfClerks: 1)
     
-    //MARK: - Internal 메서드
-    mutating func startConsoleProgram() {
-        while true {
-            do {
-                showMenu()
-                let userInput = try userInput()
-                
-                switch userInput {
-                case .openBank:
-                    supplyClientsToBank()
-                    bank.openBank()
-                case .exit:
-                    return
-                }
-            } catch BankManagerError.invalidUserInput {
-                print(BankManagerError.invalidUserInput.description)
-                print("다시 입력해주십시오.\n")
-            } catch {
-                print(error)
-            }
-        }
+    mutating func openBank() {
+        bank.openBank()
     }
     
-    //MARK: - Private 메서드
-    private func showMenu() {
-        let menu = """
-            1 : 은행개점
-            2 : 종료
-            입력 :
-            """
-        print(menu, terminator: " ")
-    }
-    
-    private func userInput() throws -> MenuOption {
-        guard let input = readLine(),
-              let menuOption = MenuOption(rawValue: input) else {
-            throw BankManagerError.invalidUserInput
-        }
-        
-        return menuOption
-    }
-    
-    private func generateClients() -> [Client] {
+    func generateClients() -> [Client] {
         let clientNumber = Int.random(in: 10...30)
         var clients: [Client] = []
         
@@ -71,7 +27,7 @@ struct BankManager {
         return clients
     }
     
-    private mutating func supplyClientsToBank() {
+    mutating func supplyClientsToBank() {
         let clients = generateClients()
         bank.receive(clients: clients)
     }
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -6,6 +6,54 @@
 
 import Foundation
 
+//MARK: - 타입
+fileprivate enum MenuOption: String {
+    case openBank = "1"
+    case exit = "2"
+}
+
+//MARK: - 메서드
+fileprivate func startConsoleProgram() {
+    while true {
+        do {
+            showMenu()
+            let userInput = try userInput()
+            
+            switch userInput {
+            case .openBank:
+                bankManager.supplyClientsToBank()
+                bankManager.openBank()
+            case .exit:
+                return
+            }
+        } catch BankManagerError.invalidUserInput {
+            print(BankManagerError.invalidUserInput.description)
+            print("다시 입력해주십시오.\n")
+        } catch {
+            print(error)
+        }
+    }
+}
+
+fileprivate func userInput() throws -> MenuOption {
+    guard let input = readLine(),
+          let menuOption = MenuOption(rawValue: input) else {
+        throw BankManagerError.invalidUserInput
+    }
+    
+    return menuOption
+}
+
+
+fileprivate func showMenu() {
+    let menu = """
+        1 : 은행개점
+        2 : 종료
+        입력 :
+        """
+    print(menu, terminator: " ")
+}
+
 var bankManager = BankManager()
 
-bankManager.startConsoleProgram()
+startConsoleProgram()
