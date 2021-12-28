6eb986eab684b1d388799fc62edf7830581b4652
smart8612 <smart8612@gmail.com>
2021-12-28 15:40:09 +0900

    feat: BankService 타입 구현 #7

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankService.swift
@@ -0,0 +1,15 @@
+//
+//  BankService.swift
+//  BankManagerConsoleApp
+//
+//  Created by JeongTaek Han on 2021/12/28.
+//
+
+import Foundation
+
+enum BankService {
+    
+    case deposit
+    case loan
+    
+}
