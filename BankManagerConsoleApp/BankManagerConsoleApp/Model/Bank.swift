a543972d0fa83ac962e0b673823b82d134c9425b
forestjae <forestsj30@gmail.com>
2021-12-28 14:31:25 +0900

    style: Bank타입 내부 메서드 순서 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -57,18 +57,18 @@ class Bank {
         )
     }
     
+    private func makeBankClerk(for number: Int) {
+        for _ in 1...number {
+            let bankClerk = BankClerk()
+            bankClerks.append(bankClerk)
+        }
+    }
+    
     private func receiveClient() {
         let numberOfClient = Int.random(in: 10...30)
         for number in 1...numberOfClient {
             let client = Client(number: number)
             clientQueue.enqueue(client)
-        }
-    }
-    
-    private func makeBankClerk(for number: Int) {
-        for _ in 1...number {
-            let bankClerk = BankClerk()
-            bankClerks.append(bankClerk)
         }
     }
 }
