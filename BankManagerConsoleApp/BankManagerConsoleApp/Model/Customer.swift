881e983e402a8d37ff07193dfd1eca691be492d6
hwangjeha <hyhpwang@gmail.com>
2021-12-28 14:18:29 +0900

    feat: Banking 열거형 타입 구현 #8
    - 랜덤한 케이스를 반환하는 타입 메소드 random() 구현
    - 케이스 별 업무 시간을 반환하는 연산 프로퍼티 speed 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
@@ -1,5 +1,29 @@
 import Foundation
 
+enum Banking: CaseIterable {
+    case loan
+    case deposit
+    
+    static var random: Banking {
+        return .allCases.randomElement() ?? .loan
+    }
+    
+    var speed: TimeInterval {
+        switch self {
+        case .loan:
+            return 1.1
+        case .deposit:
+            return 0.7
+        }
+    }
+}
+
 struct Customer {
+    let banking: Banking
     let waitingNumber: Int
+    
+    init(banking: Banking = .random, waitingNumber: Int) {
+        self.banking = banking
+        self.waitingNumber = waitingNumber
+    }
 }
