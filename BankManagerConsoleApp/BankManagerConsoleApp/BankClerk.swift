05d143c5848f23691b13e76094a05f22f88b19e6
ICS-Asan <carpediem1015@gmail.com>
2021-12-28 15:19:27 +0900

    feat: BankClerk 타입 및 내부 프로퍼티 구현 #7

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
@@ -0,0 +1,17 @@
+//
+//  BankClerk.swift
+//  BankManagerConsoleApp
+//
+//  Created by Seul Mac on 2021/12/28.
+//
+
+import Foundation
+
+struct BankClerk {
+    
+    let depositDepartment: [DispatchQueue]
+    let loanDepartment: [DispatchQueue]
+    private var depositClerkNumber: Int
+    private var loanClerkNumber: Int
+
+}
