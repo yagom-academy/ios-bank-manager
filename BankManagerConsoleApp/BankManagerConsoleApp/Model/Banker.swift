6e4cc7c2e8252574a8739779cbc206e08f8a0664
hwangjeha <hyhpwang@gmail.com>
2021-12-28 15:46:39 +0900

    style: Banker 타입 work() 내부 줄바꿈 개선

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
@@ -7,7 +7,6 @@ struct Banker {
     
     func work(for customer: Customer) {
         let banking = customer.banking
-        
         print("\(customer.waitingNumber)번 고객 \(banking.description)업무 시작")
         Thread.sleep(forTimeInterval: workSpeed(customer.banking))
         print("\(customer.waitingNumber)번 고객 \(banking.description)업무 완료")
