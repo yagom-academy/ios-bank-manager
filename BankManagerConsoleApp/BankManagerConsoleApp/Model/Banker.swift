b358f12cd40246e7c3fc67d7e77a291ff3907b40
hwangjeha <hyhpwang@gmail.com>
2021-12-29 13:25:48 +0900

    refactor: Banker 타입 workSpeed 메소드 제거 #10
    - Banking 연산 프로터피 speed 사용

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
@@ -1,14 +1,10 @@
 import Foundation
 
 struct Banker {
-    private func workSpeed(_ banking: Banking) -> TimeInterval {
-        return banking.speed
-    }
-    
     func work(for customer: Customer) {
         let banking = customer.banking
         print("\(customer.waitingNumber)번 고객 \(banking.description)업무 시작")
-        Thread.sleep(forTimeInterval: workSpeed(customer.banking))
+        Thread.sleep(forTimeInterval: banking.speed)
         print("\(customer.waitingNumber)번 고객 \(banking.description)업무 완료")
     }
 }
