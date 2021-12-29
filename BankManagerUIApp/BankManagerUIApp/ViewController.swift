4c921f52e5e8c22f4bc9092f62ca01902c79f380
hwangjeha <hyhpwang@gmail.com>
2021-12-29 15:09:49 +0900

    feat: 화면 전체를 감싸는 Vertical Stack View 생성 #11

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -1,18 +1,24 @@
-//
-//  BankManagerUIApp - ViewController.swift
-//  Created by yagom. 
-//  Copyright © yagom academy. All rights reserved.
-// 
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    var backgroundStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpBackgroundStackView()
     }
-
-
 }
 
+extension ViewController {
+    private func setUpBackgroundStackView() {
+        view.addSubview(backgroundStackView)
+        backgroundStackView.axis = .vertical
+        backgroundStackView.backgroundColor = .brown
+        
+        backgroundStackView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        backgroundStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        backgroundStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+}
