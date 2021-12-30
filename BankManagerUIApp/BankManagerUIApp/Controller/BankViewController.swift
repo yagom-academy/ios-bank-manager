0bec97ef92a81ad14a6d3938138e54e47c30852b
namu <simjh27@gmail.com>
2021-12-30 17:54:54 +0900

    feat: 스토리보드를 통하지 않고 화면을 표시하도록 구현
    - 스토리보드 관련 설정 삭제 시, LaunchScreen까지 지우면 Window의 크기가 줄어드는 현상이 발생함.
    - 정확한 원인인지는 모르겠지만 LaunchScreen이 초기 윈도우 configuration에 영향을 미치는 듯 함.

---
+++ BankManagerUIApp/BankManagerUIApp/Controller/BankViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BankManagerUIApp - BankViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class BankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBankView()
+    }
+
+    func configureBankView() {
+        view.backgroundColor = .white
+    }
+}
+
