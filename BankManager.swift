f3b61624a27fe50895455c4071764756cfb0aed3
zoe <kny0651@naver.com>
2021-12-30 18:58:35 +0900

    refactor: BankManager 전체적인 리팩토링 #8
    - 예금 및 대출 대기줄 하나의 라인으로 합침
    - assignWork() 메서드에서 고객의 업무종류에 따라 직원이 일을 수행
    - work() 메서드에서 직원업무가 끝날때까지 기다렸다가 최종 보고서 제출

--- BankManager.swift
+++ BankManager.swift
@@ -12,17 +12,9 @@ class BankManager {
     private let speaker = Speaker()
     private let calculator = Calculator()
     private var randomNumber = 0
-    
-    let firstEmployeeQueue = DispatchQueue(label: "FirstEmployeeQueue")
-    let secondEmployeeQueue = DispatchQueue(label: "SecondEmployeeQueue")
-    let thirdEmployeeQueue = DispatchQueue(label: "ThirdEmployeeQueue")
-    
-    let group1 = DispatchGroup()
-    let group2 = DispatchGroup()
-    let group3 = DispatchGroup()
-    
-    let semaphore1 = DispatchSemaphore(value: 1)
-    let semaphore2 = DispatchSemaphore(value: 2)
+    private let employeeGroup = DispatchGroup()
+    private let depositSemaphore = DispatchSemaphore(value: 2)
+    private let loanSemaphore = DispatchSemaphore(value: 1)
     
     init(employee: Employee) {
         self.employee = employee
@@ -34,6 +26,7 @@ class BankManager {
         lineUp()
         assignWork()
         
+        employeeGroup.wait()
         let closeTime = CFAbsoluteTimeGetCurrent()
         
         reportResult(from: openTime, to: closeTime)
@@ -48,93 +41,28 @@ extension BankManager {
         for number in 1...randomNumber {
             let bankWork = BankWork.allCases.randomElement()!
             let customer = Customer(waitingNumber: number, requestedWork: bankWork)
-            
-            switch customer.requestedWork {
-            case .deposit:
-                bank?.depositWaitingLine.enqueue(customer)
-            case .loan:
-                bank?.loanWaitingLine.enqueue(customer)
-            }
+            bank?.waitingLine.enqueue(customer)
         }
     }
     
     private func assignWork() {
-//        let firstEmployeeQueue = DispatchQueue(label: "FirstEmployeeQueue")
-//        let secondEmployeeQueue = DispatchQueue(label: "SecondEmployeeQueue")
-//        let thirdEmployeeQueue = DispatchQueue(label: "ThirdEmployeeQueue")
-//
-//        let group1 = DispatchGroup()
-//        let group2 = DispatchGroup()
-//        let group3 = DispatchGroup()
-//
-//        let semaphore1 = DispatchSemaphore(value: 1)
-//        let semaphore2 = DispatchSemaphore(value: 2)
-        
-        group1.notify(queue: firstEmployeeQueue) {
-            print("group1 끝")
-        }
-        group2.notify(queue: secondEmployeeQueue) {
-            print("group2 끝")
-        }
-        group3.notify(queue: thirdEmployeeQueue) {
-            print("group3 끝")
-        }
-        
-        repeat {
-            // First Employee
-            group1.enter()
-            firstEmployeeQueue.asyncAfter(deadline: .now() + 0.7) {
-                self.semaphore1.wait()
-                self.employee.startDepositJob()
-                guard let customer = self.bank?.depositWaitingLine.dequeue() else {
-                    self.group1.leave()
-                    
-                    return
+        while let customer = bank?.waitingLine.dequeue() {
+            switch customer.requestedWork {
+                
+            case .deposit:
+                DispatchQueue.global().async(group: employeeGroup) {
+                    self.depositSemaphore.wait()
+                    self.employee.work(for: customer)
+                    self.depositSemaphore.signal()
                 }
-                
-                print("직원1 시작")
-                self.semaphore1.signal()
-                
-                self.semaphore1.wait()
-                self.employee.finishDepositJob(customer: customer)
-                print("직원1 끝")
-                self.semaphore1.signal()
-                self.group1.leave()
-            }
-            
-            // Second Employee
-            group2.enter()
-            secondEmployeeQueue.asyncAfter(deadline: .now() + 0.7) {
-                self.semaphore1.wait()
-                self.employee.startDepositJob()
-                guard let customer = self.bank?.depositWaitingLine.dequeue() else {
-                    self.group2.leave()
-                    
-                    return
+            case .loan:
+                DispatchQueue.global().async(group: employeeGroup) {
+                    self.loanSemaphore.wait()
+                    self.employee.work(for: customer)
+                    self.loanSemaphore.signal()
                 }
-                
-                print("직원2 시작")
-                self.semaphore1.signal()
-                
-                self.semaphore1.wait()
-                self.employee.finishDepositJob(customer: customer)
-                print("직원2 끝")
-                self.semaphore1.signal()
-                self.group2.leave()
-                
             }
-        } while self.bank?.depositWaitingLine.isEmpty == false
-        
-        repeat {
-            // Third Employee
-            group3.enter()
-            thirdEmployeeQueue.asyncAfter(deadline: .now() + 1.1) {
-                self.employee.startLoanJob()
-                self.employee.finishLoanJob()
-                
-                self.group3.leave()
-            }
-        } while self.bank?.loanWaitingLine.isEmpty == false
+        }
     }
     
     
--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,15 +8,13 @@
 import Foundation
 
 protocol WaitingLineManageable {
-    var depositWaitingLine: Queue<Customer> { get }
-    var loanWaitingLine: Queue<Customer> { get }
+    var waitingLine: Queue<Customer> { get }
 }
     
 class Bank: WaitingLineManageable  {
     private weak var employee: Employee?
     private weak var bankManager: BankManager?
-    let depositWaitingLine = Queue<Customer>()
-    let loanWaitingLine = Queue<Customer>()
+    let waitingLine = Queue<Customer>()
 
     init(employee: Employee, bankManager: BankManager) {
         self.employee = employee
