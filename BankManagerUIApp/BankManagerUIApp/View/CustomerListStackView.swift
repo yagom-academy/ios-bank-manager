b48ddc8ee8857e6b9341a0908def6edd4687378b
Ari <lee_ari95@icloud.com>
2021-12-30 17:43:26 +0900

    refactor: CustomerListStackView를 생성하고 ViewController 리팩토링 #11
    - UILabel과 UIScrollView를 담는 StackView를 Custom View로 생성
    - Custom View를 재사용하여 ViewController 내부 코드 수정
    - 필요없는 backgroundColor 삭제

---
+++ BankManagerUIApp/BankManagerUIApp/View/CustomerListStackView.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+class CustomerListStackView: UIStackView {
+    
+    var notifyLabel = UILabel()
+    var listScrollView = UIScrollView()
+    var listStackView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    convenience init(title: String, color: UIColor) {
+        self.init(frame: CGRect.zero)
+        setUp()
+        notifyLabel.text = title
+        notifyLabel.backgroundColor = color
+    }
+    
+    private func setUp() {
+        axis = .vertical
+        spacing = 12
+        translatesAutoresizingMaskIntoConstraints = false
+        setUpNotifyLabel()
+        setUpListStackView()
+        addSubviews()
+        setUpLayout()
+    }
+    
+    private func addSubviews() {
+        addArrangedSubview(notifyLabel)
+        addArrangedSubview(listScrollView)
+        listScrollView.addSubview(listStackView)
+    }
+    
+    private func setUpNotifyLabel() {
+        notifyLabel.font = .preferredFont(forTextStyle: .title1)
+        notifyLabel.textColor = .white
+        notifyLabel.textAlignment = .center
+    }
+    
+    private func setUpListStackView() {
+        listStackView.axis = .vertical
+        listStackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setUpLayout() {
+        listStackView.topAnchor.constraint(equalTo: listScrollView.topAnchor).isActive = true
+        listStackView.bottomAnchor.constraint(equalTo: listScrollView.bottomAnchor).isActive = true
+        listStackView.leadingAnchor.constraint(equalTo: listScrollView.leadingAnchor).isActive = true
+        listStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        listStackView.widthAnchor.constraint(equalTo: listScrollView.widthAnchor).isActive = true
+        let listStackViewHeight = listStackView.heightAnchor.constraint(equalTo: listScrollView.heightAnchor)
+        listStackViewHeight.priority = .defaultLow
+        listStackViewHeight.isActive = true
+    }
+    
+    func addCustomerLabel(label: UILabel) {
+        listStackView.addArrangedSubview(label)
+    }
+}
--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -8,8 +8,7 @@ class ViewController: UIViewController {
         setUpBackgroundStackView()
         setUPButtonStackView()
         setUPTimerStackView()
-        setUPNotifyStackView()
-        setUpCustomerListStackView()
+        setUpCustomerListStackViews()
     }
 }
 
@@ -17,7 +16,7 @@ extension ViewController {
     private func setUpBackgroundStackView() {
         view.addSubview(backgroundStackView)
         backgroundStackView.axis = .vertical
-        backgroundStackView.backgroundColor = .brown
+        backgroundStackView.spacing = 12
         
         backgroundStackView.translatesAutoresizingMaskIntoConstraints = false
         backgroundStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
@@ -29,7 +28,6 @@ extension ViewController {
     private func setUPButtonStackView() {
         let buttonStackView = UIStackView()
         buttonStackView.translatesAutoresizingMaskIntoConstraints = false
-        buttonStackView.backgroundColor = .yellow
         buttonStackView.axis = .horizontal
         buttonStackView.distribution = .fillEqually
         backgroundStackView.addArrangedSubview(buttonStackView)
@@ -49,7 +47,6 @@ extension ViewController {
     
     private func setUPTimerStackView() {
         let timerStackView = UIStackView()
-        timerStackView.backgroundColor = .blue
         timerStackView.axis = .horizontal
         timerStackView.distribution = .fillEqually
         backgroundStackView.addArrangedSubview(timerStackView)
@@ -67,88 +64,17 @@ extension ViewController {
         timerStackView.addArrangedSubview(timerLabel)
     }
     
-    private func setUPNotifyStackView() {
-        let notifyStackView = UIStackView()
-        notifyStackView.backgroundColor = .blue
-        notifyStackView.axis = .horizontal
-        notifyStackView.distribution = .fillEqually
-        backgroundStackView.addArrangedSubview(notifyStackView)
+    private func setUpCustomerListStackViews() {
+        let customersBackgroundStackView = UIStackView()
+        customersBackgroundStackView.axis = .horizontal
+        customersBackgroundStackView.distribution = .fillEqually
+        customersBackgroundStackView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundStackView.addArrangedSubview(customersBackgroundStackView)
         
-        let waitingLabel = UILabel()
-        waitingLabel.text = "대기중"
-        waitingLabel.font = .preferredFont(forTextStyle: .title2) 
-        waitingLabel.textAlignment = .center
-        waitingLabel.textColor = .white
-        waitingLabel.backgroundColor = .systemGreen
+        let waitingListStackView = CustomerListStackView(title: "대기중", color: .systemGreen)
+        customersBackgroundStackView.addArrangedSubview(waitingListStackView)
         
-        let workingLabel = UILabel()
-        workingLabel.text = "업무중"
-        workingLabel.font = .preferredFont(forTextStyle: .title2) 
-        workingLabel.textAlignment = .center
-        workingLabel.textColor = .white
-        workingLabel.backgroundColor = .systemPurple
-        
-        notifyStackView.addArrangedSubview(waitingLabel)
-        notifyStackView.addArrangedSubview(workingLabel)
-    }
-    
-    private func setUpCustomerListStackView() {
-        let customerListStackView = UIStackView()
-        customerListStackView.backgroundColor = .red
-        customerListStackView.axis = .horizontal
-        customerListStackView.distribution = .fillEqually
-        backgroundStackView.addArrangedSubview(customerListStackView)
-        
-        let waitingScrollView = UIScrollView()
-        waitingScrollView.backgroundColor = .yellow
-        customerListStackView.addArrangedSubview(waitingScrollView)
-        
-        let waitingStackView = UIStackView()
-        waitingStackView.translatesAutoresizingMaskIntoConstraints = false
-        waitingStackView.axis = .vertical
-        waitingStackView.backgroundColor = .cyan
-        waitingScrollView.addSubview(waitingStackView)
-                
-        waitingStackView.topAnchor.constraint(equalTo: waitingScrollView.topAnchor).isActive = true
-        waitingStackView.bottomAnchor.constraint(equalTo: waitingScrollView.bottomAnchor).isActive = true
-        waitingStackView.leadingAnchor.constraint(equalTo: waitingScrollView.leadingAnchor).isActive = true
-        waitingStackView.trailingAnchor.constraint(equalTo: waitingScrollView.trailingAnchor).isActive = true
-        waitingStackView.widthAnchor.constraint(equalTo: waitingScrollView.widthAnchor).isActive = true
-        let waitingStackViewHeight = waitingStackView.heightAnchor.constraint(equalTo: waitingScrollView.heightAnchor)
-        waitingStackViewHeight.priority = .defaultLow
-        waitingStackViewHeight.isActive = true
-        
-        let test = UILabel()
-        test.text = "asdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\n"
-        test.numberOfLines = 0
-        test.font = .preferredFont(forTextStyle: .largeTitle)
-        waitingStackView.addArrangedSubview(test)
-        
-        
-        
-        let workingScrollView = UIScrollView()
-        workingScrollView.backgroundColor = .brown
-        customerListStackView.addArrangedSubview(workingScrollView)
-        
-        let workingStackView = UIStackView()
-        workingStackView.translatesAutoresizingMaskIntoConstraints = false
-        workingStackView.axis = .vertical
-        workingStackView.backgroundColor = .gray
-        workingScrollView.addSubview(workingStackView)
-        
-        workingStackView.topAnchor.constraint(equalTo: workingScrollView.topAnchor).isActive = true
-        workingStackView.bottomAnchor.constraint(equalTo: workingScrollView.bottomAnchor).isActive = true
-        workingStackView.leadingAnchor.constraint(equalTo: workingScrollView.leadingAnchor).isActive = true
-        workingStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        workingStackView.widthAnchor.constraint(equalTo: workingScrollView.widthAnchor).isActive = true
-        let workingStackViewHeight = workingStackView.heightAnchor.constraint(equalTo: workingScrollView.heightAnchor)
-        workingStackViewHeight.priority = .defaultLow
-        workingStackViewHeight.isActive = true
-        
-        let test2 = UILabel()
-        test2.text = "asdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\nasdfasdfasdfasf\n"
-        test2.numberOfLines = 0
-        test2.font = .preferredFont(forTextStyle: .largeTitle)
-        workingStackView.addArrangedSubview(test2)
+        let workingListStackView = CustomerListStackView(title: "업무중", color: .systemIndigo)
+        customersBackgroundStackView.addArrangedSubview(workingListStackView)
     }
 }
