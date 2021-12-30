036253dd3e19226eb0110241e878e954172426a0
yeha <dev.yeha@gmail.com>
2021-12-30 22:39:56 +0900

    feat: 타이머 기능 구현 #12
    - workingTimeLabel 생성
    - Label Namespace 생성
    - setupWorkingTimeLabel 메서드로 오토 레이아웃 지정
    - touchUpAddClientButton IBAction 메서드 구현

---
+++ BankManagerUIApp/BankManagerUIApp/Namespace/Label.swift
@@ -0,0 +1,12 @@
+//
+//  Label.swift
+//  BankManagerUIApp
+//
+//  Created by yeha on 2021/12/30.
+//
+
+import Foundation
+
+enum Label {
+    static let workingTime = "업무시간 - "
+}
--- BankManagerUIApp/BankManagerUIApp/ViewController/StackViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController/StackViewController.swift
@@ -12,17 +12,22 @@ class StackViewController: UIViewController {
     let addClientButton = UIButton()
     let resetButton = UIButton()
     let buttonHorizontalStackView = UIStackView()
-
+    let workingTimeLabel = UILabel()
+    var count: Int = 0
+    
+    //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUIStackView(stackView: buttonHorizontalStackView, axis: .horizontal)
         setupUIButton(button: addClientButton, title: Button.addClientButtonTitle, color: .systemBlue)
         setupUIButton(button: resetButton, title: Button.resetButton, color: .systemRed)
         setupButtonHorizontalStackViewAutoLayout()
+        setupWorkingTimeLabel()
+        addClientButton.addTarget(self, action: #selector(self.touchUpAddClientButton(_:)), for: .touchUpInside)
     }
     
     //MARK: - Method
-    func setupUIButton(button:UIButton, title: String, color: UIColor) {
+    func setupUIButton(button: UIButton, title: String, color: UIColor) {
         button.setTitle(title, for: .normal)
         button.setTitleColor(color, for: .normal)
         buttonHorizontalStackView.addArrangedSubview(button)
@@ -35,13 +40,54 @@ class StackViewController: UIViewController {
         buttonHorizontalStackView.distribution = .fillEqually
     }
     
-    func setupUIStackView(stackView:UIStackView , axis: NSLayoutConstraint.Axis) {
+    func setupUIStackView(stackView: UIStackView , axis: NSLayoutConstraint.Axis) {
         buttonHorizontalStackView.axis = axis
         buttonHorizontalStackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(stackView)
     }
     
-    
-
+    func setupWorkingTimeLabel() {
+        let initialTime = makeFormattedTime(minute: .zero, second: .zero, millisecond: .zero)
+        view.addSubview(workingTimeLabel)
+        workingTimeLabel.topAnchor.constraint(equalTo: buttonHorizontalStackView.bottomAnchor, constant: 10).isActive = true
+        workingTimeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        workingTimeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        workingTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        workingTimeLabel.textAlignment = .center
+        workingTimeLabel.text = "\(Label.workingTime)\(initialTime)"
+    }
 }
 
+//MARK: - IBAction
+extension StackViewController {
+    @IBAction func touchUpAddClientButton(_ sender: UIButton) {
+        Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timeCounter), userInfo: nil, repeats: true)
+    }
+}
+
+//MARK: - Timer
+extension StackViewController {
+    @objc func timeCounter() {
+        count += 1
+        let (minute, second, millisecond) = convertTime(milliseconds: count)
+        let formattedTime = makeFormattedTime(minute: minute, second: second, millisecond: millisecond)
+        workingTimeLabel.text = "\(Label.workingTime)\(formattedTime)"
+    }
+    
+    func convertTime(milliseconds: Int) -> (Int, Int, Int) {
+        let minute = (milliseconds / 1000) / 60
+        let second = (milliseconds / 1000) % 60
+        let millisecond = milliseconds % 1000
+        
+        return (minute, second, millisecond)
+    }
+    
+    func makeFormattedTime(minute: Int, second: Int, millisecond: Int) -> String {
+        let formattedMinute = String(format: "%02d", minute)
+        let formattedSecond = String(format: "%02d", second)
+        let formattedMillisecond = String(format: "%003d", millisecond)
+        let formattedTime = "\(formattedMinute) : \(formattedSecond) : \(formattedMillisecond)"
+        
+        return formattedTime
+    }
+}
