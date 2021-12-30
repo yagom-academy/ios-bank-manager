2ff7e431db07fed4588c68d4a87294be3d883f53
horeng2 <huski.a.a@gmail.com>
2021-12-30 14:57:35 +0900

    style: indent 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -18,14 +18,14 @@ class Bank {
     self.bankers = bankers
     self.operatingTimeManager = operatingTimeManager
   }
-
+  
   func doBanking() {
-      operatingTimeManager.openTime = CFAbsoluteTimeGetCurrent()
-      clientLineUp()
-      doWork()
-      operatingTimeManager.closeTime = CFAbsoluteTimeGetCurrent()
-      print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClients)명이며,"
-            + "총 업무시간은 \(operatingTimeManager.workingTime())초입니다.")
+    operatingTimeManager.openTime = CFAbsoluteTimeGetCurrent()
+    clientLineUp()
+    doWork()
+    operatingTimeManager.closeTime = CFAbsoluteTimeGetCurrent()
+    print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClients)명이며,"
+          + "총 업무시간은 \(operatingTimeManager.workingTime())초입니다.")
   }
 
   private func clientLineUp() {
