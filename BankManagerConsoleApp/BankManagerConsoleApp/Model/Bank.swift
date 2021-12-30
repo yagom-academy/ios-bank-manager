9ce1e9fbbe638f795b8c847de687f71817c6bbff
yim2627 <yim2627@gmail.com>
2021-12-30 16:08:37 +0900

    refactor: startWork() 분리 #10

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -3,6 +3,12 @@ import Foundation
 class Bank {
     private var waitingLine = Queue<Customer>()
     private var bankClerk = BankClerk()
+
+    private let depositQueue = DispatchQueue(label: "depositQueue", attributes: .concurrent)
+    private let loanQueue = DispatchQueue(label: "loanQueue")
+    private let sequenceQueue = DispatchQueue(label: "sequenceQueue")
+    
+    private var totalCustomer = 0
     
     func setWaitingLine(with numberOfCustomer: Int) {
         let totalNumber = numberOfCustomer
@@ -16,15 +22,17 @@ class Bank {
     }
     
     func startWork() {
-        let depositQueue = DispatchQueue(label: "depositQueue", attributes: .concurrent)
-        let loanQueue = DispatchQueue(label: "loanQueue")
+        let date = Date()
+        
+        work()
+        
+        let taskTime = abs(date.timeIntervalSinceNow)
+        finishWork(workingTime: taskTime)
+    }
+    
+    private func work() {
         let workGroup = DispatchGroup()
         let semaphore = DispatchSemaphore(value: 2)
-        let sequenceQueue = DispatchQueue(label: "sequenceQueue")
-        
-        var numberOfCustomer = 0
-        
-        let date = Date()
         
         while waitingLine.isEmpty == false {
             guard let customer = dequeueWaitingLine() else {
@@ -33,32 +41,30 @@ class Bank {
             
             switch customer.task {
             case .deposit:
-                sequenceQueue.async {
+                sequenceQueue.async { [self] in
                     depositQueue.async(group: workGroup) {
                         semaphore.wait()
-                        self.bankClerk.handleTask(of: customer)
-                        numberOfCustomer += 1
+                        bankClerk.handleTask(of: customer)
+                        totalCustomer += 1
                         semaphore.signal()
                     }
                 }
             case .loan:
-                loanQueue.async(group: workGroup) {
-                    self.bankClerk.handleTask(of: customer)
-                    numberOfCustomer += 1
+                loanQueue.async(group: workGroup) { [self] in
+                    bankClerk.handleTask(of: customer)
+                    totalCustomer += 1
                 }
             default:
                 return
             }
         }
         workGroup.wait()
-        
-        let taskTime = abs(date.timeIntervalSinceNow)
-        self.close(totalCustomer: numberOfCustomer, taskTime: taskTime)
     }
     
-    private func close(totalCustomer: Int, taskTime: Double) {
-        let totalTime = String(format: "%.2f", taskTime)
+    private func finishWork(workingTime: Double) {
+        let totalTime = String(format: "%.2f", workingTime)
         
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(totalTime)초입니다.")
+        totalCustomer = 0
     }
 }
