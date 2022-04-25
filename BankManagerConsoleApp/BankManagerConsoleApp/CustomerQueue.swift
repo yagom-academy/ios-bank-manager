aef289b73680f6bdc75842763746f9613393c154
Grumpy <sewoong2890@gmail.com>
2022-04-25 20:31:22 +0900

    [feat]: CustomerQueue 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/CustomerQueue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/CustomerQueue.swift
@@ -1,1 +1,25 @@
 import Foundation
+
+struct CustomerQueue<T> {
+    private var list = LinkedList<T>()
+    
+    func enqueue(element: T) {
+        list.append(element)
+    }
+    
+    func dequeue() -> T? {
+        return list.removeHead()
+    }
+    
+    func clear() {
+        list.clear()
+    }
+    
+    func peek() -> T? {
+        return list.head?.data
+    }
+    
+    func isEmpty() -> Bool {
+        return list.isEmpty
+    }
+}
