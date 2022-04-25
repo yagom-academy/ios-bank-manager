29cf7b2c67831f9b1bf2055a866d17a320b21d30
Lingo <llingo@kakao.com>
2022-04-25 22:50:47 +0900

    style: 줄바꿈 및 생략 되었던 return 키워드 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/BankList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankList.swift
@@ -10,8 +10,9 @@ import Foundation
 struct BankList<T> {
   private var head: BankNode<T>?
   private var tail: BankNode<T>?
+  
   var isEmpty: Bool {
-    head == nil
+    return head == nil
   }
   
   var first: T? {
