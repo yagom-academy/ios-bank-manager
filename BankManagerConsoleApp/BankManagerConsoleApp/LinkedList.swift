17d03d3365bdd4a88e1b16d9452e5ffc27d8c1a6
DuDu <whqtkf12@naver.com>
2022-04-25 13:34:06 +0900

    feat: LinkedList 구현 (#2)

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,55 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by Eddy, dudu on 2022/04/25.
+//
+
+import Foundation
+
+class LinkedList<Element> {
+    private var head: Node<Element>? = nil
+    private var tail: Node<Element>? = nil
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    var peek: Element? {
+        return head?.data
+    }
+    
+    func append(_ data: Element) {
+        let newNode = Node(data: data)
+        
+        if head == nil {
+            head = newNode
+            tail = head
+        } else {
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
+    
+    func removeFirst() -> Element? {
+        if head == nil {
+            return nil
+        }
+        
+        let target = head?.data
+        
+        if head === tail {
+            head = nil
+            tail = nil
+        } else {
+            head = head?.next
+        }
+        
+        return target
+    }
+    
+    func clear() {
+        head = nil
+        tail = nil
+    }
+}
