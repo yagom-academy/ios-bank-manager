3ec5cc10a2f60ee93743bb8286f68a6d753255fd
Red <seryprk@gmail.com>
2022-04-26 01:56:46 +0900

    ✨FEAT : Queue struct 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -6,4 +6,25 @@
 //
 
 struct Queue<T> {
+    var linkedList = LinkedList<T>()
+
+    var isEmpty: Bool {
+        return linkedList.isEmpty
+    }
+
+    var peek: T? {
+        return linkedList.peek
+    }
+
+    mutating func clear() {
+        return linkedList.removeAll()
+    }
+
+    mutating func enQueue(data: T) {
+        return linkedList.append(data: data)
+    }
+
+    mutating func deQueue() -> T? {
+        return linkedList.pop()
+    }
 }
