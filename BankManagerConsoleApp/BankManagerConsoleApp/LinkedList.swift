a9bdccf9b4a97189bb7998cb1479dea52a1539b6
Grumpy <sewoong2890@gmail.com>
2022-04-25 20:31:08 +0900

    [feat]: LinkedList class 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -1,1 +1,32 @@
 import Foundation
+
+final class LinkedList<T> {
+    private(set) var head: Node<T>?
+    private(set) var tail: Node<T>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    func append(_ data: T) {
+        let newNode = Node(data)
+        if head == nil {
+            head = newNode
+        } else {
+            newNode.prev = tail
+            tail?.next = newNode
+        }
+        tail = newNode
+    }
+    
+    func removeHead() -> T? {
+        let data = head?.data
+        head = head?.next
+        return data
+    }
+    
+    func clear() {
+        head = nil
+        tail = nil
+    }
+}
