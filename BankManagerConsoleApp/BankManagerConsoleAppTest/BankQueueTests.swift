96c126732098ba1cec99ad651f389efb28a9cb26
GoMyungju171 <mooooongdu171@gmail.com>
2022-04-26 14:46:15 +0900

    feat: BankManagerConsoleAppTest 파일 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleAppTest/BankQueueTests.swift
@@ -0,0 +1,20 @@
+//
+//  BankQueueTests.swift
+//  BankManagerConsoleAppTest
+//
+//  Created by Lingo, Cathy on 2022/04/26.
+//
+
+import XCTest
+@testable import BankManagerConsoleApp
+
+final class BankQueueTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+  }
+  
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
+}
