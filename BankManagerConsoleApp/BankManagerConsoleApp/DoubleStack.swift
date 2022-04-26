7e7c8d24003b01d00f57d1c69103bc04874094d5
onetool <xogns8696@gmail.com>
2022-04-26 23:59:51 +0900

    ✨FEAT : DoubleStack 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/DoubleStack.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/DoubleStack.swift
@@ -8,4 +8,39 @@
 import Foundation
 
 final class DoubleStack<T> {
+    private var dequeueStack: [T] = []
+    private var enqueueStack: [T] = []
+    
+    var count: Int {
+        return enqueueStack.count + dequeueStack.count
+    }
+    
+    var isEmpty: Bool {
+        return enqueueStack.isEmpty && dequeueStack.isEmpty
+    }
+    
+    var first: T? {
+        return dequeueStack.isEmpty ? enqueueStack.first : dequeueStack.last
+    }
+    
+    var last: T? {
+        return enqueueStack.isEmpty ? dequeueStack.first : enqueueStack.last
+    }
+    
+    func append(value: T) {
+        enqueueStack.append(value)
+    }
+    
+    func removeAll() {
+        enqueueStack.removeAll()
+        dequeueStack.removeAll()
+    }
+    
+    func removeFirst() -> T? {
+        if dequeueStack.isEmpty {
+            dequeueStack = enqueueStack.reversed()
+            enqueueStack.removeAll()
+        }
+        return dequeueStack.popLast()
+    }
 }
