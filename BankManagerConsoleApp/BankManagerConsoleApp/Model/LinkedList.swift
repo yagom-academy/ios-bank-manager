d4cec5d294e27ea38b47eddfd5af33b548ca6c61
Quokkaa <rktrkdml@gmail.com>
2022-04-26 11:13:53 +0900

    💎feat : LinkedList 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/LinkedList.swift
@@ -0,0 +1,10 @@
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//   Created by 쿼카, 두기 on 2022/04/26
+
+import Foundation
+
+final class LinkedList<Element> {
+    private var head: Node<Element>?
+    private var tail: Node<Element>?
+}
