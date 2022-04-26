be6dfa6d2b550bf312a40edd68ee47d00478aa1a
Donnie <seo_17@naver.com>
2022-04-26 10:47:29 +0900

    ➕[add]: BankWaitingQueue 테스트 코드 작성

--- BankManagerConsoleApp/BankManagerConsoleAppTest/BankWaitingQueueTest.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTest/BankWaitingQueueTest.swift
@@ -17,4 +17,51 @@ class BankWaitingQueueTest: XCTestCase {
     override func tearDownWithError() throws {
         sut = nil
     }
+    
+    func test_1개_enqueue_후에_isEmpty호출_결과는_false() {
+        //given
+        sut.enqueue(1)
+        
+        //when
+        let result = sut.isEmpty
+        
+        //then
+        XCTAssertFalse(result)
+    }
+    
+    func test_non_enqueue_했을때_isEmpty호출_결과는_true() {
+        //given
+        
+        //when
+        let result = sut.isEmpty
+        
+        //then
+        XCTAssertTrue(result)
+    }
+    
+    func test_2개_enqueue하고_1개_dequeue_했을때_isEmpty호출_결과는_false() {
+        //given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        
+        //when
+        let peekValue = sut.peek
+        let result = sut.dequeue()
+        
+        //then
+        XCTAssertEqual(peekValue, result)
+    }
+    
+    func test_2개_enqueue하고_clear_했을때_isEmpty_호출_결과는_true() {
+        //given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        sut.clear()
+        
+        //when
+        let result = sut.isEmpty
+        
+        //then
+        XCTAssertTrue(result)
+    }
 }
