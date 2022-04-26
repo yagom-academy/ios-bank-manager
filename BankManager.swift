a26c29edd5b21f2bb4d0dfdd856066ca822af8a4
Eddy <kimkyunghun3@naver.com>
2022-04-26 18:07:07 +0900

    feat: BankManager 구현

--- BankManager.swift
+++ BankManager.swift
@@ -4,4 +4,41 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+private enum MenuOption: Int {
+    case bankOpen = 1
+    case exit
+}
+
+struct BankManager {
+    private var bank = Bank()
+    
+    mutating func start() {
+        printMenu()
+        
+        switch inputUserOption() {
+        case .bankOpen:
+            bank.open()
+            start()
+        case .exit:
+            break
+        case .none:
+            print("잘못 입력했습니다. 다시 입력해주세요")
+            start()
+        }
+    }
+    
+    private func printMenu() {
+        print("1 : 은행개점")
+        print("2 : 종료")
+        print("입력 : ", terminator: "")
+    }
+    
+    private func inputUserOption() -> MenuOption? {
+        guard let userInput = readLine()?.trimmingCharacters(in: .whitespaces),
+              let userNumber = Int(userInput),
+              let userSelection = MenuOption(rawValue: userNumber) else { return nil }
+        
+        return userSelection
+    }
+}
+
