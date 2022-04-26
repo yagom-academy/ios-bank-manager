439733f5e69b61d01225d0637984cb362deb6079
Eddy <kimkyunghun3@naver.com>
2022-04-26 18:06:20 +0900

    feat: Bank의 printBankResult 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -38,4 +38,9 @@ struct Bank {
 
         endTime = CFAbsoluteTimeGetCurrent()
     }
+
+    private mutating func printBankResult() {
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(handledCustomerCount)명이며, 총 업무 시간은 \(businessHours)초입니다.")
+        handledCustomerCount = 0
+    }
 }
