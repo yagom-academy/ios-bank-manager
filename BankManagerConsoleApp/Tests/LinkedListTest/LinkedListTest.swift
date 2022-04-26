e11593481af910bebe277d56001a79a5cea42efe
Red <seryprk@gmail.com>
2022-04-27 02:23:33 +0900

    🩹CHORE : 컨벤션 수정

--- BankManagerConsoleApp/Tests/LinkedListTest/LinkedListTest.swift
+++ BankManagerConsoleApp/Tests/LinkedListTest/LinkedListTest.swift
@@ -9,45 +9,44 @@ import XCTest
 
 class LinkedListTest: XCTestCase {
     var sut: LinkedList<Int>!
-    
+
     override func setUp() {
         sut = LinkedList<Int>()
     }
-    
+
     override func tearDown() {
         self.sut = nil
     }
-    
+
     func test_append_3번_후에_isEmpty호출_removeAll호출_결과가_true인지() {
         sut.append(value: 1)
         sut.append(value: 2)
         sut.append(value: 3)
-        
+
         sut.removeAll()
         let result = sut.isEmpty
-        
+
         XCTAssertTrue(result)
     }
-    
+
     func test_append_3번_후에_first호출_결과가_1인지() {
         sut.append(value: 1)
         sut.append(value: 2)
         sut.append(value: 3)
-        
+
         let result = sut.first
-        
+
         XCTAssertEqual(result, 1)
     }
-    
+
     func test_append_3번_후에_removeFirst호출_first호출_결과가_2인지() {
         sut.append(value: 1)
         sut.append(value: 2)
         sut.append(value: 3)
-        
+
         _ = sut.removeFirst()
         let result = sut.first
-        
+
         XCTAssertEqual(result, 2)
     }
 }
-
