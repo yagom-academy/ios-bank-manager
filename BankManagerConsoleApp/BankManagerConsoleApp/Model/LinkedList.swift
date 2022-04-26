8b88cca0d6c676f22e2f848026b3cbefa8c7a348
Quokkaa <rktrkdml@gmail.com>
2022-04-26 11:19:15 +0900

    💎feat : append메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/LinkedList.swift
@@ -7,4 +7,15 @@ import Foundation
 final class LinkedList<Element> {
     private var head: Node<Element>?
     private var tail: Node<Element>?
+    
+    func append(data: Element) {
+        let newNode = Node(date: data)
+        if head == nil {
+            head = newNode
+            tail = newNode
+        } else {
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
 }
