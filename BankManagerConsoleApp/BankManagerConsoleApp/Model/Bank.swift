2731ada66163e0b15c0cb397640e32d2f5cbfb74
Taeangel <wngks2050@gmail.com>
2022-04-26 20:52:34 +0900

    feat: Bank 타입 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -0,0 +1,11 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by Taeangel, Tiana 2022/04/26.
+//
+
+final class Bank {
+    var clerks: [BankClerk] = []
+    var clients: Queue<Client> = Queue()
+}
