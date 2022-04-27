b35ad9c7969a258dd66fd3f8148462e773380f90
Mino777 <jomino7777@gmail.com>
2022-04-27 14:23:50 +0900

    feat: printMenu 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -2,4 +2,13 @@
 //  BankManagerConsoleApp - main.swift
 //  Created by yagom. 
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
+
+fileprivate func printMenu() {
+    let menu = """
+    1 : 은행개점
+    2 : 종료
+    입력 :
+    """
+    print(menu, terminator: " ")
+}
