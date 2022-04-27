52a709e29732bf9b886bccedeeb85512c556d7df
Grumpy <sewoong2890@gmail.com>
2022-04-27 16:38:15 +0900

    [chore]: QueueTest에서 queue 강제 언래핑하여 사용하도록 수정

--- BankManagerConsoleApp/QueueTest/QueueTest.swift
+++ BankManagerConsoleApp/QueueTest/QueueTest.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 class QueueTest: XCTestCase {
-    var queue: Queue<Int>?
+    var queue: Queue<Int>!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -15,12 +15,12 @@ class QueueTest: XCTestCase {
     
     func test_queue에_enqueue를3번했을때_첫번째값이_1이어야한다() {
         // given
-        queue?.enqueue(1)
-        queue?.enqueue(2)
-        queue?.enqueue(3)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
         
         // when
-        let value = queue?.dequeue()
+        let value = queue.dequeue()
         
         // then
         XCTAssertEqual(value, 1)
@@ -28,13 +28,13 @@ class QueueTest: XCTestCase {
     
     func test_queue에_dequeue를2번했을때_2를리턴해야한다() {
         // given
-        queue?.enqueue(1)
-        queue?.enqueue(2)
-        queue?.enqueue(3)
-        _ = queue?.dequeue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        _ = queue.dequeue()
         
         // when
-        let value = queue?.dequeue()
+        let value = queue.dequeue()
         
         // then
         XCTAssertEqual(value, 2)
@@ -42,24 +42,24 @@ class QueueTest: XCTestCase {
     
     func test_queue를_clear하면_isEmpty가true이어야한다() {
         // given
-        queue?.enqueue(1)
-        queue?.enqueue(2)
-        queue?.enqueue(3)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
         
         // when
-        queue?.clear()
-        let result = queue?.isEmpty
+        queue.clear()
+        let result = queue.isEmpty
         
         // then
-        XCTAssertEqual(result, true)
+        XCTAssert(result)
     }
     
     func test_queue의head가1일때_peek했을때_1을리턴해야한다() {
         // given
-        queue?.enqueue(1)
+        queue.enqueue(1)
         
         // when
-        let value = queue?.peek()
+        let value = queue.peek()
         
         // then
         XCTAssertEqual(value, 1)
