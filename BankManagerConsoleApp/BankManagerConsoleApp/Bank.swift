1bd24da7e87bc9bf2fc917baa56973cb5786f585
marisol <marisol.develop@gmail.com>
2022-04-28 20:01:07 +0900

    [feat]: 업무 완료 후 문구 출력하는 기능 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -26,5 +26,7 @@ struct Bank {
             
             bankManager.work(for: customer)
         }
+        let workTime = Double(numberOfCustomer) * 0.7
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomer)명이며, 총 업무시간은 \(String(format: "%.2f", workTime))")
     }
 }
