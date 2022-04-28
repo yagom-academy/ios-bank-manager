7174dad3906ad9a5805a812d41ce0cb305fc87e0
Grumpy <sewoong2890@gmail.com>
2022-04-28 19:33:00 +0900

    [feat]: Bank 타입의 프로퍼티와 생성자 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 struct Bank {
+    let numberOfBankManager: Int
+    let numberOfCustomer: Int
+    let customerQueue: Queue = Queue<Customer>()
+    let bankManager: BankManager = BankManager()
     
+    init(numberOfBankManager: Int, numberOfCustomer: Int) {
+        self.numberOfBankManager = numberOfBankManager
+        self.numberOfCustomer = numberOfCustomer
+    }
 }
