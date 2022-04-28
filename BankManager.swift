936bf1dee02f32c80a3b14123f84b160cc552e73
Quokkaa <rktrkdml@gmail.com>
2022-04-28 19:36:01 +0900

    💎feat : BankManager 타입 및 selectMenu 메서드 구현

--- BankManager.swift
+++ BankManager.swift
@@ -3,3 +3,25 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
+struct BankManager {
+    func selectMenu() {
+        print("""
+                1 : 은행개점
+                2 : 종료
+                입력 :
+              """, terminator: "")
+        
+        guard let input = readLine() else {
+            return
+        }
+        
+        switch input {
+        case "1":
+            print("업무시작")
+        case "2":
+            return
+        default:
+            return selectMenu()
+        }
+    }
+}
