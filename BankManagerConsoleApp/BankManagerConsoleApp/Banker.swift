b06a5bc18b80c4974db3b2dde338421e54ad069b
Red <seryprk@gmail.com>
2022-04-28 19:39:36 +0900

    ✨FEAT : distributeCustomer 메서드 구현, startWork 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
@@ -2,31 +2,18 @@
 //  Banker.swift
 //  BankManagerConsoleApp
 //
-//  Created by 김태훈 on 2022/04/28.
+//  Created by OneTool, Red on 2022/04/28.
 //
 
 import Foundation
 
 class Banker {
-    var customber: Customer?
-    var numberOfOwnCustomer: Int = 0
+    var customer: Customer?
     
-    func printWorkStart(_ number: Int) {
-        print("\(number)번 고객 업무 시작")
-    }
-    
-    func printWorkEnd(_ number: Int) {
-        print("\(number)번 고객 업무 종료")
-    }
-    
-    func work() -> Double {
-        var workTime: Double = 0
-        for number in 1...numberOfOwnCustomer {
-            printWorkStart(number)
-            usleep(7000)
-            printWorkEnd(number)
-            workTime += 0.7
-        }
-        return workTime
+    func work() {
+        print("\(String(describing: customer?.numberTicekt))번 고객 업무 시작")
+        sleep(1)
+        print("\(String(describing: customer?.numberTicekt))번 고객 업무 종료")
+        customer = nil
     }
 }
