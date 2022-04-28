ec817316fbbf4a3169a0df65b74dc990fccf4147
Grumpy <sewoong2890@gmail.com>
2022-04-28 19:25:49 +0900

    [feat]: BankManager 구조체 내부의 work 함수 구현

--- BankManager.swift
+++ BankManager.swift
@@ -1,1 +1,9 @@
 import Foundation
+
+struct BankManager {
+    func work(for customer: Customer) {
+        print("\(customer.number)번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: 0.7)
+        print("\(customer.number)번 고객 업무 완료")
+    }
+}
