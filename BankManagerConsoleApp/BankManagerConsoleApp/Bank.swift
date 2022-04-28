bb56d3cde3c36fc9fdbb51dbe00954e81c528cb9
JoSH0318 <jsh9870@naver.com>
2022-04-29 00:28:17 +0900

    ✨[feat]: Bank 클래스 내부, executeBankWork() 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -10,3 +10,11 @@ import Foundation
 final class Bank {
     private var queue = Queue(list: LinkedList<Client>())
     private let clerk = DispatchQueue(label: "clerk")
+    
+    func executeBankWork() {
+        let work = DispatchWorkItem {
+            self.serveClients()
+        }
+        receiveClients()
+        clerk.sync(execute: work)
+    }
