ab46656ed07c132cb497f0811103ad53a262e737
dnwhd0112 <dnwhd0112@naver.com>
2022-04-28 22:08:20 +0900

    feat: work 메소드 구현 및 내부에서 사용할 String값을  BankClerkConstant 정의

--- BankManagerConsoleApp/Model/Bank/BankClerk.swift
+++ BankManagerConsoleApp/Model/Bank/BankClerk.swift
@@ -8,13 +8,29 @@
 import Foundation
 
 struct BankClerk {
+    private enum BankClerkConstant {
+        static let startWorkMessage = "고객 업무 시작"
+        static let finishedWorkMessage = "고객 업무 완료"
+    }
     unowned private var bank: Bank
     private let spendingTimeForAClient: Double
-
+    
     init(bank: Bank, spendingTimeForAClient: Double) {
-            self.bank = bank
-            self.spendingTimeForAClient = spendingTimeForAClient
+        self.bank = bank
+        self.spendingTimeForAClient = spendingTimeForAClient
+    }
+    
+    func work() {
+        while true {
+            guard let client = bank.allocateCustomer() else {
+                return
+            }
+            
+            print("\(client.waitingNumber) \(BankClerkMessage.startWorkMessage)")
+            let usecondsTimeForAClient = useconds_t(spendingTimeForAClient * 1000000)
+            usleep(usecondsTimeForAClient)
+            bank.updateWorkData()
+            print("\(client.waitingNumber) \(BankClerkMessage.finishedWorkMessage)")
         }
-    // 추후구현
-    func work() {}
+    }
 }
