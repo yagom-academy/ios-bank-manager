b703832a26e95700461d68071856bd0999105cc5
malrang <ruehd7924@gmail.com>
2022-04-29 01:59:31 +0900

    ✨[feat]: BankManager 구조체 구현
    *Constant enum 구현
    *bank 프로퍼티 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,14 @@
 //
 
 import Foundation
+
+struct BankManager {
+    private enum Constant {
+        static let open = "1 : 은행개점"
+        static let close = "2 : 종료"
+        static let input = "입력 : "
+        static let empty = ""
+    }
+    
+    private let bank = Bank()
+    
