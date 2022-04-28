e44ab4442395daa4deefe75597b88dd5f4708700
Lingo <llingo@kakao.com>
2022-04-28 18:39:46 +0900

    feat: BankQueue의 개수 제한 및 enqueue Count 기능 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/BankQueue/BankQueue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankQueue/BankQueue.swift
@@ -9,6 +9,16 @@ import Foundation
 
 struct BankQueue<T> {
   private var list = LinkedList<T>()
+  private var count: Int = .zero
+  private let limit: Int
+
+  init(limit: Int) {
+    self.limit = limit
+  }
+
+  var isFull: Bool {
+    return limit == count
+  }
 
   var isEmpty: Bool {
     return list.isEmpty
@@ -19,10 +29,13 @@ struct BankQueue<T> {
   }
 
   mutating func enqueue(_ data: T?) {
+    guard !isFull else { return }
+    count += 1
     list.append(data)
   }
 
   mutating func dequeue() -> T? {
+    if count > Int.zero { count -= 1 }
     return list.removeFirst()
   }
 
