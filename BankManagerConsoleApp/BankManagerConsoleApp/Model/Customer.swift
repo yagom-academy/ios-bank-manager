f7e7e7ed923924dcc7386881f1e70b638db4e7e8
doogie97 <chris_97@naver.com>
2022-04-28 18:15:51 +0900

    💎feat : Customer 구조체 생성
    bringQueueNumber() 메서드 또한 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by 쿼카, 두기 on 2022/04/28.
+//
+struct Customer {
+    private var number: Int
+    
+    func bringQueueNumber() -> Int {
+        return number
+    }
+}
