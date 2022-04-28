f59148d48fb37b1a8b2ed58d7f91a263fc6758f7
JoSH0318 <jsh9870@naver.com>
2022-04-29 04:22:03 +0900

    ♻️[refactor]: Bank의 코드를 BankClerk로 코드분리
    *그에 따른 로직 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -7,46 +7,35 @@
 
 import Foundation
 
-final class Bank {
-    enum Constant {
-        static let startMessage = "번 고객 업무 시작"
-        static let endMessage = "번 고객 업무 종료"
-        static let workTime = 0.7
-    }
+struct Bank {
+    private let clerk: Workable = BankClerk()
     private var queue = Queue(list: LinkedList<Client>())
-    private let clerk = DispatchQueue(label: "clerkDispatchQueue")
+    private var numberOfClients = 0
     
-    func executeBankWork() {
-        let work = DispatchWorkItem {
-            self.serveClients()
-        }
-        receiveClients()
-        clerk.sync(execute: work)
-    }
-    
-    func receiveClients() {
-        for order in 1...Int.random(in: 10...30) {
-            queue.enqueue(Client(order))
-        }
-    }
-    
-    func serveClients() {
+    mutating func executeBankWork() {
         let start = CFAbsoluteTimeGetCurrent()
-        let numberOfClients = queue.count
-        while !queue.isEmpty {
-            dealWithClient()
+
+        receiveClients()
+        DispatchQueue.global().sync {
+            serveClients()
         }
+        
         let interval = CFAbsoluteTimeGetCurrent() - start
         let totalTime = String(format: "%.2f", interval)
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClients)명이며, 총 업무시간은 \(totalTime)입니다.")
     }
     
-    func dealWithClient() {
-        guard let client = queue.dequeue() else {
-            return
+    private mutating func receiveClients() {
+        for order in 1...Int.random(in: 10...30) {
+            queue.enqueue(Client(order))
         }
-        print("\(client.orderNumber)\(Constant.startMessage)")
-        Thread.sleep(forTimeInterval: Constant.workTime)
-        print("\(client.orderNumber)\(Constant.endMessage)")
+        numberOfClients = queue.count
+    }
+    
+    private mutating func serveClients() {
+        while !queue.isEmpty {
+            let client = queue.dequeue()
+            clerk.deal(with: client)
+        }
     }
 }
