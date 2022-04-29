efc2d14d4d6f605715c1f2a584d671b678fb67e8
Lingo <llingo@kakao.com>
2022-04-29 21:30:47 +0900

    feat: Bank의 close 메서드 및 네임스페이스 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 final class Bank {
+  private enum Constants {
+    static let closed = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 %d명이며, 총 업무시간은 %.2f초입니다."
+  }
+
   private var clientQueue: BankQueue<Client> = BankQueue(limit: 50)
   private var totalClientCount: Int = .zero
   private var totalExecuteTime: Double = .zero
@@ -26,5 +30,9 @@ final class Bank {
       let time = BankTask().execute(client)
       totalExecuteTime += time
     }
+  }
+
+  private func close() {
+    print(String(format: Constants.closed, totalClientCount, totalExecuteTime))
   }
 }
