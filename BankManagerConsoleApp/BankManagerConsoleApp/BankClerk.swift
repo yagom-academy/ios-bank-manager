6826b050c7d32ffccd52b228320697940a448f11
saafaaari <111dltldnjs@naver.com>
2022-04-30 00:09:51 +0900

    ♻️[refactor]: SingleQueue와 Group을 이용하여 sleep메소드 대체

--- BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 struct BankClerk {
+    let taskQueue = DispatchQueue(label: "taskQueue")
+    let taskGroup = DispatchGroup()
+    
     func processTask(for customer: Customer) {
+        startTask(customer)
+        taskGroup.enter()
+        taskQueue.asyncAfter(deadline: .now() + 0.7) {
+            finishTask(customer)
+            taskGroup.leave()
+        }
+        taskGroup.wait()
+    }
+    
+    private func startTask(_ customer: Customer) {
         print("\(customer.numberTicket)번 고객 업무 시작")
-        Thread.sleep(forTimeInterval: 0.7)
+    }
+    
+    private func finishTask(_ customer: Customer) {
         print("\(customer.numberTicket)번 고객 업무 완료")
     }
 }
