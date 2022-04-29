47c35cdc0fd10c38f4cacd27e5c3c24855f427a8
Lingo <llingo@kakao.com>
2022-04-29 21:14:57 +0900

    feat: BankTask 타입 및 execute 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/BankTask.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankTask.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct BankTask {
+  func execute(_ client: Client) -> Double {
+    let clientNumber = client.waitingNumber
+    let time = 0.7
+
+    print("\(clientNumber)번 고객 업무 시작")
+    Thread.sleep(forTimeInterval: time)
+    print("\(clientNumber)번 고객 업무 완료")
+    return time
+  }
+}
