53f538769d68958abc40407f74e24cfd7b46415b
doogie97 <chris_97@naver.com>
2022-04-29 11:29:32 +0900

    💎feat : bankClerk 생성 및 sendCustomerToClerk 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -5,6 +5,7 @@
 //  Created by 쿼카, 두기 on 2022/04/28.
 
 struct Bank {
+    let bankClerk = BankClerk()
     var customerQueue = Queue<Customer>()
     var totalCustomerCount = 0
     
@@ -17,4 +18,16 @@ struct Bank {
     func printCloseMessage() {
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerCount)명이며, 총 업무시간은 XX초입니다.")
     }
+    
+    mutating func sendCustomerToClerk() {
+        while customerQueue.isEmpty == false {
+            guard let customer = customerQueue.dequeue() else {
+                return
+            }
+            
+            bankClerk.work(customer: customer)
+            totalCustomerCount += 1
+        }
+    }
+    
 }
