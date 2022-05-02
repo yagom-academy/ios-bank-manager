27ad88186e48aa38dcd61428b6c0c238826c8df2
Red <seryprk@gmail.com>
2022-05-02 18:49:51 +0900

    ♻️REFACTOR : BankManager 타입 수정

--- BankManager.swift
+++ BankManager.swift
@@ -8,60 +8,43 @@
 import Foundation
 
 final class BankManager {
-    var banker = Banker()
-    private var customers = Queue(listType: DoubleStack<Customer>())
-    private var numberOfCustomer: Int = Const.defaultCount
-    private var wholeWorkTime: Double = Double.zero
-    
-    init(numberOfBanker: Int = Const.defaultBankerCount) {
-        initialize(numberOfBanker: numberOfBanker)
-    }
+    var bank: Bank = Bank(numberOfBankers: 3)
     
     func manageBanker() {
-        let semaphore = DispatchSemaphore(value: 3)
+        let banker = Banker()
         let workGroup = DispatchGroup()
-        for _ in 1...self.numberOfCustomer {
-                semaphore.wait()
-                let custormer = self.customers.deQueue()
-                DispatchQueue.global().async(group: workGroup) {
-                print(Thread.current)
-                self.banker.customer = custormer
-                self.banker.work()
-                self.wholeWorkTime += Const.workTimeForCustomer
-                semaphore.signal()
-                }
+        for _ in 1...bank.numberOfCustomer {
+            bank.numberOfBankers.wait()
+            let custormer = bank.customers.deQueue()
+            DispatchQueue.global().async(group: workGroup) {
+                banker.work(customer: custormer)
+                self.bank.numberOfBankers.signal()
             }
+            self.bank.wholeWorkTime += Const.workTimeForCustomer
+        }
         workGroup.wait()
-        }
-       
-    
-    private func initialize(numberOfBanker: Int = Const.defaultBankerCount) {
-        numberOfCustomer = Int.random(in: Const.customerRange)
-        for numberTicekt in Const.startCount...numberOfCustomer {
-            customers.enQueue(data: Customer(numberTicekt: numberTicekt))
-        }
     }
     
-    func openBank() -> Bool {
+    func openBank() {
         print(Const.startBankSelect, terminator: Const.blank)
         guard let userChoice = readLine() else {
             print(Const.wrongInput)
-            return true
+            return
         }
-        
+        choose(userChoice: userChoice)
+    }
+    
+    private func choose(userChoice: String) {
         switch userChoice {
         case Const.OpeningInput:
             manageBanker()
             print(Const.finishWork)
-            print("오늘 업무를 처리한 고객은 총 \(numberOfCustomer)명이며, 총 업무 시간은 \(String(format: Const.twoDecimal, wholeWorkTime))초 입니다.")
-            wholeWorkTime = Double.zero
+            print("오늘 업무를 처리한 고객은 총 \(bank.numberOfCustomer)명이며, 총 업무 시간은 \(String(format: Const.twoDecimal, bank.wholeWorkTime))초 입니다.")
+            bank.wholeWorkTime = Double.zero
         case Const.exitInput:
-            return false
+            return
         default:
             print(Const.wrongInput)
         }
-        
-        initialize()
-        return true
     }
 }
