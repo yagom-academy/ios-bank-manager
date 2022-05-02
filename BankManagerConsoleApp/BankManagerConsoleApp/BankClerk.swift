12a65bc4f23c0fbb58a0e51addbd18fc426180cf
saafaaari <111dltldnjs@naver.com>
2022-05-02 12:03:34 +0900

    ✨[feat]: 업무별 업무 시간을 반환해주는 sortTaskTime()메소드 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
@@ -12,13 +12,26 @@ struct BankClerk {
     let taskGroup = DispatchGroup()
     
     func processTask(for customer: Customer) {
+        guard let taskTime = sortTaskTime(customer) else { return }
+        
         startTask(customer)
         taskGroup.enter()
-        taskQueue.asyncAfter(deadline: .now() + 0.7) {
+        taskQueue.asyncAfter(deadline: .now() + taskTime) {
             finishTask(customer)
             taskGroup.leave()
         }
         taskGroup.wait()
+    }
+    
+    private func sortTaskTime(_ customer: Customer) -> Double? {
+        guard let task = customer.task else { return nil }
+        
+        switch task {
+        case .deposit:
+            return 0.7
+        case .loan:
+            return 1.1
+        }
     }
     
     private func startTask(_ customer: Customer) {
