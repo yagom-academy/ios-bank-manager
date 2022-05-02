bb81169a6e363fda4df87b976df2ae227bc5b556
Quokkaa <rktrkdml@gmail.com>
2022-05-02 10:27:49 +0900

    ♻️refactor : 네이밍 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank/BankClerk.swift
@@ -8,15 +8,15 @@ import Foundation
 
 struct BankClerk {
     private enum Constant {
-        static let startWork = "번 고객 업무 시작"
-        static let endWork = "번 고객 업무 시작"
+        static let workStartText = "번 고객 업무 시작"
+        static let workEndText = "번 고객 업무 시작"
         static let workTime = 0.7
     }
     
     func work(customer: Customer) {
-        print("\(customer.number) \(Constant.startWork)")
+        print("\(customer.number) \(Constant.workStartText)")
         Thread.sleep(forTimeInterval: Constant.workTime)
-        print("\(customer.number) \(Constant.endWork)")
+        print("\(customer.number) \(Constant.workEndText)")
     }
 }
 
