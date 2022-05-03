dbce24ba5a378453f35bdb912a859c529bddeea7
marisol <marisol.develop@gmail.com>
2022-05-03 21:46:05 +0900

    [feat]: TaskType의 연산 프로퍼티 workTime 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/TaskType.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/TaskType.swift
@@ -3,4 +3,13 @@ import Foundation
 enum TaskType: String, CaseIterable {
     case deposit = "예금업무"
     case loan = "대출업무"
+    
+    var workTime: Double {
+        switch self {
+        case .deposit:
+            return 0.7
+        case .loan:
+            return 1.1
+        }
+    }
 }
