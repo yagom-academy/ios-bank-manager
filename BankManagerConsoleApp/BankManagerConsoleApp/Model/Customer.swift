e0afb0fa25403a8a7ca05943fedeca329a2d9d5f
marisol <marisol.develop@gmail.com>
2022-05-03 21:49:45 +0900

    [feat]: Customer 구조체에 taskType 프로퍼티 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Customer.swift
@@ -2,8 +2,10 @@ import Foundation
 
 struct Customer {
     let number: Int
+    let taskType: TaskType?
     
     init(number: Int) {
         self.number = number
+        self.taskType = TaskType.allCases.randomElement()
     }
 }
