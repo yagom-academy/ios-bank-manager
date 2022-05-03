a6632863c25f99c2f3356a9e01c246eb232d3218
JoSH0318 <jsh9870@naver.com>
2022-05-03 17:14:12 +0900

    ✏️[correct]: Workable, semaphore 프로퍼티 이름 변경
    * semaphore -> clerksCountByWork

--- BankManagerConsoleApp/BankManagerConsoleApp/Workable.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Workable.swift
@@ -9,7 +9,7 @@ import Foundation
 
 protocol Workable {
     var workType: WorkType { get }
-    var semaphore: DispatchSemaphore { get }
+    var clerksCountByWork: DispatchSemaphore { get }
     
     func deal(with client: Client?)
 }
