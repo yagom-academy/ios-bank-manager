c69ff66f094cec64e46504d9df368c9ef2613639
Taeangel <wngks2050@gmail.com>
2022-05-03 11:35:30 +0900

    refeat: Clerkalbe 프로토콜을 타입메서드로 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-enum ClerkCount {
+private enum ClerkCount {
     static let deposit = 2
     static let loan = 1
 }
@@ -14,4 +14,15 @@ enum ClerkCount {
 enum BankClerk {
     static let deposit = DispatchSemaphore(value: ClerkCount.deposit)
     static let loan = DispatchSemaphore(value: ClerkCount.loan)
+    
+    static func work(client: Client, group: DispatchGroup) {
+        let semaphore = client.task.clerk
+        DispatchQueue.global().async(group: group) {
+            semaphore.wait()
+            print("\(client.waitingNumber)번 고객 \(client.task.rawValue)업무 시작")
+            Thread.sleep(forTimeInterval: client.task.time)
+            print("\(client.waitingNumber)번 고객 \(client.task.rawValue)업무 완료")
+            semaphore.signal()
+        }
+    }
 }
