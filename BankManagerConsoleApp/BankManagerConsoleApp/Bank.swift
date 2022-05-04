9e4756aff7748f293f72da2961c3e49fe68ab16b
onetool <xogns8696@gmail.com>
2022-05-04 14:21:39 +0900

    ♻️REFACTOR : 함수 재배치 및 private 접근제어자 선언

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -31,26 +31,18 @@ final class Bank: Manageable {
         wholeWorkTime = CFAbsoluteTimeGetCurrent() - startTime
     }
     
-    func makeSemaphore() -> [BankTask: DispatchSemaphore] {
-        var semaphores: [BankTask: DispatchSemaphore] = [:]
-        for task in BankTask.allCases {
-            semaphores[task] = .init(value: task.numberOfBankers)
-        }
-        return semaphores
-    }
-    
     func manageBanker() {
         let banker = Banker()
         let workGroup = DispatchGroup()
-        let semaphore = makeSemaphore()
+        let semaphores = makeSemaphore()
         for _ in Const.startCount...numberOfCustomer {
             guard let custormer = customers.deQueue(), let task = custormer.task else {
                 return
             }
-            semaphore[task]?.wait()
+            semaphores[task]?.wait()
             DispatchQueue.global().async(group: workGroup) {
                 banker.work(customer: custormer)
-                semaphore[task]?.signal()
+                semaphores[task]?.signal()
             }
         }
         workGroup.wait()
@@ -60,6 +52,14 @@ final class Bank: Manageable {
         let report = "오늘 업무를 처리한 고객은 총 \(numberOfCustomer)명이며, 총 업무 시간은 \(String(format: Const.twoDecimal, wholeWorkTime))초 입니다."
         reset()
         return report
+    }
+    
+    private func makeSemaphore() -> [BankTask: DispatchSemaphore] {
+        var semaphores: [BankTask: DispatchSemaphore] = [:]
+        for task in BankTask.allCases {
+            semaphores[task] = .init(value: task.numberOfBankers)
+        }
+        return semaphores
     }
     
     private func reset(numberOfBankers: Int = Const.defaultBankerCount) {
