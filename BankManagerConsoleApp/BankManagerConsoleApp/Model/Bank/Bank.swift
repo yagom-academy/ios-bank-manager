4a6782a36c13c304599b3a8a07cf8e43c921f8f8
Quokkaa <rktrkdml@gmail.com>
2022-05-04 11:22:11 +0900

    ♻️refactor : totalCustomerCount 초기화 구문 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank/Bank.swift
@@ -65,8 +65,9 @@ struct Bank {
         }
     }
     
-    private func printCloseMessage() {
+    private mutating func printCloseMessage() {
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerCount)명이며, 총 업무시간은 \(workingTime)초입니다.")
+        totalCustomerCount = Int.zero
     }
     
     func timeCheck(_ block: () -> Void) -> String {
