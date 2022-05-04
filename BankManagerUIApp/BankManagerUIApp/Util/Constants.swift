16c2034c93a2be455aa84dfff40ef59ddf7ae294
Mino777 <jomino7777@gmail.com>
2022-05-04 22:04:58 +0900

    chore: 누락된 파일 복구

---
+++ BankManagerUIApp/BankManagerUIApp/Util/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  BankManagerUIApp
+//
+//  Created by 조민호 on 2022/05/04.
+//
+
+enum Constants {
+    static let numberOfDepositBankClerks: Int = 2
+    static let numberOfLoanBankClerks: Int = 1
+}
