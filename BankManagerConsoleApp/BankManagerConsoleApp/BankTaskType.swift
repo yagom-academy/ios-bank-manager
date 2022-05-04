4031c0324e5de8452334d18ebc1338325b6c1d51
Lingo <llingo@kakao.com>
2022-05-04 13:59:59 +0900

    feat: BankTaskType 프로토콜의 요소 추가
    semaphore 프로퍼티 및 execute 메서드 정의를 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/BankTaskType.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankTaskType.swift
@@ -2,9 +2,12 @@
 //  BankTaskType.swift
 //  BankManagerConsoleApp
 //
-//  Created by Lingo on 2022/05/04.
+//  Created by Lingo, Cathy on 2022/05/04.
 //
 
 import Foundation
 
-protocol BankTaskType {}
+protocol BankTaskType {
+  var semaphore: DispatchSemaphore { get }
+  func execute(_ client: Client)
+}
