d6c69f377ba446e193cf0dfd55fa4ab971d02425
Donnie <seo_17@naver.com>
2022-05-04 17:19:36 +0900

    ✨[feat]: OperationQueue를 분류하는 start() 메서드 구현

---
+++ BankManagerUIApp/BankManagerUIApp/Model/Customer.swift
@@ -0,0 +1,51 @@
+//
+//  Customer.swift
+//  BankManagerUIApp
+//
+//  Created by Donnie, Safari on 2022/05/04.
+//
+
+import Foundation
+
+struct Customer {
+    
+    enum TaskList: CaseIterable {
+        case deposit
+        case loan
+        
+        var information: String {
+            switch self {
+            case .deposit:
+                return "예금"
+            case .loan:
+                return "대출"
+            }
+        }
+        
+        var taskTime: Double {
+            switch self {
+            case .deposit:
+                return 0.7
+            case .loan:
+                return 1.1
+            }
+        }
+        
+        func start(deposit: OperationQueue, loan: OperationQueue) -> OperationQueue {
+            switch self {
+            case .deposit:
+                return deposit
+            case .loan:
+                return loan
+            }
+        }
+    }
+    
+    let numberTicket: Int
+    let task: TaskList?
+    
+    init(numberTicket: Int) {
+        self.numberTicket = numberTicket
+        self.task = TaskList.allCases.randomElement()
+    }
+}
