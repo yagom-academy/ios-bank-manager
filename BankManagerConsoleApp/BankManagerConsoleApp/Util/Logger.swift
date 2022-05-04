83e7a968810e660f79eed7ad8fd858ad31524d91
Lingo <llingo@kakao.com>
2022-05-04 20:35:15 +0900

    style: Swift Lint에 의한 불필요한 띄어쓰기 제거

--- BankManagerConsoleApp/BankManagerConsoleApp/Util/Logger.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Util/Logger.swift
@@ -14,11 +14,11 @@ final class Logger {
     static let closed = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 %d명이며, 총 업무시간은 %.2f초입니다."
     static let menu = "1 : 은행개점\n2 : 종료\n입력 : "
   }
-  
+
   func log(_ messageType: MessageType) {
     print(messageType.description)
   }
-  
+
   func log(_ messageType: MessageType, terminator: String) {
     print(messageType.description, terminator: terminator)
   }
@@ -30,7 +30,7 @@ extension Logger {
     case taskFinish(number: Int, taskName: String)
     case closed(totalClientCount: Int, totalExecuteTime: Double)
     case menu
-    
+
     var description: String {
       switch self {
       case let .taskStart(number, taskName):
