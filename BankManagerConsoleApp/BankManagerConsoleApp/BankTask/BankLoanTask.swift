11d83159d3832d8935c085533f314db88c00ef4d
Lingo <llingo@kakao.com>
2022-05-04 16:15:28 +0900

    feat: BankLoanTask BankTaskType 프로토콜 채택 후 기능 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/BankTask/BankLoanTask.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankTask/BankLoanTask.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct BankLoanTask: BankTaskType {
+  private enum Constants {
+    static let time = 1.1
+    static let start = "%d번 고객 대출업무 시작"
+    static let finished = "%d번 고객 대출업무 완료"
+  }
+
+  private(set) var semaphore: DispatchSemaphore
+
+  init(_ numberOfClerk: Int) {
+    semaphore = DispatchSemaphore(value: numberOfClerk)
+  }
+
+  func execute(_ client: Client) {
+    let clientNumber = client.waitingNumber
+    let time = Constants.time
+
+    print(String(format: Constants.start, clientNumber))
+    Thread.sleep(forTimeInterval: time)
+    print(String(format: Constants.finished, clientNumber))
+  }
+}
