e8790db20fc73920fe968c09aa10480730b70de8
onetool <xogns8696@gmail.com>
2022-05-05 17:16:33 +0900

    ✨FEAT : 스토리보드 삭제 및 테스트

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let test = UILabel()
+        view.backgroundColor = .white // 배경색
+        view.addSubview(test)
+        test.text = "text" // test를 위해서 출력할 라벨
+        test.translatesAutoresizingMaskIntoConstraints = false
+        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
-
-
 }
 
