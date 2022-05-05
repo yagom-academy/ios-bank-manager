53970c07e301e803689c30c80b440ccc0a69d021
Quokkaa <rktrkdml@gmail.com>
2022-05-05 14:59:58 +0900

    💎feat : 고객 추가, 초기화 Button Target추가

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -6,12 +6,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    lazy var bankView = BankView(frame: view.bounds)
+final class ViewController: UIViewController {
+    private lazy var bankView = BankView(frame: view.bounds)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view = bankView
+        setButtons()
+    }
+    
+    private func setButtons() {
+        bankView.addCustomerButton.addTarget(self, action: #selector(addCustomer), for: .touchUpInside)
+        bankView.resetButton.addTarget(self, action: #selector(setInitialState), for: .touchUpInside)
+    }
+    
+    @objc private func addCustomer() {
+        print("고객추가됨")
+    }
+    
+    @objc private func setInitialState() {
+        print("초기화됨")
     }
 }
 
