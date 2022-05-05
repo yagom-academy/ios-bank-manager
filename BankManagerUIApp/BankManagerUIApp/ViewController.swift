b3aa47d572d79f5c34656ee769233ca2777a5c02
marisol <marisol.develop@gmail.com>
2022-05-05 16:29:12 +0900

    [feat]: baseVerticalStackView 생성 및 레이아웃 설정

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -8,22 +8,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let baseVerticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let safeArea = view.safeAreaLayoutGuide
+        view.addSubview(baseVerticalStackView)
+        baseVerticalStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        baseVerticalStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        baseVerticalStackView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        baseVerticalStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        baseVerticalStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
     }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
 }
 
