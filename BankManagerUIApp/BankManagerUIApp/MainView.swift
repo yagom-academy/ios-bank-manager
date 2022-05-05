2c57e8baf7204d869de220be3aa2a0b9de06b887
Red <seryprk@gmail.com>
2022-05-05 20:48:07 +0900

    ✨FEAT : 스크롤뷰 구현

--- BankManagerUIApp/BankManagerUIApp/MainView.swift
+++ BankManagerUIApp/BankManagerUIApp/MainView.swift
@@ -9,14 +9,23 @@ import UIKit
 
 class MainView: UIView {
     lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [buttonStackView, labelStackView])
+        let stackView = UIStackView(arrangedSubviews: [buttonStackView, workingTimeLabel, labelStackView])
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.alignment = .fill
-        stackView.distribution = .fillEqually
+        stackView.distribution = .fill
         
         return stackView
+    }()
+    
+    let workingTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "업무시간 - 03:00:00"
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.textColor = .black
+        return label
     }()
     
     let addTenCustomerButton: UIButton = {
@@ -76,10 +85,71 @@ class MainView: UIView {
         return stackView
     }()
     
+    lazy var customerScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.addSubview(customerStackView)
+        
+        customerStackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        customerStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        customerStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        customerStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        customerStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        
+        return scrollView
+    }()
+    
+    lazy var customerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [watingStackView, workingStackView])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    func createCustomerLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "5 - 예금"
+        label.textColor = .black
+        return label
+    }
+    
+    lazy var watingStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        for _ in 1...50 {
+            stackView.addArrangedSubview(createCustomerLabel())
+        }
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    lazy var workingStackView: UIStackView = {
+        let stackView = UIStackView()
+        for _ in 1...50 {
+            stackView.addArrangedSubview(createCustomerLabel())
+        }
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
         self.addSubview(mainStackView)
+        self.addSubview(customerScrollView)
         
         setUpAutoLayout()
     }
@@ -93,6 +163,12 @@ class MainView: UIView {
         mainStackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         mainStackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         mainStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        customerScrollView.topAnchor.constraint(equalTo: mainStackView.bottomAnchor).isActive = true
+        customerScrollView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        customerScrollView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        customerScrollView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
     }
     
     
