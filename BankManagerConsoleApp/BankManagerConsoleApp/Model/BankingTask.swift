614615d1d396eb9d91388a65a49d4da216ca0d2c
marisol <marisol.develop@gmail.com>
2022-05-05 12:23:53 +0900

    [refactor]: WorkTime을 BankingTask의 nested type으로 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankingTask.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankingTask.swift
@@ -13,3 +13,10 @@ enum BankingTask: String, CaseIterable {
         }
     }
 }
+
+extension BankingTask {
+    private enum WorkTime {
+        static let deposit: Double = 0.7
+        static let loan: Double = 1.1
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Namespace/WorkTime.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum WorkTime {
-    static let deposit: Double = 0.7
-    static let loan: Double = 1.1
-}
