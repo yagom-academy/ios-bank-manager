cad46265da15f8003a9c4b11c0d7df7eda9bfe85
marisol <marisol.develop@gmail.com>
2022-05-08 00:03:40 +0900

    [feat]: ViewController의 Timer 관련 메서드를 BankingTimer로 이동

--- BankManagerUIApp/BankManagerUIApp/Model/BankingTimer.swift
+++ BankManagerUIApp/BankManagerUIApp/Model/BankingTimer.swift
@@ -21,4 +21,38 @@ struct BankingTimer {
     var currentTimerState: TimerState = .timerNotRunning
     weak var delegate: WorkTimeSendable?
     
+    mutating func startTimer() {
+        startTime = Date()
+        if timer == nil {
+            timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            timer?.schedule(deadline: .now(), repeating: 0.01)
+            timer?.setEventHandler(handler: { [self] in
+                delegate?.updateWorkTime(startTime, durationTime)
+            })
+        }
+        
+        if currentTimerState != .timerRunning {
+            currentTimerState = .timerRunning
+            timer?.resume()
+        }
+    }
+    
+    mutating func pauseTimer() {
+        currentTimerState = .timerPaused
+        timer?.suspend()
+        let timeInterval = Date().timeIntervalSince(self.startTime)
+        durationTime += timeInterval
+    }
+    
+    mutating func clearTimer() {
+        if currentTimerState != .timerRunning {
+            timer?.resume()
+        }
+        
+        timer?.cancel()
+        currentTimerState = .timerNotRunning
+        timer = nil
+        durationTime = 0
+    }
+
 }
