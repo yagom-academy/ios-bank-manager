964c2b262da9522a55cc3f8e02c69fb5af231272
ic_6_user_28 <toskw@naver.com>
2022-06-27 15:00:57 +0900

    add: swiftlint 라이브러리 추가

--- BankManagerUIApp/BankManagerUIApp/SceneDelegate.swift
+++ BankManagerUIApp/BankManagerUIApp/SceneDelegate.swift
@@ -10,7 +10,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -48,4 +47,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
