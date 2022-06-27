40003c69a8faa0c4fa5225465054358767a65b0b
BaekGom <jihunmm02@gmail.com>
2022-06-27 17:44:52 +0900

    feat : UnitTest 메소드 추가

--- BankManagerConsoleApp/BankItemQueueTest/BankItemQueueTest.swift
+++ BankManagerConsoleApp/BankItemQueueTest/BankItemQueueTest.swift
@@ -2,34 +2,90 @@
 //  BankItemQueueTest.swift
 //  BankItemQueueTest
 //
-//  Created by Baek on 2022/06/27.
+//  Created by BaekGom, unchain on 2022/06/27.
 //
 
 import XCTest
 
 class BankItemQueueTest: XCTestCase {
 
+    var sut: BankItemQueue<String>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = BankItemQueue<String>()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_isEmpty호출시_비어있으면_true반환하는지() {
+        //when
+        let result = sut.isEmpty
+        
+        //then
+        XCTAssertTrue(result)
+    }
+    
+    func test_isEmpty호출시_비어있지않으면_False반환하는지() {
+        //given
+        sut.enQueue("1")
+
+        //when
+        let result = sut.isEmpty
+
+        //then
+        XCTAssertFalse(result)
+    }
+    
+    func test_peek함수가_리스트_첫번째_값을가져오는지() {
+        //given
+        sut.enQueue("1")
+        sut.enQueue("2")
+        sut.enQueue("3")
+        let testNumber = "1"
+        
+        //when
+        let peekGetNumber = sut.peek?.data
+
+        //then
+        XCTAssertEqual(testNumber, peekGetNumber)
+    }
+    
+    func test_deQueue_리스트에아무것도없을때_nil반환하는지() {
+        //when
+        let result = sut.deQueue()
+
+        //then
+        XCTAssertNil(result)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func test_deQueue_리스트에서_맨앞에_갚을제거하는지() {
+        //given
+        sut.enQueue("1")
+        sut.enQueue("2")
+        sut.enQueue("3")
+        let testNumber = "1"
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        //when
+        let result = sut.deQueue()
 
+        //then
+        XCTAssertEqual(testNumber, result)
+    }
+    
+    func test_clear_로_리스트모든값제거() {
+        //given
+        sut.enQueue("1")
+        sut.enQueue("2")
+        sut.enQueue("3")
+        
+        //when
+        sut.clear()
+        
+        //then
+        XCTAssertTrue(sut.isEmpty)
+    }
 }
