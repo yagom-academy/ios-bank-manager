2deb43594e30c20e0c3a89c0cfbdd91ae5865137
seohyeon2 <seo_hyeon418@naver.com>
2022-06-27 18:34:25 +0900

    test: CustomerQueue 타입 모든 메서드 테스트

--- BankManagerConsoleApp/BankManagerConsoleApp/CustomerQueue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/CustomerQueue.swift
@@ -1,26 +1,23 @@
-class CustomerQueue<T> {
-    var linkedList: LinkedList<T>?
+final class CustomerQueue<T> {
+    private var linkedList = LinkedList<T>()
     
     func enqueue(data: T) {
-        linkedList?.append(data: data)
+        linkedList.append(data: data)
     }
     
     func dequeue() -> T? {
-        return linkedList?.remove()
+        return linkedList.remove()
     }
     
     func clear() {
-        linkedList?.removeAll()
+        linkedList.removeAll()
     }
     
     func peek() -> T? {
-        return linkedList?.peek()
+        return linkedList.peek()
     }
     
     func isEmpty() -> Bool {
-        guard let linkedList = linkedList else {
-            return true
-        }
         return linkedList.isEmpty()
     }
 }
---
+++ BankManagerConsoleApp/CustomerQueueTest/CustomerQueueTest.swift
@@ -0,0 +1,70 @@
+//
+//  CustomerQueueTest.swift
+//  CustomerQueueTest
+//
+//  Created by seohyeon park on 2022/06/27.
+//
+
+import XCTest
+
+class CustomerQueueTest: XCTestCase {
+    var sut: CustomerQueue<Int>!
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = CustomerQueue()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func test_enqueue가_잘되는지() {
+        // given
+        sut.enqueue(data: 1)
+        // when
+        let result = sut.peek()
+        // then
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_dequeue가_잘되는지() {
+        // given
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        // when
+        _ = sut.dequeue()
+        let result = sut.dequeue()
+        // then
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_dequeue가_잘되는지1() {
+        // when
+        let result = sut.dequeue()
+        // then
+        XCTAssertEqual(result, nil)
+    }
+    
+    func test_clear가_잘되는지() {
+        // given
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        // when
+        sut.clear()
+        // then
+        let result: Int? = nil
+        XCTAssertEqual(result, nil)
+    }
+    
+    func test_isEmpty가_잘되는지() {
+        // given
+        sut.enqueue(data: 1)
+        sut.enqueue(data: 2)
+        // when
+        sut.clear()
+        // then
+        let result = sut.isEmpty()
+        XCTAssertEqual(result, true)
+    }
+}
