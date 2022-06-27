abc8b01092f5b0ba6be45c1635ebd411ffcddf52
ic_6_user_28 <toskw@naver.com>
2022-06-27 15:44:29 +0900

    feat: Node,LinkedList,Queue 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManagerQueue.swift
@@ -0,0 +1,26 @@
+//
+//  BankManagerQueue.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kiwi, Kay on 2022/06/27.
+//
+
+struct BankManagerQueue<T> {
+    private var list = Linkedlist<T>()
+    
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    mutating func enqueue(data: T) {
+        list.append(data: data)
+    }
+    
+    mutating func dequeue() -> T? {
+        list.removeFirst()
+    }
+
+    mutating func clear() {
+        list.removeAll()
+    }
+}
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,41 @@
+//
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kiwon Song on 2022/06/27.
+//
+
+struct Linkedlist<T> {
+    private(set) var head: Node<T>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating func append(data: T) {
+        if head == nil {
+            head = Node(data: data)
+            return
+        }
+        
+        var node = head
+        while node?.next != nil {
+            node = node?.next
+        }
+        node?.next = Node(data: data)
+    }
+    
+    mutating func removeFirst() -> T? {
+        if head == nil {
+            return nil
+        }
+        
+        let firstElement = head?.data
+        head = head?.next
+        return firstElement
+    }
+    
+    mutating func removeAll() {
+        head = nil
+    }
+}
