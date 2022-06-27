b461825e95c2fbeca8486a3aa295bf2693362e4e
unchain123 <unchain123@kakao.com>
2022-06-27 16:57:58 +0900

    feat: isEmpty, peek 변수 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList/BankItemQueue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList/BankItemQueue.swift
@@ -10,6 +10,14 @@ import Foundation
 struct BankItemQueue<T> {
     var linkedList = LinkedList<T>()
     
+    var isEmpty: Bool {
+        return linkedList.isEmpty
+    }
+    
+    var peek: Node<T>? {
+        return linkedList.peek
+    }
+    
     mutating func enQueue(_ data: T) {
         linkedList.append(data: data)
     }
