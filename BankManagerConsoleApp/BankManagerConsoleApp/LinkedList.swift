860d4184169c3c021bfbd9a1b4f4186b8f64de0d
seohyeon2 <seo_hyeon418@naver.com>
2022-06-27 15:40:24 +0900

    feat: LinkedList 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,24 @@
+final class LinkedList<T> {
+    private var head: Node<T>?
+    private var tail: Node<T>?
+
+    init(head: Node<T>? = nil, tail: Node<T>? = nil) {
+        self.head = head
+        self.tail = tail
+    }
+
+    func append(data: T) {
+        if head == nil {
+            head?.data = data
+            head?.next = tail
+        }
+
+        let node = Node(data: data)
+        tail?.next = node
+        node.next = tail
+    }
+
+    func peak() -> T? {
+        return head?.data
+    }
+}
