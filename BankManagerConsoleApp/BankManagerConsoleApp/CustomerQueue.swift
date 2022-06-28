7829aceea0b04365706dac96ec4e0ab0350bc369
seohyeon2 <seo_hyeon418@naver.com>
2022-06-28 11:28:43 +0900

    refactor: CustomerQueue안의 연산 프로퍼티 리턴 키워드 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/CustomerQueue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/CustomerQueue.swift
@@ -2,13 +2,13 @@ final class CustomerQueue<T> {
     private var linkedList = LinkedList<T>()
 
     var isEmpty: Bool {
-        return linkedList.isEmpty
+        linkedList.isEmpty
     }
-    
+
     var peek: T? {
-        return linkedList.peek
+        linkedList.peek
     }
-    
+
     func enqueue(data: T) {
         linkedList.append(data: data)
     }
