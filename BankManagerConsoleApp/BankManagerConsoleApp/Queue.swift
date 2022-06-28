8e36247c60d4835c00f8c74a586df2094b2f22c2
ZZBAE <jebyun2@gmail.com>
2022-06-28 17:44:38 +0900

    feat: Queue와 LinkedList을 사용하는 CustomerQueue 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -9,7 +9,8 @@ protocol Queue {
     associatedtype Element
     var isEmpty: Bool { get }
     var peek: Element? { get }
-    mutating func enqueue(_ element: Element) -> Bool
+    
+    mutating func enqueue(_ element: Element)
     mutating func dequeue() -> Element?
     mutating func clear()
 }
