1cf3c9e3c301c9324dae5edcde96d3ca84455efe
ZZBAE <jebyun2@gmail.com>
2022-06-28 16:37:50 +0900

    feat: 연결리스트에서 사용할 Node 정의

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -0,0 +1,18 @@
+//
+//  Node.swift
+//  BankManagerConsoleApp
+//
+//  Created by 변재은 on 2022/06/28.
+//
+
+import Foundation
+
+class Node<T> {
+    let value: T
+    var next: Node<T>?
+    
+    init(value: T, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
