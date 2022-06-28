4731597efd4515757f26b4599528158502f25b74
yusw10 <yusw10@naver.com>
2022-06-28 13:49:05 +0900

    feat: 양방향 연결 리스트를 위한 노드 작성
    - Node타입 선언

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -0,0 +1,17 @@
+//
+//  Node.swift
+//  BankManagerConsoleApp
+//
+//  Created by 휴, 보리사랑 on 2022/06/28.
+//
+class Node<T> {
+    var data: T?
+    var previousNode: Node?
+    var nextNode: Node?
+    
+    init(previousNode: Node? = nil, _ data: T?, nextNode: Node? = nil) {
+        self.previousNode = previousNode
+        self.data = data
+        self.nextNode = nextNode
+    }
+}
