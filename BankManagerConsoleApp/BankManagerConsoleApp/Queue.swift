7a3ee5165af5e76cbc345d35161fa239eba9a5d8
Gordon Choi <thefirate@gmail.com>
2022-06-28 16:29:27 +0900

    feat: Queue 프로토콜 정의

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -0,0 +1,15 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by Gordon Choi on 2022/06/28.
+//
+
+protocol Queue {
+    associatedtype Element
+    var isEmpty: Bool { get }
+    var peek: Element? { get }
+    mutating func enqueue(_ element: Element) -> Bool
+    mutating func dequeue() -> Element?
+    mutating func clear()
+}
