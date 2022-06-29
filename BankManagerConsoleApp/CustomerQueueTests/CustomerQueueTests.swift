cc6ee178c0db27df3600e2d513ec3fd900bc23d5
OldStarKR <tyr2293@naver.com>
2022-06-29 16:56:00 +0900

    test: CustomerQueue 테스트 코드 작성

--- BankManagerConsoleApp/CustomerQueueTests/CustomerQueueTests.swift
+++ BankManagerConsoleApp/CustomerQueueTests/CustomerQueueTests.swift
@@ -6,20 +6,78 @@
 //
 
 import XCTest
-@testable import BankManagerConsoleApp
 
 class CustomerQueueTests: XCTestCase {
-    var sut: CustomerQueue<Int>
+    var sut: CustomerQueue<Int>!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
         
-        sut = CustomerQueue<Int>
+        sut = CustomerQueue<Int>(list: LinkedList<Int>())
     }
     
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         
         sut = nil
+    }
+    
+    func test_queue가_비어있으면_isEmpty가_true반환() {
+        // when
+        let result = sut.isEmpty
+        
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_queue에_1과2를_enqueue하고_peek을_수행하면_1반환() {
+        // given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        
+        // when
+        let result = sut.peek
+        
+        // then
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_queue에_3과4를_enqueue하고_dequeue하면_3반환() {
+        // given
+        sut.enqueue(3)
+        sut.enqueue(4)
+        
+        // when
+        let result = sut.dequeue()
+        
+        // then
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_queue에_3과4를_enqueue하고_dequeue를_진행한후_peek하면_4반환() {
+        // given
+        sut.enqueue(3)
+        sut.enqueue(4)
+        
+        // when
+        _ = sut.dequeue()
+        let result = sut.peek
+        
+        // then
+        XCTAssertEqual(result, 4)
+    }
+    
+    func test_queue에_값들을넣고_clear하면_리스트가_비워지고_isEmpty가_true반환() {
+        // given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        sut.enqueue(3)
+        
+        // when
+        sut.clear()
+        let result = sut.isEmpty
+        
+        // then
+        XCTAssertTrue(result)
     }
 }
