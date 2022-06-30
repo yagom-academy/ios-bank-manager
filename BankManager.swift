e31a6455568edbacebc24f8581a826a28220a3fa
seohyeon2 <seo_hyeon418@naver.com>
2022-06-30 14:21:58 +0900

    feat: BankManager 구조체 구현

--- BankManager.swift
+++ BankManager.swift
@@ -1,7 +1,15 @@
-//
-//  BankManager.swift
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
+
+struct BankManager {
+    private var customer: Customer
+
+    init(customer: Customer) {
+        self.customer = customer
+    }
+    
+    func work() {
+        print("(customer.name) 고객 업무 시작")
+        Thread.sleep(forTimeInterval: 0.7)
+        print("(customer.name) 고객 업무 완료")
+    }
+}
