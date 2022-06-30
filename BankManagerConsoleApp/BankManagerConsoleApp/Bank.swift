9f465618189f8118cbf7be4d6bc6b33129a3f3eb
seohyeon2 <seo_hyeon418@naver.com>
2022-06-30 16:28:09 +0900

    feat: Bank 구조체 안에 execute, printStartMessage 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -3,7 +3,22 @@ import Foundation
 struct Bank {
     private var customerQueue = CustomerQueue<Customer>(linkedList: LinkedList<Customer>())
 
-    func excuteWork(totalCustomerCount: Int, workTime: TimeInterval) {
+    func execute(totalCustomerCount: Int, workTime: TimeInterval) {
+    menuLoop:
+        while true {
+            printStartMessage()
+
+            switch readLine() {
+            case "1" : executeWork(totalCustomerCount, workTime)
+            case "2" :
+                break menuLoop
+            default :
+                print("잘못된 입력입니다.")
+            }
+        }
+    }
+
+    private func executeWork(_ totalCustomerCount: Int, _ workTime: TimeInterval) {
         var time: Double = 0
         appendCustomerQueue(numbers: totalCustomerCount)
         for _ in 1...totalCustomerCount {
@@ -27,5 +42,14 @@ struct Bank {
     private func printCloseMessage(number: Int, time: Double) {
         let convertedTime = String(format: "%.2f", time)
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(number)명이며, 총 업무시간은 \(convertedTime)초 입니다.")
+    }
+
+    private func printStartMessage() {
+        print(
+        """
+        1 : 은행개점
+        2 : 종료
+        입력 :
+        """, terminator: " ")
     }
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -1,7 +1,10 @@
 //
 //  BankManagerConsoleApp - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import Foundation
+
+let bank = Bank()
+bank.execute(totalCustomerCount: 10, workTime: 0.1)
