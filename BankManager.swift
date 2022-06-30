8db9e00d09f6458acb9b74687eb9003685b6a84c
unchain123 <unchain123@kakao.com>
2022-06-30 20:56:02 +0900

    refactor: BankClerk파일 삭제 및 BankManager로 변경

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,15 @@
 //
 
 import Foundation
+
+struct BankManager {
+    mutating func handleBanking(customerList: BankItemQueue<Customer>) {
+        var customerList = customerList
+        
+        while let completeCuntomer = customerList.deQueue() {
+            print("\(completeCuntomer.bankNumberTicket)번 고객 업무 시작")
+            usleep(700000)
+            print("\(completeCuntomer.bankNumberTicket)번 고객 업무 완료")
+        }
+    }
+}
