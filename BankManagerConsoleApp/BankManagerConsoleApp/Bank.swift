b28b0fe9d82909be1fafe456d19843060fdf7bfd
MinsuJeon <chso294@naver.com>
2022-06-30 23:57:54 +0900

    feat: Client 구조체 구현
    - waitingNumber: 고객의 대기순번을 담는 프로퍼티 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,6 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by 수꿍, 브래드 on 2022/06/30.
+//
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
@@ -0,0 +1,14 @@
+//
+//  Client.swift
+//  BankManagerConsoleApp
+//
+//  Created by 수꿍, 브래드 on 2022/06/30.
+//
+
+struct Client {
+    let waitingNumber: Int
+    
+    init(waitingNumber: Int) {
+        self.waitingNumber = waitingNumber
+    }
+}
