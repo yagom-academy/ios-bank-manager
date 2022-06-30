8f394deb024782caddf8d9ca8f2d0137e1336308
unchain123 <unchain123@kakao.com>
2022-06-30 15:46:46 +0900

    feat: Bank구조체 생성 및 printBankInterface()메서드 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -2,7 +2,18 @@
 //  Bank.swift
 //  BankManagerConsoleApp
 //
-//  Created by 오경식 on 2022/06/30.
+//  Created by unchain, BaekGom on 2022/06/30.
 //
 
 import Foundation
+
+struct Bank {
+    var clerk: BankClerk
+    var watingQueue: BankItemQueue<Customer>
+    var customerCount: Int
+    var businessHour: Double
+    
+    func printBankInterface() {
+        print("1 : 은행개점 \n2 : 종료")
+    }
+}
