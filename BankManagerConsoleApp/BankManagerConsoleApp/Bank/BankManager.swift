db370ab52abb8133f6f1fef8f94c6fda3704ff28
KayAhnDS <dkseotn123@gmail.com>
2022-07-01 11:19:45 +0900

    feat: 고객 응대 DispatchWorkItem 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
@@ -19,3 +19,15 @@ func insertCustomersIntoQueue() {
         lineOfCustomers.enqueue(data: customer)
     }
 }
+
+let handleCustomers = DispatchWorkItem {
+    var totalTimeOfWork: Double = 0.0
+    while !lineOfCustomers.isEmpty {
+        guard let verifiedCustomer = lineOfCustomers.dequeue() else { return }
+        print("\(verifiedCustomer.customerNumber)번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: Time.perPerson.rawValue)
+        print("\(verifiedCustomer.customerNumber)번 고객 업무 완료")
+        totalTimeOfWork += Time.perPerson.rawValue
+    }
+    print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomers)명이며, 총 업무시간은 \(String(format: "%.2f", totalTimeOfWork))초 입니다.")
+}
