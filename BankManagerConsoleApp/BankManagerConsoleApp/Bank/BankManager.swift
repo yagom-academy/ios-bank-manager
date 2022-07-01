cd2eac304f0bbfee0f6327e61cc169f96e950190
ic_6_user_28 <toskw@naver.com>
2022-07-01 11:21:04 +0900

    feat: 실행문 함수 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
@@ -31,3 +31,20 @@ let handleCustomers = DispatchWorkItem {
     }
     print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomers)명이며, 총 업무시간은 \(String(format: "%.2f", totalTimeOfWork))초 입니다.")
 }
+
+var userInput: String?
+let openBank = DispatchWorkItem {
+    repeat {
+        print("1 : 은행 개점")
+        print("2 : 종료")
+        print("입력 : ", terminator: "")
+        userInput = readLine()
+        if let verifiedInput = userInput {
+            if verifiedInput == "1" {
+                generateRandomNumberOfCustomers()
+                insertCustomersIntoQueue()
+                DispatchQueue.global().sync(execute: handleCustomers)
+            }
+        }
+    } while userInput != "2"
+}
