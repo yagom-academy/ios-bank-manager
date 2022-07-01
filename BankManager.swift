133245ebb02728c1045cbe7836a2932a7a373e19
웡빙 <wb277@naver.com>
2022-07-01 11:20:42 +0900

    feat: "BankManager" struct 생성
    고객을 받아 업무처리하는 메서드 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,13 @@
 //
 
 import Foundation
+
+struct BankManager {
+    func handle(customer: Customer) {
+        let customerNumber = customer.number
+        
+        print("\(customerNumber)번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: 0.7)
+        print("\(customerNumber)번 고객 업무 완료")
+    }
+}
