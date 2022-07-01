f9b5eeb2ea8a1342c68cea73a9623e4824cb03a5
finnn1 <tgss3535@gmail.com>
2022-07-01 16:59:53 +0900

    feat: NameSpace 열거형 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/NameSpace.swift
@@ -0,0 +1,15 @@
+//
+//  NameSpace.swift
+//  BankManagerConsoleApp
+//
+//  Created by finnn, bard on 2022/07/01.
+//
+
+enum NameSpace {
+    static let consoleAppStartOutput = """
+                1 : 은행개점
+                2 : 종료
+                입력 :
+                """
+    static let randomCustomerNumberRange = 10...30
+}
