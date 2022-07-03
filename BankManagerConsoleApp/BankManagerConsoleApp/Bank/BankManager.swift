a7af766b7a8b64e6369059ef0ab163ad04784ddf
ic_6_user_28 <toskw@naver.com>
2022-07-03 21:39:48 +0900

    refactor: 네이밍 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
@@ -25,10 +25,10 @@ final class BankManager {
         let handleCustomers = DispatchWorkItem {
             var totalTimeOfWork: Double = 0.0
             while !self.lineOfCustomers.isEmpty {
-                guard let verifiedCustomer = self.lineOfCustomers.dequeue() else { return }
-                print("\(verifiedCustomer.customerNumber)번 고객 업무 시작")
+                guard let customer = self.lineOfCustomers.dequeue() else { return }
+                print("\(customer.customerNumber)번 고객 업무 시작")
                 Thread.sleep(forTimeInterval: ProcessingTime.general.workTime)
-                print("\(verifiedCustomer.customerNumber)번 고객 업무 완료")
+                print("\(customer.customerNumber)번 고객 업무 완료")
                 totalTimeOfWork += ProcessingTime.general.workTime
             }
             print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(self.numberOfCustomers)명이며, 총 업무시간은 \(String(format: "%.2f", totalTimeOfWork))초 입니다.")
@@ -48,8 +48,8 @@ final class BankManager {
             repeat {
                 self.inputMenu()
                 userInput = readLine()
-                if let verifiedInput = userInput {
-                    if verifiedInput == "1" {
+                if let input = userInput {
+                    if input == "1" {
                         self.generateRandomNumberOfCustomers()
                         self.insertCustomersIntoQueue()
                         self.handle()
