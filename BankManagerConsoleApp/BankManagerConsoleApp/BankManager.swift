42f198d36d580235d96b10647eb051c6f08a7a0c
Judy <joo00ee@naver.com>
2022-07-04 22:31:09 +0900

    refactor: 은행의 상태를 BankStatus 열거형으로 관리

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -1,24 +1,32 @@
 struct BankManager {
+    private enum BankStatus: String {
+        case open = "1"
+        case close = "2"
+    }
+    
     func openUpBank() {
         print("1 : 은행개점 \n2 : 종료\n입력 :", terminator: " ")
         
         guard let status = readLine() else { return }
         
-        selectWork(status)
-    }
-
-    private func selectWork(_ status: String) {
-        let bank = Bank(banker: createBanker(3), customer: createCustomerQueue())
-        switch status {
-        case "1":
-            bank.startBanking()
-            openUpBank()
-        case "2":
-            print("업무를 종료합니다.")
-            return
-        default:
+        guard let bankStatus = BankStatus(rawValue: status) else {
             print("입력이 잘못되었습니다.")
             openUpBank()
+            return
+        }
+        
+        selectWork(bankStatus)
+    }
+
+    private func selectWork(_ status: BankStatus) {
+        let bank = Bank(banker: createBanker(1), customer: createCustomerQueue())
+        switch status {
+        case .open:
+            bank.startBanking()
+            openUpBank()
+        case .close:
+            print("업무를 종료합니다.")
+            return
         }
     }
     
