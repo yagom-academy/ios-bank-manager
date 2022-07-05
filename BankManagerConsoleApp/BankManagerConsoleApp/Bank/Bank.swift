a9a4965e50e06577ae4c940f197f15bfaa27d89b
KayAhnDS <dkseotn123@gmail.com>
2022-07-05 16:18:10 +0900

    refactor: 불필요한 프로퍼티 및 메서드 삭제

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -7,10 +7,8 @@
 import Foundation
 
 class Bank {
-    var bankManagers: Array<BankManager> = []
     var lineOfCustomers = BankManagerQueue<Customer>()
     var numberOfCustomers: Int = 0
-    var totalTimeOfWork: Double = 0.0
     
     func inputMenu() {
         print("1 : 은행 개점")
@@ -31,35 +29,21 @@ class Bank {
     }
     
     func distributeTask () {
-        for worker in bankManagers {
-            worker.handleCustomers()
-        }
-    }
     
-    func makeThreeBankers() -> [BankManager] {
-        let banker1 = BankManager(bank: self, specialized: .deposit)
-        let banker2 = BankManager(bank: self, specialized: .deposit)
-        let banker3 = BankManager(bank: self, specialized: .loan)
-        return [banker1, banker2, banker3]
     }
     
     func openBank() {
-        self.bankManagers = makeThreeBankers()
-        DispatchQueue.global().sync {
-            var userInput: String?
-            repeat {
-                self.inputMenu()
-                userInput = readLine()
-                if let input = userInput {
-                    if input == "1" {
-                        self.generateRandomNumberOfCustomers()
-                        self.insertCustomersIntoQueue()
-                        self.distributeTask()
-                        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomers)명이며, 총 업무시간은 \(String(format: "%.2f", totalTimeOfWork))초 입니다.")
-                        totalTimeOfWork = 0.0
-                    }
+        var userInput: String?
+        repeat {
+            self.inputMenu()
+            userInput = readLine()
+            if let input = userInput {
+                if input == "1" {
+                    self.generateRandomNumberOfCustomers()
+                    self.insertCustomersIntoQueue()
+                    self.distributeTask()
                 }
-            } while userInput != "2"
-        }
+            }
+        } while userInput != "2"
     }
 }
