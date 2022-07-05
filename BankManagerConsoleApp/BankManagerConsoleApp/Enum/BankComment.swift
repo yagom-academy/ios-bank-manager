faf177b575e49a1da7a5ec5caf0e898e0495b05a
unchain123 <unchain123@kakao.com>
2022-07-05 11:16:18 +0900

    refactor: 기존 열거형 파일 이름 변경 및 삭제

--- BankManagerConsoleApp/BankManagerConsoleApp/Enum/BankComment.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Enum/BankComment.swift
@@ -1,21 +1,9 @@
 //
-//  BankManagerNameSpace.swift
+//  BankComment.swift
 //  BankManagerConsoleApp
 //
-//  Created by unchain, BaekGom on 2022/06/30.
+//  Created by unchain, BaekGom on 2022/07/05.
 //
-
-enum BusinessType: Double {
-    case work = 0.7
-}
-
-enum SetNumber: Int {
-    case decimalRange = 2
-    case minimumCustomerCount = 1
-    case failNumberFormatter = 0
-    case minCustomer = 10
-    case maxCustomer = 30
-}
 
 enum BankManagerComment: String {
     case handleBankingStart = "번 고객 업무 시작"
@@ -30,8 +18,4 @@ enum BankComment: String {
     case emptyValue = ""
     case bankOpen = "1"
     case bankClose = "2"
-}
-
-enum NoneValueError: Error {
-    case noneValue
 }
