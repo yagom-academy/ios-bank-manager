daa0bc43a573751f73d236fa3e4ae38c27f72640
ic_6_user_28 <toskw@naver.com>
2022-07-05 16:30:08 +0900

    refactor: customer 구조체 클래스로 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Customer.swift
@@ -5,7 +5,12 @@
 //  Created by Kiwi, Kay on 2022/07/01.
 //
 
-struct Customer {
+final class Customer {
     let customerNumber: Int
     let business: WorkType
+    
+    init (customerNumber: Int, business: WorkType) {
+        self.customerNumber = customerNumber
+        self.business = business
+    }
 }
