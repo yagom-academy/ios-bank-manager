2c8733d2b1be4492e034e81513d81edb05f2f1fc
yusw10 <yusw10@naver.com>
2022-07-05 16:38:43 +0900

    feat: Custom Operation 생성
    - LoanOperation 타입 생성
    - DepositOperation 타입 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/DepositOperation.swift
@@ -0,0 +1,26 @@
+//
+//  DepositOperation.swift
+//  BankManagerConsoleApp
+//
+//  Created by 휴, 보리 on 2022/07/05.
+//
+
+import Foundation
+
+class DepositOperation: Operation {
+    let task: String
+    let watingNumber: Int
+    
+    init(task: String, watingNumber: Int) {
+        self.task = task
+        self.watingNumber = watingNumber
+        super.init()
+    }
+    
+    override func main() {
+        super.main()
+        print("\(self.watingNumber)번 고객 \(self.task)업무 시작")
+        usleep(700000)
+        print("\(self.watingNumber)번 고객 \(self.task)업무 완료")
+    }
+}
