b50b168ec88c93742a8c5d6c9097208569e792ce
apwierk2451 <apwierk2451@gmail.com>
2022-07-06 10:58:10 +0900

    feat: 3명의 은행원 업무 비동기 실행 메서드 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -1,13 +1,41 @@
+import Foundation
+
 struct Bank {
+    let depositSemaphore = DispatchSemaphore(value: 2)
+    let loanSemaphore = DispatchSemaphore(value: 1)
+    
     func startBanking(customer: Queue<Customer>, bankers: [BankerLogic]) -> Double {
         var customerNumber: Double = 0
         
         while customer.isEmpty == false {
             guard let customer = customer.dequeue() else { return 0 }
-            bankers[0].serve(customer: customer)
+            
+            switch customer.banking {
+            case .loan:
+                loan(bankers: bankers, customer: customer)
+            case .deposit:
+                deposit(bankers: bankers, customer: customer)
+            }
+            
             customerNumber += 1
         }
         
         return customerNumber
+    }
+    
+    func loan(bankers: [BankerLogic], customer: Customer) {
+        DispatchQueue.global().async {
+            loanSemaphore.wait()
+            bankers[2].serve(customer: customer)
+            loanSemaphore.signal()
+        }
+    }
+    
+    func deposit(bankers: [BankerLogic], customer: Customer) {
+        DispatchQueue.global().async {
+            depositSemaphore.wait()
+            bankers[0].serve(customer: customer)
+            depositSemaphore.signal()
+        }
     }
 }
