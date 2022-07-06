450c2cbd0697aa59a53bdf8b7203012b25271264
yeeton37 <lyeeun37@naver.com>
2022-07-06 11:04:09 +0900

    feat: struct(Bank) 내부 프로퍼티 및 메서드 선언
    - group.wait을 사용하여 endMessage가 미리 실행되는 것을 방지
    - 현재 시간을 알려주는 CFAbsoluteTimeGetCurrent 메서드를 사용해서 totalProcessTime을 구함

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -10,12 +10,14 @@ import Foundation
 struct Bank {
     private var bankManager: BankManager
     private var queue: CustomerQueue
-    let loanQueue = DispatchQueue(label: "loanQueue")
+    let loanQueue = DispatchQueue(label: "loanQueue", attributes: .concurrent)
     let depositQueue = DispatchQueue(label: "depositQueue", attributes: .concurrent)
     private(set) var randomNumberOfCustomer: Int = 0
     let group = DispatchGroup()
     let depositSemaphore = DispatchSemaphore(value: 2)
-    var totalTime: TimeInterval = 0
+    let loanSemaphore = DispatchSemaphore(value: 1)
+
+    var totalProcessTime: TimeInterval = 0
     
     init(employee bankManager: BankManager, customer queue: CustomerQueue) {
         self.bankManager = bankManager
@@ -65,9 +67,11 @@ struct Bank {
     }
     
     private mutating func handleCustomer() {
+        let startTime = CFAbsoluteTimeGetCurrent()
+        
         for _ in 0..<queue.count {
             while let customer = queue.dequeue() {
-                let workItem = DispatchWorkItem { [self] in
+                let depositWorkItem = DispatchWorkItem { [self] in
                     group.enter()
                     depositSemaphore.wait()
                     bankManager.handle(customer: customer)
@@ -75,17 +79,27 @@ struct Bank {
                     depositSemaphore.signal()
                 }
                 
+                let loanWorkItem = DispatchWorkItem { [self] in
+                    group.enter()
+                    loanSemaphore.wait()
+                    bankManager.handle(customer: customer)
+                    group.leave()
+                    loanSemaphore.signal()
+                }
+                
                 if customer.business == .loan {
-                    loanQueue.async(group: group, execute: workItem)
+                    loanQueue.async(group: group, execute: loanWorkItem)
                 } else {
-                    depositQueue.async(group: group, execute: workItem)
+                    depositQueue.async(group: group, execute: depositWorkItem)
                 }
             }
         }
+        group.wait()
+        totalProcessTime = CFAbsoluteTimeGetCurrent() - startTime
     }
     
     private func displayEndMessage() {
-        let totalHandlingTime = String(format: "%.2f",totalTime)
+        let totalHandlingTime = String(format: "%.2f", totalProcessTime)
         
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(randomNumberOfCustomer)명이며, 총 업무시간은 \(totalHandlingTime)초입니다.")
     }
