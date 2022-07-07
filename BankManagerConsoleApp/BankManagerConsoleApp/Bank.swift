73b5ca82b91291cc28e67b2f4e35bb48065151d5
finnn1 <tgss3535@gmail.com>
2022-07-07 18:48:59 +0900

    refactor: 프로퍼티 네이밍 수정 및 메서드 분리

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 
-struct Bank: Agency {
-    var banker2 = Banker(task: .loan)
-    var banker1 = Banker(task: .deposit)
+class Bank: Agency {
     var customerCount: Int?
+    var loanBanker = Banker(task: .loan)
+    var depositBanker = Banker(task: .deposit)
+    
     var ticketNumberQueue: Queue<BankCustomer> = {
         let randomNumber = Int.random(in: NameSpace.randomCustomerNumberRange)
         var randomCustomerQueue = Queue<BankCustomer>()
@@ -31,32 +32,32 @@ struct Bank: Agency {
     let loansemaphore = DispatchSemaphore(value: 1)
     let group = DispatchGroup()
     
-    mutating func open() {
+    func open() {
         customerCount = ticketNumberQueue.count
-        
         while let currentCustomer = ticketNumberQueue.dequeue() {
-            if currentCustomer.task == .deposit {
-                group.enter()
-                depositQueue.async { [self] in
-                    depositsemaphore.wait()
-                    banker1.startWork(of: currentCustomer)
-                    depositsemaphore.signal()
-                    group.leave()
-                }
-            } else if currentCustomer.task == .loan {
-                group.enter()
-                loanQueue.async { [self] in
-                    loansemaphore.wait()
-                    banker2.startWork(of: currentCustomer)
-                    loansemaphore.signal()
-                    group.leave()
-                }
+            switch currentCustomer.task {
+            case .deposit:
+                matchWork(to: depositBanker, with: currentCustomer, queue: depositQueue, semaphore: depositsemaphore)
+            case .loan:
+                matchWork(to: loanBanker, with: currentCustomer, queue: loanQueue, semaphore: loansemaphore)
+            case .none:
+                break
             }
         }
         group.wait()
     }
     
-    mutating func close() {
+    func matchWork(to banker: Banker, with currentCustomer: BankCustomer, queue: DispatchQueue, semaphore: DispatchSemaphore) {
+        group.enter()
+        queue.async { [self] in
+            semaphore.wait()
+            banker.startWork(of: currentCustomer)
+            semaphore.signal()
+            group.leave()
+        }
+    }
+    
+    func close() {
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerCount ?? 0)명이며, 총 업무시간은 \(totalWorkTime.formatDoubleToString())초입니다.")
     }
 }
