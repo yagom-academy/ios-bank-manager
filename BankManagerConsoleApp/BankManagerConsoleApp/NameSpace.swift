be68c40d7414cc484c2cf08850627a5bc5f3a4cc
bard <schy2503@naver.com>
2022-07-08 15:25:00 +0900

    refactor: Console 내부 하드코딩 제거 - ConsoleTask 열거형 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/NameSpace.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/NameSpace.swift
@@ -13,3 +13,8 @@ enum NameSpace {
                 """
     static let randomCustomerNumberRange = 10...30
 }
+enum ConsoleTask {
+    static let open = "1"
+    static let close = "2"
+    static let wrongInput = "잘못된 입력입니다. 다시 시도해주세요🥲"
+}
