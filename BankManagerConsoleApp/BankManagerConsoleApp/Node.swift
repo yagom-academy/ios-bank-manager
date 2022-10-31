6afff5306748af143a3832300dd0026bd589569e
zhilly11 <zhzh1x4@gmail.com>
2022-10-31 11:25:17 +0900

    feat: Queue에 사용될 Node타입 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -0,0 +1,14 @@
+//
+//  CustomerQueueTests.swift
+//  CustomerQueueTests
+//
+//  Copyright (c) 2022 Zhilly, Minii All rights reserved.
+
+class Node<T> {
+    let data: T
+    var next: Node?
+    
+    init(data: T) {
+        self.data = data
+    }
+}
