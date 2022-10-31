58f41533cac17ed2056a96d1caa703c0d1dc6e02
미니 <cow970814@naver.com>
2022-10-31 11:00:48 +0900

    feat : 프로젝트 초기 설정

---
+++ BankManagerConsoleApp/ManagerQueueTests/CustomerQueueTests.swift
@@ -0,0 +1,14 @@
+//
+//  CustomerQueueTests.swift
+//  CustomerQueueTests
+//
+//  Copyright (c) 2022 Zhilly, Minii All rights reserved.
+        
+
+import XCTest
+
+final class CustomerQueueTests: XCTestCase {
+    override func setUpWithError() throws { }
+
+    override func tearDownWithError() throws { }
+}
