b1713fa339cef1ecae136e1ba06d00fc553e71b8
lxodud <xodud1998223@naver.com>
2022-10-31 19:56:30 +0900

    feat: LinkedList 타입 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -8,7 +8,7 @@ final class Node<Element> {
     
     init(value: Element, next: Node<Element>? = nil) {
         self.value = value
-        self.next = next// 5.next 4
+        self.next = next
     }
     
     func changeValue(_ value: Element) {
@@ -20,6 +20,28 @@ final class Node<Element> {
     }
 }
 
-class LinkedList {
+struct LinkedList<Element> {
+    private(set) var head: Node<Element>?
+    private var tail: Node<Element>?
+    var isEmpty: Bool {
+        if head == nil { return true }
+        else { return false }
+    }
     
+    mutating func append(_ node: Node<Element>) {
+        if isEmpty {
+            head = node
+            tail = node
+        } else {
+            tail?.changeNext(node)
+            tail = node
+        }
+    }
+    
+    mutating func removeFirst() -> Node<Element>? {
+        guard !isEmpty else { return nil }
+        let firstNode = head
+        head = head?.next
+        return firstNode
+    }
 }
