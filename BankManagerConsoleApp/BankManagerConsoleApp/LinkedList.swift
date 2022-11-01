cdf3992d4fa305f2eb88ec1816676a80301ca4a4
jonghancha <jonghancha@gmail.com>
2022-11-01 12:12:30 +0900

    feat: appendNode(), removeHead()메서드 구현, Queue구조체 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -14,12 +14,28 @@ struct LinkedList<T> {
         return self.head == nil
     }
     
-    mutating func appendNode(_ node: T) {
+    mutating func appendNode(_ value: T) {
+        let node = Node<T>(data: value)
         
+        if self.isEmpty {
+            self.head = node
+        }
+        
+        self.tail?.next = node
+        self.tail = node
     }
     
     mutating func removeHead() -> T? {
-        return self.head?.data
+        guard !self.isEmpty else { return nil }
+        
+        if self.head === self.tail {
+            self.tail = nil
+        }
+        let removedValue = self.head?.data
+        
+        self.head = self.head?.next
+        
+        return removedValue
     }
     
     mutating func removeAll() {
--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -1,3 +1,25 @@
 //  Created by stone, 애종 on 2022/11/01.
 
-import Foundation
+struct Queue<T> {
+    var linkedList = LinkedList<T>()
+    
+    var peek: T? {
+        return linkedList.headData
+    }
+    
+    var isEmpty: Bool {
+        return self.linkedList.isEmpty
+    }
+    
+    mutating func enqueue(_ item: T) {
+        self.linkedList.appendNode(item)
+    }
+    
+    mutating func dequeue() -> T? {
+        return self.linkedList.removeHead()
+    }
+    
+    mutating func clear() {
+        self.linkedList.removeAll()
+    }
+}
