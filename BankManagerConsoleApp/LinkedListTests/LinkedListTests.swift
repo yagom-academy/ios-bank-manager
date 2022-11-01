b33f248c1db90547342027865c19d5a247e77059
dylan-yoon <d.dylany21@gmail.com>
2022-11-01 15:20:02 +0900

    test: LinkedList enqueue() UnitTest 구현

--- BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
+++ BankManagerConsoleApp/LinkedListTests/LinkedListTests.swift
@@ -3,10 +3,10 @@
 //  Created by Baem & Bella on 2022/11/01.
 
 import XCTest
-@testable import BankManagerConsoleApp
 
 class LinkedListTests: XCTestCase {
     var sut: LinkedList<Int>!
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
         sut = LinkedList()
@@ -15,5 +15,27 @@ class LinkedListTests: XCTestCase {
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
+    }
+    
+    func test_enqueue_호출이_잘_되는지() {
+        //given
+        let input = 1
+        
+        //when
+        sut.enqueue(value: input)
+        
+        //then
+        XCTAssertEqual(sut.isEmpty, false)
+    }
+    
+    func test_enqueue호출후_dequeue호출하면_같은_값이_나오는지() {
+        //given
+        sut.enqueue(value: 3)
+        
+        //when
+        let result = sut.dequeue()
+        
+        //then
+        XCTAssertEqual(result, 3)
     }
 }
