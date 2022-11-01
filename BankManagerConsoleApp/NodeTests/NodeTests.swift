9972e6260af37584cfeee178778a17dcb7cd13e3
DragonYG <kioswe92@gmail.com>
2022-11-01 17:32:19 +0900

    refactor: Node가 가지는 값을 value에서 data로네이밍 변경

--- BankManagerConsoleApp/NodeTests/NodeTests.swift
+++ BankManagerConsoleApp/NodeTests/NodeTests.swift
@@ -7,8 +7,8 @@ import XCTest
 class NodeTests: XCTestCase {
     func test_1과_2를_순서대로_Node로_생성한_후_firstNode와_secondNode를_연결할_때_firstNode의_nextNode에_value는_2와_같다() {
         // given
-        let firstNode: Node = Node(value: 1)
-        let secondNode: Node = Node(value: 2)
+        let firstNode: Node = Node(data: 1)
+        let secondNode: Node = Node(data: 2)
         
         // when
         firstNode.nextNode = secondNode
@@ -19,11 +19,11 @@ class NodeTests: XCTestCase {
     
     func test_1부터_5를_순서대로_Node로_생성한_후_각각의_Node를_순서대로_연결할_때_firstNode의_nextNode의_nextNode에_value는_3와_같다() {
         // given
-        let firstNode: Node = Node(value: 1)
-        let secondNode: Node = Node(value: 2)
-        let thirdNode: Node = Node(value: 3)
-        let fourthNode: Node = Node(value: 4)
-        let fifthNode: Node = Node(value: 5)
+        let firstNode: Node = Node(data: 1)
+        let secondNode: Node = Node(data: 2)
+        let thirdNode: Node = Node(data: 3)
+        let fourthNode: Node = Node(data: 4)
+        let fifthNode: Node = Node(data: 5)
         
         // when
         firstNode.nextNode = secondNode
