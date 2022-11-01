6d735b168394c1faf5f228e857d00708e8c620df
woong <workplayhard1@naver.com>
2022-11-01 13:20:15 +0900

    feat: Node 타입에 final 추가, 접근제어자 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -5,8 +5,8 @@
 //  Created by junho, woong on 2022/11/01.
 //
 
-class Node<T> {
-    var data: T?
+final class Node<T> {
+    private (set) var data: T?
     var next: Node<T>?
     
     init(data: T?, next: Node? = nil) {
