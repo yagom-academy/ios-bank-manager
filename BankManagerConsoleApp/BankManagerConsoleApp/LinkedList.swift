ce8ccc19287c464ad2d17a4593a1b2f0367e8a95
chacha <happyhy0429@gmail.com>
2022-11-01 11:43:33 +0900

    feat: LinkedList 파일생성 및 프로퍼티 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/LinkedList.swift
@@ -0,0 +1,30 @@
+//  LinkedList.swift
+//  BankManagerConsoleApp
+//  Created by Baem & Bella on 2022/11/01.
+
+import Foundation
+
+struct LinkedList<T> {
+    private var head: Node<T>?
+    private var tail: Node<T>?
+    
+    var isEmpty: Bool {
+      head == nil
+    }
+    
+    func enqueue() {
+        
+    }
+    
+    func dequeue() {
+        
+    }
+    
+    func clear() {
+        
+    }
+    
+    func peek() {
+        
+    }
+}
