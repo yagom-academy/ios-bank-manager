70e839f5bf2caa73b68a0dea5b99e8f219487b9c
jsa0224 <94514250+jsa0224@users.noreply.github.com>
2022-11-02 13:21:32 +0900

    feat: Person 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Person.swift
@@ -0,0 +1,8 @@
+//  BankManagerConsoleApp - Person.swift
+//  Created by Ayaan/Dragon/som on 2022/11/02.
+//  Copyright © yagom academy. All rights reserved.
+
+struct Person {
+    let name: String
+    let job: Job
+}
