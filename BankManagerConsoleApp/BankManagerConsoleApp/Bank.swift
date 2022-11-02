acddef20a28822da6eb5877c0580286b9a90773a
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-11-02 15:16:51 +0900

    feat:  Bank 구조체 뼈대 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,30 @@
+//
+//  Bank.swift
+//  Created by sunnycookie, inho
+//
+
+import Foundation
+
+struct Bank {
+    var customers: Queue<Customer>
+    var customerCount: Int
+    var processingTime: Double {
+        return 0
+    }
+    
+    func showManual() {
+        
+    }
+    
+    func startBanking() {
+        
+    }
+    
+    func receiveCustomer() {
+        
+    }
+    
+    func closeBanking() {
+        
+    }
+}
