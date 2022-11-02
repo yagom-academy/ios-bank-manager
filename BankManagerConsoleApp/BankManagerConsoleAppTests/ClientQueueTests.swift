2f84715c4ffa689043378e50c41e7358a871da07
wonbi92 <bin9239@gmail.com>
2022-11-02 23:57:49 +0900

    fix : ClientQueue 구조체 로직 변경에 따른 테스트 케이스 오류 수정

--- BankManagerConsoleApp/BankManagerConsoleAppTests/ClientQueueTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/ClientQueueTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 class ClientQueueTests: XCTestCase {
-    var sut: ClientQueue<Int>?
+    var sut: ClientQueue?
     
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -23,26 +23,26 @@ class ClientQueueTests: XCTestCase {
     }
     
     func test_LinkedList에1을append했을때_bringHead메서드의반환값이1이나오는지() {
-        sut?.enqueue(1)
+        sut?.enqueue(Client(waitingTicket: 1))
         
-        XCTAssertEqual(sut?.peek, 1)
+        XCTAssertEqual(sut?.peek?.waitingTicket, 1)
     }
 
     func test_append를한후_peek값이첫번째랑같은지() {
         let numbers = [1, 2, 3]
         
         numbers.forEach { number in
-            sut?.enqueue(number)
+            sut?.enqueue(Client(waitingTicket: number))
         }
         
-        XCTAssertEqual(sut?.peek, numbers.first)
+        XCTAssertEqual(sut?.peek?.waitingTicket, numbers.first)
     }
     
     func test_모든Element들을clear할때_isEmpty가true인지() {
         let numbers = [1, 2, 3]
         
         numbers.forEach { number in
-            sut?.enqueue(number)
+            sut?.enqueue(Client(waitingTicket: number))
         }
         
         sut?.clear()
@@ -54,13 +54,13 @@ class ClientQueueTests: XCTestCase {
         let numbers = [1, 2, 3]
         
         numbers.forEach { number in
-            sut?.enqueue(number)
+            sut?.enqueue(Client(waitingTicket: number))
         }
         
         let result = sut?.dequeue()
         
         XCTAssertEqual(sut?.isEmpty, false)
-        XCTAssertEqual(result, numbers.first)
+        XCTAssertEqual(result?.waitingTicket, numbers.first)
     }
     
     func test_List가비어있을때_dequeue메서드의반환값이nil인지() {
