29b3a57f782d50cb33e9c5dff448a1163ea4e67c
yjjem <marksim0606@gmail.com>
2022-11-02 23:40:31 +0900

    feat (Bank): openBank 메서드 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -9,4 +9,19 @@ struct Bank {
     private var bankManagers: [BankManager]  = []
     private var workLoadManager: WorkLoadManager = WorkLoadManager()
 
+    mutating func openBank() {
+        print("openBank")
+        let randomIntNumber = Int.random(in: 10...30)
+        let customerCount = Array<Int>(1...randomIntNumber)
+        customerCount.forEach {
+            workLoadManager.taskQueue.enqueue(data: $0)
+        }
+        
+        while workLoadManager.taskQueue.isEmpty() == true {
+            workLoadManager.checkManagers(managers: bankManagers)
+        }
+        
+        closeBank(with: randomIntNumber)
+    }
 }
+
