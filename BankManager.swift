2653930c93b4a35a8f4c0dedd07b21b3679a623e
lj-7-77 <swtw72@gmail.com>
2022-11-02 17:30:36 +0900

    feat: BankManager 구조체 추가

--- BankManager.swift
+++ BankManager.swift
@@ -3,5 +3,11 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
-import Foundation
+struct BankManager {
+    var isAvailable: Bool = false
+    
+    func work(customerNumber: Int) {
+        print("\(customerNumber)번 고객 업무 시작")
+        print("\(customerNumber)번 고객 업무 종료")
+    }
+}
