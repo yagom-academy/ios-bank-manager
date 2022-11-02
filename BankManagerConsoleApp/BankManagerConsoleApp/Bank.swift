059ef2d12191ade5fb107a3a17fd6b4ea2f2ce1c
Ayaan <onestar7984@gmail.com>
2022-11-02 13:25:18 +0900

    feat: Bank 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,15 @@
+//  BankManagerConsoleApp - Bank.swift
+//  Created by Ayaan/Dragon/som on 2022/11/02.
+//  Copyright © yagom academy. All rights reserved.
+
+struct Bank {
+    private let banker: Banker
+    private var customerQueue: Queue<Customer>
+    
+    func start() {
+        
+    }
+    func complete() {
+        
+    }
+}
