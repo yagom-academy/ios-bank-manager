001aa610d33dca45a289602334f7bf2eb74e4e04
DragonYG <kioswe92@gmail.com>
2022-11-02 13:18:59 +0900

    feat: Job 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Job/Banker.swift
@@ -0,0 +1,8 @@
+//  BankManagerConsoleApp - Banker.swift
+//  Created by Ayaan/Dragon/som on 2022/11/02.
+//  Copyright © yagom academy. All rights reserved.
+
+struct Banker: Job {
+    func service(_ : BankingService) {
+    }
+}
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Job/Job.swift
@@ -0,0 +1,6 @@
+//  BankManagerConsoleApp - Job.swift
+//  Created by Ayaan/Dragon/som on 2022/11/02.
+//  Copyright © yagom academy. All rights reserved.
+
+protocol Job {
+}
