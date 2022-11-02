167dab78fe4b2f290840fe1fc20e91650db18f56
Mangdi <lkw4670@naver.com>
2022-11-02 11:26:20 +0900

    feat: BankManager의 selectMenu메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -7,3 +7,30 @@
 
 import Foundation
 
+struct BankManager {
+//        let numberOfCustomer: Int
+//        var customerQueue: CustomerQueue<Customer>
+    
+    func openUp() {
+        selectMenu()
+    }
+    
+    private func selectMenu() {
+        print("""
+              1 : 은행개점
+              2 : 종료
+              입력 :
+              """, terminator: " ")
+        guard let userInput = readLine()?.replacingOccurrences(of: " ", with: "") else { return }
+        
+        switch userInput {
+        case "1":
+            break
+        case "2":
+            break
+        default:
+            print("입력이 잘못되었습니다. \n")
+            selectMenu()
+        }
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,7 @@
 // 
 
 import Foundation
+
+let bankManager = BankManager()
+
+bankManager.openUp()
