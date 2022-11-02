b4ac51819146286cce739f47511e50cb5673c2fa
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-11-02 11:29:15 +0900

    docs: SwiftLint의 Pods파일삭제

--- BankManagerUIApp/Pods/Target Support Files/Pods-BankManagerUIApp-BankManagerUIAppUITests/Pods-BankManagerUIApp-BankManagerUIAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BankManagerUIApp_BankManagerUIAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_BankManagerUIApp_BankManagerUIAppUITests
-@end
--- BankManagerUIApp/Pods/Target Support Files/Pods-BankManagerUIAppTests/Pods-BankManagerUIAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BankManagerUIAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_BankManagerUIAppTests
-@end
