a07bc368105a31830b21701f8e078cf21cd27d7d
lnhk98 <hozzang666@gmail.com>
2022-11-02 17:37:26 +0900

    feat: closeBanking메서드 내부 구현 및 completedCustomerCount프로퍼티 선언

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,6 +8,7 @@ import Foundation
 struct Bank {
     var customers: Queue<Customer>
     var customerCount: Int
+    var completedCustomerCount: Int = 0
     var processingTime: Double {
         return 0
     }
@@ -49,13 +50,16 @@ struct Bank {
         closeBanking()
     }
     
-    func serveCustomer(number: Int) {
+    mutating func serveCustomer(number: Int) {
         print("\(number)번 고객 업무 시작")
         sleep(1)
+        completedCustomerCount += 1
         print("\(number)번 고객 업무 완료")
     }
     
     func closeBanking() {
-        
+        print("업무가 마감되었습니다. " +
+              "오늘 업무를 처리한 고객은 총 \(completedCustomerCount)명이며, " +
+              "총 업무시간은 \(processingTime)초입니다.")
     }
 }
