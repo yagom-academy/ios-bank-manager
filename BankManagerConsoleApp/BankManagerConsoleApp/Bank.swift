33bb4738b3bdfe3f28f859da73c02177aa6ea057
woong <workplayhard1@naver.com>
2022-11-03 20:05:05 +0900

    feat: 은행업무 마감 시, 누적소요시간 및 누적고객을 출력해주는 printClosingMessage 함수 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -5,9 +5,13 @@
 //  Created by junho, woong on 2022/11/03.
 //
 
+import Foundation
+
 struct Bank {
     private var customerQueue: Queue<Customer> = Queue()
     private var bankers: [Banker] = []
+    private let processedCustomerNumber: Int = 0
+    private let processedTotalTime: Double = 0
     
     mutating func addCustomerToQueue(_ customer: Customer) {
         customerQueue.enqueue(customer)
@@ -15,5 +19,16 @@ struct Bank {
     
     mutating func addBanker(_ banker: Banker) {
         bankers.append(banker)
+    }
+    
+    mutating func printClosingMesage() {
+        let numberFormatter: NumberFormatter = NumberFormatter()
+        numberFormatter.minimumFractionDigits = 2
+        guard let formattedProcessedTotalTime: String = numberFormatter.string(from: NSNumber(value: processedTotalTime)) else {
+            return
+        }
+        let processingClosedMessage: String = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(processedCustomerNumber)명이며, 총 업무시간은 \(formattedProcessedTotalTime)초입니다."
+        
+        print(processingClosedMessage)
     }
 }
