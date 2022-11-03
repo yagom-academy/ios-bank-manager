11dc02c445342bf20d3a7a6d0970b333574dd84c
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-11-03 19:33:12 +0900

    refactor: processingTime소수점 두자리까지 출력을위해 String타입으로 수정 및 sleep 에 소수점사용을 위해 sleep(forTimeInterval) 로 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -8,13 +8,8 @@ import Foundation
 struct Bank {
     var customers: Queue<Customer>
     var completedCustomerCount: Int = 0
-    var processingTime: Double {
-        guard let result = Double(String(format: "%.2f",
-                                         0.7 * Double(completedCustomerCount)))
-        else {
-            return 0.0
-        }
-        
+    var processingTime: String {
+        let result = String(format: "%.2f", 0.7 * Double(completedCustomerCount))
         return result
     }
 
@@ -32,7 +27,7 @@ struct Bank {
     
     mutating func serveCustomer(number: Int) {
         print("\(number)번 고객 업무 시작")
-        sleep(1)
+        Thread.sleep(forTimeInterval: 0.7)
         completedCustomerCount += 1
         print("\(number)번 고객 업무 완료")
     }
