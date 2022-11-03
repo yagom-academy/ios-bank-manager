c9cb7b0aaa48e2ca8a6af7a9b1b90b61cfcb78b2
junho <junhoL1046@gmail.com>
2022-11-03 20:14:44 +0900

    feat: 대기열에 있는 고객의 업무를 처리하는 메서드 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -10,8 +10,8 @@ import Foundation
 struct Bank {
     private var customerQueue: Queue<Customer> = Queue()
     private var bankers: [Banker] = []
-    private let processedCustomerNumber: Int = 0
-    private let processedTotalTime: Double = 0
+    private var processedCustomerNumber: Int = 0
+    private var processedTotalTime: Double = 0
     
     mutating func addCustomerToQueue(_ customer: Customer) {
         customerQueue.enqueue(customer)
@@ -28,7 +28,20 @@ struct Bank {
             return
         }
         let processingClosedMessage: String = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(processedCustomerNumber)명이며, 총 업무시간은 \(formattedProcessedTotalTime)초입니다."
-        
         print(processingClosedMessage)
+    }
+    
+    mutating func processBankingBusinessOfCustomers() {
+        while customerQueue.isEmpty == false {
+            for banker in bankers {
+                guard let customer = customerQueue.dequeue() else {
+                    break
+                }
+                banker.processBankingBusiness(of: customer)
+                self.processedCustomerNumber += 1
+                self.processedTotalTime += banker.processingTimePerCustomer
+            }
+        }
+        printClosingMesage()
     }
 }
