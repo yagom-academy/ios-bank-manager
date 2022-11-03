69f99081b3ef8a72de74f975a4a4d2a93131f55f
lxodud <xodud1998223@naver.com>
2022-11-03 21:42:44 +0900

    refactor: BankManager 접근제어 권한 수정

--- BankManager.swift
+++ BankManager.swift
@@ -16,7 +16,7 @@ struct BankManager {
         try identifyMenu(userInput)
     }
     
-    func printMenu() {
+    private func printMenu() {
         print(
             """
             1 : 은행개점
@@ -26,7 +26,7 @@ struct BankManager {
             terminator: " : ")
     }
     
-    mutating func identifyMenu(_ userInput: String?) throws {
+    mutating private func identifyMenu(_ userInput: String?) throws {
         switch userInput {
         case Constant.firstMenu:
             open()
@@ -39,7 +39,7 @@ struct BankManager {
         }
     }
     
-    mutating func createCustomer() -> Int {
+    mutating private func createCustomer() -> Int {
         let customers: Int = Int.random(in: Constant.customerNumberRange)
         
         for number in 1...customers {
@@ -50,7 +50,7 @@ struct BankManager {
         return customers
     }
     
-    mutating func open() {
+    mutating private func open() {
         let customers: Int = createCustomer()
         
         for _ in 1...customers {
@@ -58,7 +58,7 @@ struct BankManager {
         }
     }
     
-    func close() throws {
+    private func close() throws {
         let totalTime: String = try (Double(bank.customerCount) * timePerTask).format()
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(bank.customerCount)명이며, 총 업무시간은 \(totalTime)초 입니다.")
     }
