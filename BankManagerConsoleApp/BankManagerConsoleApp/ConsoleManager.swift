77c936577dfcf6f8c19f048dbd7481bfe3b33ef0
zhilly11 <zhzh1x4@gmail.com>
2022-11-03 17:50:34 +0900

    feat: 사용자 입력에 대한 처리 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/ConsoleManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/ConsoleManager.swift
@@ -5,12 +5,23 @@
 //  Copyright (c) 2022 Minii All rights reserved.
 
 struct ConsoleManager {
-    func startConsole() {
+    func startConsole() -> Int {
         print("""
         1 : 은행개점
         2 : 종료
         입력 :
         """, terminator: " ")
-        let input = readLine()
+        
+        return validateUserInput()
+    }
+    
+    private func inputUserSelect() -> Int? {
+        guard let input = readLine() else { return nil }
+        return Int(input)
+    }
+    
+    private func validateUserInput() -> Int {
+        guard let filteredInput = inputUserSelect() else { return 0 }
+        return filteredInput
     }
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -1,8 +1,18 @@
 //
-//  CustomerQueueTests.swift
-//  CustomerQueueTests
+//  main.swift
+//  BankManagerConsoleApp
 //
 //  Copyright (c) 2022 Zhilly, Minii All rights reserved.
 
 let console = ConsoleManager()
-console.startConsole()
+
+while true {
+    switch console.startConsole() {
+    case 1:
+        print("은행 개점")
+    case 2:
+        break
+    default:
+        print("다시 입력하세요")
+    }
+}
