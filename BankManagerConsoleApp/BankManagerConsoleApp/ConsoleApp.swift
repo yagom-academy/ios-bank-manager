bddbc22a639cf77ceaa6a40f0ce1d28a0887a2ff
woong <workplayhard1@naver.com>
2022-11-03 21:07:31 +0900

    feat: ConsoleApp 타입 구현 및 start, inputMenu, startBankByCreatingManager 함수 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/ConsoleApp.swift
@@ -0,0 +1,37 @@
+//
+//  ConsoleApp.swift
+//  BankManagerConsoleApp
+//
+//  Created by junho, woong on 2022/11/03.
+//
+
+struct ConsoleApp {
+    func start() {
+        ConsoleAppMenu.printMenuList()
+        switch inputMenu() {
+        case .startBank:
+            startBankByCreatingBankManager()
+            self.start()
+        case .quit:
+            return
+        }
+    }
+    
+    func inputMenu() -> ConsoleAppMenu {
+        print("입력 :", terminator: " ")
+        guard let input: Int = Int(readLine() ?? ""),
+              let inputMenu: ConsoleAppMenu = ConsoleAppMenu(rawValue: input) else {
+            print("다시 입력하세요.")
+            return inputMenu()
+        }
+        return inputMenu
+    }
+    
+    func startBankByCreatingBankManager() {
+        var bankManager: BankManager = BankManager()
+        let customerCount: Int = Int.random(in: 10...30)
+        bankManager.addCustomers(count: customerCount)
+        bankManager.addBankers(count: 1)
+        bankManager.startBank()
+    }
+}
