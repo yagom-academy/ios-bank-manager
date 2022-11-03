69ee7b7e4852f75935a46146af0b7deb11f64591
junho <junhoL1046@gmail.com>
2022-11-03 20:41:45 +0900

    feat: 열거형 ConsoleAppMenu 추가 및 리스트 출력하는 타입 메서드 추가

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/ConsoleAppMenu.swift
@@ -0,0 +1,27 @@
+//
+//  ConsoleAppMenu.swift
+//  BankManagerConsoleApp
+//
+//  Created by junho, woong on 2022/11/03.
+//
+
+enum ConsoleAppMenu: Int, CaseIterable {
+    case startBank = 1
+    case quit = 2
+    
+    var description: String {
+        switch self {
+        case .startBank:
+            return "은행개점"
+        case .quit:
+            return "종료"
+        }
+    }
+    
+    static func printMenuList() {
+        for menu in Self.allCases {
+            let menuInfo: String = "\(menu.rawValue) : \(menu.description)"
+            print(menuInfo)
+        }
+    }
+}
