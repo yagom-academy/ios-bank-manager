9f188cbecab53b658d37bbdb005d5f31d95f6ee1
미니 <cow970814@naver.com>
2022-11-03 18:06:40 +0900

    refactor : 콘솔 메뉴 관련 메서드 분리 및 재귀함수 처리

--- BankManagerConsoleApp/BankManagerConsoleApp/ConsoleManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/ConsoleManager.swift
@@ -5,14 +5,17 @@
 //  Copyright (c) 2022 Minii All rights reserved.
 
 struct ConsoleManager {
-    func startConsole() -> Int {
+    func startConsole() {
+        showMenu()
+        doAct(userInput: validateUserInput())
+    }
+    
+    private func showMenu() {
         print("""
         1 : 은행개점
         2 : 종료
         입력 :
         """, terminator: " ")
-        
-        return validateUserInput()
     }
     
     private func inputUserSelect() -> Int? {
@@ -23,5 +26,19 @@ struct ConsoleManager {
     private func validateUserInput() -> Int {
         guard let filteredInput = inputUserSelect() else { return 0 }
         return filteredInput
+    }
+    
+    private func doAct(userInput: Int) {
+        switch userInput {
+        case 1:
+            // TODO: - Bank문열고 업무 시작하기
+            print("업무를 마감합니다. 오늘 업무를 처리한 고객은 총 XX명이며, 총 업무시간은 XX초입니다.")
+            startConsole()
+        case 2:
+            return
+        default:
+            print("잘못된 입력입니다.")
+            startConsole()
+        }
     }
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,14 +5,4 @@
 //  Copyright (c) 2022 Zhilly, Minii All rights reserved.
 
 let console = ConsoleManager()
-
-while true {
-    switch console.startConsole() {
-    case 1:
-        print("은행 개점")
-    case 2:
-        break
-    default:
-        print("다시 입력하세요")
-    }
-}
+console.startConsole()
