22c2ec44c32920108eb7f50f884df01bc718ef21
ash-youu <ash.you.dev@gmail.com>
2022-11-04 10:39:45 +0900

    feat: Bank open 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -16,4 +16,24 @@ struct Bank {
     func updateClientList(_ client: Client) {
         numberTicketQueue.enqueue(client)
     }
+    
+    func open() {
+        print("""
+            1. 은행개점
+            2. 종료
+            입력
+            """, terminator: " ")
+        
+        let menuNumber: String?  = readLine()
+        
+        switch menuNumber {
+        case "1":
+            return
+        // 실행할 메서드 구현
+        case "2":
+            return
+        default:
+            print("올바른 메뉴 번호를 입력해주세요.")
+        }
+    }
 }
