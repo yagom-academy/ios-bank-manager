36b0d171ea787b183450b99ab3aff56e45dd60ed
chacha <happyhy0429@gmail.com>
2022-11-04 16:55:32 +0900

    refactor: Bank 타입 내부 processedCustomer 프로퍼티 생성 및 startTask() 네이밍 변경

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -7,7 +7,9 @@ import Foundation
 struct Bank {
     private let manager: Int = 1
     private var lineOfCustomer = LinkedList<Customer>()
-
+    private var processedCustomer: Int = 0
+    private var taskTime: Double = 0
+    
     mutating func selectMenu() {
         print(" 1 : 은행개점\n 2 : 종료\n입력 :", terminator: " ")
         switch receiveUserInput() {
@@ -20,41 +22,43 @@ struct Bank {
             selectMenu()
         }
     }
-
+    
     private func receiveUserInput() -> String {
         guard let userInput = readLine() else {
             return receiveUserInput()
         }
         return userInput
     }
-
+    
     mutating private func openBank() {
         let totalCustomer = Int.random(in: 10...30)
-
+        
         listUpCustomer(totalCustomer)
-        var taskTime = getWorkTimeAfterRunTask()
-
-        taskTime = round(taskTime * 100)/100
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomer)명이며, 총 업무시간은 \(taskTime)초입니다.")
+        startTask()
         selectMenu()
     }
-
+    
     mutating private func listUpCustomer(_ customerCount: Int) {
         for customerIndex in 1...customerCount {
             lineOfCustomer.enqueue(value: Customer(number: customerIndex))
         }
     }
     
-    mutating private func getWorkTimeAfterRunTask() -> Double {
-        var taskTime: Double = 0
+    mutating private func startTask() {
+        taskTime = 0
+        processedCustomer = 0
         
         while lineOfCustomer.isEmpty == false {
             let currentCustomer = lineOfCustomer.dequeue()
+            
             print("\(currentCustomer!.number)번 고객 업무 시작")
             taskTime += 0.7
             usleep(70_000)
+            processedCustomer += 1
             print("\(currentCustomer!.number)번 고객 업무 종료")
         }
-        return taskTime
+        
+        taskTime = round(taskTime * 100)/100
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(processedCustomer)명이며, 총 업무시간은 \(taskTime)초입니다.")
     }
 }
