124fb30f82ee4bb1517654b021ca1ee8d819e8c3
ash-youu <ash.you.dev@gmail.com>
2022-11-04 10:53:59 +0900

    feat: Bank close 메서드 정의

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -9,6 +9,7 @@ import Foundation
 struct Bank {
     let bankWorker: BankWorker
     let numberTicketQueue: Queue<Client>
+    let cumulativeClientCount: Int
     
     func returnTicketNumber() -> Int {
         return numberTicketQueue.linkedList.count
@@ -40,11 +41,21 @@ struct Bank {
         switch menuNumber {
         case "1":
             directBankWorker()
-            return
+            close()
         case "2":
             exit(0)
         default:
             print("올바른 메뉴 번호를 입력해주세요.")
         }
+    }
+    
+    func close() {
+        let time: Double = Double(cumulativeClientCount) * 0.7
+        let message: String =
+        String(format: "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 %d이며, 총 업무시간은 %.2f초 입니다.",
+               cumulativeClientCount,
+               time)
+        
+        print("\(message)")
     }
 }
