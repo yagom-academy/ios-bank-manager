61ca2508c26a9baf2438239dc212683d4f8226d8
chacha <happyhy0429@gmail.com>
2022-11-07 21:23:17 +0900

    feat: Bank 타입 내부 예금/대출 고객대기열 구분에 따른 대기열 프로퍼티와 randomTask() 메서드 생성 및 SideEffect 제거

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -5,8 +5,9 @@
 import Foundation
 
 struct Bank {
-    private let manager: Int = 1
-    private var lineOfCustomer = LinkedList<Customer>()
+    private let manager: [BankManager]
+    private var lineOfDepositCustomer = LinkedList<Customer>()
+    private var lineOfLoanCustomer = LinkedList<Customer>()
     private var processedCustomer: Int = 0
     private var taskTime: Double = 0
     
@@ -34,33 +35,41 @@ struct Bank {
         let totalCustomer = Int.random(in: 10...30)
         
         listUpCustomer(totalCustomer)
-        startTask()
+//        startTask()
         selectMenu()
     }
     
     mutating private func listUpCustomer(_ customerCount: Int) {
         for customerIndex in 1...customerCount {
-//            lineOfCustomer.enqueue(value: Customer(waitingNumber: customerIndex))
+            switch randomTask() {
+            case .deposit:
+                lineOfDepositCustomer.enqueue(value: Customer(waitingNumber: customerIndex, purposeOfServie: .deposit))
+            case .loan:
+                lineOfLoanCustomer.enqueue(value: Customer(waitingNumber: customerIndex, purposeOfServie: .loan))
+            }
         }
     }
     
-    mutating private func startTask() {
-        taskTime = 0
-        processedCustomer = 0
-        
-        while lineOfCustomer.isEmpty == false {
-            guard let currentCustomer = lineOfCustomer.dequeue() else {
-                break
-            }
-
-            print("\(currentCustomer.waitingNumber)번 고객 업무 시작")
-            taskTime += 0.7
-            usleep(70_000)
-            processedCustomer += 1
-            print("\(currentCustomer.waitingNumber)번 고객 업무 종료")
-        }
-        
-        taskTime = round(taskTime * 100)/100
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(processedCustomer)명이며, 총 업무시간은 \(taskTime)초입니다.")
+    func randomTask() -> Task {
+        Int.random(in: 1...2) == 1 ? .deposit : .loan
     }
+    
+//    mutating private func startTask() {
+//        taskTime = 0
+//        processedCustomer = 0
+//
+//        while lineOfCustomer.isEmpty == false {
+//            guard let currentCustomer = lineOfCustomer.dequeue() else {
+//                break
+//            }
+//            print("\(currentCustomer.waitingNumber)번 고객 업무 시작")
+//            taskTime += 0.7
+//            usleep(70_000)
+//            processedCustomer += 1
+//            print("\(currentCustomer.waitingNumber)번 고객 업무 종료")
+//        }
+//
+//        taskTime = round(taskTime * 100)/100
+//        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(processedCustomer)명이며, 총 업무시간은 \(taskTime)초입니다.")
+//    }
 }
