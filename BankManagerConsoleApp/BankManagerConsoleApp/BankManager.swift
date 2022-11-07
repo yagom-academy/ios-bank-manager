0112a19bac0a1945b53c27a9db5056f7d079d62b
ash-youu <ash.you.dev@gmail.com>
2022-11-07 10:14:58 +0900

    refactor: BankManager의 총 고객수와 directBankWork 메서드 리네이밍

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -8,18 +8,18 @@
 struct BankManager {
     private let bankWorker: BankWorkable
     private let clientQueue: Queue<Client> = Queue()
-    private var cumulativeClientCount: Int = 0
+    private var totalClientCount: Int = 0
     
     init(bankWorker: BankWorkable) {
         self.bankWorker = bankWorker
     }
     
     mutating func addClientQueue(_ client: Client) {
-        cumulativeClientCount += 1
+        totalClientCount += 1
         clientQueue.enqueue(client)
     }
     
-    private func directBankWorker() {
+    private func directBankWork() {
         while !clientQueue.isEmpty {
             guard let client = clientQueue.dequeue() else { return }
             
@@ -28,17 +28,17 @@ struct BankManager {
     }
     
     func open() {
-        directBankWorker()
+        directBankWork()
     }
     
     mutating func close() {
-        let time: Double = Double(cumulativeClientCount) * 0.7
+        let time: Double = Double(totalClientCount) * 0.7
         let message: String =
         String(format: "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 %d명이며, 총 업무시간은 %.2f초 입니다.",
-               cumulativeClientCount,
+               totalClientCount,
                time)
         
         print("\(message)")
-        cumulativeClientCount = 0
+        totalClientCount = 0
     }
 }
