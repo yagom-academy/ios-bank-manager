c71bbcd7340c221cd707c9d8238d1d3a7b604247
wonbi92 <bin9239@gmail.com>
2022-11-07 20:14:57 +0900

    refactor : Bank 구조체 메서드 리팩토링
    - updateClientQueue() 메서드 내부 변수 네이밍 리팩토링
    - startBankWork() 메서드 내부에서 업무 시간을 체크하도록 리팩토링

--- BankManagerConsoleApp/BankManagerConsoleApp/Models/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Models/Bank/Bank.swift
@@ -11,6 +11,7 @@ struct Bank<Queue: ClientQueueable> {
     private let bankDispatchGroup = DispatchGroup()
     private let depositBooth = DispatchSemaphore(value: 2)
     private let loanBooth = DispatchQueue(label: "loanBanker")
+    
     private let banker: Banker
     private var clientQueue: Queue
     private var bankManager: BankManager = BankManager()
@@ -22,28 +23,30 @@ struct Bank<Queue: ClientQueueable> {
     
     mutating func openBank() {
         updateClientQueue()
-        bankManager.resetWorkTime()
         startBankWork()
-        bankDispatchGroup.wait()
-        bankManager.addWorkTime()
         endBankWork()
     }
     
     mutating private func updateClientQueue() {
         for number in 1...Int.random(in: 10...30) {
-            guard let randomWork = Client.Purpose.allCases.randomElement() else { return }
-            let client = Client(waitingTicket: number, purpose: randomWork)
+            guard let randomPurpose = Client.Purpose.allCases.randomElement() else { return }
+            let client = Client(waitingTicket: number, purpose: randomPurpose)
             clientQueue.enqueue(client)
             bankManager.addClientCount()
         }
     }
     
     mutating private func startBankWork() {
+        bankManager.resetWorkTime()
+        
         while !clientQueue.isEmpty {
             guard let client = clientQueue.dequeue() else { return }
             
             divideWork(client: client)
         }
+        
+        bankDispatchGroup.wait()
+        bankManager.addWorkTime()
     }
     
     private func divideWork(client: Client) {
