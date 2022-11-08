5dc4ca29e5bef1e60ee80dd5c83af97d2c52bde6
Ayaan <onestar7984@gmail.com>
2022-11-08 22:04:24 +0900

    feat: CustomerScrollView 및 CustomerQueueStackView 구현

---
+++ BankManagerUIApp/BankManagerUIApp/CustomerQueueStackView.swift
@@ -0,0 +1,34 @@
+//
+//  BankManagerUIApp - CustomerQueueStackView.swift
+//  Created by Ayaan, Som, Dragon.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import UIKit
+
+final class CustomerQueueStackView: UIStackView {
+    private let waitingCustomerScrollView: CustomerScrollView = CustomerScrollView()
+    private let workingCustomerScrollView: CustomerScrollView = CustomerScrollView()
+    
+    init() {
+        super.init(frame: .zero)
+        configure()
+        setSubView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func configure() {
+        self.alignment = .fill
+        self.distribution = .fillEqually
+        self.axis = .vertical
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setSubView() {
+        self.addArrangedSubview(waitingCustomerScrollView)
+        self.addArrangedSubview(workingCustomerScrollView)
+    }
+}
---
+++ BankManagerUIApp/BankManagerUIApp/CustomerScrollView.swift
@@ -0,0 +1,53 @@
+//
+//  BankManagerUIApp - CustomerScrollView.swift
+//  Created by Ayaan, Som, Dragon.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import UIKit
+
+final class CustomerScrollView: UIScrollView {
+    private let customerStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        configure()
+        setSubView()
+        setCustomerStackViewConstraints()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func configure() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setSubView() {
+        self.addSubview(customerStackView)
+    }
+    
+    private func setCustomerStackViewConstraints() {
+        NSLayoutConstraint.activate([
+            customerStackView.topAnchor.constraint(equalTo: self.contentLayoutGuide.topAnchor),
+            customerStackView.bottomAnchor.constraint(equalTo: self.contentLayoutGuide.bottomAnchor),
+            customerStackView.trailingAnchor.constraint(equalTo: self.contentLayoutGuide.trailingAnchor),
+            customerStackView.leadingAnchor.constraint(equalTo: self.contentLayoutGuide.leadingAnchor),
+            customerStackView.widthAnchor.constraint(equalTo: self.frameLayoutGuide.widthAnchor)
+        ])
+        
+        let heightConstraint: NSLayoutConstraint = customerStackView.heightAnchor.constraint(equalTo: self.frameLayoutGuide.heightAnchor)
+        heightConstraint.priority = .defaultLow
+        heightConstraint.isActive = true
+    }
+}
