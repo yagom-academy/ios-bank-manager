e857795db822d30e329af6d706b8748ae0f532f5
zhilly11 <zhzh1x4@gmail.com>
2022-11-08 16:22:00 +0900

    refactor : conflict resolved

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -7,19 +7,27 @@
 import Foundation
 
 struct Bank {
+    enum Constant {
+        static let depositTakenSeconds: Double = 0.7
+    }
+
     private var customerQueue: CustomerQueue<Customer>?
     private var isQueueEmpty: Bool {
-        return customerQueue?.isEmpty ?? false
+        return customerQueue?.isEmpty ?? true
     }
-    
+
+    init(customerQueue: CustomerQueue<Customer>? = nil) {
+        self.customerQueue = customerQueue
+    }
+
     mutating func createQueue() {
         customerQueue = CustomerQueue()
     }
-    
+
     mutating func addCustomer(customer: Customer) {
         customerQueue?.enqueue(value: customer)
     }
-    
+
     mutating func startWork() {
         var time: Double = 0
         var customerCount: Int = 0
@@ -30,22 +38,22 @@ struct Bank {
             }
 
             print("\(customer.ticketNumber)번 고객 업무 시작")
-            
+
             DispatchQueue.global().sync {
-                usleep(700000)
-                time += (700000 / 1000000)
+                Thread.sleep(forTimeInterval: Constant.depositTakenSeconds)
+                time += (Constant.depositTakenSeconds)
                 customerCount += 1
                 print("\(customer.ticketNumber)번 고객 업무 종료")
             }
         }
-        
+
         endWork(customerCount: customerCount, duringTime: time)
     }
-    
+
     private func endWork(customerCount: Int, duringTime: Double) {
-        print("업무를 마감합니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(duringTime.convertedTime())초입니다.")
+        print("업무를 마감합니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(duringTime.convertedNumberToTime)초입니다.")
     }
-    
+
     mutating func resetCustomerQueue() {
         customerQueue?.clear()
         customerQueue = nil
@@ -53,8 +61,8 @@ struct Bank {
 }
 
 private extension Double {
-    static let thousand = 1000.0
-    func convertedTime() -> Double {
-        return (self * Double.thousand).rounded() / Double.thousand
+    static let hundred: Self = 100.0
+    var convertedNumberToTime: String {
+        return String(format: "%.2f", (self * Self.hundred).rounded() / Self.hundred)
     }
 }
