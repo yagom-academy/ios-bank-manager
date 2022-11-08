6d2e4b5b8e55e66c616b2477c0c21d4a7bccb775
미니 <cow970814@naver.com>
2022-11-08 17:50:01 +0900

    feat : Bank 내 업무 순서 및 고객 수 세기 클로저 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -6,23 +6,13 @@
 
 import Foundation
 
-/*
- 1. 은행원의 수 구현 - 3명으로 제한
- 2. 업무 종류와 소요 시간 구현
- 
- 구현 방법
- - Semephore + DispatchQueue
- - OperationQueue
- 
- */
-
-
 struct Bank {
     enum Constant {
         static let depositTakenSeconds: Double = 0.7
     }
 
     private var customerQueue: CustomerQueue<Customer>?
+    
     private var isQueueEmpty: Bool {
         return customerQueue?.isEmpty ?? true
     }
@@ -39,41 +29,31 @@ struct Bank {
         customerQueue?.enqueue(value: customer)
     }
 
-    mutating func startWork() {
-        let depositQueue = OperationQueue()
-        let loansQueue = OperationQueue()
-        depositQueue.maxConcurrentOperationCount = 2
-        loansQueue.maxConcurrentOperationCount = 1
+    mutating func startWork() -> Int {
+        let waitingLines = setUpWaitingLine()
+        
+        var userCount = 0
         
         while !isQueueEmpty {
-            guard let customer = customerQueue?.dequeue() else { return }
+            guard let customer = customerQueue?.dequeue() else {
+                return 0
+            }
+            
             let customerOperation = BankerOperation(customer: customer)
+            
+            customerOperation.completionBlock = { userCount += 1 }
             
             switch customer.business {
             case .deposit:
-                depositQueue.addOperation(customerOperation)
-                
+                waitingLines.dispositQueue.addOperation(customerOperation)
             case .loans:
-                loansQueue.addOperation(customerOperation)
+                waitingLines.loansQueue.addOperation(customerOperation)
             }
-            
-            // 대기열에서 손님이 차례가 되어서 일어났다.
-            
-            // 손님은 해당되는 업무가 있기에 해당되는 직원을 찾아가야 한다.
-                // 손님의 업무가 예금이라면 예금 은행원이 있는 곳
-                // 손님의 업무가 대출이라면 대출 은행원이 있는 곳
-            
-            // 은행원은 이에 맞춰서 업무를 볼 수 있어야 한다.
-                // 즉, 대출업무와 예금 업무를 모두 볼 수 있어야 한다.
-            
-            // 은행 업무는 해당되는 시간만큼 걸리기 때문에 시간이 지난 후, 업무를 종료한다.
-            
-            // 은행원은 업무가 끝나는 즉시 다른 업무를 할 수 있어야 한다.
         }
-    }
-
-    private func endWork(customerCount: Int, duringTime: Double) {
-        print("업무를 마감합니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(duringTime.convertedNumberToTime)초입니다.")
+        waitingLines.dispositQueue.waitUntilAllOperationsAreFinished()
+        waitingLines.loansQueue.waitUntilAllOperationsAreFinished()
+        
+        return userCount
     }
 
     mutating func resetCustomerQueue() {
@@ -82,9 +62,14 @@ struct Bank {
     }
 }
 
-private extension Double {
-    static let hundred: Self = 100.0
-    var convertedNumberToTime: String {
-        return String(format: "%.2f", (self * Self.hundred).rounded() / Self.hundred)
+private extension Bank {
+    func setUpWaitingLine() -> (dispositQueue: OperationQueue, loansQueue: OperationQueue) {
+        let depositQueue = OperationQueue()
+        let loansQueue = OperationQueue()
+        
+        depositQueue.maxConcurrentOperationCount = 2
+        loansQueue.maxConcurrentOperationCount = 1
+        
+        return (depositQueue, loansQueue)
     }
 }
