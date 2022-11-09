89d63c7b386e4375fe4e27ac38aae422a4428c22
hashswim <ss991117@naver.com>
2022-11-09 19:51:19 +0900

    feat: loan, deposit 고객 큐를 분리 및 Dispatch Queue를 활용해 deposit, loan 업무 동시적 처리 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -9,6 +9,8 @@ import Foundation
 struct Bank {
     let clerks: [BankClerk]
     let customerQueue = Queue<Customer>()
+    let loanQueue = Queue<Customer>()
+    let depositQueue = Queue<Customer>()
     var customerCount: Int = 0
     
     init(clerkCount: Int) {
@@ -21,19 +23,58 @@ struct Bank {
     
     func startBankJob() {
         let startTime = CFAbsoluteTimeGetCurrent()
-        manageCustomer()
+        manageCustomerQueue()
+        serve()
         let endTime = CFAbsoluteTimeGetCurrent()
         let timeInterval = endTime - startTime
         
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(customerCount)명이며, 총 업무시간은 \(timeInterval.formattedNumber)초입니다.")
     }
     
-    func manageCustomer() {
+    func manageCustomerQueue() {
         while let customer = customerQueue.dequeue() {
-            clerks.first?.work(for: customer)
+            switch customer.serviceType {
+            case .deposit:
+                depositQueue.enqueue(customer)
+                
+            case .loan:
+                loanQueue.enqueue(customer)
+                
+            }
+        }
+    }
+    
+    func serve() {
+        let serialQueue = DispatchQueue(label: "serial")
+        
+        DispatchQueue.global().async {
+            while depositQueue.isEmpty == false {
+                serialQueue.sync {
+                    guard let customer = depositQueue.dequeue() else {
+                        return
+                    }
+                    clerks.first?.work(for: customer)
+                }
+            }
+        }
+        
+        DispatchQueue.global().async {
+            while depositQueue.isEmpty == false {
+                serialQueue.sync {
+                    guard let customer = depositQueue.dequeue() else {
+                        return
+                    }
+                    clerks.first?.work(for: customer)
+                }
+            }
+        }
+        
+        DispatchQueue.global().async {
+            while let customer = loanQueue.dequeue() {
+                clerks.first?.work(for: customer)
+            }
         }
     }
     
     func close() {}
-    
 }
