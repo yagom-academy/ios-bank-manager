364546beeb22bfd7e1445df901587d5116ddfa12
yjjem <marksim0606@gmail.com>
2022-11-10 20:17:46 +0900

    feat (WorkLoadManager): makeDispatchWorkItem 메서드 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/WorkLoadManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/WorkLoadManager.swift
@@ -9,6 +9,36 @@ import Foundation
 struct WorkLoadManager {
     var taskQueue: CustomerQueue = CustomerQueue<Int>()
     let loanQueue: DispatchQueue = DispatchQueue(label: "loanQueue", attributes: .concurrent)
+    let loanSemaphore = DispatchSemaphore(value: 1)
     let depositQueue: DispatchQueue = DispatchQueue(label: "depositQueue", attributes: .concurrent)
-
+    let depositSemaphore = DispatchSemaphore(value: 2)
+    
+    func makeDispatchWorkItem(number: Int) -> (Task, DispatchWorkItem)? {
+        guard let task = Task(rawValue: Int.random(in: 1...2)) else { return nil }
+        
+        switch task {
+        case .deposit:
+            let deposit = DispatchWorkItem {
+                depositSemaphore.wait()
+                print("\(number)번째 고객, \(task)업무 진행")
+                usleep(700000)
+                depositSemaphore.signal()
+            }
+            deposit.notify(queue: depositQueue) {
+                print("\(number)번째 고객, \(task)업무 완료")
+            }
+            return (task, deposit)
+        case .loan:
+            let loan = DispatchWorkItem {
+                loanSemaphore.wait()
+                print("\(number)번째 고객, \(task)업무 진행")
+                usleep(1100000)
+                loanSemaphore.signal()
+            }
+            loan.notify(queue: depositQueue) {
+                print("\(number)번째 고객, \(task)업무 완료")
+            }
+            return (task, loan)
+        }
+    }
 }
