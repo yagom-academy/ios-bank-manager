978e38ba153305041b31a743aea61ac1d4aac13d
junho <junhoL1046@gmail.com>
2022-11-10 10:30:37 +0900

    refactor: 네이밍 수정 및 공백 제거, 코드 순서 변경
    - totalCustomerNumber -> totalCustomerCount
    - 불필요한 공백 제거
    - formattedProcessedTotalTime 연산 프로퍼티의 위치를 메서드 안으로 이동
    - private 메서드를 맨 아래로 이동

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -10,27 +10,17 @@ import Foundation
 struct Bank {
     private var customerQueue: Queue<Customer> = Queue()
     private var bankers: [Banker] = []
-    private var totalCustomerNumber: Int = 0
+    private var totalCustomerCount: Int = 0
     private var totalTime: Double = 0
-    private var formattedProcessedTotalTime: String {
-        return String(format: "%.2f", totalTime)
-    }
     
     mutating func addCustomerToQueue(_ customer: Customer) {
         customerQueue.enqueue(customer)
     }
     
     mutating func addBanker(_ banker: Banker) {
-        
-        
         bankers.append(banker)
     }
-    
-    private mutating func printClosingMessage() {
-        let processingClosedMessage: String = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerNumber)명이며, 총 업무시간은 \(formattedProcessedTotalTime)초입니다."
-        print(processingClosedMessage)
-    }
-    
+      
     mutating func startBankBusiness() {
         while customerQueue.isEmpty == false {
             for banker in bankers {
@@ -38,10 +28,16 @@ struct Bank {
                     break
                 }
                 banker.processBankingBusiness(of: customer)
-                self.totalCustomerNumber += 1
+                self.totalCustomerCount += 1
                 self.totalTime += banker.processingTimePerCustomer
             }
         }
         printClosingMessage()
+    }
+    
+    private mutating func printClosingMessage() {
+        let formattedProcessedTotalTime: String = String(format: "%.2f", totalTime)
+        let processingClosedMessage: String = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerCount)명이며, 총 업무시간은 \(formattedProcessedTotalTime)초입니다."
+        print(processingClosedMessage)
     }
 }
