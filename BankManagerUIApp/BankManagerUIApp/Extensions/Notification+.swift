56432113d0f555e713fea1fcd4c8313fd8b67cdd
wonbi92 <bin9239@gmail.com>
2022-11-13 23:39:47 +0900

    chore : Extention 폴더 오류 정리

---
+++ BankManagerUIApp/BankManagerUIApp/Extensions/Notification+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+.swift
+//  BankManagerUIApp
+//
+//  Created by Kyo, Wonbi on 2022/11/13.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let client = Notification.Name("Client")
+}
