9184d3288d90c8374b4e85ec896247900190548b
kaki <ycg02109@naver.com>
2023-03-06 15:49:59 +0900

    feat: dequeue 테스트코드 작성

--- BankManagerUIApp/QueueTests/QueueTests.swift
+++ BankManagerUIApp/QueueTests/QueueTests.swift
@@ -21,23 +21,56 @@ final class QueueTests: XCTestCase {
         sut = nil
     }
 
-    func test_Queue가비어있을때_isEmpty가_true를반환한다() {
-        //given
+    func test_Queue가_비어있을때_isEmpty가_true를_반환한다() {
+        // given
         let expectation = true
-        //when
+        // when
         let result = sut.isEmpty
-        //then
+        // then
         XCTAssertEqual(result, expectation)
     }
     
-    func test_enqueue되었을경우_false를반환한다() {
-        //given
+    func test_enqueue되었을경우_false를_반환한다() {
+        // given
         let expectation = false
-        //when
+        // when
         sut.enqueue("hello")
         let result = sut.isEmpty
-        //then
+        // then
         XCTAssertEqual(result, expectation)
     }
     
+    func test_Queue가_비어있을때_dequeue할경우_nil이_반환된다() {
+        // given
+        let expectation: String? = nil
+        // when
+        let result = sut.dequeue()
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_dequeue됐을경우_처음_enqueue된_데이터가_반환된다() {
+        // given
+        let expectation = "hello"
+        // when
+        sut.enqueue("hello")
+        sut.enqueue("kaki")
+        sut.enqueue("songjun")
+        let result = sut.dequeue()!
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_2번_dequeue됐을경우_두번째로_enqueue된_데이터가_반환된다() {
+        // given
+        let expectation = "kaki"
+        // when
+        sut.enqueue("hello")
+        sut.enqueue("kaki")
+        sut.enqueue("songjun")
+        let _ = sut.dequeue()
+        let result = sut.dequeue()!
+        // then
+        XCTAssertEqual(result, expectation)
+    }
 }
