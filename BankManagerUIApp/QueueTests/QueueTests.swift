a8cea12f6681a035e5a35bbf522648dca0bba32d
kimseongj <ksj8277@naver.com>
2023-03-06 16:04:33 +0900

    feat: Queue에서 peek과 testClear 테스트코드 작성

--- BankManagerUIApp/QueueTests/QueueTests.swift
+++ BankManagerUIApp/QueueTests/QueueTests.swift
@@ -15,12 +15,12 @@ final class QueueTests: XCTestCase {
         try super.setUpWithError()
         sut = Queue()
     }
-
+    
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         sut = nil
     }
-
+    
     func test_Queue가_비어있을때_isEmpty가_true를_반환한다() {
         // given
         let expectation = true
@@ -30,7 +30,7 @@ final class QueueTests: XCTestCase {
         XCTAssertEqual(result, expectation)
     }
     
-    func test_enqueue되었을경우_false를_반환한다() {
+    func test_enqueue되었을경우_isEmpty가_false를_반환한다() {
         // given
         let expectation = false
         // when
@@ -70,6 +70,44 @@ final class QueueTests: XCTestCase {
         sut.enqueue("songjun")
         let _ = sut.dequeue()
         let result = sut.dequeue()!
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_peek했을경우_dequeue될_데이터가_반환된다() {
+        // given
+        let expectation = "hello"
+        // when
+        sut.enqueue("hello")
+        sut.enqueue("kaki")
+        sut.enqueue("songjun")
+        let result = sut.peek()!
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_peek이후에_dequeue될경우_처음enqueue된_데이터가_반환된다() {
+        // given
+        let expectation = "hello"
+        // when
+        sut.enqueue("hello")
+        sut.enqueue("kaki")
+        sut.enqueue("songjun")
+        let _ = sut.peek()
+        let result = sut.dequeue()!
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_clear했을경우_isEmpty가_true를_반환한다() {
+        // given
+        let expectation = true
+        // when
+        sut.enqueue("hello")
+        sut.enqueue("kaki")
+        sut.enqueue("songjun")
+        sut.clear()
+        let result = sut.isEmpty
         // then
         XCTAssertEqual(result, expectation)
     }
