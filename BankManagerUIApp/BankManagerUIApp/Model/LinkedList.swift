3546eb4a65281e0a37c0b873e52311caa866ca1a
yijiye <jiyee4013@naver.com>
2023-03-06 14:20:18 +0900

    feat: LinkedList enqueue 구현

--- BankManagerUIApp/BankManagerUIApp/Model/LinkedList.swift
+++ BankManagerUIApp/BankManagerUIApp/Model/LinkedList.swift
@@ -6,9 +6,25 @@
 //
 
 struct LinkedList<T> {
-    var head: Node<T>?
-    var tail: Node<T>?
-    var isEmpty: Bool {
+    private(set) var head: Node<T>?
+    private(set) var tail: Node<T>?
+    private var isEmpty: Bool {
         return head == nil
+    }
+    
+    mutating func enqueue(_ data: T) {
+        if isEmpty {
+            head = Node(data: data)
+            tail = head
+        }
+        
+        var node = head
+        
+        while node?.next != nil {
+            node = node?.next
+        }
+        
+        node?.next = Node(data: data)
+        tail = node?.next
     }
 }
--- BankManagerUIApp/BankManagerUIApp/Model/Node.swift
+++ BankManagerUIApp/BankManagerUIApp/Model/Node.swift
@@ -9,7 +9,7 @@ final class Node<T> {
     let data: T
     var next: Node<T>?
     
-    init(data: T, next: Node<T>?) {
+    init(data: T, next: Node<T>? = nil) {
         self.data = data
         self.next = next
     }
