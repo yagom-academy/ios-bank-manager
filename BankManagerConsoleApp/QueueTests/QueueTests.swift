8ed755f4c97794db85bacd9a3332a224e2c8e174
vetto <gzzjk159@naver.com>
2023-03-06 17:04:35 +0900

    feat: dequeue, clear, peek 메서드 테스트 작성

--- BankManagerConsoleApp/QueueTests/QueueTests.swift
+++ BankManagerConsoleApp/QueueTests/QueueTests.swift
@@ -16,13 +16,13 @@ final class QueueTests: XCTestCase {
         
         sut = Queue()
     }
-
+    
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         
         sut = nil
     }
-
+    
     func test_비어있는큐에서isEmpty는_true이다() {
         // given
         
@@ -79,6 +79,83 @@ final class QueueTests: XCTestCase {
         // when
         let result = sut.linkedList.tail?.data
         let expectation = "tail"
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_빈큐에서dequeue호출시_nil이반환된다() {
+        // given
+        
+        // when
+        let result = sut.dequeue()
+        
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_비어있지않은큐에서dequeue호출시_처음enqueue한값이반환된다() {
+        // given
+        sut.enqueue("head")
+        sut.enqueue("middle")
+        sut.enqueue("tail")
+        
+        // when
+        let result = sut.dequeue()
+        let expectation = "head"
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_비어있지않은큐에서dequeue호출시_head가head의next로바뀐다() {
+        // given
+        sut.enqueue("head")
+        sut.enqueue("headNext")
+        sut.enqueue("tail")
+        let _ = sut.dequeue()
+        
+        // when
+        let result = sut.linkedList.head?.data
+        let expectation = "headNext"
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_비어있지않은큐에서clear호출시_isEmpty는true이다() {
+        // given
+        sut.enqueue("head")
+        sut.enqueue("headNext")
+        sut.enqueue("tail")
+        sut.clear()
+        
+        // when
+        let result = sut.isEmpty
+        
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_빈큐에서peek호출시_nil이반환된다() {
+        // given
+        
+        // when
+        let result = sut.peek()
+        
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_비어있지않은큐에서peek호출시_처음enqueue한값이반환된다() {
+        // given
+        sut.enqueue("head")
+        sut.enqueue("middle")
+        sut.enqueue("tail")
+        
+        // when
+        let result = sut.peek()
+        let expectation = "head"
         
         // then
         XCTAssertEqual(result, expectation)
