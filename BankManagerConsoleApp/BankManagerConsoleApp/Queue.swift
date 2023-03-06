d8a3412e200fac76a03d287fc9b9e89e0a93c00e
Jun <59365211+juun97@users.noreply.github.com>
2023-03-06 14:58:20 +0900

    feat : Queue 타입 정의 및 enqueue 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -2,7 +2,13 @@
 //  Queue.swift
 //  BankManagerConsoleApp
 //
-//  Created by Rowan on 2023/03/06.
+//  Created by Rowan, 릴라 on 2023/03/06.
 //
 
-import Foundation
+struct CustomerQueue<T> {
+    private var list: LinkedList<T> = LinkedList()
+    
+    mutating func enqueue(_ data: T) {
+        list.append(data: data)
+    }
+}
