e0a5aa9c1041e5c7c37cf24a01266cac22f803ec
whdghks8654 <62684851+whdghks8654@users.noreply.github.com>
2023-03-06 15:00:33 +0900

    feat: BankQueue - Node 클래스 생성 및 linkedList 구조생성

--- BankManagerUIApp/BankManagerUIApp/BankQueue.swift
+++ BankManagerUIApp/BankManagerUIApp/BankQueue.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct BankQueue<T> {
+    final class Node<T> {
+        private var data: T?
+        private var next: Node?
+        
+        init(data: T?, next: Node? = nil) {
+            self.data = data
+            self.next = next
+        }
+    }
+    private var head: Node<T>?
+}
