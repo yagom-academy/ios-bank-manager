2cbd5dda3c4c2ce4ca9975fa22e3ef4f9bc97d41
kimseongj <ksj8277@naver.com>
2023-03-07 11:43:33 +0900

    feat: 메뉴 while문 구현

--- BankManager.swift
+++ BankManager.swift
@@ -1,6 +1,6 @@
 //
 //  BankManager.swift
-//  Created by yagom.
+//  Created by kaki, songjun.
 //  Copyright © yagom academy. All rights reserved.
 //
 
--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -1,7 +1,21 @@
 //
 //  BankManagerConsoleApp - main.swift
-//  Created by yagom. 
+//  Created by kaki, songjun. 
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
+var bankManager = BankManager()
+
+
+while true {
+    print("1 : 은행개점\n2 : 종료")
+    print("입력 : ", terminator: "")
+    guard let userInput = readLine() else { break }
+    
+    if userInput == "1" {
+        bankManager.setupWaitingQueueAndClientNumber()
+        bankManager.startBusiness()
+    } else if userInput == "2" {
+        break
+    }
+}
