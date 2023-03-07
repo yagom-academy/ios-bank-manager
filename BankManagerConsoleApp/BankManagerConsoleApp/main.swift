e3839960223d0574dafeb300c6a433aa76dbd7ad
Andrew-0411 <whk1140@naver.com>
2023-03-07 15:29:19 +0900

    feat: ConsoleApp 입출력 STEP2 요구사항에 맞게 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,21 @@
 // 
 
 import Foundation
+
+print("1 : 은행개점")
+print("2 : 종료")
+print("입력 : ", terminator: "")
+let inputNumber = readLine()!
+
+if inputNumber == "1" {
+    let customer = Int.random(in: 10...30)
+    for num in 1...customer {
+        print("\(num)번 고객 업무 시작")
+        print("\(num)번 고객 업무 종료")
+    }
+    print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총\(customer)명이며, 총 업무시간은 초입니다.")
+} else if inputNumber == "2" {
+    
+} else {
+    print("입력이 잘못 되었습니다.")
+}
