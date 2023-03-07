dfb924e8d2c976e06eac7bc10933d0fd279c6ffb
kimseongj <ksj8277@naver.com>
2023-03-07 10:48:43 +0900

    feat: BankManager 구조체 생성, updateWaitingQueue 구현, dequeueWaitingQueue 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,23 @@
 //
 
 import Foundation
+
+struct BankManager {
+    var clientNumber: Int = Int.random(in: 10...30)
+    var waitingQueue = Queue<String>()
+    
+    mutating func updateWaitingQueue() {
+        for number in 1...clientNumber {
+            waitingQueue.enqueue("\(number)번 고객")
+        }
+    }
+    
+    mutating func dequeueWaitingQueue() {
+        while !waitingQueue.isEmpty {
+            guard let client = waitingQueue.dequeue() else { return }
+            print("\(client) 업무 시작")
+            Thread.sleep(forTimeInterval: 0.7)
+            print("\(client) 업무 완료")
+        }
+    }
+}
