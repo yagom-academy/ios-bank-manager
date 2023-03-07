3aa54407a8f3a84d4d4ea7798e8bff3add77b567
ROWAN <kimkj93@naver.com>
2023-03-07 11:04:13 +0900

    refactor: CustomerQueue test case 수정 및 추가

--- BankManagerConsoleApp/UnitTest/CustomerQueueTest.swift
+++ BankManagerConsoleApp/UnitTest/CustomerQueueTest.swift
@@ -18,19 +18,6 @@ final class CustomerQueueTest: XCTestCase {
         sut = nil
     }
 
-    func test_enqueue호출시_list의count가_plus1이된다() {
-        // given
-        let testValue = 10
-        let expectedResult = 1
-
-        // when
-        sut?.enqueue(testValue)
-        let result = sut?.count
-
-        // then
-        XCTAssertEqual(expectedResult, result)
-    }
-
     func test_dequeue호출시_Queue의_첫번째값이_반환된다() {
         // given
         sut?.enqueue(5)
@@ -57,7 +44,7 @@ final class CustomerQueueTest: XCTestCase {
         // then
         XCTAssertEqual(expectedResult, result)
     }
-
+    
     func test_isEmpty호출시_Queue가_비어있다면_true를반환한다() {
         // given
         let expectedResult = true
@@ -75,6 +62,32 @@ final class CustomerQueueTest: XCTestCase {
         let expectedResult = false
 
         // when
+        let result = sut?.isEmpty
+
+        // then
+        XCTAssertEqual(expectedResult, result)
+    }
+
+    func test_enqueue호출시_전달한값이_정상적으로들어간다() {
+        // given
+        let testValue = 10
+        let expectedResult = 10
+
+        // when
+        sut?.enqueue(testValue)
+        let result = sut?.dequeue()
+
+        // then
+        XCTAssertEqual(expectedResult, result)
+    }
+    
+    func test_enqueue호출시_isEmpty가_false이다() {
+        // given
+        let testValue = 10
+        let expectedResult = false
+
+        // when
+        sut?.enqueue(testValue)
         let result = sut?.isEmpty
 
         // then
