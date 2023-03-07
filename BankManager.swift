5144edfcead9178e4d83d38d8bf3a7bead98c43f
kaki <ycg02109@naver.com>
2023-03-07 11:33:31 +0900

    feat: Queue size 프로퍼티 추가, BankManager 로직 수정 및 함수명 변경

--- BankManager.swift
+++ BankManager.swift
@@ -7,21 +7,26 @@
 import Foundation
 
 struct BankManager {
-    var clientNumber: Int = Int.random(in: 10...30)
+    var clientNumber = 0
     var waitingQueue = Queue<String>()
     
-    mutating func updateWaitingQueue() {
-        for number in 1...clientNumber {
+    mutating func setupWaitingQueueAndClientNumber() {
+        for number in 1...Int.random(in: 10...30) {
             waitingQueue.enqueue("\(number)번 고객")
         }
+        
+        clientNumber = waitingQueue.size
     }
     
-    mutating func dequeueWaitingQueue() {
+    mutating func startBusiness() {
         while !waitingQueue.isEmpty {
             guard let client = waitingQueue.dequeue() else { return }
             print("\(client) 업무 시작")
             Thread.sleep(forTimeInterval: 0.7)
             print("\(client) 업무 완료")
         }
+        
+        let businessHours = Double(clientNumber) * 0.7
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(clientNumber)명이며, 총 엄무시간은 \(String(format: "%.2f", businessHours))초입니다.")
     }
 }
