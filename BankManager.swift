61044639ebe98f4137fabc36ae828f183b85be44
parkmuri <smxhmu@naver.com>
2023-03-08 13:25:43 +0900

    feat: BankManager 타입 및 openBank메서드, startBankApp메서드 구현, todayClient 변수명 리네이밍

--- BankManager.swift
+++ BankManager.swift
@@ -1,7 +1,38 @@
 //
 //  BankManager.swift
-//  Created by yagom.
+//  Created by 리지, 무리.
 //  Copyright © yagom academy. All rights reserved.
 //
 
 import Foundation
+
+struct BankManager {
+    private var bank = Bank()
+    
+    mutating private func openBank() {
+        bank.lineUpClient()
+        bank.doTask()
+        startBankApp()
+    }
+    
+    mutating func startBankApp() {
+        let open = "1"
+        let close = "2"
+        let noticeMenu = "1 : 은행 개점 \n2 : 종료 \n입력 : "
+        let inputError = "입력이 잘못되었습니다."
+        
+        print(noticeMenu, terminator: "")
+        
+        guard let input = readLine() else { return }
+        
+        switch input {
+        case open:
+            openBank()
+        case close:
+            return
+        default:
+            print(inputError)
+            startBankApp()
+        }
+    }
+}
--- BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/Model/Bank.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct Bank {
     var waitingLine = Queue<Client>()
-    var todayClient: Int = 10
+    var clientCount: Int = 10
     var BankClerkCount: Int = 1
     var bankClerk = BankClerk()
 
     mutating func lineUpClient() {
-        todayClient = Int.random(in: 10...30)
-        for number in 1...todayClient {
+        clientCount = Int.random(in: 10...30)
+        for number in 1...clientCount {
             let currentClient = Client(number: number)
             waitingLine.enqueue(currentClient)
         }
@@ -31,7 +31,7 @@ struct Bank {
         
         let timeOfTask = CFAbsoluteTimeGetCurrent() - startTime
         let totalTime = String(format: "%.2f", floor(timeOfTask))
-        let success = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(todayClient)명이며, 총 업무시간은 \(totalTime)초입니다."
+        let success = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(clientCount)명이며, 총 업무시간은 \(totalTime)초입니다."
         print(success)
     }
 }
