34f3a9786220a2acda221d29c89034a89a3e49df
yijiye <jiyee4013@naver.com>
2023-03-08 13:41:55 +0900

    refactor: totalTime floor 삭제

--- BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/Model/Bank.swift
@@ -30,7 +30,7 @@ struct Bank {
         }
         
         let timeOfTask = CFAbsoluteTimeGetCurrent() - startTime
-        let totalTime = String(format: "%.2f", floor(timeOfTask))
+        let totalTime = String(format: "%.2f", timeOfTask)
         let success = "업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(clientCount)명이며, 총 업무시간은 \(totalTime)초입니다."
         print(success)
     }
