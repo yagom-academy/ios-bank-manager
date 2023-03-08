a1d0c77b7284798d2bc938f49ff2579da7c7c014
Rhode-park <jinah.park.16@gmail.com>
2023-03-08 13:30:47 +0900

    feat: Bank 구조체 및 은행개점을 알리는 openBank, startWork 메서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -5,3 +5,30 @@
 //
 
 import Foundation
+
+struct Bank {
+    private enum BankStatus: String {
+        case open = "1"
+        case close = "2"
+    }
+    
+    func openBank() {
+        print("1 : 은행개점 \n2 : 종료\n입력 :", terminator: " ")
+        guard let status = readLine(),
+              let bankStatus = BankStatus(rawValue: status) else {
+            print("입력이 잘못되었습니다.")
+            openBank()
+            return
+        }
+        startWork(bankStatus)
+    }
+    
+    private func startWork(_ status: BankStatus) {
+        switch status {
+        case .open:
+            return
+        case .close:
+            return
+        }
+    }
+}
