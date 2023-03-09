9f45e78965b6f979d055adc1925ff818a3cac1f8
Jun <59365211+juun97@users.noreply.github.com>
2023-03-09 14:30:29 +0900

    feat : Business 타입 정의 및 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Business.swift
@@ -0,0 +1,20 @@
+//
+//  Business.swift
+//  BankManagerConsoleApp
+//
+//  Created by Rowan, 릴라 on 2023/03/09.
+//
+
+enum Business {
+    case loan
+    case deposit
+    
+    var processtime: Double {
+        switch self {
+        case .loan:
+            return 0.7
+        case .deposit:
+            return 1.1
+        }
+    }
+}
