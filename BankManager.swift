c3486726b5304f1c149b5e27b9f710cc2732181f
kimseongj <ksj8277@naver.com>
2023-03-09 15:36:13 +0900

    refactor: BankManager 구조체 내부의 clientNumber 변수명을 numberOfClient로 수정, setupWaitingQueueAndClientNumber 메서드 내에 랜덤값 상수 선언

--- BankManager.swift
+++ BankManager.swift
@@ -7,15 +7,17 @@
 import Foundation
 
 struct BankManager {
-    private var clientNumber = 0
+    private var numberOfClient = 0
     private var waitingQueue = Queue<String>()
     
     mutating func setupWaitingQueueAndClientNumber() {
-        for number in 1...Int.random(in: 10...30) {
+        let randomNumberOfClient = Int.random(in: 10...30)
+        
+        for number in 1...randomNumberOfClient {
             waitingQueue.enqueue("\(number)번 고객")
         }
         
-        clientNumber = waitingQueue.size
+        numberOfClient = waitingQueue.size
     }
     
     mutating func startBusiness() {
@@ -26,7 +28,7 @@ struct BankManager {
             print("\(client) 업무 완료")
         }
         
-        let businessHours = Double(clientNumber) * 0.7
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(clientNumber)명이며, 총 엄무시간은 \(String(format: "%.2f", businessHours))초입니다.")
+        let businessHours = Double(numberOfClient) * 0.7
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfClient)명이며, 총 엄무시간은 \(String(format: "%.2f", businessHours))초입니다.")
     }
 }
