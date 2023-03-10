6456b8307440b02548285d300302c05fc49840ca
sehong <tjsgh0801@naver.com>
2023-03-10 14:40:40 +0900

    feat: Client 구조체 BankingType , 소요시간 Property 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Client.swift
@@ -5,5 +5,19 @@
 //
 
 struct Client {
+    enum BankingType: String {
+        case deposit = "예금"
+        case loan = "대출"
+    }
+    
     let clientWaitingNumber: Int
+    let bankingType: BankingType
+    var bankingTime: Double {
+        switch bankingType {
+        case .deposit:
+            return 0.7
+        case .loan:
+            return 1.1
+        }
+    }
 }
