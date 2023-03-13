093633d06a692d2da8281120ed13eb66a6206605
minsu3007 <minsu3007@gmail.com>
2023-03-13 15:46:55 +0900

    init: 스토리보드 제거 및 환경 설정

--- BankManager.swift
+++ BankManager.swift
@@ -3,19 +3,19 @@
 //  Created by 레옹아범, 혜모리.
 //  Copyright © yagom academy. All rights reserved.
 //
-import Foundation
-
-struct BankManager {
-    static func work(for customer: Customer, duty: Banking) {
-        let workQueue = DispatchQueue(label: "WorkQueue")
-        let time = duty == .deposit ? BankOption.processingTimeAtDeposit : BankOption.processingTimeAtLoan
-        
-        let queueItem = DispatchWorkItem {
-            print("\(customer.waitingNumber)번 고객 \(duty.rawValue)업무 시작")
-            Thread.sleep(forTimeInterval: time)
-            print("\(customer.waitingNumber)번 고객 \(duty.rawValue)업무 완료")
-        }
-        
-        workQueue.sync(execute: queueItem)
-    }
-}
+//import Foundation
+//
+//struct BankManager {
+//    static func work(for customer: Customer, duty: Banking) {
+//        let workQueue = DispatchQueue(label: "WorkQueue")
+//        let time = duty == .deposit ? BankOption.processingTimeAtDeposit : BankOption.processingTimeAtLoan
+//
+//        let queueItem = DispatchWorkItem {
+//            print("\(customer.waitingNumber)번 고객 \(duty.rawValue)업무 시작")
+//            Thread.sleep(forTimeInterval: time)
+//            print("\(customer.waitingNumber)번 고객 \(duty.rawValue)업무 완료")
+//        }
+//
+//        workQueue.sync(execute: queueItem)
+//    }
+//}
