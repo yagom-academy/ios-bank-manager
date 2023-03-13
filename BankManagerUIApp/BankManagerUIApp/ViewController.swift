4ad50f29144a15c52c0715a42b1faf88b550d707
parkmuri <smxhmu@naver.com>
2023-03-13 17:44:01 +0900

    feat: QueueStackView 및 waitStackView, doTaskStackView 구현

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.backgroundColor = .white
         stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fill
 
         return stackView
     }()
@@ -46,8 +48,56 @@ class ViewController: UIViewController {
     
     private let taskTimerLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
         label.font = UIFont.preferredFont(forTextStyle: .title2)
+        label.text = "업무시간 - "
+        
+        return label
+    }()
+    
+    private let queueStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        //stackView.setContentCompressionResistancePriority(UILayoutPriority.1, for: .horizontal)
+        return stackView
+    }()
+    
+    private let waitQueueStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+
+        return stackView
+    }()
+    
+    private let waitQueueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.text = "대기중"
+        label.textColor = .white
+        label.backgroundColor = .systemGreen
+        
+        return label
+    }()
+    
+    private let doTaskStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+
+        return stackView
+    }()
+    
+    private let doTaskLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.text = "업무중"
+        label.backgroundColor = .systemIndigo
+        label.textColor = .white
         
         return label
     }()
@@ -64,12 +114,23 @@ class ViewController: UIViewController {
         buttonStackView.addArrangedSubview(addClientsButton)
         buttonStackView.addArrangedSubview(resetButton)
         screenStackView.addArrangedSubview(taskTimerLabel)
+        screenStackView.addArrangedSubview(queueStackView)
+        queueStackView.addArrangedSubview(waitQueueStackView)
+        queueStackView.addArrangedSubview(doTaskStackView)
+        waitQueueStackView.addArrangedSubview(waitQueueLabel)
+        doTaskStackView.addArrangedSubview(doTaskLabel)
+        
         NSLayoutConstraint.activate([
             screenStackView.topAnchor.constraint(equalTo: view.topAnchor),
             screenStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             screenStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             screenStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            buttonStackView.topAnchor.constraint(equalTo: screenStackView.topAnchor, constant: 50)
+            buttonStackView.topAnchor.constraint(equalTo: screenStackView.topAnchor, constant: 50),
+            taskTimerLabel.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor),
+            taskTimerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            queueStackView.topAnchor.constraint(equalTo: taskTimerLabel.bottomAnchor),
+            queueStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            queueStackView.bottomAnchor.constraint(equalTo: screenStackView.bottomAnchor)
         ])
 
     }
