316682e1a0023b90426c872d1e28029a296a8af9
minsu3007 <minsu3007@gmail.com>
2023-03-13 16:45:11 +0900

    feat: 전체 스택뷰 구현 및, 업무시간 Label 구현

--- BankManagerUIApp/BankManagerUIApp/MainViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/MainViewController.swift
@@ -9,17 +9,20 @@ import UIKit
 class MainViewController: UIViewController {
     private let addCustomerButton = CustomButton(type: .system)
     private let resetButton = CustomButton(type: .system)
+    private let stackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        configureStackView()
         configureLayout()
         configureUI()
     }
 
     private func configureLayout() {
         createButtonStackView()
+        createWorkTimeLabel()
     }
     
     private func configureUI() {
@@ -28,18 +31,46 @@ class MainViewController: UIViewController {
         resetButton.setTitleColor(.red, for: .normal)
     }
     
+    private func configureStackView() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 16
+        
+        self.view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
     private func createButtonStackView() {
         let stackView = UIStackView(arrangedSubviews: [addCustomerButton, resetButton])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
-        view.addSubview(stackView)
+
+        self.stackView.addArrangedSubview(stackView)
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.stackView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: self.stackView.trailingAnchor),
         ])
+    }
+    
+    private func createWorkTimeLabel() {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = false
+        label.text = "업무시간 - 04:00:123"
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.numberOfLines = 1
+        
+        self.stackView.addArrangedSubview(label)
     }
 }
 
