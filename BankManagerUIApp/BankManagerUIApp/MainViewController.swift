242e9c7043b81e613066d73a9aa38125356f36d5
hyemory <cjstksla73@gmail.com>
2023-03-13 17:23:51 +0900

    feat: QueueStackView 메서드 구현

--- BankManagerUIApp/BankManagerUIApp/MainViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/MainViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 class MainViewController: UIViewController {
     private let addCustomerButton = CustomButton(type: .system)
     private let resetButton = CustomButton(type: .system)
-    private let stackView = UIStackView()
+    private let mainStackView = UIStackView()
+    private let waitingStackView = UIStackView()
+    private let workingStackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .white
         configureStackView()
         configureLayout()
@@ -23,6 +24,9 @@ class MainViewController: UIViewController {
     private func configureLayout() {
         createButtonStackView()
         createWorkTimeLabel()
+        
+        createWaitingTitleLabel()
+        createWorkingTitleLabel()
     }
     
     private func configureUI() {
@@ -30,20 +34,23 @@ class MainViewController: UIViewController {
         resetButton.setTitle("초기화", for: .normal)
         resetButton.setTitleColor(.red, for: .normal)
     }
-    
+}
+
+// MARK: - UIStackView
+extension MainViewController {
     private func configureStackView() {
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.distribution = .fill
-        stackView.alignment = .center
-        stackView.spacing = 16
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fill
+        mainStackView.alignment = .center
+        mainStackView.spacing = 16
         
-        self.view.addSubview(stackView)
+        view.addSubview(mainStackView)
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
     }
     
@@ -53,14 +60,34 @@ class MainViewController: UIViewController {
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
 
-        self.stackView.addArrangedSubview(stackView)
+        mainStackView.addArrangedSubview(stackView)
         
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: self.stackView.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: self.stackView.trailingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
         ])
     }
     
+    private func createWaitingLabelStackView() {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 4
+        
+        stackView.addArrangedSubview(addWaitingLabel())
+        waitingStackView.addArrangedSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
+        ])
+    }
+}
+
+// MARK: - UILabel
+extension MainViewController {
     private func createWorkTimeLabel() {
         let label = UILabel()
         
@@ -70,7 +97,48 @@ class MainViewController: UIViewController {
         label.font = .preferredFont(forTextStyle: .title2)
         label.numberOfLines = 1
         
-        self.stackView.addArrangedSubview(label)
+        mainStackView.addArrangedSubview(label)
+    }
+    
+    private func createWaitingTitleLabel() {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = false
+        label.text = "대기중"
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.numberOfLines = 1
+        label.textColor = .white
+        label.backgroundColor = .systemGreen
+        
+        waitingStackView.addArrangedSubview(label)
+    }
+    
+    private func addWaitingLabel() -> UILabel {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = false
+        label.text = "1 - 예금"
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.numberOfLines = 1
+        
+        waitingStackView.addArrangedSubview(label)
+        
+        return label
+    }
+    
+    private func createWorkingTitleLabel() {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = false
+        label.text = "업무중"
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.numberOfLines = 1
+        label.textColor = .white
+        label.backgroundColor = .systemIndigo
+        
+        workingStackView.addArrangedSubview(label)
     }
 }
-
