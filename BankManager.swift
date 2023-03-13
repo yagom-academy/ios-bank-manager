036086f650bed88f1b49e58cc11ee319d6fcf626
minsu3007 <minsu3007@gmail.com>
2023-03-13 14:11:07 +0900

    refactor: BankManager 타입 수정

--- BankManager.swift
+++ BankManager.swift
@@ -5,26 +5,17 @@
 //
 import Foundation
 
-class BankManager {
-    var customer: Customer?
-    let duty: Banking
-    
-    init(duty: Banking) {
-        self.duty = duty
-    }
-    
-    func work() {
-        guard let customer = customer else { return }
-        
-        let serialQueue = DispatchQueue(label: "WorkQueue")
+struct BankManager {
+    static func work(for customer: Customer, duty: Banking) {
+        let workQueue = DispatchQueue(label: "WorkQueue")
         let time = duty == .deposit ? BankOption.processingTimeAtDeposit : BankOption.processingTimeAtLoan
         
         let queueItem = DispatchWorkItem {
-            print("\(customer.waitingNumber)번 고객 \(self.duty.rawValue)업무 시작")
+            print("\(customer.waitingNumber)번 고객 \(duty.rawValue)업무 시작")
             Thread.sleep(forTimeInterval: time)
-            print("\(customer.waitingNumber)번 고객 \(self.duty.rawValue)업무 완료")
+            print("\(customer.waitingNumber)번 고객 \(duty.rawValue)업무 완료")
         }
         
-        serialQueue.sync(execute: queueItem)
+        workQueue.sync(execute: queueItem)
     }
 }
