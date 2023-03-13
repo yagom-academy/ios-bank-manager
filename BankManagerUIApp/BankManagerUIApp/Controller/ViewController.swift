e68626cbf2584500192f716cbf97cc00d421f2af
Jun <59365211+juun97@users.noreply.github.com>
2023-03-13 17:51:07 +0900

    feat : Canvas 사용 위한 타입선언

--- BankManagerUIApp/BankManagerUIApp/Controller/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/Controller/ViewController.swift
@@ -6,6 +6,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,3 +17,21 @@ class ViewController: UIViewController {
 
 }
 
+import SwiftUI
+
+struct SwiftUIViewController: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) { }
+}
+
+@available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewController()
+    }
+}
