3494fa6de78fc7ae8bbebf1857e986732b27d425
seunghyunCheon <csh7215@naver.com>
2023-03-14 14:07:41 +0900

    remove: 메인 스토리보드 삭제, launch 스크린 삭제

---
+++ BankManagerUIApp/BankManagerUIApp/BankManagerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BankManagerUIApp - BankManagerViewController.swift
+//  Created by brody, christy, harry.
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class BankManagerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
