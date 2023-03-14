84bff514c9daa4e1c9b766df177ec846bc571981
Jun <59365211+juun97@users.noreply.github.com>
2023-03-14 11:31:54 +0900

    refactor : 기능 분리 재정의

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -10,6 +10,7 @@ import Foundation
 protocol workable {
     var customerQueue: CustomerQueue<Customer> { get }
     
+    
     func open(totalCustomer: Int)
     func setCustomerQueue(totalCustomer: Int)
     func reportResult(totalCustomer: Int, processTime: CFAbsoluteTime)
@@ -17,15 +18,15 @@ protocol workable {
 
 final class Bank {
     private let customerQueue: CustomerQueue = CustomerQueue<Customer>()
+    private let banker: Banker
     
-    private let loanDepartment: DispatchSemaphore
-    private let depositDepartment: DispatchSemaphore
+//    private let loanDepartment: DispatchSemaphore
+//    private let depositDepartment: DispatchSemaphore
     private let workQueue: DispatchQueue = DispatchQueue(label: "workQueue", attributes: .concurrent)
     private let workGroup: DispatchGroup = DispatchGroup()
     
     init(loanBankerCount: Int, depositBankerCount: Int) {
-        self.loanDepartment = DispatchSemaphore(value: loanBankerCount)
-        self.depositDepartment = DispatchSemaphore(value: depositBankerCount)
+        banker = Banker(loanDepartment: loanBankerCount, depositDepartment: depositBankerCount)
     }
     
     func open(totalCustomer: Int) {
@@ -52,33 +53,12 @@ final class Bank {
     
     private func startWork() {
         while let customer = customerQueue.dequeue() {
-            respond(to: customer)
+            banker.respond(to: customer)
         }
         
         workGroup.wait()
     }
 
-    private func respond(to customer: Customer) {
-        guard let business = customer.business else { return }
-        switch business {
-        case .deposit:
-            let bankerTask = makeBankerTask(for: customer, Department: depositDepartment)
-            workQueue.async(group: workGroup, execute: bankerTask)
-        case .loan:
-            let bankerTask = makeBankerTask(for: customer, Department: loanDepartment)
-            workQueue.async(group: workGroup, execute: bankerTask)
-        }
-    }
-    
-    private func makeBankerTask(for customer: Customer, Department: DispatchSemaphore) -> DispatchWorkItem {
-        let task = DispatchWorkItem {
-            Department.wait()
-            Banker.doWork(for: customer)
-            Department.signal()
-        }
-        
-        return task
-    }
     
     private func reportResult(totalCustomer: Int, processTime: CFAbsoluteTime) {
         let roundedProcessTime = round(processTime * 100) / 100
--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Banker.swift
@@ -8,7 +8,40 @@
 import Foundation
 
 struct Banker {
-    static func doWork(for customer: Customer) {
+    private let loanDepartment: DispatchSemaphore
+    private let depositDepartment: DispatchSemaphore
+    private let workQueue: DispatchQueue = DispatchQueue(label: "workQueue", attributes: .concurrent)
+    private let workGroup: DispatchGroup = DispatchGroup()
+    
+    init(loanDepartment: Int, depositDepartment: Int) {
+        self.loanDepartment = DispatchSemaphore(value: loanDepartment)
+        self.depositDepartment = DispatchSemaphore(value: depositDepartment)
+    }
+    
+    
+    private func makeTask(for customer: Customer, Department: DispatchSemaphore) -> DispatchWorkItem {
+        let task = DispatchWorkItem {
+            Department.wait()
+            doWork(for: customer)
+            Department.signal()
+        }
+        
+        return task
+    }
+    
+    func respond(to customer: Customer) {
+        guard let business = customer.business else { return }
+        switch business {
+        case .deposit:
+            let bankerTask = makeTask(for: customer, Department: depositDepartment)
+            workQueue.async(group: workGroup, execute: bankerTask)
+        case .loan:
+            let bankerTask = makeTask(for: customer, Department: loanDepartment)
+            workQueue.async(group: workGroup, execute: bankerTask)
+        }
+    }
+   
+    private func doWork(for customer: Customer) {
         guard let business = customer.business else { return }
         let startMessage = "\(customer.numberTicket)번 고객 \(business.rawValue)업무 시작"
         let endMessage = "\(customer.numberTicket)번 고객 \(business.rawValue)업무 완료"
