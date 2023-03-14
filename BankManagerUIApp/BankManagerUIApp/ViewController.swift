406682d48889a417e5df3e83f49e651e2f3f5207
kimseongj <ksj8277@naver.com>
2023-03-14 11:56:24 +0900

    feat: wholeStackView autoLayout 구현 및 translatesAutoresizingMaskIntoConstraints를 false로 선언

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -9,28 +9,34 @@ import UIKit
 class ViewController: UIViewController {
     private let wholeStackView: UIStackView = {
         let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.alignment = .fill
-        stackView.distribution = .fillProportionally
+        stackView.distribution = .fill
         stackView.spacing = 15
         return stackView
     }()
     
     private let buttonStackView: UIStackView = {
         let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
         stackView.alignment = .center
         return stackView
     }()
     
     private let addClientButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("고객 10명 추가", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
         return button
     }()
     
     private let resetButton: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("초기화", for: .normal)
         button.setTitleColor(.red, for: .normal)
         return button
@@ -38,68 +44,66 @@ class ViewController: UIViewController {
     
     private let leadTimeLabel: UILabel = {
         let label = UILabel()
+        label.text = "업무시간"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
-    private let taskStatusStackView: UIStackView = {
+
+    private let taskStatusLabelStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.alignment = .center
         stackView.distribution = .fillEqually
-        return stackView
-    }()
-    
-    private let waitTaskStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 15
         return stackView
     }()
     
     private let waitTaskLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "대기중"
+        label.textAlignment = .center
         label.textColor = .white
         label.backgroundColor = .systemGreen
         return label
     }()
     
-    private let processTaskStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 15
-        return stackView
-    }()
-    
     private let processTaskLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "업무중"
+        label.textAlignment = .center
         label.textColor = .white
         label.backgroundColor = .systemBlue
         return label
     }()
-
-    private let waitClientStatusStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        stackView.spacing = 10
-        return stackView
-    }()
-    
-    private let processClientStatusStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        stackView.spacing = 10
-        return stackView
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureUI()
     }
     
-    
+    private func configureUI() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        view.addSubview(wholeStackView)
+        
+        wholeStackView.addArrangedSubview(buttonStackView)
+        wholeStackView.addArrangedSubview(leadTimeLabel)
+        wholeStackView.addArrangedSubview(taskStatusLabelStackView)
+        
+        buttonStackView.addArrangedSubview(addClientButton)
+        buttonStackView.addArrangedSubview(resetButton)
+        
+        taskStatusLabelStackView.addArrangedSubview(waitTaskLabel)
+        taskStatusLabelStackView.addArrangedSubview(processTaskLabel)
+
+        // autolayout
+        NSLayoutConstraint.activate([
+            wholeStackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            wholeStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            wholeStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            wholeStackView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
 }
 
