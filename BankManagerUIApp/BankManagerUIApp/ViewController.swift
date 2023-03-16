a9009652168debd37028f88d048252b08e53eecb
vetto <gzzjk159@naver.com>
2023-03-16 16:06:33 +0900

    feat: viewController addQueueLabel 구현, addButton 수정, 폰트사이즈 적용

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -14,11 +14,13 @@ class BankManagerViewController: UIViewController {
         setupMainStackView()
         addButton()
         addTimeLabel()
+        addQueueLabel()
     }
     
     private func setupMainStackView() {
         view.addSubview(mainStackView)
         mainStackView.axis = .vertical
+        mainStackView.spacing = 12
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -31,11 +33,12 @@ class BankManagerViewController: UIViewController {
     
     private func addButton() {
         let addClientButton: UIButton = .init()
+        addClientButton.setTitle("고객 10명 추가", for: .normal)
+        addClientButton.setTitleColor(.systemBlue, for: .normal)
+        
         let resetButton: UIButton = .init()
-        addClientButton.setAttributedTitle(NSAttributedString(string: "고객 10명 추가"), for: .normal)
-        addClientButton.titleLabel?.textColor = .systemBlue
-        resetButton.setAttributedTitle(NSAttributedString(string: "초기화"), for: .normal)
-        resetButton.titleLabel?.textColor = .systemRed
+        resetButton.setTitle("초기화", for: .normal)
+        resetButton.setTitleColor(.systemRed, for: .normal)
         
         let buttonStackView: UIStackView = .init()
         buttonStackView.axis = .horizontal
@@ -55,7 +58,7 @@ class BankManagerViewController: UIViewController {
         
         businessTime.text = "업무 시간 - 04:33:253"
         businessTime.textAlignment = .center
-        businessTime.backgroundColor = .green
+        businessTime.font = .systemFont(ofSize: 24)
         
         businessTime.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 20)
         
@@ -63,7 +66,32 @@ class BankManagerViewController: UIViewController {
         mainStackView.addArrangedSubview(businessTime)
         
         businessTime.translatesAutoresizingMaskIntoConstraints = false
-        businessTime.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        businessTime.heightAnchor.constraint(equalToConstant: 24).isActive = true
     }
+    
+    private func addQueueLabel() {
+        let waitingQueue: UILabel = .init()
+        waitingQueue.text = "대기중"
+        waitingQueue.textAlignment = .center
+        waitingQueue.backgroundColor = .systemGreen
+        waitingQueue.textColor = .white
+        waitingQueue.font = .systemFont(ofSize: 32)
+        
+        let businessQueue: UILabel = .init()
+        businessQueue.text = "업무중"
+        businessQueue.textAlignment = .center
+        businessQueue.backgroundColor = .systemBlue
+        businessQueue.textColor = .white
+        businessQueue.font = .systemFont(ofSize: 32)
+        
+        let queueStackView: UIStackView = .init()
+        queueStackView.axis = .horizontal
+        queueStackView.distribution = .fillEqually
+        
+        mainStackView.addArrangedSubview(queueStackView)
+        queueStackView.addArrangedSubview(waitingQueue)
+        queueStackView.addArrangedSubview(businessQueue)
+    }
+    
 }
 
