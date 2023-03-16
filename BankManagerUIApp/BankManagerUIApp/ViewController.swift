7e0b4ff173ccf211a23be4722a3e8c924261b30d
vetto <gzzjk159@naver.com>
2023-03-16 16:00:06 +0900

    feat: setupMainStackView 메서드 생성, addButton 메서드 생성

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -6,13 +6,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class BankManagerViewController: UIViewController {
+    private let mainStackView: UIStackView = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMainStackView()
+        addButton()
     }
-
-
+    
+    private func setupMainStackView() {
+        view.addSubview(mainStackView)
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
+    
+    private func addButton() {
+        let addClientButton: UIButton = .init()
+        let resetButton: UIButton = .init()
+        addClientButton.setAttributedTitle(NSAttributedString(string: "고객 10명 추가"), for: .normal)
+        addClientButton.titleLabel?.textColor = .systemBlue
+        resetButton.setAttributedTitle(NSAttributedString(string: "초기화"), for: .normal)
+        resetButton.titleLabel?.textColor = .systemRed
+        
+        let buttonStackView: UIStackView = .init()
+        buttonStackView.axis = .horizontal
+        buttonStackView.alignment = .center
+        buttonStackView.distribution = .fillEqually
+        
+        mainStackView.addArrangedSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(addClientButton)
+        buttonStackView.addArrangedSubview(resetButton)
+    }
 }
 
