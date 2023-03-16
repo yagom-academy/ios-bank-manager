e86b4c3484b48331d9d37432a822f34a7c304540
parkmuri <smxhmu@naver.com>
2023-03-16 17:30:41 +0900

    refactor: ViewController 및 내부 메소드 리네이밍, 개행정리

--- BankManagerUIApp/BankManagerUIApp/BankAppViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/BankAppViewController.swift
@@ -1,18 +1,23 @@
 //
-//  BankManagerUIApp - ViewController.swift
+//  BankManagerUIApp - BankAppViewController.swift
 //  Created by 리지, 무리.
 //  Copyright © yagom academy. All rights reserved.
 // 
 
 import UIKit
 
-class ViewController: UIViewController {
+final class BankAppViewController: UIViewController {
+    //MARK: View Objects
     private let screenStackView = ScreenStackView()
     private let buttonStackView = ButtonStackView()
     private let taskTimerLabel = TaskTimerLabel()
     private let queueStackView = QueueStackView()
+    
+    //MARK: Bank Object
     private var bank = Bank()
     private let main = OperationQueue.main
+    
+    //MARK: Timer Properties
     private var timer: Timer?
     private var taskTime: Double = 0
     private var isOpened: Bool = false
@@ -21,11 +26,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         bank.delegate = self
         setUpScreenStackView()
-        configureConstraint()
+        configureConstraintScreenStackView()
         setUpButton()
-        setTimer()
+        setUpTimer()
     }
     
+    //MARK: stackView Method
     private func setUpScreenStackView() {
         view.backgroundColor = .white
         view.addSubview(screenStackView)
@@ -34,7 +40,7 @@ class ViewController: UIViewController {
         screenStackView.addArrangedSubview(queueStackView)
     }
     
-    private func configureConstraint() {
+    private func configureConstraintScreenStackView() {
         NSLayoutConstraint.activate([
             screenStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             screenStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -42,52 +48,6 @@ class ViewController: UIViewController {
             screenStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             taskTimerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50)
         ])
-    }
-    
-    private func setUpButton() {
-        addClientButtonTapped()
-        resetButtonTapped()
-    }
-    
-    private func addClientButtonTapped() {
-        buttonStackView.addClientButton.addTarget(self, action: #selector(addTenClients), for: .touchUpInside)
-    }
-    
-    @objc private func addTenClients() {
-        bank.lineUpClient()
-        bank.doTask()
-        isOpened = true
-    }
-    
-    private func setTimer() {
-        self.timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timeUp), userInfo: nil, repeats: true)
-    }
-    
-    @objc private func timeUp() {
-        if isOpened == true {
-            taskTime += 0.001
-        }
-        let timeInterval = taskTime
-        let minute = (Int)(fmod((timeInterval/60), 60))
-        let second = (Int)(fmod(timeInterval, 60))
-        let milliSecond = (Int)((timeInterval - floor(timeInterval))*1000)
-        
-        let minuteLabel = String(format: "%02d", minute)
-        let secondLabel = String(format: "%02d", second)
-        let milliSecondLabel = String(format: "%03d", milliSecond)
-        taskTimerLabel.text = " 업무시간 - \(minuteLabel) : \(secondLabel) : \(milliSecondLabel)"
-    }
-    
-    private func resetButtonTapped() {
-        buttonStackView.resetButton.addTarget(self, action: #selector(resetAll), for: .touchUpInside)
-    }
-    
-    @objc func resetAll() {
-        isOpened = false
-        taskTime = 0
-        queueStackView.waitingQueueStackView.waitingScrollView.waitingClientStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
-        queueStackView.doingTaskStackView.doingTaskScrollView.taskClientStackView.arrangedSubviews.forEach { $0.removeFromSuperview()}
-        bank.reset()
     }
     
     private func makeLabel(of client: Client) -> UILabel {
@@ -106,9 +66,61 @@ class ViewController: UIViewController {
         
         return clientLabel
     }
+    
+    //MARK: Button Method
+    private func setUpButton() {
+        addClientButtonTapped()
+        resetButtonTapped()
+    }
+    
+    private func addClientButtonTapped() {
+        buttonStackView.addClientButton.addTarget(self, action: #selector(addTenClients), for: .touchUpInside)
+    }
+    
+    @objc private func addTenClients() {
+        bank.lineUpClient()
+        bank.doTask()
+        isOpened = true
+    }
+    
+    private func resetButtonTapped() {
+        buttonStackView.resetButton.addTarget(self, action: #selector(resetAll), for: .touchUpInside)
+    }
+    
+    @objc func resetAll() {
+        isOpened = false
+        taskTime = 0
+        queueStackView.waitingQueueStackView.waitingScrollView.waitingClientStackView
+            .arrangedSubviews.forEach { $0.removeFromSuperview() }
+        queueStackView.doingTaskStackView.doingTaskScrollView.taskClientStackView
+            .arrangedSubviews.forEach { $0.removeFromSuperview()}
+        bank.reset()
+    }
+    
+    //MARK: Timer Method
+    private func setUpTimer() {
+        self.timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timeUp), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func timeUp() {
+        if isOpened == true {
+            taskTime += 0.001
+        }
+        let timeInterval = taskTime
+        let minute = (Int)(fmod((timeInterval/60), 60))
+        let second = (Int)(fmod(timeInterval, 60))
+        let milliSecond = (Int)((timeInterval - floor(timeInterval))*1000)
+        
+        let minuteLabel = String(format: "%02d", minute)
+        let secondLabel = String(format: "%02d", second)
+        let milliSecondLabel = String(format: "%03d", milliSecond)
+        taskTimerLabel.text = " 업무시간 - \(minuteLabel) : \(secondLabel) : \(milliSecondLabel)"
+    }
 }
 
-extension ViewController: BankDelegate {
+//MARK: - BankAppDelegate
+
+extension BankAppViewController: BankDelegate {
     func sendData(of client: Client) {
         let waitingClientLabel = makeLabel(of: client)
         
@@ -117,8 +129,10 @@ extension ViewController: BankDelegate {
     
     func startTask(of client: Client) {
         main.addOperation { [self] in
-            queueStackView.waitingQueueStackView.waitingScrollView.waitingClientStackView.arrangedSubviews.forEach {
+            queueStackView.waitingQueueStackView.waitingScrollView.waitingClientStackView
+                .arrangedSubviews.forEach {
                 let label = $0 as? UILabel
+                    
                 if label?.text == "\(client.waitingNumber) - \(client.purposeOfVisit.rawValue)" {
                     $0.removeFromSuperview()
                     queueStackView.doingTaskStackView.doingTaskScrollView.taskClientStackView.addArrangedSubview($0)
@@ -129,13 +143,17 @@ extension ViewController: BankDelegate {
     
     func completeTask(of client: Client) {
         main.addOperation { [self] in
-            queueStackView.doingTaskStackView.doingTaskScrollView.taskClientStackView.arrangedSubviews.forEach {
+            queueStackView.doingTaskStackView.doingTaskScrollView.taskClientStackView
+                .arrangedSubviews.forEach {
                 let label = $0 as? UILabel
+                    
                 if label?.text == "\(client.waitingNumber) - \(client.purposeOfVisit.rawValue)" {
                     $0.removeFromSuperview()
                 }
             }
-            if queueStackView.waitingQueueStackView.waitingScrollView.waitingClientStackView.subviews.isEmpty && queueStackView.doingTaskStackView.doingTaskScrollView.taskClientStackView.subviews.isEmpty {
+            if queueStackView.waitingQueueStackView.waitingScrollView.waitingClientStackView
+                .subviews.isEmpty && queueStackView.doingTaskStackView.doingTaskScrollView.taskClientStackView
+                .subviews.isEmpty {
                 isOpened = false
             }
         }
@@ -148,13 +166,13 @@ extension ViewController: BankDelegate {
 import SwiftUI
 
 struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    typealias UIViewControllerType = ViewController
+    typealias UIViewControllerType = BankAppViewController
     
-    func makeUIViewController(context: Context) -> ViewController {
-        return ViewController()
+    func makeUIViewController(context: Context) -> BankAppViewController {
+        return BankAppViewController()
     }
     
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: BankAppViewController, context: Context) {
     }
 }
 
--- BankManagerUIApp/BankManagerUIApp/SceneDelegate.swift
+++ BankManagerUIApp/BankManagerUIApp/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
                 window = UIWindow(windowScene: windowScene)
 
-                window?.rootViewController = ViewController()
+                window?.rootViewController = BankAppViewController()
                 window?.makeKeyAndVisible()
     }
 
