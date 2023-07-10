c7a2e6566344940a73fa3fce57f800c56c520efb
iOS-Yetti <yechan123456@gmail.net>
2023-07-10 20:56:15 +0900

    feat: Linked List 구현을 위한 Node 생성

---
+++ BankManagerUIApp/BankManagerUIApp/LinkedList.swift
@@ -0,0 +1,8 @@
+//
+//  LinkedList.swift
+//  BankManagerUIApp
+//
+//  Created by 예찬 on 2023/07/10.
+//
+
+import Foundation
---
+++ BankManagerUIApp/BankManagerUIApp/Node.swift
@@ -0,0 +1,18 @@
+//
+//  Node.swift
+//  BankManagerUIApp
+//
+//  Created by Yetti, Redmango on 2023/07/10.
+//
+
+import Foundation
+
+final class Node<Element> {
+    var data: Element?
+    var next: Node<Element>?
+    
+    init(data: Element, next: Node<Element>? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
