1b57d0e52bdce46e63fd542ec27ed8383f050ce1
yyss99 <yyssjj9999@gmail.com>
2023-07-10 19:22:20 +0900

    ✨feat: CustomerWaitingQueue 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/CustomerWaitingQueue.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerWaitingQueue.swift
+//  BankManagerConsoleApp
+//
+//  Created by yyss99, 비모 on 2023/07/10.
+//
+
+struct CustomerWaitingQueue<Element> {
+    private var list: LinkedList<Element> = LinkedList()
+    
+    var peek: Element? {
+        return list.first
+    }
+    
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    mutating func enqueue(_ newElemet: Element) {
+        list.append(newElemet)
+    }
+    
+    mutating func dequeue() -> Element? {
+        return list.popFirst()
+    }
+    
+    mutating func clear() {
+        list = LinkedList()
+    }
+}
