85e8e9f56d9b2ef3ddb4047099296489809fc7ce
YaRkyungmin <kkgm94@naver.com>
2023-07-10 20:08:28 +0900

    ♻️refactor: 프로토콜 프로퍼티 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/QueueType.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/QueueType.swift
@@ -8,9 +8,6 @@
 protocol QueueType {
     associatedtype Element
     
-    var headNode: CustomerNode<Element>? { get }
-    var tailNode: CustomerNode<Element>? { get }
-    
     mutating func enqueue(_ value: Element)
     mutating func dequeue() -> Element?
     mutating func clear()
