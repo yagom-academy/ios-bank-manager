78198cacd09acb4ad82b802c1079469e8efc31b3
hemg2 <hemg2@naver.com>
2023-07-10 18:13:21 +0900

    feat: Node 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Node.swift
@@ -0,0 +1,15 @@
+//
+//  Node.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kobe, Hemg on 2023/07/10.
+//
+
+class Node<Type> {
+    var value: Type
+    var next: Node<Type>?
+    
+    init(value: Type) {
+        self.value = value
+    }
+}
