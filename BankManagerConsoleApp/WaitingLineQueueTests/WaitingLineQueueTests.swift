6a314508074989b82fdbe04ad05dcbed269b33c9
h-suo <phstoo1021@naver.com>
2023-07-10 22:14:31 +0900

    test: 테스터 생성
    test_Queue에_dequeue를_호출하면_해당_노드의_value를_반환합니다, test_빈_Queue에_dequeue를_호출하면_nil을_반환합니다 테스터 생성

--- BankManagerConsoleApp/WaitingLineQueueTests/WaitingLineQueueTests.swift
+++ BankManagerConsoleApp/WaitingLineQueueTests/WaitingLineQueueTests.swift
@@ -37,4 +37,23 @@ final class WaitingLineQueueTests: XCTestCase {
         // then
         XCTAssertEqual(result, expectation)
     }
+    
+    func test_Queue에_dequeue를_호출하면_해당_노드의_value를_반환합니다() {
+        // given
+        let expectation = "가나다"
+        // when
+        sut?.enqueue("가나다")
+        let result = sut?.dequeue()
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_빈_Queue에_dequeue를_호출하면_nil을_반환합니다() {
+        // given
+        let expectation: String? = nil
+        // when
+        let result = sut?.dequeue()
+        // then
+        XCTAssertEqual(result, expectation)
+    }
 }
