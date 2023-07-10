435f7329203f10039c785cc96974444adfe35651
DasanKim <tempzipp@gmail.com>
2023-07-10 20:36:13 +0900

    ✨feat: queue 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue/Queue.swift
@@ -5,4 +5,26 @@
 //  Created by Dasan & Mary on 2023/07/10.
 //
 
-import Foundation
+struct Queue<T> {
+    private var queue: LinkedList = LinkedList<T>()
+    
+    var peek: T? {
+        return queue.first
+    }
+    
+    var isEmpty: Bool {
+        return queue.isEmpty
+    }
+    
+    mutating func enqueue(_ element: T) {
+        queue.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        return queue.removeFirst()
+    }
+    
+    mutating func clear() {
+        queue.removeAll()
+    }
+}
