513f89d09ff04524de756720ce1423fd9945273c
redmango1446@gmail.com <redmango1446>
2023-07-11 15:09:42 +0900

    feat: CustomerQueue 타입 구현

--- BankManagerUIApp/BankManagerUIApp/CustomerQueue.swift
+++ BankManagerUIApp/BankManagerUIApp/CustomerQueue.swift
@@ -5,4 +5,26 @@
 //  Created by Yetti, Redmango on 2023/07/11.
 //
 
-import Foundation
+struct CustomerQueue<Element> {
+    var customerQueue: LinkedList = LinkedList<Element>()
+    
+    var peek: Element? {
+        return customerQueue.peek
+    }
+
+    var isEmpty: Bool {
+        return customerQueue.isEmpty
+    }
+    
+    mutating func clear() {
+        customerQueue.clear()
+    }
+    
+    mutating func enqueue(_ newElement: Element) {
+        customerQueue.addLast(newElement)
+    }
+    
+    mutating func dequeue() -> Element? {
+        return customerQueue.popFirst()
+    }
+}
