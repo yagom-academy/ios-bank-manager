ac6069d4d9a43925e878108f956983db2c40224d
redmango1446@gmail.com <redmango1446>
2023-07-11 15:44:55 +0900

    test: CustomelQueueTests 파일 추가 및 isEmpty 연산프로퍼티 테스트 추가

---
+++ BankManagerUIApp/BankManagerUIAppTests/CustomelQueueTests.swift
@@ -0,0 +1,87 @@
+//
+//  CustomelQueueTests.swift
+//  BankManagerUIAppTests
+//
+//  Created by redmango1446 on 2023/07/11.
+//
+
+import XCTest
+@testable import BankManagerUIApp
+
+final class CustomelQueueTests: XCTestCase {
+    var sut: CustomerQueue<Int>!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = CustomerQueue()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func test_isEmpty_Queue에_아무값도_넣지않으면_isEmpty의값은_true이다() {
+        // given
+        // when
+        let expectation = true
+        
+        // then
+        XCTAssertTrue(expectation)
+    }
+    
+    func test_isEmpty_1을_enqueue하면_isEmpty의값은_false이다() {
+        // given
+        sut.enqueue(1)
+        
+        // when
+        let expectation = false
+        
+        // then
+        XCTAssertFalse(expectation)
+    }
+    
+    func test_isEmpty_1을_enqueue하고_dequeue하면_isEmpty의값은_true이다() {
+        // given
+        sut.enqueue(1)
+        let _ = sut.dequeue()
+        
+        // when
+        let expectation = true
+        
+        // then
+        XCTAssertTrue(expectation)
+    }
+    
+    func test_enqueue_() {
+        // given
+        
+        // when
+        
+        // then
+    }
+    
+    func test_dequeue_() {
+        // given
+        
+        // when
+        
+        // then
+    }
+    
+    func test_peek_() {
+        // given
+        
+        // when
+        
+        // then
+    }
+    
+    func test_clear_() {
+        // given
+        
+        // when
+        
+        // then
+    }
+}
