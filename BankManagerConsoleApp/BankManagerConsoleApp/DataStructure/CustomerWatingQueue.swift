7a4f363c74240f0e49eebc70ef4af36d70ee95d8
karen <karenrodanthe@icloud.com>
2023-07-11 16:36:56 +0900

    ✴️feat: CustomerWatingQueue 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/DataStructure/CustomerWatingQueue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/DataStructure/CustomerWatingQueue.swift
@@ -5,4 +5,21 @@
 //  Created by karen on 2023/07/11.
 //
 
-import Foundation
+struct CustomerWatingQueue<Element> {
+    private var queue = LinkedList<Element>()
+    var isEmpty: Bool { return queue.isEmpty }
+    var peek: Element? { return queue.peek }
+
+    mutating func enqueue(_ value: Element) {
+        queue.append(value)
+    }
+    
+    mutating func dequeue() -> Element? {
+        return queue.removeFirst()
+    }
+    
+    mutating func clear() {
+        queue.clear()
+    }
+}
+
