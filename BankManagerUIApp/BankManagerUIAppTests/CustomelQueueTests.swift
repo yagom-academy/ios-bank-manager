2f57d8243350a5439d23f9b5876e845966e1670a
iOS-Yetti <yechan123456@gmail.net>
2023-07-11 16:23:31 +0900

    test: peek() 메서드 테스트 추가

--- BankManagerUIApp/BankManagerUIAppTests/CustomelQueueTests.swift
+++ BankManagerUIApp/BankManagerUIAppTests/CustomelQueueTests.swift
@@ -24,10 +24,10 @@ final class CustomelQueueTests: XCTestCase {
     func test_isEmpty_Queue에_아무값도_넣지않으면_isEmpty의값은_true이다() {
         // given
         // when
-        let expectation = true
+        let result = sut.isEmpty
         
         // then
-        XCTAssertTrue(expectation)
+        XCTAssertTrue(result)
     }
     
     func test_isEmpty_1을_enqueue하면_isEmpty의값은_false이다() {
@@ -35,10 +35,10 @@ final class CustomelQueueTests: XCTestCase {
         sut.enqueue(1)
         
         // when
-        let expectation = false
+        let result = sut.isEmpty
         
         // then
-        XCTAssertFalse(expectation)
+        XCTAssertFalse(result)
     }
     
     func test_isEmpty_1을_enqueue하고_dequeue하면_isEmpty의값은_true이다() {
@@ -47,10 +47,10 @@ final class CustomelQueueTests: XCTestCase {
         let _ = sut.dequeue()
         
         // when
-        let expectation = true
+        let result = sut.isEmpty
         
         // then
-        XCTAssertTrue(expectation)
+        XCTAssertTrue(result)
     }
     
     func test_enqueue_enqueue로Queue에_1을넣고_dequeue하면_1을반환한다() {
@@ -58,10 +58,11 @@ final class CustomelQueueTests: XCTestCase {
         sut.enqueue(1)
         
         // when
-        let expectation = sut.dequeue()
+        let expectation = 1
+        let result = sut.dequeue()
         
         // then
-        XCTAssertEqual(expectation, 1)
+        XCTAssertEqual(expectation, result)
     }
     
     func test_dequeue_1_2_3을_순서대로_enqueue하고_dequeue를_2번하면_마지막으로_반환되는값은_2이다() {
@@ -71,20 +72,21 @@ final class CustomelQueueTests: XCTestCase {
         sut.enqueue(3)
         
         // when
-        var expectation = sut.dequeue()
-        expectation = sut.dequeue()
+        let expectation = 2
+        var result = sut.dequeue()
+        result = sut.dequeue()
         
         // then
-        XCTAssertEqual(expectation, 2)
+        XCTAssertEqual(expectation, result)
     }
     
     func test_dequeue_enqueue를_하지않고_dequeue를_하면_nil이_반환된다() {
         // given
         // when
-        let expectation: Int? = sut.dequeue()
+        let result: Int? = sut.dequeue()
         
         // then
-        XCTAssertNil(expectation)
+        XCTAssertNil(result)
     }
     
     func test_dequeue_1을_enqueue를_하고_dequeue를_2번하면_마지막으로_반환되는값은_nil이다() {
@@ -92,19 +94,59 @@ final class CustomelQueueTests: XCTestCase {
         sut.enqueue(1)
         
         // when
-        var expectation = sut.dequeue()
-        expectation = sut.dequeue()
+        var result = sut.dequeue()
+        result = sut.dequeue()
         
         // then
-        XCTAssertNil(expectation)
+        XCTAssertNil(result)
     }
     
-    func test_peek_() {
+    func test_peek_Queue에_아무값도_넣지않았을때_nil을_반환한다() {
         // given
         
         // when
+        let result = sut.peek
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_peek_1을_equeue했을때_1을반환한다() {
+        // given
+        sut.enqueue(1)
+        
+        //when
+        let expectaion = 1
+        let result = sut.peek
+        
+        //then
+        XCTAssertEqual(expectaion, result)
+    }
+    
+    func test_peek_1과2를_enqueue했을때_1을_반환한다() {
+        // given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        
+        // when
+        let expectaion = 1
+        let result = sut.peek
         
         // then
+        XCTAssertEqual(expectaion, result)
+    }
+    
+    func test_peek_1과2를_enqueue하고_한번dequeue하면_peek은_2을_반환한다() {
+        // given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        let _ = sut.dequeue()
+        
+        // when
+        let expectaion = 2
+        let result = sut.peek
+        
+        // then
+        XCTAssertEqual(expectaion, result)
     }
     
     func test_clear_() {
