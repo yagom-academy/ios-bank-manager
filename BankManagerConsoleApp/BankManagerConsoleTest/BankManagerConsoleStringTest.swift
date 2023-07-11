ca92f325181677e258760535ffb5d73b495dff91
Min Hyun <minnimida@gmail.com>
2023-07-11 09:41:31 +0900

    ✅test: BankManagerConsoleStringTest 구현

--- BankManagerConsoleApp/BankManagerConsoleTest/BankManagerConsoleStringTest.swift
+++ BankManagerConsoleApp/BankManagerConsoleTest/BankManagerConsoleStringTest.swift
@@ -19,19 +19,76 @@ final class BankManagerConsoleStringTest: XCTestCase {
         sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_isEmpty가_true일때_enqueue를_사용해_문자를_넣으면_tailNode와_headNode가_동일한_객체인지_확인() {
+        //given
+        sut.enqueue("A")
+
+        //when
+        let head = sut.headNode
+        let tail = sut.tailNode
+
+        //then
+        XCTAssertIdentical(head, tail)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_enqueue를_사용해_문자를_넣으면_tailNode의_value프로퍼티에_저장되는지_확인() {
+        //given
+        let testValue = "A"
+        sut.enqueue(testValue)
+
+        //when
+        let result = sut.tailNode?.value
+
+        //then
+        XCTAssertEqual(testValue, result)
     }
 
+    func test_clear를_사용하면_headNode와_tailNode가_모두_nil이_되는지_확인() {
+        //given
+        sut.enqueue("A")
+
+        //when
+        sut.clear()
+
+        //then
+        XCTAssertNil(sut.headNode)
+        XCTAssertNil(sut.tailNode)
+    }
+
+    func test_isEmpty가_false일때_dequeue를_사용하면_headNode의_value프로퍼티가_반환_되는지_확인() {
+        //given
+        let testValue = "A"
+        sut.enqueue(testValue)
+
+        //when
+        let result = sut.dequeue()
+
+        //then
+        XCTAssertEqual(testValue, result)
+    }
+
+    func test_isEmpty가_false일때_dequeue를_사용하면_headNode가_nextNode로_변경되는지_확인() {
+        //given
+        sut.enqueue("A")
+        sut.enqueue("B")
+
+        //when
+        let expected = sut.headNode?.nextNode
+        sut.dequeue()
+
+        //then
+        XCTAssertIdentical(sut.headNode, expected)
+    }
+
+    func test_isEmpty가_false일때_peek을_사용하면_headNode의_value프로퍼티가_반환_되는지_확인() {
+        //given
+        sut.enqueue("A")
+
+        //when
+        let result = sut.peek()
+        let headValue = sut.headNode?.value
+
+        //then
+        XCTAssertEqual(result, headValue)
+    }
 }
