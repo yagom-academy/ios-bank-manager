66d7a56676ddadf482f4693155ac4a38ca790b77
DasanKim <tempzipp@gmail.com>
2023-07-11 10:59:17 +0900

    ✅test: QueueTests 케이스 추가

--- BankManagerConsoleApp/BankManagerConsoleTests/QueueTests/QueueTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleTests/QueueTests/QueueTests.swift
@@ -23,11 +23,158 @@ final class QueueTests: XCTestCase {
         sut = nil
     }
     
+    func test_enqueue_enqueue_1을했을때_queue의_peek값은_1이다() {
+        // given
+        sut.enqueue(1)
+        let expectation = 1
+        
+        // when
+        let result = sut.peek
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_enqueue_enqueue_3_4_5를했을때_queue의_peek값은_3이다() {
+        // given
+        sut.enqueue(3)
+        sut.enqueue(4)
+        sut.enqueue(5)
+        let expectation = 3
+        
+        // when
+        let result = sut.peek
+        
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_dequeue_조건없이_queue에_dequeue를하면_nil를_반환한다() {
+        // given, when
+        let result = sut.dequeue()
+        
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_dequeue_enqueue_3_4_5를하고_dequeue를하면_3을_반환한다() {
+        // given
+        sut.enqueue(3)
+        sut.enqueue(4)
+        sut.enqueue(5)
+        let expectation = 3
+ 
+        // when
+        let result = sut.dequeue()
+
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_dequeue_enqueue_3_4_5를하고_dequeue를_3번했을때_3_4_5를_순서대로_반환한다() {
+        // given
+        sut.enqueue(3)
+        sut.enqueue(4)
+        sut.enqueue(5)
+        let expectation = [3, 4, 5]
+        
+        // when
+        var result: [Int?] = [Int?]()
+        for _ in 1...3 {
+            result.append(sut.dequeue())
+        }
+
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
+    func test_dequeue_enqueue_3_4_5를하고_dequeue를_3번했을때_peek값은_nil이다() {
+        // given
+        sut.enqueue(3)
+        sut.enqueue(4)
+        sut.enqueue(5)
+        _ = sut.dequeue()
+        _ = sut.dequeue()
+        _ = sut.dequeue()
+        
+        // when
+        let result = sut.peek
+
+        // then
+        XCTAssertNil(result)
+    }
+    
+    func test_peek_enqueue_3_4_5를하고_dequeue를하면_peek값은_4이다() {
+        // given
+        sut.enqueue(3)
+        sut.enqueue(4)
+        sut.enqueue(5)
+        _ = sut.dequeue()
+        let expectation = 4
+        
+        // when
+        let result = sut.peek
+
+        // then
+        XCTAssertEqual(result, expectation)
+    }
+    
     func test_isEmpty_조건없이_queue의_isEmpty를호출할때_isEmpty는_true이다() {
         // given, when
         let result = sut.isEmpty
         
         // then
         XCTAssertTrue(result)
+    }
+    
+    func test_isEmpty_enqueue_1을했을때_isEmpty는_false이다() {
+        // given
+        sut.enqueue(1)
+        
+        // when
+        let result = sut.isEmpty
+        
+        // then
+        XCTAssertFalse(result)
+    }
+    
+    func test_isEmpty_enqueue_1_2를하고_dequeue를_2번했을때_isEmpty는_true이다() {
+        // given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        _ = sut.dequeue()
+        _ = sut.dequeue()
+        
+        // when
+        let result = sut.isEmpty
+
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_clear_enqueue_1_2를하고_clear했을때_isEmpty은_true이다() {
+        // given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        sut.clear()
+        
+        // when
+        let result = sut.isEmpty
+
+        // then
+        XCTAssertTrue(result)
+    }
+    
+    func test_clear_enqueue_1_2를하고_clear했을때_peek값은_nil이다() {
+        // given
+        sut.enqueue(1)
+        sut.enqueue(2)
+        sut.clear()
+        
+        // when
+        let result = sut.peek
+
+        // then
+        XCTAssertNil(result)
     }
 }
