f0ddf2c41d794dac5d9590c94442faafd07752c2
agilestarskim <agilestars@kakao.com>
2023-07-11 17:54:24 +0900

    test: NodeTests 수정

--- BankManagerConsoleApp/BankManagerConsoleAppTests/NodeTests.swift
+++ BankManagerConsoleApp/BankManagerConsoleAppTests/NodeTests.swift
@@ -9,25 +9,25 @@ import XCTest
 @testable import BankManagerConsoleApp
 
 final class NodeTests: XCTestCase {
-    func test_노드를_0으로_초기화하면_value에_0이_저장된다() {
+    func test_노드를_1으로_초기화하면_value에_1이_저장된다() {
         // given
-        let input = Node(0)
+        let input = Node(1)
         
         // when
-        let expectation = 0
+        let expectation = 1
         
         // then
         XCTAssertEqual(input.value, expectation)
     }
     
-    func test_처음_노드에_1로_초기화된_다음_노드를_연결하면_처음_노드에서_다음_노드의_밸류인_1에_접근할_수_있다() {
+    func test_처음_노드에_2로_초기화된_다음_노드를_연결하면_처음_노드에서_다음_노드의_value인_1에_접근할_수_있다() {
         // given
-        let input1 = Node(0)
-        let input2 = Node(1)
+        let input1 = Node(1)
+        let input2 = Node(2)
         input1.next = input2
         
         // when
-        let expectation = 1
+        let expectation = 2
         
         // then
         XCTAssertEqual(input1.next?.value, expectation)
