ab360d1b79bfef350cceef0b5558ae8ca6a01514
Kobe <mskang@xingxingmobility.com>
2023-07-11 17:33:46 +0900

    :sparkles: Feat: Queue 타입 구현 및 생성.

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -0,0 +1,29 @@
+//
+//  Queue.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kobe, Hemg on 2023/07/11.
+//
+
+struct Queue<T> {
+	private var linkedNode: LinkedList = LinkedList<T>()
+	var isEmpty: Bool {
+		return linkedNode.isEmpty
+	}
+	
+	var peek: T? {
+		return linkedNode.first
+	}
+	
+	mutating func enqueue(_ value: T) {
+		linkedNode.appendNode(value)
+	}
+	
+	mutating func dequeue() -> T? {
+		return linkedNode.removeFirst()
+	}
+	
+	mutating func clear() {
+		linkedNode.removeAll()
+	}
+}
