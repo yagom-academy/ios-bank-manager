cf25452b5c648a134cbff2eca4da488808ba780d
karen <karenrodanthe@icloud.com>
2023-07-11 16:35:44 +0900

    ✴️feat: LinkedList 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/DataStructure/LinkedList.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/DataStructure/LinkedList.swift
@@ -5,4 +5,37 @@
 //  Created by karen on 2023/07/11.
 //
 
-import Foundation
+struct LinkedList<Element> {
+    private(set) var head: Node<Element>?
+    private(set) var tail: Node<Element>?
+    var isEmpty: Bool { return head == nil }
+    var peek: Element? { return head?.value }
+    
+    mutating func append(_ value: Element) {
+        let newNode = Node(value: value)
+        
+        if isEmpty {
+            head = newNode
+            tail = newNode
+        } else {
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
+    
+    mutating func removeFirst() -> Element? {
+        guard let dequeuedValue = head?.value else { return nil }
+        
+        head = head?.next
+        if isEmpty {
+            head = nil
+            tail = nil
+        }
+        return dequeuedValue
+    }
+    
+    mutating func clear() {
+        head = nil
+        tail = nil
+    }
+}
