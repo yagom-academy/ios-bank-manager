8f1a41ad84bae2bb70473b9f53c3536eb5b68094
YaRkyungmin <kkgm94@naver.com>
2023-07-12 16:25:17 +0900

    ✨feat: BankManger 타입 구현

--- BankManager.swift
+++ BankManager.swift
@@ -5,5 +5,24 @@
 //  Created by kyungmin, Max on 2023/07/10.
 //
 
-
-// 콘솔 메뉴 출력 / 입력 -> Bank 일일 업무 진행
+struct BankManager {
+    var bank: Bank
+    
+    mutating func workBankManager() {
+        while selectMenu() == .open {
+            bank.dailyWork()
+        }
+    }
+    
+    func selectMenu() -> Menu {
+        Menu.displayMenu()
+        print("입력 : ")
+        guard let inputMenu = readLine(),
+              let menu = Menu(rawValue: inputMenu),
+              menu == .open else {
+            return Menu.finish
+        }
+        
+        return Menu.open
+    }
+}
