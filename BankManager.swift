b1394e994dc8e401cfe841ec1a29d773bdd74d40
Serena <sha0720@naver.com>
2023-07-12 10:36:02 +0900

    feat: BankManager 생성

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,19 @@
 //
 
 import Foundation
+
+struct BankManager {
+    func carryOutBankService(_ waitingNumber: Int) {
+        startTask(waitingNumber)
+        Thread.sleep(forTimeInterval: 0.7)
+        endTask(waitingNumber)
+    }
+    
+    private func startTask(_ waitingNumber: Int) {
+        print("\(waitingNumber)번 고객 업무 시작")
+    }
+    
+    private func endTask(_ waitingNumber: Int) {
+        print("\(waitingNumber)번 고객 업무 완료")
+    }
+}
