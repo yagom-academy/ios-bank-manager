fa1844fa1ce301a679914511e37aaf3311231695
Min Hyun <minnimida@gmail.com>
2023-07-12 15:15:40 +0900

    ✨feat: Customer 타입 구현

--- BankManager.swift
+++ BankManager.swift
@@ -4,3 +4,4 @@
 //
 //  Created by kyungmin, Max on 2023/07/10.
 //
+
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,10 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by kyungmin, Max on 2023/07/12.
+//
+
+struct Customer: Equatable {
+    
+}
