61a68b6950b70275f1371d657b6b01ca1c274c80
JusBug <pksgwo123@gmail.com>
2023-07-13 10:50:50 +0900

    feat: Teller안에 은행원의 이름과 고객의 업무처리를 출력하는 processCustomer() 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Teller.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Teller.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Teller {
+    let name: String
+    
+    func processCustomer(_ customer: Int) {
+        print("\(name): \(customer)번 고객 업무 시작")
+        Thread.sleep(forTimeInterval: 0.7)
+        print("\(name): \(customer)번 고객 업무 완료")
+    }
+}
