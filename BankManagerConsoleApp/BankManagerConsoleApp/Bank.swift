5ee5b7fcfcd926610969591e7e59e15f30fe40dd
JusBug <pksgwo123@gmail.com>
2023-07-13 12:00:15 +0900

    style: waitingLine으로 이름 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -11,7 +11,7 @@ struct Bank: Manageable {
     var name: String
     var tellers: [Teller]
     let customerNumber: Int = Int.random(in: 10...30)
-    var waiting = Queue<Customer>()
+    var waitingLine = Queue<Customer>()
     var totalTime: Double = 0.0
     
     mutating func start() {
@@ -24,14 +24,14 @@ struct Bank: Manageable {
         for number in 1...numbers {
             let customer = Customer(numberTicket: number)
             
-            waiting.enqueue(customer)
+            waitingLine.enqueue(customer)
         }
     }
     
     mutating func assignCustomer() {
-        while !waiting.isEmpty {
+        while !waitingLine.isEmpty {
             for teller in tellers {
-                guard let customer = waiting.dequeue() else {
+                guard let customer = waitingLine.dequeue() else {
                     return
                 }
                 
