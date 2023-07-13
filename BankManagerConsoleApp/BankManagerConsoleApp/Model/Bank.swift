9e3e75383db751194b767b5918ee9c44ea884a97
MaryJo-github <jotjdal0215@gmail.com>
2023-07-13 21:35:48 +0900

    :recycle: Refactor: 총 업무시간 소수점 자릿수 설정

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -33,7 +33,7 @@ struct Bank {
     }
     
     private func closeBusiness() {
-        let workTime = calculateWorkTime()
+        let workTime = calculateWorkTime().numberToString
         
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(numberOfCustomer)명이며, 총 업무시간은 \(workTime)입니다.")
     }
---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Extension/Double+.swift
@@ -0,0 +1,12 @@
+//
+//  Double+.swift
+//  BankManagerConsoleApp
+//
+//  Created by Dasan & Mary on 2023/07/13.
+//
+
+extension Double {
+    var numberToString: String {
+        return String(format: "%.2f", self)
+    }
+}
