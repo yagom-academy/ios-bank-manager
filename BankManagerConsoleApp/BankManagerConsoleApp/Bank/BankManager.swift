ec5d6146c766061bcd794f5938d702f46e2ef1ab
karen <karenrodanthe@icloud.com>
2023-07-15 01:08:09 +0900

    ✂️style: BankManager의 state와 menuchoice를 각각enum으로 명시함

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankManager.swift
@@ -6,7 +6,7 @@
 
 struct BankManager {
     private var bank = Bank()
-    private var isWorking: Bool = true
+    private var state: BankManagerState = .working
     
     func displayMenu() {
         print("1 : 은행개점")
@@ -14,19 +14,32 @@ struct BankManager {
         print("입력: ", terminator: "")
     }
     
-    mutating func selectMenu() {
-        while isWorking {
+    mutating func start() {
+        while state == .working {
             displayMenu()
-            guard let input = readLine(), let choice = Int(input) else { return }
+            guard let input = readLine(), let rawChoice = UInt(input), let choice = BankMenuChoice(rawValue: rawChoice) else {
+                print("잘못된 입력입니다. 다시 시도해주세요.")
+                continue
+            }
             
             switch choice {
-            case 1:
+            case .open:
                 bank.open()
-            case 2:
-                isWorking = false
-            default:
-                print("잘못된 입력입니다. 다시 시도해주세요.")
+            case .quit:
+                state = .notWorking
             }
         }
+    }
+}
+
+extension BankManager {
+    enum BankManagerState {
+        case working
+        case notWorking
+    }
+    
+    enum BankMenuChoice: UInt {
+        case open = 1
+        case quit = 2
     }
 }
