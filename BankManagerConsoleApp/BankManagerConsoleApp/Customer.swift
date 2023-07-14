2b94818847f6c8e4920426477fcdbbfd4b341e52
agilestarskim <agilestars@kakao.com>
2023-07-14 10:44:07 +0900

    feat: Customer 타입 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by hoon, minsup on 2023/07/14.
+//
+
+import Foundation
+
+struct Customer {
+    let priority: Int
+    let taskTime = 0.7
+}
