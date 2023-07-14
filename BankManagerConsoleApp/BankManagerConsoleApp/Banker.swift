38647e272639c6d12eb60e4865fdaebe60a05d5d
hemg2 <hemg2@naver.com>
2023-07-14 09:29:28 +0900

    ✨feat: Banker 타입 생성 및 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
@@ -0,0 +1,12 @@
+//
+//  Banker.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kobe, Hemg on 2023/07/14.
+//
+
+class Banker {
+    func processCustomer(customer: Customer) {
+        print("\(customer.user)번 고객 업무 완료")
+    }
+}
