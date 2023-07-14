5f738d0c89a790c3045b476db75ebad4b84196a5
Kobe <mskang@xingxingmobility.com>
2023-07-14 09:26:15 +0900

    :sparkles: Feat: Customer 타입 생성 및 구현.

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -0,0 +1,14 @@
+//
+//  Customer.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kobe, Hemg on 2023/07/14.
+//
+
+class Customer {
+	let user: Int
+	
+	init(id: Int) {
+		self.user = id
+	}
+}
