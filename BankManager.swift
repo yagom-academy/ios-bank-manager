8d5230f45d25ea5c9fd2a008a17615d6920cc27c
Hoon94 <dleognsdl1@naver.com>
2023-07-14 10:50:41 +0900

    feat: BankManager 타입 생성

--- BankManager.swift
+++ BankManager.swift
@@ -5,3 +5,13 @@
 //
 
 import Foundation
+
+struct BankManager {
+    func work(customer: Customer) -> Double {
+        print("\(customer.priority)번 고객 업무 시작")
+        usleep(UInt32(customer.taskTime * 1000000))
+        print("\(customer.priority)번 고객 업무 완료")
+        
+        return customer.taskTime
+    }
+}
--- BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -5,8 +5,6 @@
 //  Created by hoon, minsup on 2023/07/14.
 //
 
-import Foundation
-
 struct Customer {
     let priority: Int
     let taskTime = 0.7
