1e4011f49eaf7611fd8c7b0bcf43b0ad3782fd66
Hoon94 <dleognsdl1@naver.com>
2023-07-17 22:04:04 +0900

    feat: dispatchQueue와 semaphore를 활용하여 동시에 은행업무를 수행

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/Bank.swift
@@ -5,8 +5,9 @@
 //  Created by hoon, minsup on 2023/07/14.
 //
 
-struct Bank {
-    private let bankClerk = BankClerk()
+import Foundation
+
+class Bank {
     private var customers = Queue<Customer>()
 
     init() {
@@ -16,15 +17,58 @@ struct Bank {
         }
     }
     
-    mutating func start() {
-        var totalTaskTime = 0.0
-        var totalCustomerCount = 0
+    func start() {
+        let group = DispatchGroup()
+        let semaphore = DispatchSemaphore(value: 1)
+        let depositClerk1 = DispatchQueue(label: "depositClerk1")
+        let depositClerk2 = DispatchQueue(label: "depositClerk2")
+        let loanClerk = DispatchQueue(label: "loanClerk")
         
-        while let customer = customers.dequeue() {
-            totalTaskTime += bankClerk.work(customer: customer)
-            totalCustomerCount += 1
+        while self.customers.peek != nil {
+            depositClerk1.async(group: group) {
+                guard self.customers.peek?.task == .deposit, !self.customers.isEmpty else {
+                    return
+                }
+                
+                semaphore.wait()
+                let customer = self.customers.dequeue()
+                semaphore.signal()
+                print("deposit1: \(customer!.numberTicket)번 고객 \(customer!.task.information.title)업무 시작")
+                Thread.sleep(forTimeInterval: customer!.task.information.time)
+                print("deposit1: \(customer!.numberTicket)번 고객 \(customer!.task.information.title)업무 완료")
+                
+            }
+            
+            depositClerk2.async(group: group) {
+                guard self.customers.peek?.task == .deposit, !self.customers.isEmpty else {
+                    return
+                }
+                
+                semaphore.wait()
+                let customer = self.customers.dequeue()
+                semaphore.signal()
+                print("deposit2: \(customer!.numberTicket)번 고객 \(customer!.task.information.title)업무 시작")
+                Thread.sleep(forTimeInterval: customer!.task.information.time)
+                print("deposit2: \(customer!.numberTicket)번 고객 \(customer!.task.information.title)업무 완료")
+
+            }
+        
+            loanClerk.async(group: group) {
+                guard self.customers.peek?.task == .loan, !self.customers.isEmpty else {
+                    return
+                }
+                
+                semaphore.wait()
+                let customer = self.customers.dequeue()
+                semaphore.signal()
+                print("loan: \(customer!.numberTicket)번 고객 \(customer!.task.information.title)업무 시작")
+                Thread.sleep(forTimeInterval: customer!.task.information.time)
+                print("loan: \(customer!.numberTicket)번 고객 \(customer!.task.information.title)업무 완료")
+            }
         }
         
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerCount)명이며, 총 업무시간은 \(String(format: "%.2f", totalTaskTime))초 입니다.")
+        group.wait()
+//        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(bankManagers[0].totalCustomerCount)명이며, 총 업무시간은 \(String(format: "%.2f", bankManagers[0].totalTaskTime))초 입니다.")
+
     }
 }
