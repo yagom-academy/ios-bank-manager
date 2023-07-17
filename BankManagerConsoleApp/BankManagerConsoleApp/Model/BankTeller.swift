b121f28ed3b090f98ee7a74cc68648623c6e549d
DasanKim <tempzipp@gmail.com>
2023-07-17 20:32:21 +0900

    ♻️Refactor: BankTeller의 doWork메서드 내 Handler 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankTeller.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankTeller.swift
@@ -9,17 +9,12 @@ import Foundation
 
 struct BankTeller {
     let processingTime: Double = 0.7
-    
-    func doWork(for customer: Customer) {
-        print(String(format: MessageFormat.startTask, customer.numberTicket))
+        
+    func doWork(for customer: Customer,
+                startHandler: (Customer) -> Void = { _ in },
+                completionHandler: (Customer) -> Void = { _ in }) {
+        startHandler(customer)
         Thread.sleep(forTimeInterval: processingTime)
-        print(String(format: MessageFormat.finishTask, customer.numberTicket))
-    }
-}
-
-extension BankTeller {
-    private enum MessageFormat {
-        static let startTask: String = "%u번 고객 업무 시작"
-        static let finishTask: String = "%u번 고객 업무 완료"
+        completionHandler(customer)
     }
 }
