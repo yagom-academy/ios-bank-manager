ecd1a7bed8baa270c63aa8c581689d90299e6a9d
mint3382 <mskim0926@naver.com>
2023-07-17 16:52:59 +0900

    feat: BankTask 열거형 구현, Customer에 BankTask 타입 상수 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankTask.swift
@@ -0,0 +1,31 @@
+//
+//  BankTask.swift
+//  BankManagerConsoleApp
+//
+//  Created by mint, jusbug on 2023/07/17.
+//
+
+import Foundation
+
+enum BankTask: CaseIterable {
+    case loan
+    case deposit
+    
+    var type: String {
+        switch self {
+        case .deposit:
+            return "예금"
+        case .loan:
+            return "대출"
+        }
+    }
+    
+    var time: Double {
+        switch self {
+        case .deposit:
+            return 0.7
+        case .loan:
+            return 1.1
+        }
+    }
+}
