5bb4da47ae57ee13f35b0b7bf3abea1d69a6c1e2
Hoon94 <dleognsdl1@naver.com>
2023-07-17 21:56:42 +0900

    refactor: BankClerk에 taskTime을 task.information.time으로 수정

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankClerk .swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank/BankClerk .swift
@@ -10,9 +10,9 @@ import Foundation
 struct BankClerk {
     func work(customer: Customer) -> Double {
         print("\(customer.numberTicket)번 고객 업무 시작")
-        usleep(UInt32(customer.taskTime * 1000000))
+        usleep(UInt32(customer.task.information.time * 1000000))
         print("\(customer.numberTicket)번 고객 업무 완료")
         
-        return customer.taskTime
+        return customer.task.information.time
     }
 }
