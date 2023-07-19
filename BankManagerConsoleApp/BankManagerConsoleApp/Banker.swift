04b1f56aa00dca1e33311dc10cdcfa3f75a478cb
iOS-Yetti <yechan123456@gmail.net>
2023-07-19 11:28:23 +0900

    feat: Customer, Banker타입 내부에 task프로퍼티 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Banker.swift
@@ -7,19 +7,23 @@
 import Foundation
 
 struct Banker {
-    private let workTime: Double = 0.7
-    
+    private let task: BankTask
+
+    init(task: BankTask) {
+        self.task = task
+    }
+
     func work(for customer: Customer) {
         guard let queueNumber = customer.queueNumber else {
             return
         }
-        
-        print("\(queueNumber)번 고객 업무 시작")
-        Thread.sleep(forTimeInterval: workTime)
-        print("\(queueNumber)번 고객 업무 종료")
+
+        print("\(queueNumber)번 \(task.korean)고객 업무 시작")
+        Thread.sleep(forTimeInterval: task.time)
+        print("\(queueNumber)번 \(task.korean)고객 업무 종료")
     }
-    
+
     func notifyWorkTime() -> Double {
-        return workTime
+        return task.time
     }
 }
--- BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Customer.swift
@@ -7,7 +7,13 @@
 
 struct Customer {
     private(set) var queueNumber: Int?
-    
+    private let task: BankTask
+
+    init(queueNumber: Int? = nil, task: BankTask) {
+        self.queueNumber = queueNumber
+        self.task = task
+    }
+
     mutating func receiveQueueNumber(queueNumber: Int) {
         self.queueNumber = queueNumber
     }
