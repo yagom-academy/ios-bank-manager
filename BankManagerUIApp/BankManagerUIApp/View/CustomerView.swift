f408f8f485f2899118624767dbfdcd47875f8c88
YaRkyungmin <kkgm94@naver.com>
2023-07-23 13:26:45 +0900

    ♻️refactor: CustomerView타입 UIView로 변경

--- BankManagerUIApp/BankManagerUIApp/View/CustomerView.swift
+++ BankManagerUIApp/BankManagerUIApp/View/CustomerView.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
-final class CustomerView: UILabel {
+final class CustomerView: UIView {
     let customer: Customer
+    
+    var customerLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     init(customer: Customer, frame: CGRect) {
         self.customer = customer
@@ -20,8 +26,16 @@ final class CustomerView: UILabel {
     }
     
     func configureUI() {
-        self.text = "\(customer.waitingNumber) - \(customer.purpose.name)"
-        self.textColor = customer.purpose.uiColor
-        self.textAlignment = .center
+        self.addSubview(customerLabel)
+        customerLabel.text = "\(customer.waitingNumber) - \(customer.purpose.name)"
+        customerLabel.textColor = customer.purpose.uiColor
+        customerLabel.textAlignment = .center
+        
+        NSLayoutConstraint.activate([
+            customerLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            customerLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            customerLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            customerLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
     }
 }
