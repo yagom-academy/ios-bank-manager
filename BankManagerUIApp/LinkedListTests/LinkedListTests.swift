bc900c935d6d085c7d4ea9ecfcf6095a1e0421ea
kiseok <carti1108@naver.com>
2023-11-13 21:52:49 +0900

    test: isEmpty 테스트

--- BankManagerUIApp/LinkedListTests/LinkedListTests.swift
+++ BankManagerUIApp/LinkedListTests/LinkedListTests.swift
@@ -12,11 +12,20 @@ final class LinkedListTests: XCTestCase {
     var sut: LinkedList<Int>!
 
     override func setUpWithError() throws {
+        try super.setUpWithError()
         sut = LinkedList()
     }
 
     override func tearDownWithError() throws {
+        try super.setUpWithError()
         sut = nil
     }
-
+    
+    func test_linkedlist에아무것도없을때_isEmpty호출시_true를반환하는지() {
+        //when
+        let result = sut.isEmpty
+        
+        //then
+        XCTAssertTrue(result)
+    }
 }
