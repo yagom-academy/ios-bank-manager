0a2273b0f0ba31a5619c8fe9e862ed254f1b99ca
kiseok <carti1108@naver.com>
2023-11-14 00:10:48 +0900

    feat: CustomerQueue class 구현
    queue, peek, isEmpty 프로퍼티 생성
    enqueue, dequeue, clear메서드 구현

--- BankManagerUIApp/BankManagerUIApp/CustomerQueue.swift
+++ BankManagerUIApp/BankManagerUIApp/CustomerQueue.swift
@@ -5,4 +5,27 @@
 //  Created by Kiseok on 11/13/23.
 //
 
-import Foundation
+final class CustomerQueue<T> {
+    private var queue: LinkedList<T> = LinkedList()
+    
+    var peek: T? {
+        return queue.head?.data
+    }
+    
+    var isEmpty:Bool {
+        return queue.isEmpty
+    }
+    
+    func enqueue(data: T) {
+        queue.append(data: data)
+    }
+    
+    @discardableResult
+    func dequeue() -> T? {
+        return isEmpty ? nil : queue.removeFirst()
+    }
+    
+    func clear() {
+        queue.removeAll()
+    }
+}
