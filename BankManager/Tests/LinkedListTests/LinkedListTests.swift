e8ad5bc37d0c69d94888641dbf62ddd66d0ea077
charles <kch1759@naver.com>
2023-11-14 10:44:34 +0900

    feat: LinkedList의 append 메서드에 대한 테스트 추가

--- BankManager/Tests/LinkedListTests/LinkedListTests.swift
+++ BankManager/Tests/LinkedListTests/LinkedListTests.swift
@@ -2,19 +2,22 @@ import XCTest
 @testable import BankManager
 
 final class LinkedListTests: XCTestCase {
-    var sut = LinkedList<Int>()
+    var sut: LinkedList<Int>!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
+        
+        sut = LinkedList()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
+        
+        sut = nil
     }
 
     func test_count_노드가없을때0이다() {
         // given
-        sut = LinkedList()
         let expectation = 0
         
         // when
@@ -22,5 +25,25 @@ final class LinkedListTests: XCTestCase {
         
         // then
         XCTAssertEqual(result, expectation)
+    }
+    
+    func test_LinkedList에_데이터1을넣었을때_showFirstNode의결과값과_1과동일하다() {
+        sut.append(data: 1)
+        
+        let result = sut.showFirstNode()
+        
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_LinkedList에_데이터1_2_3을_넣었을때_head데이터가_1이고_tail데이터가_3이다() {
+        sut.append(data: 1)
+        sut.append(data: 2)
+        sut.append(data: 3)
+        
+        let headResult = sut.head?.data
+        let tailResult = sut.tail?.data
+        
+        XCTAssertEqual(headResult, 1)
+        XCTAssertEqual(tailResult, 3)
     }
 }
