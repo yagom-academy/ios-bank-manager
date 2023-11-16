4d538a527e515b2c6f71f885ec2361fedb6037c5
jyubong <nane030@naver.com>
2023-11-16 17:03:54 +0900

    feat: bank open 메서드 구현, bank manager가 일을 하는 메서드

--- BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -17,6 +17,20 @@ struct Bank: BankBusinesable {
     init(customerNumber: UInt) {
         self.customerNumber = customerNumber
     }
+    
+    func open() {
+        createCustomer(customerNumber: customerNumber)
+        
+        let startTime = CFAbsoluteTimeGetCurrent()
+        
+        bankManager.assignCustomer(to: bankClerk)
+        
+        let finishTime = CFAbsoluteTimeGetCurrent() - startTime
+        let time = String(format: "%.2f", finishTime)
+        
+        close(time: time)
+    }
+    
     private func createCustomer(customerNumber: UInt) {
         (1...customerNumber).forEach {
             bankManager.standBy(customer: Customer(number: $0))
