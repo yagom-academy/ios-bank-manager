29e461044e8c3d8a63a1d22981561bb24ba6f0c0
woohyeon3382 <woohyeon3382@naver.com>
2023-11-16 16:01:44 +0900

    feat: BankManager타입 세부 기능 구현
    - 고객을 enqueue 하는 standBy(customer: CustomerNumbering) 메서드 구현
    - 고객을 dequeue 하는 assignCustomer(to bankClerk: BankClerk) 메서드 구현

--- BankManager/Sources/BankManager/BankManager.swift
+++ BankManager/Sources/BankManager/BankManager.swift
@@ -1,6 +1,13 @@
-public struct BankManager {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+public struct BankManager<BankClerk: Receivable> {
+    private let customerQueue = CustomerQueue<CustomerNumbering>(list: LinkedList<CustomerNumbering>())
+    
+    public func standBy(customer: CustomerNumbering) {
+        customerQueue.enqueue(customer)
+    }
+    
+    public func assignCustomer(to bankClerk: BankClerk) {
+        while let customer = customerQueue.dequeue() as? BankClerk.Customer {
+            bankClerk.receive(customer: customer)
+        }
     }
 }
