13e24741ed3f7ed8d85a49003932bd1b114a6bf6
kiseok <carti1108@naver.com>
2023-11-16 14:37:37 +0900

    feat: Customer 타입 구현
    waitingTicket 프로퍼티 생성

---
+++ BankManager/Sources/BankManager/Customer.swift
@@ -0,0 +1,14 @@
+//
+//  Customer.swift
+//
+//
+//  Created by Kiseok on 11/15/23.
+//
+
+public struct Customer {
+    public let waitingTicket: Int
+    
+    public init(watingTicket: Int) {
+        self.waitingTicket = watingTicket
+    }
+}
