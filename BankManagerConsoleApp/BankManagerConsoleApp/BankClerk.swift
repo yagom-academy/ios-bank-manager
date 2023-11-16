276fa6bf4901f243de72f9e63af2bd5662f3f12d
woohyeon3382 <woohyeon3382@naver.com>
2023-11-16 16:28:59 +0900

    feat: BankClerk 구조체 구현 및 receive(customer: Customer) 메서드 구현

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankClerk.swift
@@ -0,0 +1,21 @@
+//
+//  BankClerk.swift
+//  BankManagerConsoleApp
+//
+//  Created by jyubong, toy on 11/16/23.
+//
+import Foundation
+import BankManager
+
+struct BankClerk: Receivable {
+    var pace = 0.7
+    
+    func receive(customer: Customer) {
+        let number = customer.number
+        let uSeconds = UInt32(pace * 1000000)
+        
+        print("\(number)번 고객 업무 시작")
+        usleep(uSeconds)
+        print("\(number)번 고객 업무 완료")
+    }
+}
