be487c3927f18150755b28aafb7a30e81a66790e
jyubong <nane030@naver.com>
2023-11-16 20:29:51 +0900

    refactor: customerQueue typealias에 private 추가

--- BankManager/Tests/BankManagerTests/CustomerQueueTests.swift
+++ BankManager/Tests/BankManagerTests/CustomerQueueTests.swift
@@ -9,8 +9,8 @@ import XCTest
 @testable import BankManager
 
 final class CustomerQueueTests: XCTestCase {
-    typealias List = LinkedList<Int>
-    typealias Queue = CustomerQueue<Int>
+    private typealias List = LinkedList<Int>
+    private typealias Queue = CustomerQueue<Int>
     
     private var sut: Queue!
 
