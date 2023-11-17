4f89ee1b1bf8ddeae52d4f92e7ca169c85706114
jyubong <nane030@naver.com>
2023-11-17 20:09:31 +0900

    feat: BankClerk startWork, endWork 클로저 프로퍼티 추가 및 이니셜라이저 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/BankClerk.swift
@@ -9,8 +9,14 @@ import BankManager
 
 struct BankClerk: CustomerReceivable {
     private let pace = 0.7
+    private let startWork: (String) -> Void
+    private let endWork: (String) -> Void
     
     func receive(customer: Customer, work: ((UInt, Double) -> Void)) {
+    init(startWork: @escaping (String) -> Void = { print($0) }, endWork: @escaping (String) -> Void = { print($0) }) {
+        self.startWork = startWork
+        self.endWork = endWork
+    }
         let number = customer.number
         
         work(number, pace)
