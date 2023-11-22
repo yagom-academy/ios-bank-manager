0df2dae57316dcf2c21cd8f5e2160562cdc6bb92
jyubong <nane030@naver.com>
2023-11-22 16:37:54 +0900

    refactor: BankManager의 assign 프로퍼티 매개변수 타입 변경 및 적용

--- BankManager/Sources/BankManager/Model/BankManager.swift
+++ BankManager/Sources/BankManager/Model/BankManager.swift
@@ -9,9 +9,10 @@ public struct BankManager<BankClerk: CustomerReceivable> {
         customerQueue.enqueue(customer)
     }
     
-    public func assignCustomer(to bankClerk: BankClerk) {
-        while let customer = customerQueue.dequeue() as? BankClerk.Customer {
-            bankClerk.receive(customer: customer)
+    public func assignCustomer(to bankClerk: [Banking: BankClerk]) {
+        while let customer = customerQueue.dequeue() as? BankClerk.Customer,
+              let banking = customer.banking {
+            bankClerk[banking]?.receive(customer: customer)
         }
     }
 }
