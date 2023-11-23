5869f5d4e060d3453972290d075b0f96eb140b43
Hisop <khsdfg958@gmail.com>
2023-11-23 18:22:00 +0900

    refactor: Step3 2안
    GCD -> operation으로 변경

--- BankManager/Sources/BankManager/Bank.swift
+++ BankManager/Sources/BankManager/Bank.swift
@@ -13,29 +13,20 @@ public final class Bank {
     private var workTime: Double = 0
     
     private let customerQueue = CustomerQueue<Customer>()
-    private let depositQueue = CustomerQueue<Customer>()
-    private let loanQueue = CustomerQueue<Customer>()
-    
-    private let depositSemaphore = DispatchSemaphore(value: 1)
-    private let loanSemaphore = DispatchSemaphore(value: 1)
+    private let depositQueue = OperationQueue()
+    private let loanQueue = OperationQueue()
+
     private let BankSemaphore = DispatchSemaphore(value: 1)
-    
-    private var employeesList: [Employees] = []
     
     init(name: String, chargeDepositCount: Int, chargeLoanCount: Int) {
         self.name = name
-        for _ in 1...chargeDepositCount {
-            employeesList.append(Employees(business: .deposit, semaphore: depositSemaphore))
-        }
-        for _ in 1...chargeLoanCount {
-            employeesList.append(Employees(business: .loan, semaphore: loanSemaphore))
-        }
+        depositQueue.maxConcurrentOperationCount = chargeDepositCount
+        loanQueue.maxConcurrentOperationCount = chargeLoanCount
     }
     
     public func open() {
         addCustomer()
         addQueue()
-        employeesWork()
         endWork()
     }
     
@@ -44,58 +35,39 @@ public final class Bank {
             guard let customer = customerQueue.dequeue() else {
                 return
             }
-            switch customer.business {
-            case .deposit:
-                depositQueue.enqueue(value: customer)
-            case .loan:
-                loanQueue.enqueue(value: customer)
-            }
-        }
-    }
-    
-    private func employeesWork() {
-        let group = DispatchGroup()
-        
-        for employees in employeesList {
-            DispatchQueue.global().async(group: group, execute: makeWork(employees: employees))
-        }
-        group.wait()
-    }
-    
-    private func makeWork(employees: Employees) -> DispatchWorkItem {
-        DispatchWorkItem(block: { [self] in
-            var queue: CustomerQueue<Customer>
-            switch employees.business {
-            case .deposit:
-                queue = depositQueue
-            case .loan:
-                queue = loanQueue
-            }
-            while queue.isEmpty() == false {
-                employees.semaphore.wait()
-                guard let customer = queue.dequeue() else {
-                    return
-                }
-                employees.semaphore.signal()
-                
-                WorkReport.startWork(customer: customer)
-                Thread.sleep(forTimeInterval: employees.business.workTime)
-                
+            let work = makeWork(customer: customer)
+            work.completionBlock = { [self] in
                 BankSemaphore.wait()
-                workTime += employees.business.workTime
+                workTime += customer.business.workTime
                 customerCount += 1
                 BankSemaphore.signal()
                 
                 WorkReport.endWork(customer: customer)
             }
-        })
+            switch customer.business {
+            case .deposit:
+                depositQueue.addOperation(work)
+            case .loan:
+                loanQueue.addOperation(work)
+            }
+        }
+    }
+    
+    private func makeWork(customer: Customer) -> BlockOperation {
+        BlockOperation {
+            WorkReport.startWork(customer: customer)
+            Thread.sleep(forTimeInterval: customer.business.workTime)
+        }
     }
     
     private func endWork() {
+        depositQueue.waitUntilAllOperationsAreFinished()
+        loanQueue.waitUntilAllOperationsAreFinished()
+
         WorkReport.endWorkString(customerCount: customerCount, workTime: workTime)
         customerCount = 0
         workTime = 0
-    }
+   }
     
     private func addCustomer() {
         let count = Int.random(in: 10...30)
