baca8f1b2b4cce48e9bf9939d41ba1184cd2b367
KKOMGI <kkomgi90@gmail.com>
2024-03-18 22:51:06 +0900

    feat: LinkedList 타입 생성 및 구현

---
+++ BankManager/Sources/BankManager/LinkedList.swift
@@ -0,0 +1,56 @@
+//
+//  File.swift
+//  
+//
+//  Created by Diana, Hamzzi on 3/18/24.
+//
+
+class Node<Element> {
+    var value: Element
+    var next: Node?
+    
+    init(value: Element) {
+        self.value = value
+    }
+}
+
+struct LinkedList<Element> {
+    private var head: Node<Element>?
+    private var tail: Node<Element>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating func enqueue(_ value: Element) {
+        let newNode = Node(value: value)
+        guard let tailNode = tail else {
+            head = newNode
+            return
+        }
+        tail?.next = newNode
+        tail = tail?.next
+    }
+    
+    mutating func dequeue() -> Element? {
+        if let headNode = head {
+            let value = headNode.value
+            head = headNode.next
+            if head == nil {
+                tail = nil
+            }
+            return value
+        } else {
+            return nil
+        }
+    }
+    
+    func peek() -> Element? {
+        return head?.value
+    }
+    
+    mutating func clear() {
+        head = nil
+        tail = nil
+    }
+}
