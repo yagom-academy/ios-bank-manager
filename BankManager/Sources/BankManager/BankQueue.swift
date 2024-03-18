8388cf8252ae37db32b01533818c639b12cc31fd
yawoong2 <adamzoong@naver.com>
2024-03-18 23:20:30 +0900

    feat: BankQueue 생성 및 enqueue 기능 추가

---
+++ BankManager/Sources/BankManager/BankQueue.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by JIWOONG on 2024/03/18.
+//
+
+struct BankQueue<T> {
+    var list = BankLinkedList<T>()
+    
+    mutating func enqueue(element: T) {
+        list.append(element: element)
+    }
+}
