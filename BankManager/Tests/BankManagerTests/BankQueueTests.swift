6575e96fe83cfd33d114f56ceba3beb568ef6a68
PrismSpirit <yeolkeut@gmail.com>
2024-03-19 17:42:30 +0900

    test: 원소의 개수가 1인 큐에서 dequeue 후 4를 enqueue하는 테스트 케이스 추가

--- BankManager/Tests/BankManagerTests/BankQueueTests.swift
+++ BankManager/Tests/BankManagerTests/BankQueueTests.swift
@@ -34,6 +34,21 @@ final class BankQueueTests: XCTestCase {
         XCTAssertEqual(expectedCount, sut.count)
     }
     
+    func test_원소의_개수가_1인_큐에서_dequeue_후_4를_enqueue() {
+        // Given
+        test_비어있는_큐에_3을_enqueue()
+        let expectedLast = 4
+        let expectedCount = 1
+        
+        // When
+        let _ = sut.dequeue()
+        sut.enqueue(element: 4)
+        
+        // Then
+        XCTAssertEqual(expectedLast, sut.last)
+        XCTAssertEqual(expectedCount, sut.count)
+    }
+    
     func test_비어있지_않은_큐에_5를_enqueue() {
         // Given
         test_비어있는_큐에_3을_enqueue()
