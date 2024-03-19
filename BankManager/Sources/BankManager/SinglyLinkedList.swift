273884cfd44db7f96b2a94b17b79119a79aecbd8
PrismSpirit <yeolkeut@gmail.com>
2024-03-19 14:58:12 +0900

    feat: SinglyLinkedList skeleton code 작성

--- BankManager/Sources/BankManager/SinglyLinkedList.swift
+++ BankManager/Sources/BankManager/SinglyLinkedList.swift
@@ -5,14 +5,45 @@
 //  Created by Danny, Prism on 3/19/24.
 //
 
-
-class SinglyLinkedList {
-    class Node<T> {
+class SinglyLinkedList<T> {
+    class Node {
         var data: T
-        var next: Node<T>? = nil
+        var next: Node? = nil
         
         init(data: T) {
             self.data = data
         }
+        
+        func setNext(node: Node) {
+            self.next = node
+        }
+    }
+    
+    var head: Node? = nil
+    var tail: Node? = nil
+    var count: Int = 0
+    
+    var isEmpty: Bool {
+        return true
+    }
+    
+    var first: T? {
+        return nil
+    }
+    
+    var last: T? {
+        return nil
+    }
+    
+    func enqueue() {
+        
+    }
+    
+    func dequeue() -> T? {
+        return nil
+    }
+    
+    func clear() {
+        
     }
 }
