0d1c60864f6f64347b958fd563104107bf91b15f
danny <minzami1215@gmail.com>
2024-03-19 15:21:33 +0900

    feat: BankQueue 타입 초기 구현

---
+++ BankManager/Sources/BankManager/BankQueue.swift
@@ -0,0 +1,46 @@
+//
+//  BankQueue.swift
+//
+//
+//  Created by Danny, Prism on 3/19/24.
+//
+
+protocol Queueable {
+    associatedtype T
+    var isEmpty: Bool { get }
+    var count: Int { get }
+    var first: T? { get }
+    var last: T? { get }
+    func enqueue(element: T)
+    func dequeue() -> T?
+    func clear()
+}
+
+class BankQueue<T>: Queueable {
+    let list = SinglyLinkedList<T>()
+    
+    var isEmpty: Bool {
+        return true
+    }
+    
+    var count: Int {
+        return 0
+    }
+    
+    var first: T?
+    
+    var last: T?
+    
+    func enqueue(element: T) {
+        
+    }
+    
+    func dequeue() -> T? {
+        return nil
+    }
+    
+    func clear() {
+        
+    }
+    
+}
