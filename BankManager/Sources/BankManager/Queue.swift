60e506ff1b9b033752d60334f07d5aff29c02784
Diana-yjh <projin1234@gmail.com>
2024-03-19 19:42:31 +0900

    refactor: Queue에 AccessControl 적용

--- BankManager/Sources/BankManager/Queue.swift
+++ BankManager/Sources/BankManager/Queue.swift
@@ -5,30 +5,30 @@
 //  Created by Diana, Hamzzi on 3/18/24.
 //
 
-struct Queue<Element> {
-    var list = LinkedList<Element>()
+public struct Queue<Element> {
+    private var list = LinkedList<Element>()
     
-    var isEmpty: Bool {
+    public var isEmpty: Bool {
         list.isEmpty
     }
     
-    var count: Int {
+    public var count: Int {
         list.count
     }
     
-    mutating func dequeue() -> Element? {
+    public mutating func dequeue() -> Element? {
         return list.removeFirst()
     }
     
-    mutating func enqueue(_ value: Element) {
+    public mutating func enqueue(_ value: Element) {
         list.append(value)
     }
     
-    mutating func peek() -> Element? {
+    public mutating func peek() -> Element? {
         return list.peek()
     }
     
-    mutating func clear() {
+    public mutating func clear() {
         list.clear()
     }
 }
