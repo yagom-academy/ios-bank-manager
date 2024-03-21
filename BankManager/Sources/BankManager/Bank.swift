6f9dc8a4c81948e123f9d09b84b4328ecb78d4b4
danny <minzami1215@gmail.com>
2024-03-22 00:29:30 +0900

    feat: Bank 업무시간 구하는 로직 구현

--- BankManager/Sources/BankManager/Bank.swift
+++ BankManager/Sources/BankManager/Bank.swift
@@ -20,9 +20,11 @@ struct Bank {
     
     func commenceBanking() {
         enqueueTodaysVisitors()
+        let visitedCustomerNumber = bankQueue.count
         
         let concurrentLimitingSemaphore = DispatchSemaphore(value: numberOfBankClerk)
         
+        let startBankingTime = DispatchTime.now()
         while !bankQueue.isEmpty {
             concurrentLimitingSemaphore.wait()
             
@@ -30,10 +32,21 @@ struct Bank {
             
             DispatchQueue.global().async {
                 print("\(customer.waitingNumber)번 고객 업무 시작")
-                sleep(UInt32(0.7))
+                Thread.sleep(forTimeInterval: 0.7)
                 print("\(customer.waitingNumber)번 고객 업무 완료")
                 concurrentLimitingSemaphore.signal()
             }
         }
+        let endBankingTime = DispatchTime.now()
+        let elapseBankingTimeNanoseconds = endBankingTime.uptimeNanoseconds - startBankingTime.uptimeNanoseconds
+        let elapseBankingTime = (Double(elapseBankingTimeNanoseconds) / 1_000_000_000).rounded(toPlaces: 2)
+        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(visitedCustomerNumber)명이며, 총 업무시간은 \(elapseBankingTime)초입니다.")
+    }
+}
+
+extension Double {
+    func rounded(toPlaces: Int) -> Double {
+        let divisor = pow(10.0, Double(toPlaces))
+        return (self * divisor).rounded() / divisor
     }
 }
