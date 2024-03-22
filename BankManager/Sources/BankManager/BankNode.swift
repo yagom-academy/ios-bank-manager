4d821d2a4cc6ebfdcc3854b3e7fee5b940dbdb89
yawoong2 <adamzoong@naver.com>
2024-03-22 10:49:12 +0900

    refactor: BankManager 모듈 내 접근제어자 수정

--- BankManager/Sources/BankManager/BankNode.swift
+++ BankManager/Sources/BankManager/BankNode.swift
@@ -5,11 +5,11 @@
 //  Created by Gray, Gama on 3/18/24.
 //
 
-class BankNode<T> {
-    var data: T
-    var next: BankNode?
+public class BankNode<T> {
+    public var data: T
+    public var next: BankNode?
     
-    init(data: T, next: BankNode? = nil) {
+    public init(data: T, next: BankNode? = nil) {
         self.data = data
         self.next = next
     }
